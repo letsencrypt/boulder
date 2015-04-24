820cac6e060d6872abc82ac59584eeb3aa29bc97
Richard Barnes <rlb@ipv.sx>
2015-04-24 18:28:53 -0400

    Auditing for improper messages received by WFE

--- wfe/web-front-end.go
+++ wfe/web-front-end.go
@@ -29,26 +29,26 @@ type WebFrontEndImpl struct {
 	log   *blog.AuditLogger
 
 	// URL configuration parameters
-	BaseURL   string
-	NewReg    string
-	NewRegPath    string
-	RegBase   string
-	RegPath   string
-	NewAuthz  string
-	NewAuthzPath  string
-	AuthzBase string
-	AuthzPath string
-	NewCert   string
-	NewCertPath   string
-	CertBase  string
-	CertPath  string
-	TermsPath  string
+	BaseURL      string
+	NewReg       string
+	NewRegPath   string
+	RegBase      string
+	RegPath      string
+	NewAuthz     string
+	NewAuthzPath string
+	AuthzBase    string
+	AuthzPath    string
+	NewCert      string
+	NewCertPath  string
+	CertBase     string
+	CertPath     string
+	TermsPath    string
 }
 
 func NewWebFrontEndImpl(logger *blog.AuditLogger) WebFrontEndImpl {
 	logger.Notice("Web Front End Starting")
 	return WebFrontEndImpl{
-		log: logger,
+		log:          logger,
 		NewRegPath:   "/acme/new-reg",
 		RegPath:      "/acme/reg/",
 		NewAuthzPath: "/acme/new-authz",
@@ -129,9 +129,12 @@ func (wfe *WebFrontEndImpl) sendError(response http.ResponseWriter, message stri
 	problem := problem{Detail: message}
 	problemDoc, err := json.Marshal(problem)
 	if err != nil {
-		problemDoc = []byte("{\"detail\": \"Problem marshalling error message.\"}")
+		return
 	}
-	wfe.log.Debug("Sending error to client: " + string(problemDoc))
+
+	// Audit log "Receipt of improper messages"
+	wfe.log.Audit(fmt.Sprintf("Improper HTTP request - %d - %s", code, message))
+
 	// Paraphrased from
 	// https://golang.org/src/net/http/server.go#L1272
 	response.Header().Set("Content-Type", "application/problem+json")
@@ -182,7 +185,7 @@ func (wfe *WebFrontEndImpl) NewRegistration(response http.ResponseWriter, reques
 	response.Header().Set("Content-Type", "application/json")
 	response.Header().Add("Link", link(wfe.NewAuthz, "next"))
 	if len(wfe.TermsPath) > 0 {
-		response.Header().Add("Link", link(wfe.BaseURL + wfe.TermsPath, "terms-of-service"))
+		response.Header().Add("Link", link(wfe.BaseURL+wfe.TermsPath, "terms-of-service"))
 	}
 
 	response.WriteHeader(http.StatusCreated)
@@ -464,10 +467,6 @@ func (wfe *WebFrontEndImpl) Authorization(response http.ResponseWriter, request 
 
 var allHex = regexp.MustCompile("^[0-9a-f]+$")
 
-func (wfe *WebFrontEndImpl) notFound(response http.ResponseWriter) {
-	wfe.sendError(response, "Not found", http.StatusNotFound)
-}
-
 func (wfe *WebFrontEndImpl) Certificate(response http.ResponseWriter, request *http.Request) {
 	path := request.URL.Path
 	switch request.Method {
@@ -479,19 +478,19 @@ func (wfe *WebFrontEndImpl) Certificate(response http.ResponseWriter, request *h
 		// Certificate paths consist of the CertBase path, plus exactly sixteen hex
 		// digits.
 		if !strings.HasPrefix(path, wfe.CertPath) {
-			wfe.notFound(response)
+			wfe.sendError(response, "Not found", http.StatusNotFound)
 			return
 		}
 		serial := path[len(wfe.CertPath):]
 		if len(serial) != 16 || !allHex.Match([]byte(serial)) {
-			wfe.notFound(response)
+			wfe.sendError(response, "Not found", http.StatusNotFound)
 			return
 		}
 		wfe.log.Notice(fmt.Sprintf("Requested certificate ID %s", serial))
 
 		cert, err := wfe.SA.GetCertificate(serial)
 		if err != nil {
-			wfe.notFound(response)
+			wfe.sendError(response, "Not found", http.StatusNotFound)
 			return
 		}
 
