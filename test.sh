dc02fedc51588d7a362023529b6b655017ca3205
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-05-22 09:49:53 -0700

    Don't auto-run integration test.

--- test.sh
+++ test.sh
@@ -63,8 +63,6 @@ else
   run go test ${dirlist}
 fi
 
-setsid bash test/integration-test.sh
-
 unformatted=$(find . -name "*.go" -not -path "./Godeps/*" -print | xargs -n1  gofmt -l)
 if [ "x${unformatted}" != "x" ] ; then
   echo "Unformatted files found; setting failure state."
