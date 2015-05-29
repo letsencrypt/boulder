0d7555b7afd8d08c4fe177aeb799b24099259c9c
Richard Barnes <rlb@ipv.sx>
2015-05-28 20:34:47 -0700

    Rebase on master

--- core/interfaces.go
+++ core/interfaces.go
@@ -80,7 +80,7 @@ type ValidationAuthority interface {
 type CertificateAuthority interface {
 	// [RegistrationAuthority]
 	IssueCertificate(x509.CertificateRequest, int64, time.Time) (Certificate, error)
-	RevokeCertificate(serial string) error
+	RevokeCertificate(string) error
 }
 
 type PolicyAuthority interface {
