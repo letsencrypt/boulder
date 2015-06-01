letsencrypt/boulder

commit 81fa97fb211d6aef1199da72d80d9def6efc2ede

Remove duplicative calls to MergeUpdate

wfe/web-front-end.go
@@ -262,17 +262,16 @@ func (wfe *WebFrontEndImpl) NewRegistration(response http.ResponseWriter, reques
 		return
 	}
 
-	var init, unmarshalled core.Registration
-	err = json.Unmarshal(body, &unmarshalled)
+	var init core.Registration
+	err = json.Unmarshal(body, &init)
 	if err != nil {
 		wfe.sendError(response, "Error unmarshaling JSON", err, http.StatusBadRequest)
 		return
 	}
-	if len(unmarshalled.Agreement) > 0 && unmarshalled.Agreement != wfe.SubscriberAgreementURL {
-		wfe.sendError(response, fmt.Sprintf("Provided agreement URL [%s] does not match current agreement URL [%s]", unmarshalled.Agreement, wfe.SubscriberAgreementURL), nil, http.StatusBadRequest)
+	if len(init.Agreement) > 0 && init.Agreement != wfe.SubscriberAgreementURL {
+		wfe.sendError(response, fmt.Sprintf("Provided agreement URL [%s] does not match current agreement URL [%s]", init.Agreement, wfe.SubscriberAgreementURL), nil, http.StatusBadRequest)
 		return
 	}
-	init.MergeUpdate(unmarshalled)
 	init.Key = *key
 
 	reg, err := wfe.RA.NewRegistration(init)
@@ -631,11 +630,6 @@ func (wfe *WebFrontEndImpl) Registration(response http.ResponseWriter, request *
 		return
 	}
 
-	// MergeUpdate copies over only the fields that a client is allowed to modify.
-	// Note: The RA will also use MergeUpdate to filter out non-updateable fields,
-	// but we do it here too, so that/ input filtering happens as early in the
-	// request processing as possible.
-	currReg.MergeUpdate(update)
 	// Ask the RA to update this authorization.
 	updatedReg, err := wfe.RA.UpdateRegistration(currReg, currReg)
 	if err != nil {

commit 63ca50229d0e267246dc266635e7e181c7c22d3d

Fix a bug introduced in 81fa97f and make ./start.sh work

wfe/web-front-end.go
@@ -575,7 +575,7 @@ func (wfe *WebFrontEndImpl) Registration(response http.ResponseWriter, request *
 	}
 
 	// Ask the RA to update this authorization.
-	updatedReg, err := wfe.RA.UpdateRegistration(currReg, currReg)
+	updatedReg, err := wfe.RA.UpdateRegistration(currReg, update)
 	if err != nil {
 		wfe.sendError(response, "Unable to update registration", err, statusCodeFromError(err))
 		return