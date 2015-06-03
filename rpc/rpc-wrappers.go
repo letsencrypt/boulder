c6f840f1fd789cfdc823cd53ccfd0c365b8655d3
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-06-03 12:10:43 -0700

    Fix RPC error conditions for cert issuance.
    Previously an issuance failure would bubble up empty cert responses from
    CA->RA->WFE, which would generate confusing "asn1 truncated sequence" errors.
    Now we get an error indicating the RPC failed.

--- rpc/rpc-wrappers.go
+++ rpc/rpc-wrappers.go
@@ -308,7 +308,11 @@ func (rac RegistrationAuthorityClient) NewCertificate(cr core.CertificateRequest
 	}
 
 	certData, err := rac.rpc.DispatchSync(MethodNewCertificate, data)
-	if err != nil || len(certData) == 0 {
+	if err != nil {
+		return
+	}
+	if len(certData) == 0 {
+		err = errors.New("NewCertificate RPC to RA failed.")
 		return
 	}
 
@@ -542,8 +546,11 @@ func (cac CertificateAuthorityClient) IssueCertificate(csr x509.CertificateReque
 	}
 
 	jsonResponse, err := cac.rpc.DispatchSync(MethodIssueCertificate, data)
+	if err != nil {
+		return
+	}
 	if len(jsonResponse) == 0 {
-		// TODO: Better error handling
+		err = errors.New("IssueCertificate RPC to CA failed.")
 		return
 	}
 
