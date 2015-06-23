37b5808c3408d29e4bc1df24f80c0d4061e69a32
Richard Barnes <rlb@ipv.sx>
2015-06-23 14:07:01 -0700

    Plumbing through user-agent in command line programs

--- cmd/boulder/main.go
+++ cmd/boulder/main.go
@@ -85,6 +85,7 @@ func main() {
 		dnsTimeout, err := time.ParseDuration(c.VA.DNSTimeout)
 		cmd.FailOnError(err, "Couldn't parse DNS timeout")
 		va.DNSResolver = core.NewDNSResolver(dnsTimeout, []string{c.VA.DNSResolver})
+		va.UserAgent = c.VA.UserAgent
 
 		cadb, err := ca.NewCertificateAuthorityDatabaseImpl(c.CA.DBDriver, c.CA.DBName)
 		cmd.FailOnError(err, "Failed to create CA database")
