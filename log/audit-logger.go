036c80703b25daacf48b3e4951cfdcb45be1d863
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-06-23 19:16:44 +0100

    Remove duplication

--- log/audit-logger.go
+++ log/audit-logger.go
@@ -231,7 +231,7 @@ func (log *AuditLogger) Audit(msg string) (err error) {
 	return log.auditAtLevel("Logging.Notice", msg)
 }
 
-func (log *AuditLogger) formatObject(obj interface{}) (string, error) {
+func (log *AuditLogger) formatObjectMessage(msg string, obj interface{}) (string, error) {
 	jsonObj, err := json.Marshal(obj)
 	if err != nil {
 		// AUDIT[ Error Conditions ] 9cc4d537-8534-4970-8665-4b382abe82f3
@@ -239,28 +239,28 @@ func (log *AuditLogger) formatObject(obj interface{}) (string, error) {
 		return "", err
 	}
 
-	return fmt.Sprintf("JSON=%s", jsonObj), nil
+	return fmt.Sprintf("%s JSON=%s", msg, jsonObj), nil
 }
 
 // AuditObject sends a NOTICE-severity JSON-serialized object message that is prefixed
 // with the audit tag, for special handling at the upstream system logger.
 func (log *AuditLogger) AuditObject(msg string, obj interface{}) (err error) {
-	jsonLogEvent, logErr := log.formatObject(obj)
+	formattedEvent, logErr := log.formatObjectMessage(msg, obj)
 	if logErr != nil {
 		return logErr
 	}
 
-	return log.auditAtLevel("Logging.Notice", fmt.Sprintf("%s %s", msg, jsonLogEvent))
+	return log.auditAtLevel("Logging.Notice", formattedEvent)
 }
 
-// Object sends a INFO-severity JSON-serialized object message.
+// InfoObject sends a INFO-severity JSON-serialized object message.
 func (log *AuditLogger) InfoObject(msg string, obj interface{}) (err error) {
-	jsonLogEvent, logErr := log.formatObject(obj)
+	formattedEvent, logErr := log.formatObjectMessage(msg, obj)
 	if logErr != nil {
 		return logErr
 	}
 
-	return log.logAtLevel("Logging.Info", fmt.Sprintf("%s %s", msg, jsonLogEvent))
+	return log.logAtLevel("Logging.Info", formattedEvent)
 }
 
 // AuditErr can format an error for auditing; it does so at ERR level.
