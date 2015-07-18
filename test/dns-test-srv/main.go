8ad357cffd4ce3b30c889a468d00d289026ad73f
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-07-17 22:29:39 -0700

    Copyright header and cleanup

--- test/dns-test-srv/main.go
+++ test/dns-test-srv/main.go
@@ -1,3 +1,8 @@
+// Copyright 2014 ISRG.  All rights reserved
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 package main
 
 import (
@@ -16,13 +21,14 @@ func dnsHandler(w dns.ResponseWriter, r *dns.Msg) {
 
 	for _, q := range r.Question {
 		fmt.Printf("dns-srv: Query -- [%s] %s\n", q.Name, dns.TypeToString[q.Qtype])
-		if q.Qtype == dns.TypeA {
+		switch q.Qtype {
+		case dns.TypeA:
 			record := new(dns.A)
 			record.Hdr = dns.RR_Header{Name: q.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0}
 			record.A = net.ParseIP("127.0.0.1")
 
 			m.Answer = append(m.Answer, record)
-		} else if q.Qtype == dns.TypeMX {
+		case dns.TypeMX:
 			record := new(dns.MX)
 			record.Hdr = dns.RR_Header{Name: q.Name, Rrtype: dns.TypeMX, Class: dns.ClassINET, Ttl: 0}
 			record.Mx = "mail." + q.Name
@@ -30,7 +36,6 @@ func dnsHandler(w dns.ResponseWriter, r *dns.Msg) {
 
 			m.Answer = append(m.Answer, record)
 		}
-
 	}
 
 	w.WriteMsg(m)
@@ -50,8 +55,8 @@ func serveTestResolver() {
 }
 
 func main() {
-	forever := make(chan bool, 1)
 	fmt.Println("dns-srv: Starting test DNS server")
 	serveTestResolver()
+	forever := make(chan bool, 1)
 	<-forever
 }
