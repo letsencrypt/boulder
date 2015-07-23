77418e28d3c88d66ceb400f9c812ac4b08c05158
Jeff Hodges <jeff@somethingsimilar.com>
2015-07-23 15:26:44 -0700

    fix first failure comment in test.sh
    A better fix might be to fail earlier on when, say, go vet fails.

--- test.sh
+++ test.sh
@@ -210,7 +210,7 @@ fi
 # If the unittests failed, exit before trying to run the integration test.
 if [ ${FAILURE} != 0 ]; then
   echo "--------------------------------------------------"
-  echo "---          A unit test failed.               ---"
+  echo "---        A unit test or tool failed.         ---"
   echo "--- Stopping before running integration tests. ---"
   echo "--------------------------------------------------"
   exit ${FAILURE}
