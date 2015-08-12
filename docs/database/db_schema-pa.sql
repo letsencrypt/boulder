af14fef972ef58db544844e44ac1da153ba70350
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-08-12 15:13:10 -0700

    Add PA schema file

---
+++ docs/database/db_schema-pa.sql
@@ -0,0 +1,15 @@
+--
+-- Copyright 2015 ISRG.  All rights reserved
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+-- This file defines the table schema, foreign keys and indicies of the
+-- Policy Authority database, which is logically separate from that which
+-- is utilized by the Storage Authority and administrator tools.
+--
+
+CREATE TABLE `ruleList` (
+  `rule` varchar(255) DEFAULT NULL,
+  `type` char(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
