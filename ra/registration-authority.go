ba56e7c5871d707d716865e1cb99331ac477bea7
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-10-21 15:02:15 -0700

    Make MatchesCSR downcase CommonName before checking equality

--- ra/registration-authority.go
+++ ra/registration-authority.go
@@ -327,7 +327,8 @@ func (ra *RegistrationAuthorityImpl) NewAuthorization(request core.Authorization
 //		* Subject only contains CommonName & Names
 func (ra *RegistrationAuthorityImpl) MatchesCSR(
 	cert core.Certificate,
-	csr *x509.CertificateRequest) (err error) {
+	csr *x509.CertificateRequest,
+) (err error) {
 	parsedCertificate, err := x509.ParseCertificate([]byte(cert.DER))
 	if err != nil {
 		return
@@ -345,7 +346,8 @@ func (ra *RegistrationAuthorityImpl) MatchesCSR(
 		err = core.InternalServerError("Generated certificate public key doesn't match CSR public key")
 		return
 	}
-	if len(csr.Subject.CommonName) > 0 && parsedCertificate.Subject.CommonName != csr.Subject.CommonName {
+	if len(csr.Subject.CommonName) > 0 &&
+		parsedCertificate.Subject.CommonName != strings.ToLower(csr.Subject.CommonName) {
 		err = core.InternalServerError("Generated certificate CommonName doesn't match CSR CommonName")
 		return
 	}
