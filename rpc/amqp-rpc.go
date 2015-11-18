c5fdc7f769513189bbd8a560d5f6111db7921974
Jeff Hodges <jeff@somethingsimilar.com>
2015-11-18 15:31:49 -0800

    allow ProblemDetails to be passed as RPC error
    This allows us to return a for-users ProblemDetails from services that
    the WFE calls (or services that those services call). Doing so means we
    can give better errors to the WFE once we teach the WFE how to pass
    along ProblemDetails itself.
    
    This is for #1153 and a few other tickets in the error-messages label.
    
    Fixes #1160

--- rpc/amqp-rpc.go
+++ rpc/amqp-rpc.go
@@ -225,7 +225,7 @@ func wrapError(err error) *rpcError {
 		wrapped := &rpcError{
 			Value: err.Error(),
 		}
-		switch err.(type) {
+		switch terr := err.(type) {
 		case core.InternalServerError:
 			wrapped.Type = "InternalServerError"
 		case core.NotSupportedError:
@@ -250,6 +250,10 @@ func wrapError(err error) *rpcError {
 			wrapped.Type = "RateLimitedError"
 		case core.ServiceUnavailableError:
 			wrapped.Type = "ServiceUnavailableError"
+		case *core.ProblemDetails:
+			wrapped.Type = string(terr.Type)
+			wrapped.Value = terr.Detail
+
 		}
 		return wrapped
 	}
@@ -285,6 +289,12 @@ func unwrapError(rpcError *rpcError) error {
 		case "ServiceUnavailableError":
 			return core.ServiceUnavailableError(rpcError.Value)
 		default:
+			if strings.HasPrefix(rpcError.Type, "urn:") {
+				return &core.ProblemDetails{
+					Type:   core.ProblemType(rpcError.Type),
+					Detail: rpcError.Value,
+				}
+			}
 			return errors.New(rpcError.Value)
 		}
 	}
