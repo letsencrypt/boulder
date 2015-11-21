2114f5d5ccac72cb0a207d6bcf24b35b9648d715
Jeff Hodges <jeff@somethingsimilar.com>
2015-11-20 20:29:56 -0800

    move ProblemDetails into its own package
    Part of #1161

---
+++ probs/probs_test.go
@@ -0,0 +1,14 @@
+package probs
+
+import (
+	"testing"
+
+	"github.com/letsencrypt/boulder/test"
+)
+
+func TestProblemDetails(t *testing.T) {
+	pd := &ProblemDetails{
+		Type:   MalformedProblem,
+		Detail: "Wat? o.O"}
+	test.AssertEquals(t, pd.Error(), "urn:acme:error:malformed :: Wat? o.O")
+}
--- rpc/amqp-rpc_test.go
+++ rpc/amqp-rpc_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/letsencrypt/boulder/core"
+	"github.com/letsencrypt/boulder/probs"
 	"github.com/letsencrypt/boulder/test"
 )
 
@@ -43,17 +44,17 @@ func TestWrapError(t *testing.T) {
 		expected error
 	}{
 		{
-			&core.ProblemDetails{
-				Type:   core.ConnectionProblem,
+			&probs.ProblemDetails{
+				Type:   probs.ConnectionProblem,
 				Detail: "whoops",
 			},
-			&core.ProblemDetails{
-				Type:   core.ConnectionProblem,
+			&probs.ProblemDetails{
+				Type:   probs.ConnectionProblem,
 				Detail: "whoops",
 			},
 		},
 		{
-			&core.ProblemDetails{Type: "invalid", Detail: "hm"},
+			&probs.ProblemDetails{Type: "invalid", Detail: "hm"},
 			errors.New("hm"),
 		},
 		{
