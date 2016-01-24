e320e53106b41a043eff2a902fa0dede19728231
Philip Schlump <pschlump@gmail.com>
2016-01-24 10:09:05 -0700

    Rest of fix for 10.0

---
+++ test/create_db_users.sql
@@ -0,0 +1,13 @@
+
+-- only run in 10.0.X versions of MariaDB
+
+create user  'policy'@'localhost';
+create user  'sa'@'localhost';
+create user  'ocsp_resp'@'localhost';
+create user  'revoker'@'localhost';
+create user  'importer'@'localhost';
+create user  'mailer'@'localhost';
+create user  'cert_checker'@'localhost';
+create user  'ocsp_update'@'localhost';
+create user  'test_setup'@'localhost';
+
