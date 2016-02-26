f4403803308220fcbe42fd7a29ec2bb4ba3f4e8a
Kane York <rikingcoding@gmail.com>
2016-02-26 14:54:34 -0800

    sa: use mysql.Config to set flags on connect strings
    This also exposes an interface to open a connection using a
    mysql.Config, so we can start changing config files to use those.
    
    Part of #1505

--- sa/database_test.go
+++ sa/database_test.go
@@ -6,6 +6,8 @@
 package sa
 
 import (
+	"database/sql"
+	"errors"
 	"testing"
 
 	"github.com/letsencrypt/boulder/test"
@@ -14,4 +16,31 @@ import (
 func TestInvalidDSN(t *testing.T) {
 	_, err := NewDbMap("invalid")
 	test.AssertError(t, err, "DB connect string missing the slash separating the database name")
+}
+
+var errExpected = errors.New("expected")
+
+func TestNewDbMap(t *testing.T) {
+	const mysqlConnectUrl = "mysql+tcp://policy:password@localhost:3306/boulder_policy_integration?readTimeout=800ms&writeTimeout=800ms"
+	const expectedTransformed = "policy:password@tcp(localhost:3306)/boulder_policy_integration?clientFoundRows=true&parseTime=true&readTimeout=800ms&strict=true&writeTimeout=800ms"
+
+	oldSqlOpen := sqlOpen
+	defer func() {
+		sqlOpen = oldSqlOpen
+	}()
+	sqlOpen = func(dbType, connectString string) (*sql.DB, error) {
+		if connectString != expectedTransformed {
+			t.Errorf("incorrect connection string mangling, got %v", connectString)
+		}
+		return nil, errExpected
+	}
+
+	dbMap, err := NewDbMap(mysqlConnectUrl)
+	if err != errExpected {
+		t.Errorf("got incorrect error: %v", err)
+	}
+	if dbMap != nil {
+		t.Errorf("expected nil, got %v", dbMap)
+	}
+
 }
