109bec1f21ceb22177343b7366d349e1a041986d
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-03-06 19:58:53 -0800

    Fix tests by adding DNSNames.

--- cmd/ocsp-updater/main_test.go
+++ cmd/ocsp-updater/main_test.go
@@ -50,6 +50,7 @@ var log = mocks.UseMockLog()
 func setup(t *testing.T) (*OCSPUpdater, core.StorageAuthority, *gorp.DbMap, clock.FakeClock, func()) {
 	dbMap, err := sa.NewDbMap(vars.DBConnSA)
 	test.AssertNotError(t, err, "Failed to create dbMap")
+	sa.SetSQLDebug(dbMap, true)
 
 	fc := clock.NewFake()
 	fc.Add(1 * time.Hour)
