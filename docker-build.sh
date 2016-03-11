4718f1494d9b08c2e87d7ffa8ab541fdfaf90cb9
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-03-11 00:37:17 -0800

    Also add make action

--- docker-build.sh
+++ docker-build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -o xtrace
 
-if [ -z "$VERSION"]; then
+if [ -z "$VERSION" ]; then
     VERSION="1.0.0"
 fi
-if [ -z "$COMMIT" ]; then
-    COMMIT=$(git rev-parse --short HEAD)
+if [ -z "$COMMIT_ID" ]; then
+    COMMIT_ID=$(git rev-parse --short HEAD)
 fi
-
-rm -r build/
-mkdir build
+if [ -z "$ARCHIVEDIR" ]; then
+    ARCHIVEDIR=$(pwd)
+fi
 
 docker build -f Dockerfile-build -t boulder-build .
 docker rm builder
 docker run --name builder boulder-build
-docker cp builder:/tmp/boulder-$VERSION-$COMMIT.x86_64.rpm build/
+docker cp builder:/tmp/boulder-$VERSION-$COMMIT_ID.x86_64.rpm $ARCHIVEDIR/
 docker rm builder
