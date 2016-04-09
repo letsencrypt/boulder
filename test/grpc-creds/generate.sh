956f36a4b772ef54b0de381e062730eb7d281029
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-04-08 18:39:55 -0700

    review fixes

--- test/grpc-creds/generate.sh
+++ test/grpc-creds/generate.sh
@@ -14,7 +14,7 @@ client_path="client.pem"
 openssl genrsa -out $key_path $size
 # generate ca
 openssl req -x509 -new -nodes -key $key_path -sha256 -days $validity -subj "/O=boulder/CN=grpc-test-ca" -out $ca_path
-# generate csr for server + client (they are the same :|)
+# generate csr for server + client (TODO(#1719): generate individual certs for each service name)
 openssl req -new -key $key_path -out $duocsr_path -subj "/O=boulder/CN=boulder"
 # generate server cert
 openssl x509 -req -in $duocsr_path -CA $ca_path -CAkey $key_path -CAcreateserial -days $validity -sha256 -out $server_path
