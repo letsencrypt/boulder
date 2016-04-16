d9a48a785ca0978ee0ebded1778e39f0d5fe7460
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-16 10:39:01 -0700

    Restore master fetch.

--- test/travis-before-install.sh
+++ test/travis-before-install.sh
@@ -11,6 +11,11 @@ if [ ! -d $GOPATH/src/github.com/letsencrypt/boulder ] ; then
   ln -s $TRAVIS_BUILD_DIR $GOPATH/src/github.com/letsencrypt/boulder
 fi
 
+# Travis does shallow clones, so there is no master branch present.
+# But test-no-outdated-migrations.sh needs to check diffs against master.
+# Fetch just the master branch from origin.
+( git fetch origin master
+git branch master FETCH_HEAD ) &
 # Github-PR-Status secret
 if [ -n "$encrypted_53b2630f0fb4_key" ]; then
   openssl aes-256-cbc \
