36d7d4aa205af0ec343bd881a108c273584f5653
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-18 14:01:24 -0700

    Fix generate commands for vendored world.

--- cmd/caa-checker/proto/generate.go
+++ cmd/caa-checker/proto/generate.go
@@ -1,3 +1,3 @@
 package caaChecker
 
-//go:generate sh -c "protoc --go_out=plugins=grpc:. caaChecker.proto && sed -i 's,golang.org/x/net/context\\|google.golang.org/grpc,github.com/letsencrypt/boulder/Godeps/_workspace/src/&', caaChecker.pb.go"
+//go:generate protoc --go_out=plugins=grpc:. caaChecker.proto
