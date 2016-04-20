91538569a0fdc60bc4b148e3d140dc10889a634d
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-19 17:39:40 -0700

    Restore and generate

--- grpc/bcodes.go
+++ grpc/bcodes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/letsencrypt/boulder/probs"
 )
 
-//go:generate cd ../; stringer -type=BCode grpc/bcodes.go
+//go:generate stringer -type=BCode grpc/bcodes.go
 
 // BCode is an alias so we can use a stringer
 type BCode codes.Code
