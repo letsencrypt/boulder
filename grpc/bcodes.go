ad8958b3075e834dd51c888a2c78504a5c07d494
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-19 18:59:32 -0700

    Fix path.

--- grpc/bcodes.go
+++ grpc/bcodes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/letsencrypt/boulder/probs"
 )
 
-//go:generate stringer -type=BCode grpc/bcodes.go
+//go:generate stringer -type=BCode bcodes.go
 
 // BCode is an alias so we can use a stringer
 type BCode codes.Code
