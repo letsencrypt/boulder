6d5348f9753405214f8d685f91633cdc5b47ec35
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-21 15:23:06 -0700

    Run go generate in Travis (#1762)
    * Fix go generate command in metrics.
    
    The previous command only worked on OS X. This one works on Linux but not
    OS X.
    
    Also add generate phase of test.sh.
    
    * Add mockgen to test setup.
    
    * Fix github-pr-status output.
    
    * Fix envvar style.
    
    * Set xtrace.
    
    * Fix test.sh
    
    * Fix test.sh some more.
    
    * Fix mockgen command.
    
    * Add dependencies for running `go generate`.
    
    * Add protoc-gen-go.
    
    * Fix go get command.
    
    * Fix generate.
    
    * Wait for all.
    
    * Fix generate.
    
    * Update generated pb.
    
    * Fix generate commands for vendored world.
    
    * Update documentation for new vendor style.
    
    * Update grpc package to latest.
    
    * Update caaChecker proto with latest.
    
    * Run go generate only over TESTPATHS
    
    * See if Travis passes under 1.6
    
    * Switch back to 1.5.
    
    * Trim run command.
    
    * Run stringer from correct directory.
    
    * Move generate command.
    
    * Restore and generate
    
    * Fix path.
    
    * list contents of GOPATH.
    
    * Fix stringer by prebuilding.
    
    * Try another import path.
    
    * regenerate bcode_string.
    
    * remove excess package
    
    * pull jsha fork of protoc-gen-go that echoes
    
    * Echo protoc version.
    
    * install from source
    
    * CD back.
    
    * Go back to normal protoc-gen-go
    
    * Fix path
    
    * Move protobuf install into test/setup.sh
    
    * Move before_install to install.
    
    * Set PATH.
    
    * Follow 301 with curl.
    
    * Shuffle test order.
    
    * Swap back test order.
    
    * Restore all tests.
    
    * Restore 1.5.3 to Travis.
    
    * Remove unnecessary wait-or-exit
    
    * Generate metrics mock with latest mockgen.
    
    * Wrap TESTPATHS in curlies
    
    * Remove spurious bracket

--- test.sh
+++ test.sh
@@ -77,26 +77,24 @@ function run() {
 
 function run_and_comment() {
   echo "$@"
-  result=$("$@" 2>&1)
-  cat <<<"${result}"
+  result_file=$(mktemp -t bouldertestXXXX)
+  "$@" 2>&1 | tee ${result_file}
 
-  if [ "x${result}" == "x" ]; then
-    update_status --state success
-  else
+  # Fail if result_file is nonempty.
+  if [ -s ${result_file} ]; then
+    echo "[!] FAILURE: $@"
     FAILURE=1
     update_status --state failure
-    echo "[!] FAILURE: $@"
-  fi
-
-  # If this is a travis PR run, post a comment
-  if [ "x${TRAVIS}" != "x" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ -f "${GITHUB_SECRET_FILE}" ] ; then
-    # If the output is non-empty, post a comment and mark this as a failure
-    if [ -n "${result}" ] ; then
-      echo $'```\n'${result}$'\n```' | github-pr-status --authfile $GITHUB_SECRET_FILE \
+    # If this is a travis PR run, post a comment
+    if [ "x${TRAVIS}" != "x" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ -f "${GITHUB_SECRET_FILE}" ] ; then
+      (echo '```' ; cat ${result_file} ; echo -e '\n```') | github-pr-status --authfile $GITHUB_SECRET_FILE \
         --owner "letsencrypt" --repo "boulder" \
         comment --pr "${TRAVIS_PULL_REQUEST}" -b -
     fi
+  else
+    update_status --state success
   fi
+  rm ${result_file}
 }
 
 function die() {
@@ -122,7 +120,7 @@ function run_unit_tests() {
     # are not stdlib packages. We can then install them with the race
     # detector enabled to prevent our individual `go test` calls from
     # building them multiple times.
-    all_shared_imports=$(go list -f '{{ join .Imports "\n" }}' $TESTPATHS | sort | uniq)
+    all_shared_imports=$(go list -f '{{ join .Imports "\n" }}' ${TESTPATHS} | sort | uniq)
     deps=$(go list -f '{{ if not .Standard }}{{ .ImportPath }}{{ end }}' ${all_shared_imports})
     echo "go installing race detector enabled dependencies"
     go install -race $deps
@@ -148,7 +146,7 @@ function run_unit_tests() {
     # spuriously because one test is modifying a table (especially
     # registrations) while another test is reading it.
     # https://github.com/letsencrypt/boulder/issues/1499
-    run go test -p 1 $GOTESTFLAGS $TESTPATHS
+    run go test -p 1 $GOTESTFLAGS ${TESTPATHS}
   fi
 }
 
@@ -161,7 +159,7 @@ GOBIN=${GOBIN:-$HOME/gopath/bin}
 #
 if [[ "$RUN" =~ "vet" ]] ; then
   start_context "vet"
-  run_and_comment go vet $TESTPATHS
+  run_and_comment go vet ${TESTPATHS}
   end_context #vet
 fi
 
@@ -287,8 +285,30 @@ if [[ "$RUN" =~ "errcheck" ]] ; then
   start_context "errcheck"
   run_and_comment errcheck \
     -ignore io:Write,os:Remove,net/http:Write,github.com/letsencrypt/boulder/metrics:.*,github.com/cactus/go-statsd-client/statsd:.* \
-    $(echo $TESTPATHS | tr ' ' '\n' | grep -v test)
+    $(echo ${TESTPATHS} | tr ' ' '\n' | grep -v test)
   end_context #errcheck
+fi
+
+# Run generate to make sure all our generated code can be re-generated with
+# current tools.
+# Note: Some of the tools we use seemingly don't understand ./vendor yet, and
+# so will fail if imports are not available in $GOPATH. So, in travis, this
+# always needs to run after `godep restore`.
+if [[ "$RUN" =~ "generate" ]] ; then
+  start_context "generate"
+  # Additionally, we need to run go install before go generate because the stringer command
+  # (using in ./grpc/) checks imports, and depends on the presence of a built .a
+  # file to determine an import really exists. See
+  # https://golang.org/src/go/internal/gcimporter/gcimporter.go#L30
+  # Without this, we get error messages like:
+  #   stringer: checking package: grpc/bcodes.go:6:2: could not import
+  #     github.com/letsencrypt/boulder/probs (can't find import:
+  #     github.com/letsencrypt/boulder/probs)
+  go install ./probs
+  go install google.golang.org/grpc/codes
+  run_and_comment go generate ${TESTPATHS}
+  run_and_comment git diff --exit-code .
+  end_context #"generate"
 fi
 
 exit ${FAILURE}
