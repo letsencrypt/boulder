26de87dfa22f3f012e5e626b3dc0d64ace4d1d15
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-21 11:05:17 -0700

    Update grpc dep and regenerate caa-checker. (#1761)
    * Update grpc dep and regenerate caa-checker.
    
    The latest version generates a different format. This is a precursor to running
    go generate in Travis.

--- vendor/google.golang.org/grpc/transport/transport.go
+++ vendor/google.golang.org/grpc/transport/transport.go
@@ -63,13 +63,11 @@ type recvMsg struct {
 	err error
 }
 
-func (recvMsg) isItem() bool {
-	return true
-}
+func (*recvMsg) item() {}
 
 // All items in an out of a recvBuffer should be the same type.
 type item interface {
-	isItem() bool
+	item()
 }
 
 // recvBuffer is an unbounded channel of item.
@@ -89,12 +87,14 @@ func newRecvBuffer() *recvBuffer {
 func (b *recvBuffer) put(r item) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
-	b.backlog = append(b.backlog, r)
-	select {
-	case b.c <- b.backlog[0]:
-		b.backlog = b.backlog[1:]
-	default:
+	if len(b.backlog) == 0 {
+		select {
+		case b.c <- r:
+			return
+		default:
+		}
 	}
+	b.backlog = append(b.backlog, r)
 }
 
 func (b *recvBuffer) load() {
