a0e9320c95cc108c61dda3165d6b5a9fe5e1d666
Kane York <rikingcoding@gmail.com>
2016-04-22 14:44:42 -0700

    Regenerate protobufs

--- rpc/pb/va/va.pb.go
+++ rpc/pb/va/va.pb.go
@@ -167,7 +167,7 @@ var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion1
+const _ = grpc.SupportPackageIsVersion2
 
 // Client API for VA service
 
@@ -213,28 +213,40 @@ func RegisterVAServer(s *grpc.Server, srv VAServer) {
 	s.RegisterService(&_VA_serviceDesc, srv)
 }
 
-func _VA_IsSafeDomain_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _VA_IsSafeDomain_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(IsSafeDomainRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(VAServer).IsSafeDomain(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(VAServer).IsSafeDomain(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/va.VA/IsSafeDomain",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(VAServer).IsSafeDomain(ctx, req.(*IsSafeDomainRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
-func _VA_PerformValidation_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+func _VA_PerformValidation_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(PerformValidationRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
-	out, err := srv.(VAServer).PerformValidation(ctx, in)
-	if err != nil {
-		return nil, err
+	if interceptor == nil {
+		return srv.(VAServer).PerformValidation(ctx, in)
 	}
-	return out, nil
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/va.VA/PerformValidation",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(VAServer).PerformValidation(ctx, req.(*PerformValidationRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 var _VA_serviceDesc = grpc.ServiceDesc{
