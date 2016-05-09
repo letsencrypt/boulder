ada450404e668bdbb2d04083bd3a4d4afee127a0
Kane York <rikingcoding@gmail.com>
2016-05-09 16:41:05 -0700

    Manual integration test passes

--- ra/registration-authority.go
+++ ra/registration-authority.go
@@ -319,7 +319,7 @@ func (ra *RegistrationAuthorityImpl) checkPendingAuthorizationLimit(ctx context.
 }
 
 func (ra *RegistrationAuthorityImpl) checkValidAuthorizationLimit(ctx context.Context, identifier core.AcmeIdentifier, regID int64) error {
-	limit := ra.rlPolicies.PendingAuthorizationsPerAccount
+	limit := ra.rlPolicies.ValidAuthorizationsPerName
 	if limit.Enabled() {
 		now := ra.clk.Now()
 		count, err := ra.SA.CountValidAuthorizations(ctx, identifier.Value, limit.WindowBegin(now), now)
