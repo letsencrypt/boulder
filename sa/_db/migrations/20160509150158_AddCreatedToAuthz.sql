105cfa701ca4b6ca2dcd21ca32cc595c19effebf
Kane York <rikingcoding@gmail.com>
2016-05-10 11:21:30 -0700

    Fix migration

--- sa/_db/migrations/20160509150158_AddCreatedToAuthz.sql
+++ sa/_db/migrations/20160509150158_AddCreatedToAuthz.sql
@@ -4,9 +4,11 @@
 
 ALTER TABLE `authz` ADD COLUMN `created` DATETIME DEFAULT NULL;
 ALTER TABLE `pendingAuthorizations` ADD COLUMN `created` DATETIME DEFAULT NULL;
+CREATE INDEX `authz_countValidAuthz` on `authz` (`identifier`, `created`, `status`);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
 ALTER TABLE `authz` DROP COLUMN `created`;
 ALTER TABLE `pendingAuthorizations` DROP COLUMN `created`;
+DROP INDEX `authz_countValidAuthz` on `authz`;
