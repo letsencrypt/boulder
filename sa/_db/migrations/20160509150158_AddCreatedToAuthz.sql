ae930b08ec8ca8b8e625287562f14d8e181f5bd5
Kane York <rikingcoding@gmail.com>
2016-05-13 08:44:13 -0700

    rename index

--- sa/_db/migrations/20160509150158_AddCreatedToAuthz.sql
+++ sa/_db/migrations/20160509150158_AddCreatedToAuthz.sql
@@ -4,11 +4,11 @@
 
 ALTER TABLE `authz` ADD COLUMN `created` DATETIME DEFAULT NULL;
 ALTER TABLE `pendingAuthorizations` ADD COLUMN `created` DATETIME DEFAULT NULL;
-CREATE INDEX `authz_countValidAuthz` on `authz` (`identifier`, `created`, `status`);
+CREATE INDEX `authz_identifier_created_status` on `authz` (`identifier`, `created`, `status`);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
 ALTER TABLE `authz` DROP COLUMN `created`;
 ALTER TABLE `pendingAuthorizations` DROP COLUMN `created`;
-DROP INDEX `authz_countValidAuthz` on `authz`;
+DROP INDEX `authz_identifier_created_status` on `authz`;
