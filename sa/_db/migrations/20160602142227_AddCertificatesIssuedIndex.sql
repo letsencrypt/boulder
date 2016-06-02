12af5763d1376264c997a4317b8461806c679787
Daniel <daniel@binaryparadox.net>
2016-06-02 16:06:52 -0400

    Rename index to use _idx suffix

--- sa/_db/migrations/20160602142227_AddCertificatesIssuedIndex.sql
+++ sa/_db/migrations/20160602142227_AddCertificatesIssuedIndex.sql
@@ -2,9 +2,9 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-ALTER TABLE `certificates` ADD INDEX `issued` (`issued`);
+ALTER TABLE `certificates` ADD INDEX `issued_idx` (`issued`);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
-ALTER TABLE `certificates` DROP INDEX `issued`;
+ALTER TABLE `certificates` DROP INDEX `issued_idx`;
