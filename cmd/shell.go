d8b4128351dc03e97843604a7489f5dc83de8a4c
Ben Irving <jammin.irving@gmail.com>
2016-06-14 09:03:52 -0700

    Remove server config option as UnixSocket is the default

--- cmd/shell.go
+++ cmd/shell.go
@@ -184,7 +184,7 @@ func StatsAndLogging(statConf StatsdConfig, logConf SyslogConfig) (metrics.Statt
 	tag := path.Base(os.Args[0])
 	syslogger, err := syslog.Dial(
 		"",
-		logConf.Server,
+		"",
 		syslog.LOG_INFO|syslog.LOG_LOCAL0, // default, overridden by log calls
 		tag)
 	FailOnError(err, "Could not connect to Syslog")
