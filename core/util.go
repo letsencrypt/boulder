3d903260eda617653d0222edc75c9043cc4c0c21
Maxime Boisvert <maxime.boisvert@shopify.com>
2016-06-20 11:01:54 -0400

    Adds RejectedIdentifierError on Blacklisted error.

--- core/util.go
+++ core/util.go
@@ -89,6 +89,9 @@ type TooManyRPCRequestsError string
 // BadNonceError indicates an empty of invalid nonce was provided
 type BadNonceError string
 
+// RejectedIdentifierError indicates the user data was rejected
+type RejectedIdentifierError string
+
 func (e InternalServerError) Error() string      { return string(e) }
 func (e NotSupportedError) Error() string        { return string(e) }
 func (e MalformedRequestError) Error() string    { return string(e) }
@@ -100,6 +103,7 @@ func (e NoSuchRegistrationError) Error() string  { return string(e) }
 func (e RateLimitedError) Error() string         { return string(e) }
 func (e TooManyRPCRequestsError) Error() string  { return string(e) }
 func (e BadNonceError) Error() string            { return string(e) }
+func (e RejectedIdentifierError) Error() string  { return string(e) }
 
 // statusTooManyRequests is the HTTP status code meant for rate limiting
 // errors. It's not currently in the net/http library so we add it here.
--- policy/pa.go
+++ policy/pa.go
@@ -124,7 +124,7 @@ var (
 	errInvalidIdentifier   = core.MalformedRequestError("Invalid identifier type")
 	errNonPublic           = core.MalformedRequestError("Name does not end in a public suffix")
 	errICANNTLD            = core.MalformedRequestError("Name is an ICANN TLD")
-	errBlacklisted         = core.MalformedRequestError("Policy forbids issuing for name")
+	errBlacklisted         = core.RejectedIdentifierError("Policy forbids issuing for name")
 	errNotWhitelisted      = core.MalformedRequestError("Name is not whitelisted")
 	errInvalidDNSCharacter = core.MalformedRequestError("Invalid character in DNS name")
 	errNameTooLong         = core.MalformedRequestError("DNS name too long")
