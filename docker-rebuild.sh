85432d88a006070a685099a5add7f5efc9227c84
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-07-13 12:56:54 -0700

    Review fixes

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
-# stop any running boulder containers
-docker-compose stop boulder
-# rebuild anything that has changed since the last run
-docker-compose build boulder
-# if docker-compose run is used before using `up -d` docker will
+# Stop any running boulder containers
+docker-compose stop
+# Rebuild anything that has changed since the last run
+docker-compose build
+# Ff docker-compose run is used before using `up -d` docker will
 # default to using old containers built from previous images
 # (if anything has changed). `up` takes any changes in configuration
 # or image into consideration when choosing which containers to
 # bring back up and will recreate any that are out of date
-docker-compose up -d boulder
+docker-compose up -d
