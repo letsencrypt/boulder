3c530a8369574a40542b6eb5994d4ea89e803ff4
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-07-13 13:01:14 -0700

    Fix typo

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -3,7 +3,7 @@
 docker-compose stop
 # Rebuild anything that has changed since the last run
 docker-compose build
-# Ff docker-compose run is used before using `up -d` docker will
+# If docker-compose run is used before using `up -d` docker will
 # default to using old containers built from previous images
 # (if anything has changed). `up` takes any changes in configuration
 # or image into consideration when choosing which containers to
