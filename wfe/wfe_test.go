5da7728cc59bb81a810cba663b9db9b650021a88
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-07-20 08:04:12 -0700

    Add helpful note to CSR parsing error. (#2055)
    A few people have run into #565. This updated error message will make it more quickly obvious what's wrong.

--- wfe/wfe_test.go
+++ wfe/wfe_test.go
@@ -685,7 +685,7 @@ func TestIssueCertificate(t *testing.T) {
 		makePostRequest(signRequest(t, `{"resource":"new-cert"}`, wfe.nonceService)))
 	assertJSONEquals(t,
 		responseWriter.Body.String(),
-		`{"type":"urn:acme:error:malformed","detail":"Error parsing certificate request","status":400}`)
+		`{"type":"urn:acme:error:malformed","detail":"Error parsing certificate request. Extensions in the CSR marked critical can cause this error: https://github.com/letsencrypt/boulder/issues/565","status":400}`)
 
 	// Valid, signed JWS body, payload has an invalid signature on CSR and no authorizations:
 	// alias b64url="base64 -w0 | sed -e 's,+,-,g' -e 's,/,_,g'"
