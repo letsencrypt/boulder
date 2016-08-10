d2c13190cceef4183fbedc2587e72ca0332488bf
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-08-10 14:05:33 -0700

    Update stringer

--- features/featureflag_string.go
+++ features/featureflag_string.go
@@ -4,9 +4,9 @@ package features
 
 import "fmt"
 
-const _FeatureFlag_name = "unusedDNSAllowLoopbackAddressesCheckMalformedCSRDoNotForceCNReuseValidAuthzEnableMustStaple"
+const _FeatureFlag_name = "unusedDNSAllowLoopbackAddressesCheckMalformedCSRDoNotForceCNReuseValidAuthzEnableMustStapleAcceptRevocationReason"
 
-var _FeatureFlag_index = [...]uint8{0, 6, 31, 48, 60, 75, 91}
+var _FeatureFlag_index = [...]uint8{0, 6, 31, 48, 60, 75, 91, 113}
 
 func (i FeatureFlag) String() string {
 	if i < 0 || i >= FeatureFlag(len(_FeatureFlag_index)-1) {
