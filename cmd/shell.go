a0ab66da18354ce6c723acd1e3570d8024b2e136
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-10-28 15:22:10 -0700

    Update deps; use promhttp.Handler()

--- cmd/shell.go
+++ cmd/shell.go
@@ -36,6 +36,7 @@ import (
 	cfsslLog "github.com/cloudflare/cfssl/log"
 	"github.com/go-sql-driver/mysql"
 	prom "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/letsencrypt/boulder/core"
 	"github.com/letsencrypt/boulder/features"
@@ -203,7 +204,7 @@ func DebugServer(addr string) {
 	if err != nil {
 		log.Fatalf("unable to boot debug server on %#v", addr)
 	}
-	http.Handle("/metrics", prom.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 	err = http.Serve(ln, nil)
 	if err != nil {
 		log.Fatalf("unable to boot debug server: %v", err)
