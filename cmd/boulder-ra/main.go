38a92fd704041220548c7d3f4dce6228bc52723e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-11-04 18:34:59 -0700

    Revert RA change.

--- cmd/boulder-ra/main.go
+++ cmd/boulder-ra/main.go
@@ -193,8 +193,8 @@ func main() {
 	rai.CA = cac
 	rai.SA = sac
 
-	//err = rai.UpdateIssuedCountForever()
-	//cmd.FailOnError(err, "Updating total issuance count")
+	err = rai.UpdateIssuedCountForever()
+	cmd.FailOnError(err, "Updating total issuance count")
 
 	ras, err := rpc.NewAmqpRPCServer(amqpConf, c.RA.MaxConcurrentRPCServerRequests, scope, logger)
 	cmd.FailOnError(err, "Unable to create RA RPC server")
