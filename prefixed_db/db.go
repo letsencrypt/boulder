673cc6eaa4f84509d1efc7c5844249a0309b99d3
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-11-08 06:07:36 -0800

    Fix max_statement_time and long_query_time (#2311)
    We try to set `max_statement_time` on new database connections so that long queries can be interrupted server side. However, the existing code is broken because of connection pooling. The `SET SESSION max_statement_time=...` gets executed on on connection, but subsequent queries won't necessarily be executed on the same connection.
    
    This changes fixed the problem by introducing an alternate DB driver that wraps the MySQL driver to prefix every query with `SET STATEMENT max_statement_time=...`
    
    This also changes `vars.go` to use the DSN form of a database name instead of the URL form. This allows using it directly in `prefixed_db`'s tests, and this is the direction we're moving all of our database URLs. We previously used a homebrewed URL syntax because it allowed us to extract certain fields and set config options, but now `mysql.Config` serves that need.
    
    Fixes #2251

---
+++ prefixed_db/db.go
@@ -0,0 +1,47 @@
+package prefixed_db
+
+import "database/sql/driver"
+
+// New clones a database driver to create a new driver with the property that
+// every statement executed will have the given prefix prepended.
+// This is useful, for instance, to set statement-level variables like
+// max_statement_time and long_query_time.
+func New(prefix string, underlying driver.Driver) driver.Driver {
+	return &prefixedDB{
+		prefix:     prefix,
+		underlying: underlying,
+	}
+}
+
+type prefixedDB struct {
+	prefix     string
+	underlying driver.Driver
+}
+
+func (p *prefixedDB) Open(name string) (driver.Conn, error) {
+	conn, err := p.underlying.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return &prefixedConn{
+		prefix: p.prefix,
+		conn:   conn,
+	}, nil
+}
+
+type prefixedConn struct {
+	prefix string
+	conn   driver.Conn
+}
+
+func (c *prefixedConn) Prepare(query string) (driver.Stmt, error) {
+	return c.conn.Prepare(c.prefix + " " + query)
+}
+
+func (c *prefixedConn) Close() error {
+	return c.conn.Close()
+}
+
+func (c *prefixedConn) Begin() (driver.Tx, error) {
+	return c.conn.Begin()
+}
---
+++ prefixed_db/db_test.go
@@ -0,0 +1,36 @@
+package prefixed_db
+
+import (
+	"database/sql"
+	"log"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/letsencrypt/boulder/test/vars"
+)
+
+func TestPrefixing(t *testing.T) {
+	sql.Register("prefixedmysql", New("SET STATEMENT max_statement_time=0.1 FOR", mysql.MySQLDriver{}))
+	db, err := sql.Open("prefixedmysql", vars.DBConnSA)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	var wg sync.WaitGroup
+	for i := 1; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			_, err := db.Exec("SELECT 1 FROM (SELECT SLEEP(?)) as subselect;", i)
+			if err == nil || !strings.HasPrefix(err.Error(), "Error 1969:") {
+				t.Error("Expected to get Error 1969 (timeout), got", err)
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	_ = db.Close()
+}
