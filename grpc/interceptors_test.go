e55a45860a84b4fce39687347d77950efe2edec3
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-12-08 09:30:07 -0800

    Add a test for failfast.

--- grpc/interceptors_test.go
+++ grpc/interceptors_test.go
@@ -1,7 +1,12 @@
 package grpc
 
+//go:generate sh -c "cd .. && protoc --go_out=plugins=grpc,Mcore/proto/core.proto=github.com/letsencrypt/boulder/grpc:. grpc/interceptors_test.proto"
+
 import (
 	"errors"
+	"log"
+	"net"
+	"strconv"
 	"testing"
 	"time"
 
@@ -24,7 +29,7 @@ func testHandler(_ context.Context, i interface{}) (interface{}, error) {
 	return nil, nil
 }
 
-func testInvoker(_ context.Context, method string, _, _ interface{}, _ *grpc.ClientConn, _ ...grpc.CallOption) error {
+func testInvoker(_ context.Context, method string, _, _ interface{}, _ *grpc.ClientConn, opts ...grpc.CallOption) error {
 	if method == "-service-brokeTest" {
 		return errors.New("")
 	}
@@ -80,6 +85,62 @@ func TestClientInterceptor(t *testing.T) {
 	statter.EXPECT().Inc("fake.gRPCClient.service_brokeTest.Failed", int64(1), float32(1.0)).Return(nil)
 	err = ci.intercept(context.Background(), "-service-brokeTest", nil, nil, nil, testInvoker)
 	test.AssertError(t, err, "ci.intercept didn't fail when handler returned a error")
+}
+
+// testServer is used to implement InterceptorTest
+type testServer struct{}
+
+// Chill implements InterceptorTest.Chill
+func (s *testServer) Chill(ctx context.Context, in *Time) (*Time, error) {
+	start := time.Now()
+	time.Sleep(time.Duration(*in.Time) * time.Nanosecond)
+	spent := int64(time.Since(start) / time.Nanosecond)
+	return &Time{Time: &spent}, nil
+}
+
+func TestInterceptors(t *testing.T) {
+	// Set up a connection to a nonexistent server.
+	ci := &clientInterceptor{metrics.NewNoopScope(), clock.Default(), 1100 * time.Millisecond}
+	conn, err := grpc.Dial(net.JoinHostPort("localhost", strconv.Itoa(port)),
+		grpc.WithInsecure(),
+		grpc.WithBalancer(grpc.RoundRobin(newStaticResolver([]string{"localhost:19000"}))),
+		grpc.WithUnaryInterceptor(ci.intercept))
+	if err != nil {
+		t.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := NewInterceptorTestClient(conn)
+
+	/*
+		lis, err := net.Listen("tcp", ":0")
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		port := lis.Addr().(*net.TCPAddr).Port
+
+		si := &serverInterceptor{metrics.NewNoopScope(), clock.Default()}
+		s := grpc.NewServer(grpc.UnaryInterceptor(si.intercept))
+		RegisterInterceptorTestServer(s, &testServer{})
+		go func() {
+			start := time.Now()
+			if err := s.Serve(lis); err != nil &&
+				!strings.HasSuffix(err.Error(), "use of closed network connection") {
+				t.Fatalf("s.Serve: %v after %s", err, time.Since(start))
+			}
+		}()
+
+	*/
+	start := time.Now()
+	var second int64 = time.Second.Nanoseconds()
+	_, err = c.Chill(context.Background(), &Time{Time: &second})
+	if err == nil {
+		t.Fatal("Successful Chill when we expected failure.")
+	}
+	if time.Since(start) < 100*time.Millisecond {
+		t.Fatal("Chill failed fast, when FailFast should be disabled.")
+	}
+
+	//s.GracefulStop()
 }
 
 func TestCleanMethod(t *testing.T) {
