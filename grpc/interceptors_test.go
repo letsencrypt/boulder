5b865f1d6311c83d11767ab2910eb3d608671216
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-12-09 12:03:45 -0800

    Disable fail-fast for gRPC. (#2397)
    This allows us to restart backends with relatively little interruption in
    service, provided the backends come up promptly.
    
    Fixes #2389 and #2408

--- grpc/interceptors_test.go
+++ grpc/interceptors_test.go
@@ -10,6 +10,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
+	"github.com/letsencrypt/boulder/grpc/test_proto"
 	"github.com/letsencrypt/boulder/metrics"
 	"github.com/letsencrypt/boulder/test"
 )
@@ -24,7 +25,7 @@ func testHandler(_ context.Context, i interface{}) (interface{}, error) {
 	return nil, nil
 }
 
-func testInvoker(_ context.Context, method string, _, _ interface{}, _ *grpc.ClientConn, _ ...grpc.CallOption) error {
+func testInvoker(_ context.Context, method string, _, _ interface{}, _ *grpc.ClientConn, opts ...grpc.CallOption) error {
 	if method == "-service-brokeTest" {
 		return errors.New("")
 	}
@@ -80,6 +81,44 @@ func TestClientInterceptor(t *testing.T) {
 	statter.EXPECT().Inc("fake.gRPCClient.service_brokeTest.Failed", int64(1), float32(1.0)).Return(nil)
 	err = ci.intercept(context.Background(), "-service-brokeTest", nil, nil, nil, testInvoker)
 	test.AssertError(t, err, "ci.intercept didn't fail when handler returned a error")
+}
+
+// testServer is used to implement InterceptorTest
+type testServer struct{}
+
+// Chill implements InterceptorTest.Chill
+func (s *testServer) Chill(ctx context.Context, in *test_proto.Time) (*test_proto.Time, error) {
+	start := time.Now()
+	time.Sleep(time.Duration(*in.Time) * time.Nanosecond)
+	spent := int64(time.Since(start) / time.Nanosecond)
+	return &test_proto.Time{Time: &spent}, nil
+}
+
+// TestFailFastFalse sends a gRPC request to a backend that is
+// unavailable, and ensures that the request doesn't error out until the
+// timeout is reached, i.e. that FailFast is set to false.
+// https://github.com/grpc/grpc/blob/master/doc/wait-for-ready.md
+func TestFailFastFalse(t *testing.T) {
+	ci := &clientInterceptor{metrics.NewNoopScope(), clock.Default(), 100 * time.Millisecond}
+	conn, err := grpc.Dial("localhost:19876", // random, probably unused port
+		grpc.WithInsecure(),
+		grpc.WithBalancer(grpc.RoundRobin(newStaticResolver([]string{"localhost:19000"}))),
+		grpc.WithUnaryInterceptor(ci.intercept))
+	if err != nil {
+		t.Fatalf("did not connect: %v", err)
+	}
+	c := test_proto.NewChillerClient(conn)
+
+	start := time.Now()
+	var second int64 = time.Second.Nanoseconds()
+	_, err = c.Chill(context.Background(), &test_proto.Time{Time: &second})
+	if err == nil {
+		t.Errorf("Successful Chill when we expected failure.")
+	}
+	if time.Since(start) < 90*time.Millisecond {
+		t.Errorf("Chill failed fast, when FailFast should be disabled.")
+	}
+	_ = conn.Close()
 }
 
 func TestCleanMethod(t *testing.T) {
