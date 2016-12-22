675c3a5a212021b95cab2d904f139d66bdf31bb7
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-12-22 14:52:08 -0800

    Purge POST'd OCSP responses as well as GETs

--- cmd/ocsp-updater/main.go
+++ cmd/ocsp-updater/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/md5"
 	"crypto/sha256"
 	"crypto/x509"
 	"database/sql"
@@ -213,17 +214,30 @@ func (updater *OCSPUpdater) sendPurge(der []byte) {
 		return
 	}
 
-	// Create a GET style OCSP url for each endpoint in cert.OCSPServer (still waiting
-	// on word from Akamai on how to properly purge cached POST requests, for now just
-	// do GET)
+	// Create a GET and special Akamai POST style OCSP url for each endpoint in cert.OCSPServer
 	urls := []string{}
+	reverse := func(b []byte) []byte {
+		for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+			b[i], b[j] = b[j], b[i]
+		}
+		return b
+	}
 	for _, ocspServer := range cert.OCSPServer {
 		if !strings.HasSuffix(ocspServer, "/") {
 			ocspServer += "/"
 		}
+		// Generate GET url
 		urls = append(
 			urls,
 			fmt.Sprintf("%s%s", ocspServer, url.QueryEscape(base64.StdEncoding.EncodeToString(req))),
+		)
+		// Generate POST url, format is the URL that was POST'd to with a query string with
+		// the parameter 'body-mdy' and the value of the first two uint32s in little endian
+		// order in hex of the MD5 hash of the OCSP request body.
+		hash := md5.Sum(der)
+		urls = append(
+			urls,
+			fmt.Sprintf("%s?body-mdy=%x%x", ocspServer, reverse(hash[0:4]), reverse(hash[4:8])),
 		)
 	}
 
