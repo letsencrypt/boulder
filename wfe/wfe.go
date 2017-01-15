36839f43605db58d0baedcc813132caf13aad6b1
Josh Soref <jsoref@users.noreply.github.com>
2017-01-15 21:43:35 +0000

    spelling: unparseable

--- wfe/wfe.go
+++ wfe/wfe.go
@@ -530,7 +530,7 @@ func (wfe *WebFrontEndImpl) verifyPOST(ctx context.Context, logEvent *requestEve
 	}
 	err = json.Unmarshal([]byte(payload), &parsedRequest)
 	if err != nil {
-		wfe.stats.Inc("Errors.UnparsableJWSPayload", 1)
+		wfe.stats.Inc("Errors.UnparseableJWSPayload", 1)
 		logEvent.AddError("unable to JSON parse resource from JWS payload: %s", err)
 		return nil, nil, reg, probs.Malformed("Request payload did not parse as JSON")
 	}
