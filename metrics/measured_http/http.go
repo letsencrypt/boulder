4a8f7fe90548b1244bbd63b08282fe7dc6797d4c
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-03-27 21:42:51 -0700

    Add tests for MeasuredHandler.

--- metrics/measured_http/http.go
+++ metrics/measured_http/http.go
@@ -40,16 +40,26 @@ func (r *responseWriterWithStatus) WriteHeader(code int) {
 type MeasuredHandler struct {
 	http.Handler
 	clk clock.Clock
+	// Normally this is always responseTime, but we override it for testing.
+	stat *prometheus.HistogramVec
 }
 
 func New(h http.Handler, clk clock.Clock) *MeasuredHandler {
 	return &MeasuredHandler{
 		Handler: h,
 		clk:     clk,
+		stat:    responseTime,
 	}
 }
 
+// endpointFromPath turns a request path into a value suitable for a Prometheus
+// label value, by eliminating path components that vary widely (like user ids
+// and authorization ids). It uses a simple heuristic: Remove everything after
+// the first path component that doesn't match lowercase a-z, plus hyphen. This
+// happens to work very well with Boulder's WFE.
 func endpointFromPath(path string) string {
+	// Remove any query
+	path = strings.Split(path, "?")[0]
 	components := strings.Split(path, "/")
 	var i int
 	var v string
@@ -70,7 +80,7 @@ func (h *MeasuredHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	endpoint := endpointFromPath(r.URL.Path)
 
 	defer func() {
-		responseTime.With(prometheus.Labels{
+		h.stat.With(prometheus.Labels{
 			"endpoint": endpoint,
 			"method":   r.Method,
 			"code":     fmt.Sprintf("%d", rwws.code),
