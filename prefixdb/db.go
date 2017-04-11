be1638f1cc17b9bc870e3bd5537e511e3cb28e21
Roland Shoemaker <rolandshoemaker@gmail.com>
2017-04-11 11:57:06 -0700

    Change prefixdb semantics

--- prefixdb/db.go
+++ prefixdb/db.go
@@ -3,8 +3,8 @@ package prefixdb
 import "database/sql/driver"
 
 // New clones a database driver to create a new driver with the property that
-// every statement executed will have the given prefix prepended.
-// This is useful, for instance, to set statement-level variables like
+// every connection created will have a query executed before it is used.
+// This is useful, for instance, to set conenction-level variables like
 // max_statement_time and long_query_time.
 func New(prefix string, underlying driver.Driver) driver.Driver {
 	return &prefixedDB{
@@ -23,25 +23,13 @@ func (p *prefixedDB) Open(name string) (driver.Conn, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &prefixedConn{
-		prefix: p.prefix,
-		conn:   conn,
-	}, nil
-}
-
-type prefixedConn struct {
-	prefix string
-	conn   driver.Conn
-}
-
-func (c *prefixedConn) Prepare(query string) (driver.Stmt, error) {
-	return c.conn.Prepare(c.prefix + " " + query)
-}
-
-func (c *prefixedConn) Close() error {
-	return c.conn.Close()
-}
-
-func (c *prefixedConn) Begin() (driver.Tx, error) {
-	return c.conn.Begin()
+	stmt, err := conn.Prepare(p.prefix)
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmt.Exec(nil)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
