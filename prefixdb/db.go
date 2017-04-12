07d585b1138f4e8acc9b8c4a42b5a5dbb8b5dba9
Roland Shoemaker <rolandshoemaker@gmail.com>
2017-04-12 12:55:02 -0700

    Typo

--- prefixdb/db.go
+++ prefixdb/db.go
@@ -4,7 +4,7 @@ import "database/sql/driver"
 
 // New clones a database driver to create a new driver with the property that
 // every connection created will have a query executed before it is used.
-// This is useful, for instance, to set conenction-level variables like
+// This is useful, for instance, to set connection-level variables like
 // max_statement_time and long_query_time.
 func New(prefix string, underlying driver.Driver) driver.Driver {
 	return &prefixedDB{
