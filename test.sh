cf251b3eda7e41a2fdd28a82310e2ec366f606e3
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-05-10 16:11:23 -0700

    Remove more statsd traces.

--- test.sh
+++ test.sh
@@ -203,7 +203,7 @@ fi
 if [[ "$RUN" =~ "errcheck" ]] ; then
   start_context "errcheck"
   run_and_expect_silence errcheck \
-    -ignore io:Write,os:Remove,net/http:Write,github.com/letsencrypt/boulder/metrics:.*,github.com/letsencrypt/boulder/vendor/github.com/cactus/go-statsd-client/statsd:.* \
+    -ignore io:Write,os:Remove,net/http:Write,github.com/letsencrypt/boulder/metrics:.* \
     $(echo ${TESTPATHS} | tr ' ' '\n' | grep -v test)
   end_context #errcheck
 fi
