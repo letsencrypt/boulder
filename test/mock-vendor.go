ad7b4ba375a878725be26697d0d90f29997e8c4b
Roland Shoemaker <rolandshoemaker@gmail.com>
2017-05-31 14:37:39 -0700

    Update comment

--- test/mock-vendor.go
+++ test/mock-vendor.go
@@ -3,9 +3,9 @@ package test
 import "github.com/golang/mock/mockgen/model"
 
 // This assignment exists solely for the purpose of convincing godep
-// to vendor github.com/golang/mock/mockgen/model so that we don't
-// require users of the boulder-tools image to have extra packages
-// in their GOPATH to properly run boulder tests. Variable name is
-// chosen so that it is unlikely to clash with anything else in this
+// to vendor github.com/golang/mock/mockgen/model as gomock will fail
+// to generate code if it doesn't exist in the users GOPATH but isn't
+// actually imported by any boulder or gomock code. The variable name
+// is chosen so that it is unlikely to clash with anything else in this
 // package.
 var _ignore = model.Package{}
