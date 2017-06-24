d835d5041b871193f20252e4647d8fe87757aa3b
Lucas Amorim <lucas@lucasamorim.ca>
2017-06-24 14:18:13 -0700

    wip

--- sa/_db/migrations/20170423163751_CreateAuthorizationsTable.sql
+++ sa/_db/migrations/20170423163751_CreateAuthorizationsTable.sql
@@ -3,25 +3,26 @@
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE `authorizations` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `token` varchar(255) NOT NULL,
-  `identifierValue` varchar(255) DEFAULT NULL,
-  `identifierType` int DEFAULT NULL,
-  `registrationID` bigint(20) DEFAULT NULL,
-  `status` int DEFAULT 0,
-  `expires` datetime DEFAULT NULL,
-  `combinations` varchar(255) DEFAULT NULL,
-  `sequence` bigint(20) DEFAULT NULL,
-  `validated` datetime DEFAULT NULL,
+  `token` mediumblob,
+  `identifierValue` varchar(255) NOT NULL,
+  `identifierType` int NOT NULL,
+  `registrationID` bigint(20) NOT NULL,
+  `status` int NOT NULL,
+  `expires` datetime NOT NULL,
+  `allowedChallenges` tinyint,
+  `validated` datetime NOT NULL,
   `validationRecord` mediumblob,
   `error` mediumblob,
   `thumbprint` mediumblob,
 
   PRIMARY KEY (`id`),
-  UNIQUE KEY `token` (`token`),
   UNIQUE KEY `identifier_key` (`identifierType`, `identifierValue`),
 
-  KEY `a_regId_idx` (`registrationID`) COMMENT 'Common lookup',
-  CONSTRAINT `a_regId_authz` FOREIGN KEY (`registrationID`) REFERENCES `registrations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `registrationID_status_expires_idx` (`registrationID`, `status`, `expires`),
+  CONSTRAINT `registrationID_status_expires_idx` FOREIGN KEY (`registrationID`) REFERENCES `registrations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+  KEY `identifierType_identifierValue_status_registrationID_expires_idx` (`identifierType`, `identifierValue`, `status`, `registrationID`, `expires`),
+  CONSTRAINT `identifierType_identifierValue_status_registrationID_expires_idx` FOREIGN KEY (`registrationID`) REFERENCES `registrations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- +goose Down
