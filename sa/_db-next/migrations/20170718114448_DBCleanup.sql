a2191682c79500dfbb7ca5216312d80fe6dfa71b
Roland Shoemaker <rolandshoemaker@gmail.com>
2017-07-18 13:55:10 -0700

    Wrap pk/index changes in transaction

--- sa/_db-next/migrations/20170718114448_DBCleanup.sql
+++ sa/_db-next/migrations/20170718114448_DBCleanup.sql
@@ -9,17 +9,17 @@ ALTER TABLE certificateStatus DROP LockCol;
 
 ALTER TABLE certificateStatus DROP subscriberApproved;
 
+START TRANSACTION;
 ALTER TABLE certificateStatus ADD id bigint(20) NOT NULL;
-
 ALTER TABLE certificateStatus DROP PRIMARY KEY, ADD PRIMARY KEY(id), ADD INDEX serial (serial);
-
 ALTER TABLE certificateStatus MODIFY COLUMN id int NOT NULL AUTO_INCREMENT;
+COMMIT;
 
+START TRANSACTION;
 ALTER TABLE certificates ADD id bigint(20) NOT NULL;
-
 ALTER TABLE certificates DROP PRIMARY KEY, ADD PRIMARY KEY(id), ADD INDEX serial (serial);
-
 ALTER TABLE certificates MODIFY COLUMN id int NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 ALTER TABLE challenges DROP validated;
 
@@ -48,12 +48,14 @@ ALTER TABLE certificateStatus ADD LockCol bigint(20) NOT NULL;
 
 ALTER TABLE certificateStatus ADD subscriberApproved tinyint(1) NOT NULL;
 
+START TRANSACTION;
 ALTER TABLE certificateStatus DROP PRIMARY KEY, ADD PRIMARY KEY(serial);
-
 ALTER TABLE certificateStatus DROP id;
+COMMIT;
 
+START TRANSACTION;
 ALTER TABLE certificates DROP PRIMARY KEY, ADD PRIMARY KEY(serial);
-
 ALTER TABLE certificates DROP id;
+COMMIT;
 
 ALTER TABLE challenges ADD validated DATETIME;
