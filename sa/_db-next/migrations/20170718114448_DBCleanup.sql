b6097c80a169b1e87931396aa961f0baaa8a33c2
Roland Shoemaker <rolandshoemaker@gmail.com>
2017-07-18 12:00:16 -0700

    Remove various extraneous tables and columns

---
+++ sa/_db-next/migrations/20170718114448_DBCleanup.sql
@@ -0,0 +1,39 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+DROP TABLE externalCerts;
+
+DROP TABLE identifierData;
+
+ALTER TABLE certificateStatus DROP LockCol;
+
+ALTER TABLE certificateStatus DROP subscriberApproved;
+
+ALTER TABLE challenges DROP validated;
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+CREATE TABLE `externalCerts` (
+  `sha1` varchar(40) NOT NULL,
+  `issuer` text,
+  `subject` text,
+  `notAfter` datetime DEFAULT NULL,
+  `spki` blob,
+  `valid` tinyint(1) DEFAULT NULL,
+  `ev` tinyint(1) DEFAULT NULL,
+  `rawDERCert` blob,
+  UNIQUE KEY `sha1` (`sha1`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `identifierData` (
+  `reversedName` varchar(255) NOT NULL,
+  `certSHA1` varchar(40) NOT NULL,
+  UNIQUE KEY `certSHA1` (`certSHA1`,`reversedName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE certificateStatus ADD LockCol bigint(20) NOT NULL;
+
+ALTER TABLE certificateStatus ADD subscriberApproved tinyint(1) NOT NULL;
+
+ALTER TABLE challenges ADD validated datetime;
