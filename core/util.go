be9bc05d1478d49b589ef7eced787a137226c081
Roland Shoemaker <rolandshoemaker@gmail.com>
2017-08-15 15:29:49 -0700

    Remove features checks from wfe2

--- core/util.go
+++ core/util.go
@@ -95,10 +95,18 @@ func (e BadNonceError) Error() string           { return string(e) }
 
 // Random stuff
 
+type randSource interface {
+	Read(p []byte) (n int, err error)
+}
+
+// RandReader is used so that it can be replaced in tests that require
+// deterministic output
+var RandReader randSource = rand.Reader
+
 // RandomString returns a randomly generated string of the requested length.
 func RandomString(byteLength int) string {
 	b := make([]byte, byteLength)
-	_, err := io.ReadFull(rand.Reader, b)
+	_, err := io.ReadFull(RandReader, b)
 	if err != nil {
 		panic(fmt.Sprintf("Error reading random bytes: %s", err))
 	}
