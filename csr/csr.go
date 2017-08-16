eb73808dd547266d60e6196bfb1c4c6c4437e8bf
Roland Shoemaker <rolandshoemaker@gmail.com>
2017-08-16 14:04:05 -0700

    Update TODO

--- csr/csr.go
+++ csr/csr.go
@@ -22,7 +22,7 @@ const maxCNLength = 64
 // SHA1WithRSA is allowed because there's still a fair bit of it
 // out there, but we should try to remove it soon.
 var goodSignatureAlgorithms = map[x509.SignatureAlgorithm]bool{
-	x509.SHA1WithRSA:      true, // TODO(XXX): Remove
+	x509.SHA1WithRSA:      true, // TODO(#2988): Remove support
 	x509.SHA256WithRSA:    true,
 	x509.SHA384WithRSA:    true,
 	x509.SHA512WithRSA:    true,
