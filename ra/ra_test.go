c0521c23903443a95813e75f2945e38d47ef6378
Daniel <cpu@letsencrypt.org>
2017-10-04 13:39:28 -0400

    Revert "Add CAA parameter to restrict challenge type (#3003)"
    This reverts commit 23e2c4a836e520675e7a0366bc5ca1cc6bf2d57f.

--- ra/ra_test.go
+++ ra/ra_test.go
@@ -1832,21 +1832,9 @@ func (m *mockSAWithRecentAndOlder) GetValidAuthorizations(
 	names []string,
 	now time.Time) (map[string]*core.Authorization, error) {
 	return map[string]*core.Authorization{
-		"recent.com": &core.Authorization{
-			Identifier: core.AcmeIdentifier{Type: core.IdentifierDNS, Value: "recent.com"},
-			Challenges: []core.Challenge{core.Challenge{Status: core.StatusValid, Type: core.ChallengeTypeHTTP01}},
-			Expires:    &m.recent,
-		},
-		"older.com": &core.Authorization{
-			Identifier: core.AcmeIdentifier{Type: core.IdentifierDNS, Value: "older.com"},
-			Challenges: []core.Challenge{core.Challenge{Status: core.StatusValid, Type: core.ChallengeTypeHTTP01}},
-			Expires:    &m.older,
-		},
-		"older2.com": &core.Authorization{
-			Identifier: core.AcmeIdentifier{Type: core.IdentifierDNS, Value: "older2.com"},
-			Challenges: []core.Challenge{core.Challenge{Status: core.StatusValid, Type: core.ChallengeTypeHTTP01}},
-			Expires:    &m.older,
-		},
+		"recent.com": &core.Authorization{Expires: &m.recent},
+		"older.com":  &core.Authorization{Expires: &m.older},
+		"older2.com": &core.Authorization{Expires: &m.older},
 	}, nil
 }
 
@@ -1903,18 +1891,11 @@ func TestRecheckCAAEmpty(t *testing.T) {
 	}
 }
 
-func HTTP01Authorization(domain string) core.Authorization {
-	return core.Authorization{
-		Identifier: core.AcmeIdentifier{Type: core.IdentifierDNS, Value: domain},
-		Challenges: []core.Challenge{core.Challenge{Status: core.StatusValid, Type: core.ChallengeTypeHTTP01}},
-	}
-}
-
 func TestRecheckCAASuccess(t *testing.T) {
 	_, _, ra, _, cleanUp := initAuthorities(t)
 	defer cleanUp()
-	auths := []core.Authorization{HTTP01Authorization("a.com"), HTTP01Authorization("b.com"), HTTP01Authorization("c.com")}
-	err := ra.recheckCAA(context.Background(), auths)
+	names := []string{"a.com", "b.com", "c.com"}
+	err := ra.recheckCAA(context.Background(), names)
 	if err != nil {
 		t.Errorf("expected nil err, got %s", err)
 	}
@@ -1923,9 +1904,9 @@ func TestRecheckCAASuccess(t *testing.T) {
 func TestRecheckCAAFail(t *testing.T) {
 	_, _, ra, _, cleanUp := initAuthorities(t)
 	defer cleanUp()
-	auths := []core.Authorization{HTTP01Authorization("a.com"), HTTP01Authorization("b.com"), HTTP01Authorization("c.com")}
+	names := []string{"a.com", "b.com", "c.com"}
 	ra.caa = &caaFailer{}
-	err := ra.recheckCAA(context.Background(), auths)
+	err := ra.recheckCAA(context.Background(), names)
 	if err == nil {
 		t.Errorf("expected err, got nil")
 	} else if err.(*berrors.BoulderError).Type != berrors.CAA {
