2485989b1479dc832fa642a60f37eba92e4aaa60
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-10-16 16:25:47 -0700

    Don't drop old index.

--- sa/_db-next/migrations/20171015225800_AddRenewalBit.sql
+++ sa/_db-next/migrations/20171015225800_AddRenewalBit.sql
@@ -2,13 +2,14 @@
 -- +goose Up
 ALTER TABLE issuedNames
        ADD COLUMN renewal TINYINT(1) NOT NULL DEFAULT 0,
-       ADD INDEX `reversedName_renewal_notBefore_Idx` (`reversedName`,`renewal`,`notBefore`),
-       DROP INDEX `reversedName_notBefore_Idx`
+       ADD INDEX `reversedName_renewal_notBefore_Idx` (`reversedName`,`renewal`,`notBefore`);
 ;
+
+-- TODO: DROP INDEX `reversedName_renewal_notBefore_Idx` once we are no longer
+-- relying on it for queries.
 
 -- +goose Down
 ALTER TABLE issuedNames
        DROP COLUMN renewal,
-       ADD INDEX `reversedName_notBefore_Idx` (`reversedName`,`notBefore`),
-       DROP INDEX `reversedName_renewal_notBefore_Idx`
+       DROP INDEX `reversedName_renewal_notBefore_Idx`;
 ;
