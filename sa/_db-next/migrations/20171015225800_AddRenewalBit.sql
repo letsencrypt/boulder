d243c2008031551ce9f941873eacd85b75f7dd00
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-10-18 16:31:07 -0700

    Revert "Remove comment; MariaDB thought it was a stmt."
    This reverts commit 680203d816f9f3084373ced66c0a3d5de8ee8dfc.

--- sa/_db-next/migrations/20171015225800_AddRenewalBit.sql
+++ sa/_db-next/migrations/20171015225800_AddRenewalBit.sql
@@ -5,6 +5,9 @@ ALTER TABLE issuedNames
        ADD INDEX `reversedName_renewal_notBefore_Idx` (`reversedName`,`renewal`,`notBefore`);
 ;
 
+-- TODO: DROP INDEX `reversedName_renewal_notBefore_Idx` once we are no longer
+-- relying on it for queries.
+
 -- +goose Down
 ALTER TABLE issuedNames
        DROP COLUMN renewal,
