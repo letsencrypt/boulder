c42bacd1849ebc11cb43e269cd383716f066e4ec
Daniel <cpu@letsencrypt.org>
2017-10-20 14:16:01 -0400

    Add index to reversedName, reduce reversedName size.

--- sa/_db-next/migrations/20171006131843_FinalizeOrder.sql
+++ sa/_db-next/migrations/20171006131843_FinalizeOrder.sql
@@ -7,9 +7,13 @@ ALTER TABLE orders DROP COLUMN csr;
 CREATE TABLE requestedNames (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `orderID` BIGINT(20) NOT NULL,
-  `reversedName` varchar(640) CHARACTER SET ascii NOT NULL,
+  -- 253 is the maximum allowed DNS name length
+  -- We use ASCII explicitly here since there is no expectation that un-punycode
+  -- encoded unicode names will be stored
+  `reversedName` varchar(253) CHARACTER SET ascii NOT NULL,
   PRIMARY KEY(id),
   KEY `orderID_idx` (`orderID`),
+  KEY `reversedName_idx` (`reversedName`),
   CONSTRAINT `orderID_orders` FOREIGN KEY (`orderID`) REFERENCES `orders` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
