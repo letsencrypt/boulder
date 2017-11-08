ed08be5abeda46f1391f325d8c59f51ea41c7413
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-11-08 11:23:29 -0800

    Fix casing of index names.

--- sa/_db-next/migrations/20171107160000_AddCertificateStatusIndexes.sql
+++ sa/_db-next/migrations/20171107160000_AddCertificateStatusIndexes.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 ALTER TABLE certificateStatus
-       ADD INDEX `isExpired_ocspLastUpdated_Idx` (`isExpired`, `ocspLastUpdated`),
-       ADD INDEX `notAfter_Idx` (`notAfter`),
+       ADD INDEX `isExpired_ocspLastUpdated_idx` (`isExpired`, `ocspLastUpdated`),
+       ADD INDEX `notAfter_idx` (`notAfter`),
        DROP INDEX `status_certificateStatus_idx`,
        DROP INDEX `ocspLastUpdated_certificateStatus_idx`;
 
 -- +goose Down
 ALTER TABLE certificateStatus
-       DROP INDEX `isExpired_ocspLastUpdated_Idx`,
-       DROP INDEX `notAfter_Idx`,
+       DROP INDEX `isExpired_ocspLastUpdated_idx`,
+       DROP INDEX `notAfter_idx`,
        ADD INDEX `ocspLastUpdated_certificateStatus_idx` (`ocspLastUpdated`),
        ADD INDEX `status_certificateStatus_idx` (`status`);
