15d8e53b135e5bbe1cde1bbc213f5d4f9a8e26af
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-11-07 16:10:41 -0800

    Add indexes on certificateStatus.
    In https://github.com/letsencrypt/boulder/issues/1864 we discussed possible
    optimizations to how expiration-mailer and ocsp-updater query the
    certificateStatus table. In https://github.com/letsencrypt/boulder/pull/2177 we
    added the `notAfter` and `isExpired` fields for more efficient querying.
    However, we forgot to add indexes on these fields. This change adds new indexes
    and drops the old indexes, and should result in much more efficient querying in
    those two components.

---
+++ sa/_db-next/migrations/20171107160000_AddCertificateStatusIndexes.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+ALTER TABLE certificateStatus
+       ADD INDEX `isExpired_ocspLastUpdated_Idx` (`isExpired`, `ocspLastUpdated),
+       ADD INDEX `notAfter_Idx` (`notAfter`),
+       DROP INDEX `status_certificateStatus_idx`,
+       DROP INDEX `ocspLastUpdated_certificateStatus_idx`;
+
+-- +goose Down
+ALTER TABLE certificateStatus
+       DROP INDEX `isExpired_ocspLastUpdated_Idx`,
+       DROP INDEX `notAfter_Idx`,
+       ADD INDEX `ocspLastUpdated_certificateStatus_idx` (`ocspLastUpdated`),
+       ADD INDEX `status_certificateStatus_idx` (`status`);
