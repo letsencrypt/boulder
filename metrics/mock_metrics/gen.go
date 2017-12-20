bd2680ddfe54ab65b0264e41169a15d577cc405e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-12-20 10:29:39 -0800

    Add an arg to -i

--- metrics/mock_metrics/gen.go
+++ metrics/mock_metrics/gen.go
@@ -1,4 +1,4 @@
 package mock_metrics
 
 //go:generate mockgen -package mock_metrics -destination ./mock_scope.go github.com/letsencrypt/boulder/metrics Scope
-//go:generate sed -i mock_scope.go -e s,github.com/letsencrypt/boulder/vendor/,,
+//go:generate sed -i bak mock_scope.go -e s,github.com/letsencrypt/boulder/vendor/,,
