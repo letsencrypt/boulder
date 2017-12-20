9a59412ec34519a07ff82871a8b34d712a7a6f4a
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-12-19 16:59:51 -0800

    Try another sed invocation

--- metrics/mock_metrics/gen.go
+++ metrics/mock_metrics/gen.go
@@ -1,4 +1,4 @@
 package mock_metrics
 
 //go:generate mockgen -package mock_metrics -destination ./mock_scope.go github.com/letsencrypt/boulder/metrics Scope
-//go:generate sed -i s,github.com/letsencrypt/boulder/vendor/,, mock_scope.go
+//go:generate sed -i mock_scope.go -e s,github.com/letsencrypt/boulder/vendor/,,
