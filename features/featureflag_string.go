af0bd70196229f5df3e2dc18ceba5ac17fec8d32
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-12-21 10:36:04 -0800

    Update boulder-tools version. (#3294)
    Also update chisel.py to refer to josepy, and update featureflag_string.go based on latest stringer.
    
    Fixes #3289

--- features/featureflag_string.go
+++ features/featureflag_string.go
@@ -2,7 +2,7 @@
 
 package features
 
-import "fmt"
+import "strconv"
 
 const _FeatureFlag_name = "unusedAllowAccountDeactivationAllowKeyRolloverResubmitMissingSCTsOnlyUseAIAIssuerURLAllowTLS02ChallengesGenerateOCSPEarlyReusePendingAuthzCountCertificatesExactRandomDirectoryEntryIPv6FirstDirectoryMetaAllowRenewalFirstRLRecheckCAAUDPDNSROCACheckWildcardDomains"
 
@@ -10,7 +10,7 @@ var _FeatureFlag_index = [...]uint16{0, 6, 30, 46, 69, 84, 104, 121, 138, 160, 1
 
 func (i FeatureFlag) String() string {
 	if i < 0 || i >= FeatureFlag(len(_FeatureFlag_index)-1) {
-		return fmt.Sprintf("FeatureFlag(%d)", i)
+		return "FeatureFlag(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
 	return _FeatureFlag_name[_FeatureFlag_index[i]:_FeatureFlag_index[i+1]]
 }
