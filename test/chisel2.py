33410f42fd972ebca63641451da5cd6ee5ef5861
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-01-02 17:59:44 -0800

    Add cleanup phase.

--- test/chisel2.py
+++ test/chisel2.py
@@ -149,7 +149,10 @@ def do_dns_challenges(client, authzs):
             })).read()
         client.answer_challenge(c, c.response(client.key))
     def cleanup():
-        pass
+        urllib2.urlopen("http://localhost:8055/clear-txt",
+            data=json.dumps({
+                "host": name + ".",
+            })).read()
     return cleanup
 
 def do_http_challenges(client, authzs):
