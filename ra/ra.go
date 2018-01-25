a93bb9680ef0d090e5520033250833a7d8b80a04
Roland Bracewell Shoemaker <rolandshoemaker@gmail.com>
2018-01-25 12:03:18 -0800

    Move csr.VerifyCSR call out of ra.issueCertificate into ra.NewCertificate

--- ra/ra.go
+++ ra/ra.go
@@ -876,6 +876,10 @@ func (ra *RegistrationAuthorityImpl) FinalizeOrder(ctx context.Context, req *rap
 
 // NewCertificate requests the issuance of a certificate.
 func (ra *RegistrationAuthorityImpl) NewCertificate(ctx context.Context, req core.CertificateRequest, regID int64) (core.Certificate, error) {
+	// Verify the CSR
+	if err := csrlib.VerifyCSR(req.CSR, ra.maxNames, &ra.keyPolicy, ra.PA, ra.forceCNFromSAN, regID); err != nil {
+		return core.Certificate{}, berrors.MalformedError(err.Error())
+	}
 	// NewCertificate provides an order ID of 0, indicating this is a classic ACME
 	// v1 issuance request from the new certificate endpoint that is not
 	// associated with an ACME v2 order.
@@ -930,12 +934,7 @@ func (ra *RegistrationAuthorityImpl) issueCertificate(
 		return emptyCert, err
 	}
 
-	// Verify the CSR
 	csr := req.CSR
-	if err := csrlib.VerifyCSR(csr, ra.maxNames, &ra.keyPolicy, ra.PA, ra.forceCNFromSAN, int64(acctID)); err != nil {
-		return emptyCert, berrors.MalformedError(err.Error())
-	}
-
 	logEvent.CommonName = csr.Subject.CommonName
 	logEvent.Names = csr.DNSNames
 
