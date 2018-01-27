ccca70c8e60ae083a6118df4630400420ddbee43
Daniel <cpu@letsencrypt.org>
2018-01-27 12:25:24 -0500

    Drop new column in migration down stage

--- sa/_db-next/migrations/20180124141109_DropOrderStatusField.sql
+++ sa/_db-next/migrations/20180124141109_DropOrderStatusField.sql
@@ -9,3 +9,4 @@ ALTER TABLE `orders` ADD COLUMN `beganProcessing` BOOL NOT NULL DEFAULT FALSE;
 -- SQL section 'Down' is executed when this migration is rolled back
 
 ALTER TABLE `orders` ADD COLUMN `status` varchar(255) NOT NULL;
+ALTER TABLE `orders` DROP COLUMN `beganProcessing`;
