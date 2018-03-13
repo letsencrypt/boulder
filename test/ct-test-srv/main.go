c22601dac7c934382d9aba0fffe75f3857381476
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-03-13 10:53:18 -0700

    Revert ct-test-srv.

--- test/ct-test-srv/main.go
+++ test/ct-test-srv/main.go
@@ -19,12 +19,6 @@ import (
 
 	"github.com/letsencrypt/boulder/cmd"
 	"github.com/letsencrypt/boulder/publisher"
-
-	lintasn1 "github.com/globalsign/certlint/asn1"
-	"github.com/globalsign/certlint/certdata"
-	"github.com/globalsign/certlint/checks"
-	_ "github.com/globalsign/certlint/checks/certificate/all"
-	_ "github.com/globalsign/certlint/checks/extensions/all"
 )
 
 type ctSubmissionRequest struct {
@@ -37,37 +31,6 @@ type integrationSrv struct {
 	key             *ecdsa.PrivateKey
 	latencySchedule []float64
 	latencyItem     int
-}
-
-// linting isn't normally the role of a CT log, but since we are submitting to
-// the CT test server anyhow, it happens to be a handy place to add linting.
-// If lint fails, Boulder will not be able to get a quorum of SCTs in the test
-// environment, and will fail, triggering an integration test failure.
-func lint(der []byte) error {
-	_, err := x509.ParseCertificate(der)
-	if err != nil {
-		return err
-	}
-	al := new(lintasn1.Linter)
-	errs := al.CheckStruct(der)
-	if errs != nil {
-		for _, err := range errs.List() {
-			return err
-		}
-	}
-	d, err := certdata.Load(der)
-	if err != nil {
-		return err
-	}
-	errs = checks.Certificate.Check(d)
-	if errs != nil {
-		for _, err := range errs.List() {
-			if err.Error() != "commonName field is deprecated" {
-				return err
-			}
-		}
-	}
-	return nil
 }
 
 func (is *integrationSrv) handler(w http.ResponseWriter, r *http.Request) {
@@ -99,20 +62,6 @@ func (is *integrationSrv) handler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
 		if len(addChainReq.Chain) == 0 {
-			log.Println("no chain")
-			w.WriteHeader(400)
-			return
-		}
-
-		leaf, err := base64.StdEncoding.DecodeString(addChainReq.Chain[0])
-		if err != nil {
-			log.Println("Error decoding leaf: %s", err)
-			w.WriteHeader(400)
-			return
-		}
-
-		if err = lint(leaf); err != nil {
-			log.Println("lint:", err)
 			w.WriteHeader(400)
 			return
 		}
