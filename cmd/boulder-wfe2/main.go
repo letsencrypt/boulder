cacf1f6950e8178435e9344cbcac48dfd3f527dc
Daniel <cpu@letsencrypt.org>
2018-03-22 13:52:19 -0400

    Remove deprecated SubscriberAgreementURL config field.
    The outer `config.SubscriberAgreementURL` field has been deprecated for
    a while in favour of `config.wfe.SubscriberAgreementURL`. After
    verifying the prod/staging configurations do not use the legacy field
    this commit removes it.

--- cmd/boulder-wfe2/main.go
+++ cmd/boulder-wfe2/main.go
@@ -54,8 +54,6 @@ type config struct {
 		Features map[string]bool
 	}
 
-	SubscriberAgreementURL string
-
 	Syslog cmd.SyslogConfig
 
 	Common struct {
@@ -209,13 +207,7 @@ func main() {
 	wfe.RA = rac
 	wfe.SA = sac
 
-	// TODO: remove this check once the production config uses the SubscriberAgreementURL in the wfe section
-	if c.WFE.SubscriberAgreementURL != "" {
-		wfe.SubscriberAgreementURL = c.WFE.SubscriberAgreementURL
-	} else {
-		wfe.SubscriberAgreementURL = c.SubscriberAgreementURL
-	}
-
+	wfe.SubscriberAgreementURL = c.WFE.SubscriberAgreementURL
 	wfe.AllowOrigins = c.WFE.AllowOrigins
 	wfe.AcceptRevocationReason = c.WFE.AcceptRevocationReason
 	wfe.AllowAuthzDeactivation = c.WFE.AllowAuthzDeactivation
