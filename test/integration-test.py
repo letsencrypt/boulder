bffb8f256238b5a4dbc3508a3a3822d8c645d411
Daniel <cpu@letsencrypt.org>
2018-04-25 16:05:08 -0400

    Update exception text

--- test/integration-test.py
+++ test/integration-test.py
@@ -533,7 +533,7 @@ def main():
     args = parser.parse_args()
 
     if not (args.run_all or args.run_certbot or args.run_chisel or args.run_loadtest or args.custom is not None):
-        raise Exception("must run at least one of the letsencrypt or chisel tests with --all, --certbot, --chisel, or --custom")
+        raise Exception("must run at least one of the letsencrypt or chisel tests with --all, --certbot, --chisel, --load or --custom")
 
     now = datetime.datetime.utcnow()
     seventy_days_ago = now+datetime.timedelta(days=-70)
