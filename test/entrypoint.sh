9a889954cb57ab5206e4b558623cdaf66a0b12e0
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-04-25 17:35:42 -0700

    Limited wait for mysql.

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -14,9 +14,12 @@ wait_tcp_port() {
     local host="$1" port="$2"
 
     # see http://tldp.org/LDP/abs/html/devref1.html for description of this syntax.
-    while ! exec 6<>/dev/tcp/$host/$port; do
-	echo "$(date) - still trying to connect to $host:$port"
-	sleep 1
+    for n in `seq 1 30` ; do if exec 6<>/dev/tcp/$host/$port; then
+        break
+      else
+        echo "$(date) - still trying to connect to $host:$port"
+        sleep 1
+      fi
     done
     exec 6>&-
     echo "Connected to $host:$port"
