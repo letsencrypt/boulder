52727b1a6ffebfb532f59ea5689330e2541dba9c
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-05-07 16:54:50 -0700

    Not contains.

--- test/integration-test.py
+++ test/integration-test.py
@@ -603,7 +603,7 @@ def check_balance():
     ]
     for address in addresses:
         metrics = requests.get("http://%s/metrics" % address)
-        if not metrics.text.contains("grpc_server_handled_total"):
+        if not "grpc_server_handled_total" in metrics.text:
             raise Exception("no gRPC traffic processed by %s; load balancing problem?"
                 % address)
 
