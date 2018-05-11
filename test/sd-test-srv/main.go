33902bf830e3faebe21f9f6e20e5d8209f8285b8
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-05-11 15:42:53 -0700

    Add sd-test-srv.

---
+++ test/sd-test-srv/main.go
@@ -0,0 +1,80 @@
+// sd-test-srv runs a simple service discovery system; it returns two hardcoded
+// IP addresses for every A query.
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+func dnsHandler(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	if len(r.Question) < 1 {
+		m.Rcode = dns.RcodeServerFailure
+		w.WriteMsg(m)
+		return
+	}
+	if !strings.HasSuffix(r.Question[0].Name, ".boulder.") {
+		m.Rcode = dns.RcodeServerFailure
+		w.WriteMsg(m)
+		return
+	}
+
+	hdr := dns.RR_Header{
+		Name:   r.Question[0].Name,
+		Rrtype: dns.TypeA,
+		Class:  dns.ClassINET,
+		Ttl:    0,
+	}
+
+	m.Answer = append(m.Answer, &dns.A{
+		A:   net.ParseIP("10.77.77.77"),
+		Hdr: hdr,
+	}, &dns.A{
+		A:   net.ParseIP("10.88.88.88"),
+		Hdr: hdr,
+	})
+
+	w.WriteMsg(m)
+	return
+}
+
+func main() {
+	listen := flag.String("listen", "", "Address and port to listen on.")
+	flag.Parse()
+	if *listen == "" {
+		flag.Usage()
+		return
+	}
+	dns.HandleFunc(".", dnsHandler)
+	go func() {
+		srv := dns.Server{
+			Addr:         *listen,
+			Net:          "tcp",
+			ReadTimeout:  time.Second,
+			WriteTimeout: time.Second,
+		}
+		err := srv.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	srv := dns.Server{
+		Addr:         *listen,
+		Net:          "udp",
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
