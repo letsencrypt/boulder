a9554f5d9064b0425650c4bd0a9f3270491cde8c
Roland Bracewell Shoemaker <rolandshoemaker@gmail.com>
2018-06-14 14:41:33 -0700

    Increase timeout

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -14,14 +14,15 @@ wait_tcp_port() {
     local host="$1" port="$2"
 
     # see http://tldp.org/LDP/abs/html/devref1.html for description of this syntax.
-    for n in `seq 1 30` ; do
+    local max_tries="120"
+    for n in `seq 1 $max_tries` ; do
       if exec 6<>/dev/tcp/$host/$port; then
         break
       else
         echo "$(date) - still trying to connect to $host:$port"
         sleep 1
       fi
-      if [ "$n" -eq "30" ]; then
+      if [ "$n" -eq "$max_tries" ]; then
         echo "unable to connect"
         exit 1
       fi
