36a83150adf342764eb5b31271337a51f2fd9e15
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-07-06 13:25:51 -0700

    Add stagger to CT log submissions. (#3794)
    This allows each log a chance to respond before we move onto the next,
    spreading our load more evenly across the logs in a log group.

--- ctpolicy/ctpolicy_test.go
+++ ctpolicy/ctpolicy_test.go
@@ -167,3 +167,32 @@ func TestGetSCTsMetrics(t *testing.T) {
 	test.AssertEquals(t, test.CountCounter(ctp.winnerCounter.With(prometheus.Labels{"log": "ghi", "group": "a"})), 1)
 	test.AssertEquals(t, test.CountCounter(ctp.winnerCounter.With(prometheus.Labels{"log": "ghi", "group": "b"})), 1)
 }
+
+// A mock publisher that counts submissions
+type countEm struct {
+	count int
+}
+
+func (ce *countEm) SubmitToSingleCTWithResult(_ context.Context, _ *pubpb.Request) (*pubpb.Result, error) {
+	ce.count++
+	return &pubpb.Result{Sct: []byte{0}}, nil
+}
+
+func TestStagger(t *testing.T) {
+	countingPub := &countEm{}
+	ctp := New(countingPub, []cmd.CTGroup{
+		{
+			Name:    "a",
+			Stagger: cmd.ConfigDuration{Duration: 500 * time.Millisecond},
+			Logs: []cmd.LogDescription{
+				{URI: "abc", Key: "def"},
+				{URI: "ghi", Key: "jkl"},
+			},
+		},
+	}, nil, blog.NewMock(), metrics.NewNoopScope())
+	_, err := ctp.GetSCTs(context.Background(), []byte{0})
+	test.AssertNotError(t, err, "GetSCTs failed")
+	if countingPub.count != 1 {
+		t.Errorf("wrong number of requests to publisher. got %d, expected 1", countingPub.count)
+	}
+}
