134dc68d14c1786b27af87449eac03dd430d30b8
Daniel McCarney <daniel@binaryparadox.net>
2018-09-11 21:43:26 -0400

    Godeps: Update vendored zlint to 8093f21. (#3852)
    This updates our zlint dependency to https://github.com/zmap/zlint/commit/8093f211c43679b1ade744d238a02ba1f0c07371 - the tip of master at the time of writing. Notably this brings in a gTLD map that has effective periods so cert-checker can catch issuance for removed gTLDs after their removal date. 
    
    Unit tests are confirmed to pass:
    ```
    $> git rev-parse HEAD
    8093f211c43679b1ade744d238a02ba1f0c07371
    
    $> go test ./...
    ok      github.com/zmap/zlint   0.284s
    ?       github.com/zmap/zlint/cmd/zlint [no test files]
    ?       github.com/zmap/zlint/cmd/zlint-gtld-update     [no test files]
    ok      github.com/zmap/zlint/lints     0.165s
    ok      github.com/zmap/zlint/util      0.005s
    ```

--- vendor/github.com/zmap/zlint/util/gtld.go
+++ vendor/github.com/zmap/zlint/util/gtld.go
@@ -12,24 +12,86 @@
  * permissions and limitations under the License.
  */
 
-/* dataupdate.go
- * File used to parse newgtlds.csv and generate a map
- */
-
 package util
 
 import (
+	"fmt"
 	"strings"
+	"time"
 )
 
-func HasValidTLD(domain string) bool {
-	labels := strings.Split(domain, ".")
-	rightLabel := labels[len(labels)-1]
-	return IsInTLDMap(rightLabel)
+// This package uses the `zlint-gtld-update` command to generate a `tldMap` map.
+//go:generate zlint-gtld-update ./gtld_map.go
+
+const (
+	GTLDPeriodDateFormat = "2006-01-02"
+)
+
+// GTLDPeriod is a struct representing a gTLD's validity period. The field names
+// are chosen to match the data returned by the ICANN gTLD v2 JSON registry[0].
+// See the `zlint-gtld-update` command for more information.
+// [0] - https://www.icann.org/resources/registries/gtlds/v2/gtlds.json
+type GTLDPeriod struct {
+	// GTLD is the GTLD the period corresponds to. It is used only for friendly
+	// error messages from `Valid`
+	GTLD string
+	// DelegationDate is the date at which ICANN delegated the gTLD into existence
+	// from the root DNS, or is empty if the gTLD was never delegated.
+	DelegationDate string
+	// RemovalDate is the date at which ICANN removed the gTLD delegation from the
+	// root DNS, or is empty if the gTLD is still delegated and has not been
+	// removed.
+	RemovalDate string
 }
 
+// Valid determines if the provided `when` time is within the GTLDPeriod for the
+// gTLD. E.g. whether a certificate issued at `when` with a subject identifier
+// using the specified gTLD can be considered a valid use of the gTLD.
+func (p GTLDPeriod) Valid(when time.Time) error {
+	// NOTE: We can throw away the errors from time.Parse in this function because
+	// the zlint-gtld-update command only writes entries to the generated gTLD map
+	// after the dates have been verified as parseable
+	notBefore, _ := time.Parse(GTLDPeriodDateFormat, p.DelegationDate)
+	if when.Before(notBefore) {
+		return fmt.Errorf(`gTLD ".%s" is not valid until %s`,
+			p.GTLD, p.DelegationDate)
+	}
+	// The removal date may be empty. We only need to check `when` against the
+	// removal when it isn't empty
+	if p.RemovalDate != "" {
+		notAfter, _ := time.Parse(GTLDPeriodDateFormat, p.RemovalDate)
+		if when.After(notAfter) {
+			return fmt.Errorf(`gTLD ".%s" is not valid after %s`,
+				p.GTLD, p.RemovalDate)
+		}
+	}
+	return nil
+}
+
+// HasValidTLD checks that a domain ends in a valid TLD that was delegated in
+// the root DNS at the time specified.
+func HasValidTLD(domain string, when time.Time) bool {
+	labels := strings.Split(domain, ".")
+	rightLabel := labels[len(labels)-1]
+	// if the rightmost label is not present in the tldMap, it isn't valid and
+	// never was.
+	if tldPeriod, present := tldMap[rightLabel]; !present {
+		return false
+	} else if tldPeriod.Valid(when) != nil {
+		// If the TLD exists but the date is outside of the gTLD's validity period
+		// then it is not a valid TLD.
+		return false
+	}
+	// Otherwise the TLD exists, and was a valid TLD delegated in the root DNS
+	// at the time of the given date.
+	return true
+}
+
+// IsInTLDMap checks that a label is present in the TLD map. It does not
+// consider the TLD's validity period and whether the TLD may have been removed,
+// only whether it was ever a TLD that was delegated.
 func IsInTLDMap(label string) bool {
-	label = strings.ToUpper(label)
+	label = strings.ToLower(label)
 	if _, ok := tldMap[label]; ok {
 		return true
 	} else {
