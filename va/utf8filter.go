ac28f61760231d885b21523e57d449e7069c38a9
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-09-17 11:20:53 -0700

    Use strings.Builder.

--- va/utf8filter.go
+++ va/utf8filter.go
@@ -1,13 +1,16 @@
 package va
 
+import "strings"
+
 // replaceInvalidUTF8 replaces all invalid UTF-8 encodings with
 // Unicode REPLACEMENT CHARACTER.
 func replaceInvalidUTF8(input []byte) string {
-	var ret string
+	var b strings.Builder
+
 	// Ranging over a string in Go produces runes. When the range keyword
 	// encounters an invalid UTF-8 encoding, it returns REPLACEMENT CHARACTER.
 	for _, v := range string(input) {
-		ret = ret + string(v)
+		b.WriteRune(v)
 	}
-	return ret
+	return b.String()
 }
