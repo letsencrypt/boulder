25254b1708006360f96a90825bd4df08dcc6e2ca
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2018-09-21 17:39:34 -0700

    Add timeout values to VA RoundTripper.

--- va/va.go
+++ va/va.go
@@ -393,6 +393,11 @@ func (va *ValidationAuthorityImpl) fetchHTTP(ctx context.Context, identifier cor
 		// Intercept DialContext in order to connect to the IP address we
 		// select.
 		DialContext: dialer.DialContext,
+		// These match the values from DefaultRoundTripper in net/http.
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
 	}
 
 	// Some of our users use mod_security. Mod_security sees a lack of Accept
