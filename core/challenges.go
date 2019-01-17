b659e7f4e5e17e837630e8318b44007fee17dbc3
Roland Shoemaker <rolandshoemaker@gmail.com>
2019-01-17 14:43:21 -0800

    Add token test

--- core/challenges.go
+++ core/challenges.go
@@ -11,22 +11,26 @@ func newChallenge(challengeType string, token string) Challenge {
 	}
 }
 
-// HTTPChallenge01 constructs a random http-01 challenge
+// HTTPChallenge01 constructs a random http-01 challenge. If token is empty a random token
+// will be generated, otherwise the provided token is used.
 func HTTPChallenge01(token string) Challenge {
 	return newChallenge(ChallengeTypeHTTP01, token)
 }
 
-// TLSSNIChallenge01 constructs a random tls-sni-01 challenge
+// TLSSNIChallenge01 constructs a random tls-sni-01 challenge. If token is empty a random token
+// will be generated, otherwise the provided token is used.
 func TLSSNIChallenge01(token string) Challenge {
 	return newChallenge(ChallengeTypeTLSSNI01, token)
 }
 
-// DNSChallenge01 constructs a random dns-01 challenge
+// DNSChallenge01 constructs a random dns-01 challenge. If token is empty a random token
+// will be generated, otherwise the provided token is used.
 func DNSChallenge01(token string) Challenge {
 	return newChallenge(ChallengeTypeDNS01, token)
 }
 
-// TLSALPNChallenge01 constructs a random tls-alpn-01 challenge
+// TLSALPNChallenge01 constructs a random tls-alpn-01 challenge. If token is empty a random token
+// will be generated, otherwise the provided token is used.
 func TLSALPNChallenge01(token string) Challenge {
 	return newChallenge(ChallengeTypeTLSALPN01, token)
 }
