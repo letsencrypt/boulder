747d7a4600ff83bdb13fe9e544a98010d040ca31
Jillian Karner <jkarner@letsencrypt.org>
2019-02-12 23:15:35 +0000

    Compress and send the results file in a separate function

--- cmd/stats-exporter/main.go
+++ cmd/stats-exporter/main.go
@@ -18,6 +18,7 @@ type sqlRows interface {
 	Next() bool
 	Scan(dest ...interface{}) error
 }
+
 type dbQueryable interface {
 	Query(string, ...interface{}) (*sql.Rows, error)
 }
@@ -56,6 +57,22 @@ func writeTSVData(rows sqlRows, outFile io.Writer) error {
 	}
 	return nil
 }
+
+// is this the correct way to use string and *string
+func compressAndSend(outputFileName, destination string) error {
+	outputGZIPName := outputFileName + ".gz"
+
+	err := exec.Command("/usr/bin/gzip", outputFileName).Run()
+	if err != nil {
+		return fmt.Errorf("Could not gzip result file: %s", err)
+	}
+	err = exec.Command("/usr/bin/scp", outputGZIPName, destination).Run()
+	if err != nil {
+		return fmt.Errorf("Could not scp result file: %s", err)
+	}
+	return nil
+}
+
 func main() {
 	dbConnect := flag.String("dbConnect", "", "Path to the DB URL")
 	destination := flag.String("destination", "localhost:/tmp", "Location to SCP the TSV result file to")
@@ -66,10 +83,10 @@ func main() {
 	yesterdayDateStamp := yesterday.Format("2006-01-02")
 	endDateStamp := now.Format("2006-01-02")
 	outputFileName := fmt.Sprintf("results-%s.tsv", yesterday.Format("2006-01-02"))
-	outputGZIPName := outputFileName + ".gz"
 
 	outFile, err := os.OpenFile(outputFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	cmd.FailOnError(err, "Could not write to results file")
+
 	defer outFile.Close()
 	rows, err := queryDB(*dbConnect, yesterdayDateStamp, endDateStamp)
 	cmd.FailOnError(err, "Could not complete database work")
@@ -77,9 +94,6 @@ func main() {
 	err = writeTSVData(rows, outFile)
 	cmd.FailOnError(err, "Could not write TSV data")
 
-	err = exec.Command("/usr/bin/gzip", outputFileName).Run()
-	cmd.FailOnError(err, "Could not gzip file")
-
-	err = exec.Command("/usr/bin/scp", outputGZIPName, *destination).Run()
-	cmd.FailOnError(err, "Could not SCP file")
+	err = compressAndSend(outputFileName, *destination)
+	cmd.FailOnError(err, "Could not compress and send results")
 }
