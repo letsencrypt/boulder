6f44b8bde7f18ab05f2d3101c3beb1d60f711074
Roland Bracewell Shoemaker <roland@bracewel.net>
2019-02-22 11:05:15 -0800

    Add schema for authz2 table (#4081)
    Adds the schema described by the model added in #3967.
    
    Fixes #4051.

---
+++ sa/_db-next/migrations/20190221140139_AddAuthz2.sql
@@ -0,0 +1,26 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+CREATE TABLE `authz2` (
+    `id` BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    `identifierType` SMALLINT NOT NULL,
+    `identifierValue` VARCHAR(255) NOT NULL,
+    `registrationID` BIGINT(20) NOT NULL,
+    `status` SMALLINT NOT NULL,
+    `expires` DATETIME NOT NULL,
+    `challenges` BIT(8) NOT NULL,
+    `attempted` SMALLINT DEFAULT NULL,
+    `attemptedAt` DATETIME DEFAULT NULL,
+    `token` BINARY(32) UNIQUE NOT NULL,
+    `validationError` MEDIUMBLOB NOT NULL,
+    `validationRecord` MEDIUMBLOB NOT NULL,
+    KEY `regID_expires_idx` (`registrationID`, `status`, `expires`),
+    KEY `regID_identifier_status_expires_idx` (`registrationID`, `identifierType`, `identifierValue`, `status`, `expires`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+
+DROP TABLE `authz2`;
