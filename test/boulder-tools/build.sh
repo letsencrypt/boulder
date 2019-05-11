e4ad2d07343d032e53865dedd6f5dd6b7d6529a5
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2019-05-10 17:14:03 -0700

    Update boulder-tools.

--- test/boulder-tools/build.sh
+++ test/boulder-tools/build.sh
@@ -42,7 +42,6 @@ go get \
   github.com/kisielk/errcheck \
   github.com/mattn/goveralls \
   github.com/modocache/gover \
-  github.com/tools/godep \
   golang.org/x/tools/cover \
   golang.org/x/tools/cmd/stringer \
   github.com/gordonklaus/ineffassign
