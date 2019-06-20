0a16b5f57d7c1a3cdf4d1453ac544a42eab7a251
Roland Bracewell Shoemaker <rolandshoemaker@gmail.com>
2019-06-20 13:31:44 -0700

    Reduce akamai purger interval in integration tests (#4277)
    and reduce the verify_akamai_purge deadline/sleep to match the much smaller interval.

--- test/helpers.py
+++ test/helpers.py
@@ -129,9 +129,9 @@ def reset_akamai_purges():
     requests.post("http://localhost:6789/debug/reset-purges")
 
 def verify_akamai_purge():
-    deadline = time.time() + 10
+    deadline = time.time() + 0.25
     while True:
-        time.sleep(0.25)
+        time.sleep(0.05)
         if time.time() > deadline:
             raise Exception("Timed out waiting for Akamai purge")
         response = requests.get("http://localhost:6789/debug/get-purges")
