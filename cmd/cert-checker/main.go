8d3a246adbb63e4b37cd250a139ebe5cd63ed8f8
Daniel McCarney <cpu@letsencrypt.org>
2019-06-20 13:09:10 -0400

    cert-checker: allow ignoring lints by name. (#4272)
    This updates the `cert-checker` utility configuration with a new allow list of
    ignored lints so we can exclude known false-positives/accepted info results by
    name instead of result level. To start only the `n_subject_common_name_included`
    lint is excluded in `test/config-next/cert-checker.json`. Once this lands we can
    treat info/warning lint results as errors as a follow-up to not break
    deployability guarantees.
    
    Resolves https://github.com/letsencrypt/boulder/issues/4271

--- cmd/cert-checker/main.go
+++ cmd/cert-checker/main.go
@@ -165,9 +165,9 @@ func (c *certChecker) getCerts(unexpiredOnly bool) error {
 	return nil
 }
 
-func (c *certChecker) processCerts(wg *sync.WaitGroup, badResultsOnly bool) {
+func (c *certChecker) processCerts(wg *sync.WaitGroup, badResultsOnly bool, ignoredLints map[string]bool) {
 	for cert := range c.certs {
-		problems := c.checkCert(cert)
+		problems := c.checkCert(cert, ignoredLints)
 		valid := len(problems) == 0
 		c.rMu.Lock()
 		if !badResultsOnly || (badResultsOnly && !valid) {
@@ -206,7 +206,7 @@ var expectedExtensionContent = map[string][]byte{
 	"1.3.6.1.5.5.7.1.24": []byte{0x30, 0x03, 0x02, 0x01, 0x05}, // Must staple feature
 }
 
-func (c *certChecker) checkCert(cert core.Certificate) (problems []string) {
+func (c *certChecker) checkCert(cert core.Certificate, ignoredLints map[string]bool) (problems []string) {
 	// Check digests match
 	if cert.Digest != core.Fingerprint256(cert.DER) {
 		problems = append(problems, "Stored digest doesn't match certificate digest")
@@ -220,7 +220,9 @@ func (c *certChecker) checkCert(cert core.Certificate) (problems []string) {
 		// Run zlint checks
 		results := zlint.LintCertificate(parsedCert)
 		for name, res := range results.Results {
-			// ignore notices and warnings
+			if ignoredLints[name] {
+				continue
+			}
 			if res.Status >= lints.Error {
 				prob := fmt.Sprintf("zlint %s: %s", res.Status, name)
 				if res.Details != "" {
@@ -315,6 +317,10 @@ type config struct {
 		BadResultsOnly      bool
 		CheckPeriod         cmd.ConfigDuration
 
+		// IgnoredLints is a list of zlint names. Any lint results from a lint in
+		// the IgnoredLists list are ignored regardles of LintStatus level.
+		IgnoredLints []string
+
 		Features map[string]bool
 	}
 
@@ -386,6 +392,11 @@ func main() {
 	)
 	fmt.Fprintf(os.Stderr, "# Getting certificates issued in the last %s\n", config.CertChecker.CheckPeriod)
 
+	ignoredLintsMap := make(map[string]bool)
+	for _, name := range config.CertChecker.IgnoredLints {
+		ignoredLintsMap[name] = true
+	}
+
 	// Since we grab certificates in batches we don't want this to block, when it
 	// is finished it will close the certificate channel which allows the range
 	// loops in checker.processCerts to break
@@ -400,7 +411,7 @@ func main() {
 		wg.Add(1)
 		go func() {
 			s := checker.clock.Now()
-			checker.processCerts(wg, config.CertChecker.BadResultsOnly)
+			checker.processCerts(wg, config.CertChecker.BadResultsOnly, ignoredLintsMap)
 			scope.TimingDuration("certChecker.processingLatency", time.Since(s))
 		}()
 	}
