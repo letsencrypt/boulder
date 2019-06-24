24f150f8fcc9f4fdfa06e0e4ac3de24a7b8e122c
Roland Bracewell Shoemaker <rolandshoemaker@gmail.com>
2019-06-24 11:58:37 -0700

    Re-apply #4279 with requests fix (#4286)
    Move from using `requests` to `urllib2` in `helpers.py`. Verified
    this works with `docker-compose up`. In the future we really should
    be installing our own python dependencies in the boulder-tools image
    rather than relying on getting them by using the certbot virtualenv.

--- test/helpers.py
+++ test/helpers.py
@@ -5,10 +5,12 @@ import urllib2
 import time
 import re
 import random
-import requests
+import json
+import socket
 import tempfile
 import shutil
 import atexit
+import errno
 import subprocess
 
 import challtestsrv
@@ -63,50 +65,6 @@ def make_ocsp_req(cert_file, issuer_file):
         ocsp_req = f.read()
     return ocsp_req
 
-def fetch_until(cert_file, issuer_file, url, initial, final):
-    """Fetch OCSP for cert_file until OCSP status goes from initial to final.
-
-    Initial and final are treated as regular expressions. Any OCSP response
-    whose OpenSSL OCSP verify output doesn't match either initial or final is
-    a fatal error.
-
-    If OCSP responses by the three methods (POST, GET, URL-encoded GET) differ
-    from each other, that is a fatal error.
-
-    If we loop for more than five seconds, that is a fatal error.
-
-    Returns nothing on success.
-    """
-    ocsp_request = make_ocsp_req(cert_file, issuer_file)
-    timeout = time.time() + 5
-    while True:
-        time.sleep(0.25)
-        if time.time() > timeout:
-            raise Exception("Timed out waiting for OCSP to go from '%s' to '%s'" % (
-                initial, final))
-        responses = fetch_ocsp(ocsp_request, url)
-        # This variable will be true at the end of the loop if all the responses
-        # matched the final state.
-        all_final = True
-        for resp in responses:
-            verify_output = ocsp_verify(cert_file, issuer_file, resp)
-            if re.search(initial, verify_output):
-                all_final = False
-                break
-            elif re.search(final, verify_output):
-                continue
-            else:
-                print verify_output
-                raise Exception("OCSP response didn't match '%s' or '%s'" %(
-                    initial, final))
-        if all_final:
-            # Check that all responses were equal to each other.
-            for resp in responses:
-                if resp != responses[0]:
-                    raise Exception("OCSP responses differed: %s vs %s" %(
-                        base64.b64encode(responses[0]), base64.b64encode(resp)))
-            return
-
 def ocsp_verify(cert_file, issuer_file, ocsp_response):
     ocsp_resp_file = os.path.join(tempdir, "ocsp.resp")
     with open(ocsp_resp_file, "w") as f:
@@ -122,26 +80,7 @@ def ocsp_verify(cert_file, issuer_file, ocsp_response):
         raise Exception("OCSP verify failure")
     return output
 
-def wait_for_ocsp_good(cert_file, issuer_file, url):
-    fetch_until(cert_file, issuer_file, url, " unauthorized", ": good")
-
-def reset_akamai_purges():
-    requests.post("http://localhost:6789/debug/reset-purges")
-
-def verify_akamai_purge():
-    deadline = time.time() + 0.25
-    while True:
-        time.sleep(0.05)
-        if time.time() > deadline:
-            raise Exception("Timed out waiting for Akamai purge")
-        response = requests.get("http://localhost:6789/debug/get-purges")
-        purgeData = response.json()
-        if len(purgeData["V3"]) is not 1:
-            continue
-        break
-    reset_akamai_purges()
-
-def verify_revocation(cert_file, issuer_file, url):
+def verify_ocsp(cert_file, issuer_file, url, status):
     ocsp_request = make_ocsp_req(cert_file, issuer_file)
     responses = fetch_ocsp(ocsp_request, url)
 
@@ -151,12 +90,29 @@ def verify_revocation(cert_file, issuer_file, url):
             raise Exception("OCSP responses differed: %s vs %s" %(
                 base64.b64encode(responses[0]), base64.b64encode(resp)))
 
-    # Check response is for the correct certificate and is revoked
+    # Check response is for the correct certificate and is correct
+    # status
     resp = responses[0]
     verify_output = ocsp_verify(cert_file, issuer_file, resp)
-    if not re.search(": revoked", verify_output):
+    if not re.search("%s: %s" % (cert_file, status), verify_output):
         print verify_output
-        raise Exception("OCSP response wasn't 'revoked'")
+        raise Exception("OCSP response wasn't '%s'" % status)
+
+def reset_akamai_purges():
+    urllib2.urlopen("http://localhost:6789/debug/reset-purges", "{}")
+
+def verify_akamai_purge():
+    deadline = time.time() + 0.25
+    while True:
+        time.sleep(0.05)
+        if time.time() > deadline:
+            raise Exception("Timed out waiting for Akamai purge")
+        response = urllib2.urlopen("http://localhost:6789/debug/get-purges")
+        purgeData = json.load(response)
+        if len(purgeData["V3"]) is not 1:
+            continue
+        break
+    reset_akamai_purges()
 
 twenty_days_ago_functions = [ ]
 
@@ -172,3 +128,21 @@ def setup_twenty_days_ago():
     """
     for f in twenty_days_ago_functions:
         f()
+
+def waitport(port, prog, perTickCheck=None):
+    """Wait until a port on localhost is open."""
+    for _ in range(1000):
+        try:
+            time.sleep(0.1)
+            if perTickCheck is not None and not perTickCheck():
+                return false
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(('localhost', port))
+            s.close()
+            return True
+        except socket.error as e:
+            if e.errno == errno.ECONNREFUSED:
+                print "Waiting for debug port %d (%s)" % (port, prog)
+            else:
+                raise
+    raise Exception("timed out waiting for debug port %d (%s)" % (port, prog))
