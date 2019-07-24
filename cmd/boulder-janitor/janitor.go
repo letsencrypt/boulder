9e896325f7fbd335ea5f9e3288b6c66f9fa60c7f
Daniel McCarney <cpu@letsencrypt.org>
2019-07-24 15:09:04 -0400

    boulder-janitor: add initial daemon for tidying certificate resources. (#4354)
    A new `boulder-janitor` command is added that provides a long-running
    daemon that cleans up rows associated with expired certificate
    resources. At present this is rows from the following tables:
    
    * certificates
    * certificateStatus
    * certificatesPerName
    
    Adding cleanup of tables associated with Order resources is the next step.
    
    Three prometheus stats are exported:
    
    * janitor_deletions - CounterVec for the number of deletions by table the 
      boulder-janitor has performed.
    * janitor_workbatch - GaugeVec for the number of items of work by table
      the boulder-janitor queued for deletion.
    * janitor_errors - CounterVec for the number of errors by table and error
      type the boulder-janitor has experienced.

---
+++ cmd/boulder-janitor/janitor.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/jmhodges/clock"
+	"github.com/letsencrypt/boulder/cmd"
+	"github.com/letsencrypt/boulder/features"
+	blog "github.com/letsencrypt/boulder/log"
+	"github.com/letsencrypt/boulder/sa"
+)
+
+// janitorDB is an interface describing the two functions of a sql.DB that the
+// janitor uses. It allows easy mocking of the DB for unit tests.
+type janitorDB interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Select(i interface{}, query string, args ...interface{}) ([]interface{}, error)
+}
+
+var (
+	// errNoJobsConfigured is returned from New() when there are no jobs enabled
+	// in the provided Config.
+	errNoJobsConfigured = errors.New("no jobs enabled in configuration")
+)
+
+// janitor is a struct for a long-running cleanup daemon tasked with multiple
+// cleanup jobs.
+type janitor struct {
+	log  blog.Logger
+	clk  clock.Clock
+	db   janitorDB
+	jobs []*batchedDBJob
+}
+
+// New creates a janitor instance from the provided configuration or errors. The
+// janitor will not be running until its Run() function is invoked.
+func New(clk clock.Clock, config Config) (*janitor, error) {
+	if err := config.Valid(); err != nil {
+		return nil, err
+	}
+
+	// Enable configured feature flags
+	if err := features.Set(config.Janitor.Features); err != nil {
+		return nil, err
+	}
+
+	// Setup logging and stats
+	scope, logger := cmd.StatsAndLogging(config.Janitor.Syslog, config.Janitor.DebugAddr)
+	scope.MustRegister(deletedStat)
+	defer logger.AuditPanic()
+	logger.Info(cmd.VersionString())
+
+	// Create DB Map
+	dbURL, err := config.Janitor.DBConfig.URL()
+	if err != nil {
+		return nil, err
+	}
+	dbMap, err := sa.NewDbMap(dbURL, config.Janitor.DBConfig.MaxDBConns)
+	if err != nil {
+		return nil, err
+	}
+	sa.SetSQLDebug(dbMap, logger)
+
+	// Construct configured jobs
+	jobs, err := newJobs(dbMap, logger, clk, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &janitor{
+		log:  logger,
+		clk:  clk,
+		db:   dbMap,
+		jobs: jobs,
+	}, nil
+}
+
+// newJobs constructs a list of batchedDBJobs based on the provided config. If
+// no jobs are enabled in the config then errNoJobsConfigured is returned.
+func newJobs(
+	dbMap janitorDB,
+	logger blog.Logger,
+	clk clock.Clock,
+	config Config) ([]*batchedDBJob, error) {
+	var jobs []*batchedDBJob
+	if config.Janitor.CertificateStatus.Enabled {
+		jobs = append(jobs, newCertificateStatusJob(dbMap, logger, clk, config))
+	}
+	if config.Janitor.Certificates.Enabled {
+		jobs = append(jobs, newCertificatesJob(dbMap, logger, clk, config))
+	}
+	if config.Janitor.CertificatesPerName.Enabled {
+		jobs = append(jobs, newCertificatesPerNameJob(dbMap, logger, clk, config))
+	}
+	if len(jobs) == 0 {
+		return nil, errNoJobsConfigured
+	}
+	return jobs, nil
+}
+
+// Run starts the janitor daemon. Each configured job will start running in
+// dedicated go routines. The janitor will block on the completion of these
+// jobs (presently forever).
+func (j *janitor) Run() {
+	waitChan := make(chan bool)
+	// Run each job
+	for _, job := range j.jobs {
+		go job.RunForever()
+	}
+	// Wait forever
+	<-waitChan
+}
