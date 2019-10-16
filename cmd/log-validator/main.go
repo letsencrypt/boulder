b383073eb1a76b2736c8efd69c50bbf8b30f926c
Roland Shoemaker <rolandshoemaker@gmail.com>
2019-10-15 17:52:50 -0700

    Add a log validation service

---
+++ cmd/log-validator/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/hpcloud/tail"
+
+	"github.com/letsencrypt/boulder/cmd"
+	blog "github.com/letsencrypt/boulder/log"
+)
+
+func lineValid(text string) bool {
+	// Line format should match the following template:
+	//   timestamp hostname datacenter level binary-name[pid]: checksum msg
+	//
+	// template( name="LELogFormat" type="list" ) {
+	// 	property(name="timereported" dateFormat="rfc3339")
+	// 	constant(value=" ")
+	// 	property(name="hostname" field.delimiter="46" field.number="1")
+	// 	constant(value=" slc ")
+	// 	property(name="syslogseverity")
+	// 	constant(value=" ")
+	// 	property(name="syslogtag")
+	// 	property(name="msg" spifno1stsp="on" )
+	// 	property(name="msg" droplastlf="on" )
+	// 	constant(value="\n")
+	// }
+
+	fields := strings.Split(text, " ")
+	// Extract checksum from line
+	checksum := fields[5]
+	// Reconstruct just the message portion of the line
+	line := strings.Join(fields[6:], " ")
+	// Check the extracted checksum against the computed checksum
+	return checksum == blog.LogLineChecksum(line)
+}
+
+func main() {
+	configPath := flag.String("config", "", "")
+	flag.Parse()
+
+	var config struct {
+		Syslog cmd.SyslogConfig
+		Files  []string
+	}
+	configBytes, err := ioutil.ReadFile(*configPath)
+	cmd.FailOnError(err, "failed to read config file")
+	err = json.Unmarshal(configBytes, &config)
+	cmd.FailOnError(err, "failed to parse config file")
+
+	logger := cmd.NewLogger(config.Syslog)
+
+	var tailers []*tail.Tail
+	for _, filename := range config.Files {
+		t, err := tail.TailFile(filename, tail.Config{
+			ReOpen:    true,
+			MustExist: true,
+			Follow:    true,
+		})
+		cmd.FailOnError(err, "failed to tail file")
+		defer t.Cleanup()
+
+		go func() {
+			for line := range t.Lines {
+				if line.Err != nil {
+					fmt.Printf("bad bad tail: %s\n", err)
+					continue
+				}
+				if !lineValid(line.Text) {
+					fmt.Printf("bad checksum for line: %s\n", line.Text)
+				}
+			}
+		}()
+
+		tailers = append(tailers, t)
+	}
+
+	cmd.CatchSignals(logger, func() {
+		for _, t := range tailers {
+			err = t.Stop()
+			cmd.FailOnError(err, fmt.Sprintf("failed to stop tailing file: %s", t.Filename))
+		}
+	})
+}
