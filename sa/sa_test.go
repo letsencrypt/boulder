letsencrypt/boulder

commit 46e0468220eb5caa8ed97edc3ed50319079d9b95

Make authz2 the default storage format (#4476)
This change set makes the authz2 storage format the default format. It removes
most of the functionality related to the previous storage format, except for
the SA fallbacks and old gRPC methods which have been left for a follow-up
change in order to make these changes deployable without introducing
incompatibilities.

Fixes #4454.

sa/model.go
@@ -317,7 +317,6 @@ func modelToRegistration(reg *regModel) (core.Registration, error) {
 
 func challengeToModel(c *core.Challenge, authID string) (*challModel, error) {
 	cm := challModel{
-		ID:               c.ID,
 		AuthorizationID:  authID,
 		Type:             c.Type,
 		Status:           c.Status,
@@ -349,7 +348,6 @@ func challengeToModel(c *core.Challenge, authID string) (*challModel, error) {
 
 func modelToChallenge(cm *challModel) (core.Challenge, error) {
 	c := core.Challenge{
-		ID:                       cm.ID,
 		Type:                     cm.Type,
 		Status:                   cm.Status,
 		Token:                    cm.Token,
@@ -546,14 +544,8 @@ func hasMultipleNonPendingChallenges(challenges []*corepb.Challenge) bool {
 }
 
 // authzPBToModel converts a protobuf authorization representation to the
-// authz2Model storage representation. This method should only be used to
-// convert protobuf authorizations generated from core.Authorizations that
-// were constructed using the new authorization storage concepts (i.e.
-// authorizations that have the V2 field set).
+// authz2Model storage representation.
 func authzPBToModel(authz *corepb.Authorization) (*authz2Model, error) {
-	if authz.V2 == nil || !*authz.V2 {
-		return nil, errors.New("authorization is not v2 format")
-	}
 	expires := time.Unix(0, *authz.Expires).UTC()
 	am := &authz2Model{
 		IdentifierValue: *authz.Identifier,
@@ -677,10 +669,8 @@ func populateAttemptedFields(am *authz2Model, challenge *corepb.Challenge) error
 func modelToAuthzPB(am *authz2Model) (*corepb.Authorization, error) {
 	expires := am.Expires.UTC().UnixNano()
 	id := fmt.Sprintf("%d", am.ID)
-	v2 := true
 	status := uintToStatus[am.Status]
 	pb := &corepb.Authorization{
-		V2:             &v2,
 		Id:             &id,
 		Status:         &status,
 		Identifier:     &am.IdentifierValue,

sa/sa.go
@@ -1422,20 +1422,18 @@ func (ssa *SQLStorageAuthority) FinalizeOrder(ctx context.Context, req *corepb.O
 // authzForOrder retrieves the authorization IDs for an order. It returns these
 // IDs in two slices: one for v1 style authorizations, and another for
 // v2 style authorizations.
-func (ssa *SQLStorageAuthority) authzForOrder(ctx context.Context, orderID int64, lookForV2 bool) ([]string, []int64, error) {
+func (ssa *SQLStorageAuthority) authzForOrder(ctx context.Context, orderID int64) ([]string, []int64, error) {
 	var v1IDs []string
 	var v2IDs []int64
-	if lookForV2 {
-		_, err := ssa.dbMap.WithContext(ctx).Select(
-			&v2IDs,
-			"SELECT authzID FROM orderToAuthz2 WHERE orderID = ?",
-			orderID,
-		)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
 	_, err := ssa.dbMap.WithContext(ctx).Select(
+		&v2IDs,
+		"SELECT authzID FROM orderToAuthz2 WHERE orderID = ?",
+		orderID,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	_, err = ssa.dbMap.WithContext(ctx).Select(
 		&v1IDs, "SELECT authzID FROM orderToAuthz WHERE orderID = ?", orderID)
 	if err != nil {
 		return nil, nil, err
@@ -1472,16 +1470,7 @@ func (ssa *SQLStorageAuthority) GetOrder(ctx context.Context, req *sapb.OrderReq
 	if err != nil {
 		return nil, err
 	}
-	var useV2Authzs bool
-	if req.UseV2Authorizations != nil {
-		useV2Authzs = *req.UseV2Authorizations
-	}
-	// we set useV2Authzs if DisableAuthz2Orders is enabled as actually looking for v2
-	// authorizations is the only way to tell if an order contains them, otherwise
-	// we will fail due to the number of authorizations not matching the number of names
-	// in the order.
-	useV2Authzs = useV2Authzs || features.Enabled(features.DisableAuthz2Orders)
-	v1AuthzIDs, v2AuthzIDs, err := ssa.authzForOrder(ctx, *order.Id, useV2Authzs)
+	v1AuthzIDs, v2AuthzIDs, err := ssa.authzForOrder(ctx, *order.Id)
 	if err != nil {
 		return nil, err
 	}
@@ -1826,7 +1815,7 @@ func (ssa *SQLStorageAuthority) GetOrderForNames(
 	}
 
 	// Get the order
-	order, err := ssa.GetOrder(ctx, &sapb.OrderRequest{Id: &result.OrderID, UseV2Authorizations: req.UseV2Authorizations})
+	order, err := ssa.GetOrder(ctx, &sapb.OrderRequest{Id: &result.OrderID})
 	if err != nil {
 		return nil, err
 	}

sa/sa_test.go
@@ -1557,10 +1557,11 @@ func TestOrder(t *testing.T) {
 	expectedOrder := &corepb.Order{
 		// The registration ID, authorizations, expiry, and names should match the
 		// input to NewOrder
-		RegistrationID: inputOrder.RegistrationID,
-		Authorizations: inputOrder.Authorizations,
-		Names:          inputOrder.Names,
-		Expires:        inputOrder.Expires,
+		RegistrationID:   inputOrder.RegistrationID,
+		Authorizations:   inputOrder.Authorizations,
+		V2Authorizations: []int64{},
+		Names:            inputOrder.Names,
+		Expires:          inputOrder.Expires,
 		// The ID should have been set to 1 by the SA
 		Id: &one,
 		// The status should be pending
@@ -2740,14 +2741,12 @@ func TestNewAuthorizations2(t *testing.T) {
 	defer cleanUp()
 
 	reg := satest.CreateWorkingRegistration(t, sa)
-	v2 := true
 	ident := "aaa"
 	pending := string(core.StatusPending)
 	expires := fc.Now().Add(time.Hour).UTC().UnixNano()
 	challType := string(core.ChallengeTypeDNS01)
 	tokenA := "YXNkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 	apbA := &corepb.Authorization{
-		V2:             &v2,
 		Identifier:     &ident,
 		RegistrationID: &reg.ID,
 		Status:         &pending,
@@ -2762,7 +2761,6 @@ func TestNewAuthorizations2(t *testing.T) {
 	}
 	tokenB := "ZmdoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 	apbB := &corepb.Authorization{
-		V2:             &v2,
 		Identifier:     &ident,
 		RegistrationID: &reg.ID,
 		Status:         &pending,

cmd/expired-authz-purger/main.go
@@ -1,383 +1,0 @@
-package main
-
-import (
-	"database/sql"
-	"encoding/json"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"sync"
-	"sync/atomic"
-	"time"
-
-	"github.com/jmhodges/clock"
-	"github.com/letsencrypt/boulder/cmd"
-	"github.com/letsencrypt/boulder/features"
-	blog "github.com/letsencrypt/boulder/log"
-	"github.com/letsencrypt/boulder/metrics"
-	"github.com/letsencrypt/boulder/sa"
-	"github.com/prometheus/client_golang/prometheus"
-)
-
-type eapConfig struct {
-	ExpiredAuthzPurger struct {
-		cmd.DBConfig
-
-		DebugAddr string
-
-		Syslog cmd.SyslogConfig
-
-		GracePeriod cmd.ConfigDuration
-		BatchSize   int
-		MaxAuthzs   int
-		Parallelism uint
-		// MaxDPS controls the maximum number of deletes which will be performed
-		// per second in total from both the pendingAuthorizations and authz tables.
-		// This can be used to reduce the replication lag caused by creating very
-		// large numbers of delete statements.
-		MaxDPS int
-		// PendingCheckpointFile is the path to a file which is used to store the
-		// last pending authorization ID which was deleted. If path is to a file
-		// which does not exist it will be created.
-		PendingCheckpointFile string
-		// FinalCheckpointFile is the path to a file which is used to store the
-		// last authorization ID which was deleted. If path is to a file
-		// which does not exist it will be created.
-		FinalCheckpointFile string
-
-		Features map[string]bool
-	}
-}
-
-type eapDB interface {
-	Exec(query string, args ...interface{}) (sql.Result, error)
-	Select(i interface{}, query string, args ...interface{}) ([]interface{}, error)
-}
-
-var deletedStat = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "eap_authorizations_deleted",
-		Help: "Number of authorizations the EAP has deleted.",
-	},
-	[]string{"table"},
-)
-
-type expiredAuthzPurger struct {
-	log blog.Logger
-	clk clock.Clock
-	db  eapDB
-
-	batchSize int64
-}
-
-// loadCheckpoint reads a string (which is assumed to be an authorization ID)
-// from the file at the provided path and returns it to the caller. If the
-// file does not exist an error is not returned and the returned ID is an
-// empty string.
-func loadCheckpoint(checkpointFile string) (string, error) {
-	content, err := ioutil.ReadFile(checkpointFile)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return "", nil
-		}
-		return "", err
-	}
-	return string(content), nil
-}
-
-// saveCheckpoint atomically writes the provided ID to the provided file. The
-// method os.Rename makes use of the renameat syscall to atomically replace
-// one file with another. It creates a temporary file in a temporary directory
-// before using os.Rename to replace the old file with the new one.
-func saveCheckpoint(checkpointFile, id string) error {
-	tmpDir, err := ioutil.TempDir("", "checkpoint-tmp")
-	if err != nil {
-		return err
-	}
-	defer func() { _ = os.RemoveAll(tmpDir) }()
-	tmp, err := ioutil.TempFile(tmpDir, "checkpoint-atomic")
-	if err != nil {
-		return err
-	}
-	if _, err = tmp.Write([]byte(id)); err != nil {
-		return err
-	}
-	return os.Rename(tmp.Name(), checkpointFile)
-}
-
-// getWork selects a set of authorizations that expired before purgeBefore, bounded by batchSize,
-// that have IDs that are more than initialID from either the pendingAuthorizations or authz tables
-// and adds them to the work channel. It returns the last ID it selected and the number of IDs it
-// added to the work channel or an error.
-func (p *expiredAuthzPurger) getWork(work chan string, query string, initialID string, purgeBefore time.Time, batchSize int64) (string, int, error) {
-	var idBatch []string
-	_, err := p.db.Select(
-		&idBatch,
-		query,
-		map[string]interface{}{
-			"id":      initialID,
-			"expires": purgeBefore,
-			"limit":   batchSize,
-		},
-	)
-	if err != nil && err != sql.ErrNoRows {
-		return "", 0, fmt.Errorf("Getting a batch: %s", err)
-	}
-	if len(idBatch) == 0 {
-		return initialID, 0, nil
-	}
-	var count int
-	var lastID string
-	for _, v := range idBatch {
-		work <- v
-		count++
-		lastID = v
-	}
-	return lastID, count, nil
-}
-
-// deleteAuthorizations reads from the work channel and deletes each authorization
-// from either the pendingAuthorization or authz tables. If maxDPS is more than 0
-// it will throttle the number of DELETE statements it generates to the passed rate.
-func (p *expiredAuthzPurger) deleteAuthorizations(work chan string, maxDPS int, parallelism int, table string, checkpointFile string) {
-	wg := new(sync.WaitGroup)
-	deleted := int64(0)
-	var ticker *time.Ticker
-	if maxDPS > 0 {
-		ticker = time.NewTicker(time.Duration(float64(time.Second) / float64(maxDPS)))
-	}
-	for i := 0; i < parallelism; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for id := range work {
-				if ticker != nil {
-					<-ticker.C
-				}
-				err := deleteAuthorization(p.db, table, id)
-				if err != nil {
-					p.log.AuditErrf("Deleting %s: %s", id, err)
-				}
-				numDeleted := atomic.AddInt64(&deleted, 1)
-				// Only checkpoint every 1000 IDs in order to prevent unnecessary churn
-				// in the checkpoint file
-				if checkpointFile != "" && numDeleted%1000 == 0 {
-					err = saveCheckpoint(checkpointFile, id)
-					if err != nil {
-						p.log.AuditErrf("failed to checkpoint %q table at ID %q: %s", table, id, err)
-					}
-				}
-			}
-		}()
-	}
-
-	wg.Wait()
-	p.log.Infof("Deleted a total of %d expired authorizations from %s", deleted, table)
-}
-
-// purge looks up pending or finalized authzs (depending on the value of
-// `table`) that expire before `purgeBefore`, using `parallelism`
-// goroutines. It will delete a maximum of `max` authzs if daemon is not true.
-// Neither table has an index on `expires` by itself, so we just iterate through
-// the table with LIMIT and OFFSET using the default ordering. Note that this
-// becomes expensive once the earliest set of authzs has been purged, since the
-// database will have to scan through many rows before it finds some that meet
-// the expiration criteria. When we move to better authz storage (#2620), we
-// will get an appropriate index that will make this cheaper.
-//
-// If daemon is true purge will run indefinitely looking for authorizations to
-// purge. If getWork returns the same ID that was passed to it then it will
-// sleep a minute before looking for more authorizations again, starting at the
-// same ID.
-//
-// If maxDPS is set the number of DELETE statements from both the pendingAuthorizations
-// and authz tables will be capped at the passed rate.
-func (p *expiredAuthzPurger) purge(
-	table string,
-	purgeBefore time.Time,
-	parallelism int,
-	max int,
-	daemon bool,
-	checkpointFile string,
-	maxDPS int,
-) error {
-	var query string
-	switch table {
-	case "pendingAuthorizations":
-		query = "SELECT id FROM pendingAuthorizations WHERE id > :id AND expires <= :expires ORDER BY id LIMIT :limit"
-	case "authz":
-		query = "SELECT id FROM authz WHERE id > :id AND expires <= :expires ORDER BY id LIMIT :limit"
-	}
-
-	// id starts as "", which is smaller than all other ids.
-	var id string
-	if checkpointFile != "" {
-		startID, err := loadCheckpoint(checkpointFile)
-		if err != nil {
-			return err
-		}
-		id = startID
-	}
-
-	work := make(chan string)
-	go func() {
-		var count int
-
-		var working func() bool
-		if daemon {
-			working = func() bool { return true }
-		} else {
-			working = func() bool { return count < max }
-		}
-
-		for working() {
-			lastID, added, err := p.getWork(work, query, id, purgeBefore, p.batchSize)
-			if err != nil {
-				p.log.AuditErr(err.Error())
-				time.Sleep(time.Millisecond * 500)
-				continue
-			} else if daemon && lastID == id {
-				time.Sleep(time.Minute)
-			} else if !daemon && added < int(p.batchSize) {
-				break
-			}
-			count += added
-			id = lastID
-		}
-		close(work)
-	}()
-
-	p.deleteAuthorizations(work, maxDPS, parallelism, table, checkpointFile)
-
-	return nil
-}
-
-func deleteAuthorization(db eapDB, table, id string) error {
-	// Delete challenges + authorization. We delete challenges first and fail out
-	// if that doesn't succeed so that we don't ever orphan challenges which would
-	// require a relatively expensive join to then find.
-	// When deleting challenges, we first SELECT by authorizationID then delete
-	// individual challenges by challenge ID. This is an attempt to work around an
-	// issue we saw where sometimess the DELETE would do a full table scan,
-	// effectively hanging forever. Because this is intermittent and doesn't
-	// reproduce locally, it's hard to know whether this will definitively fix the
-	// issue. In theory deleting by the primary key (id) should never do a table
-	// scan, and we can force use of an index on the SELECT (which we can't do on
-	// a DELETE).
-	var challengeIDs []int64
-	_, err := db.Select(
-		&challengeIDs,
-		`SELECT id FROM challenges
-		 FORCE INDEX (authorizationID_challenges_idx)
-		 WHERE authorizationID=:authorizationID;`,
-		map[string]interface{}{
-			"authorizationID": id,
-		})
-	if err != nil {
-		return err
-	}
-	for _, challengeID := range challengeIDs {
-		if _, err := db.Exec("DELETE FROM challenges WHERE id = ?", challengeID); err != nil {
-			return err
-		}
-	}
-	var query string
-	switch table {
-	case "pendingAuthorizations":
-		query = "DELETE FROM pendingAuthorizations WHERE id = ?"
-	case "authz":
-		query = "DELETE FROM authz WHERE id = ?"
-	}
-	_, err = db.Exec(query, id)
-	if err != nil {
-		return err
-	}
-	deletedStat.WithLabelValues(table).Inc()
-	return nil
-}
-
-func main() {
-	daemon := flag.Bool("daemon", false, "Runs the expired-authz-purger in daemon mode")
-	configPath := flag.String("config", "config.json", "Path to Boulder configuration file")
-	flag.Parse()
-
-	configJSON, err := ioutil.ReadFile(*configPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to read config file '%s': %s\n", *configPath, err)
-		os.Exit(1)
-	}
-
-	var config eapConfig
-	err = json.Unmarshal(configJSON, &config)
-	cmd.FailOnError(err, "Failed to parse config")
-	err = features.Set(config.ExpiredAuthzPurger.Features)
-	cmd.FailOnError(err, "Failed to set feature flags")
-
-	var logger blog.Logger
-	if config.ExpiredAuthzPurger.DebugAddr != "" {
-		var scope metrics.Scope
-		scope, logger = cmd.StatsAndLogging(config.ExpiredAuthzPurger.Syslog, config.ExpiredAuthzPurger.DebugAddr)
-		scope.MustRegister(deletedStat)
-	} else {
-		logger = cmd.NewLogger(config.ExpiredAuthzPurger.Syslog)
-	}
-	defer logger.AuditPanic()
-	logger.Info(cmd.VersionString())
-
-	// Configure DB
-	dbURL, err := config.ExpiredAuthzPurger.DBConfig.URL()
-	cmd.FailOnError(err, "Couldn't load DB URL")
-	dbMap, err := sa.NewDbMap(dbURL, config.ExpiredAuthzPurger.DBConfig.MaxDBConns)
-	cmd.FailOnError(err, "Could not connect to database")
-	sa.SetSQLDebug(dbMap, logger)
-
-	purger := &expiredAuthzPurger{
-		log:       logger,
-		clk:       cmd.Clock(),
-		db:        dbMap,
-		batchSize: int64(config.ExpiredAuthzPurger.BatchSize),
-	}
-
-	if config.ExpiredAuthzPurger.GracePeriod.Duration == 0 {
-		fmt.Fprintln(os.Stderr, "Grace period is 0, refusing to purge all pending authorizations")
-		os.Exit(1)
-	}
-	if config.ExpiredAuthzPurger.Parallelism == 0 {
-		fmt.Fprintln(os.Stderr, "Parallelism field in config must be set to non-zero")
-		os.Exit(1)
-	}
-	purgeBefore := purger.clk.Now().Add(-config.ExpiredAuthzPurger.GracePeriod.Duration)
-	logger.Info("Beginning purge")
-
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err := purger.purge(
-			"authz",
-			purgeBefore,
-			int(config.ExpiredAuthzPurger.Parallelism),
-			int(config.ExpiredAuthzPurger.MaxAuthzs),
-			*daemon,
-			config.ExpiredAuthzPurger.FinalCheckpointFile,
-			config.ExpiredAuthzPurger.MaxDPS,
-		)
-		cmd.FailOnError(err, "Failed to purge authorizations")
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err := purger.purge(
-			"pendingAuthorizations",
-			purgeBefore,
-			int(config.ExpiredAuthzPurger.Parallelism),
-			int(config.ExpiredAuthzPurger.MaxAuthzs),
-			*daemon,
-			config.ExpiredAuthzPurger.PendingCheckpointFile,
-			config.ExpiredAuthzPurger.MaxDPS,
-		)
-		cmd.FailOnError(err, "Failed to purge authorizations")
-	}()
-	wg.Wait()
-}

cmd/expired-authz-purger/main_test.go
@@ -1,134 +1,0 @@
-package main
-
-import (
-	"context"
-	"database/sql"
-	"errors"
-	"fmt"
-	"testing"
-	"time"
-
-	"github.com/jmhodges/clock"
-	"github.com/letsencrypt/boulder/core"
-	blog "github.com/letsencrypt/boulder/log"
-	"github.com/letsencrypt/boulder/metrics"
-	"github.com/letsencrypt/boulder/sa"
-	"github.com/letsencrypt/boulder/sa/satest"
-	"github.com/letsencrypt/boulder/test"
-	"github.com/letsencrypt/boulder/test/vars"
-)
-
-func TestPurgeAuthzs(t *testing.T) {
-	dbMap, err := sa.NewDbMap(vars.DBConnSAFullPerms, 0)
-	if err != nil {
-		t.Fatalf("Couldn't connect the database: %s", err)
-	}
-	log := blog.UseMock()
-	fc := clock.NewFake()
-	fc.Add(time.Hour)
-	ssa, err := sa.NewSQLStorageAuthority(dbMap, fc, log, metrics.NewNoopScope(), 1)
-	if err != nil {
-		t.Fatalf("unable to create SQLStorageAuthority: %s", err)
-	}
-	cleanUp := test.ResetSATestDatabase(t)
-	defer cleanUp()
-
-	p := expiredAuthzPurger{log, fc, dbMap, 1}
-
-	err = p.purge(
-		"pendingAuthorizations",
-		time.Time{},
-		10,
-		100,
-		false,
-		"",
-		0,
-	)
-	test.AssertNotError(t, err, "purgeAuthzs failed")
-
-	old, new := fc.Now().Add(-time.Hour), fc.Now().Add(time.Hour)
-
-	reg := satest.CreateWorkingRegistration(t, ssa)
-	_, err = ssa.NewPendingAuthorization(context.Background(), core.Authorization{
-		RegistrationID: reg.ID,
-		Expires:        &old,
-		Challenges:     []core.Challenge{{ID: 1}},
-	})
-	test.AssertNotError(t, err, "NewPendingAuthorization failed")
-	_, err = ssa.NewPendingAuthorization(context.Background(), core.Authorization{
-		RegistrationID: reg.ID,
-		Expires:        &old,
-		Challenges:     []core.Challenge{{ID: 2}},
-	})
-	test.AssertNotError(t, err, "NewPendingAuthorization failed")
-	_, err = ssa.NewPendingAuthorization(context.Background(), core.Authorization{
-		RegistrationID: reg.ID,
-		Expires:        &new,
-		Challenges:     []core.Challenge{{ID: 3}},
-	})
-	test.AssertNotError(t, err, "NewPendingAuthorization failed")
-
-	deletedStat.Reset()
-	err = p.purge(
-		"pendingAuthorizations",
-		fc.Now(),
-		10,
-		100,
-		false,
-		"",
-		0,
-	)
-	test.AssertNotError(t, err, "purgeAuthzs failed")
-	count, err := dbMap.SelectInt("SELECT COUNT(1) FROM pendingAuthorizations")
-	test.AssertNotError(t, err, "dbMap.SelectInt failed")
-	test.AssertEquals(t, count, int64(1))
-	count, err = dbMap.SelectInt("SELECT COUNT(1) FROM challenges")
-	test.AssertNotError(t, err, "dbMap.SelectInt failed")
-	test.AssertEquals(t, count, int64(1))
-	test.AssertEquals(t, test.CountCounterVec("table", "pendingAuthorizations", deletedStat), 2)
-	test.AssertEquals(t, test.CountCounterVec("table", "authz", deletedStat), 0)
-
-	err = p.purge(
-		"pendingAuthorizations",
-		fc.Now().Add(time.Hour),
-		10,
-		100,
-		false,
-		"",
-		0,
-	)
-	test.AssertNotError(t, err, "purgeAuthzs failed")
-	count, err = dbMap.SelectInt("SELECT COUNT(1) FROM pendingAuthorizations")
-	test.AssertNotError(t, err, "dbMap.SelectInt failed")
-	test.AssertEquals(t, count, int64(0))
-	count, err = dbMap.SelectInt("SELECT COUNT(1) FROM challenges")
-	test.AssertNotError(t, err, "dbMap.SelectInt failed")
-	test.AssertEquals(t, count, int64(0))
-	test.AssertEquals(t, test.CountCounterVec("table", "pendingAuthorizations", deletedStat), 3)
-	test.AssertEquals(t, test.CountCounterVec("table", "authz", deletedStat), 0)
-
-}
-
-type mockDeleter struct{}
-
-func (md *mockDeleter) Exec(query string, args ...interface{}) (sql.Result, error) {
-	return nil, nil
-}
-
-func (md *mockDeleter) Select(i interface{}, query string, args ...interface{}) ([]interface{}, error) {
-	return nil, errors.New("not implemented")
-}
-
-func TestMaxDPS(t *testing.T) {
-	log := blog.UseMock()
-	md := &mockDeleter{}
-	p := &expiredAuthzPurger{db: md, log: log}
-	work := make(chan string, 2)
-	work <- "a"
-	work <- "b"
-	close(work)
-	start := time.Now()
-	p.deleteAuthorizations(work, 1, 1, "", "")
-	took := time.Since(start)
-	test.Assert(t, took >= time.Second*2, fmt.Sprintf("deleteAuthorizations was faster than expected. wanted: 2s, got: %s", took))
-}

core/challenges.go
@@ -1,9 +1,6 @@
 package core
 
 func newChallenge(challengeType string, token string) Challenge {
-	if token == "" {
-		token = NewToken()
-	}
 	return Challenge{
 		Type:   challengeType,
 		Status: StatusPending,

core/challenges_test.go
@@ -7,8 +7,6 @@ import (
 )
 
 func TestNewChallenge(t *testing.T) {
-	challenge := newChallenge(ChallengeTypeDNS01, "")
-	test.Assert(t, challenge.Token != "", "token is not set")
-	challenge = newChallenge(ChallengeTypeDNS01, "asd")
+	challenge := newChallenge(ChallengeTypeDNS01, "asd")
 	test.Assert(t, challenge.Token == "asd", "token is not set")
 }

core/core_test.go
@@ -24,13 +24,14 @@ func TestChallenges(t *testing.T) {
 		t.Errorf("Error unmarshaling JWK: %v", err)
 	}
 
-	http01 := HTTPChallenge01("")
+	token := NewToken()
+	http01 := HTTPChallenge01(token)
 	test.AssertNotError(t, http01.CheckConsistencyForClientOffer(), "CheckConsistencyForClientOffer returned an error")
 
-	dns01 := DNSChallenge01("")
+	dns01 := DNSChallenge01(token)
 	test.AssertNotError(t, dns01.CheckConsistencyForClientOffer(), "CheckConsistencyForClientOffer returned an error")
 
-	tlsalpn01 := TLSALPNChallenge01("")
+	tlsalpn01 := TLSALPNChallenge01(token)
 	test.AssertNotError(t, tlsalpn01.CheckConsistencyForClientOffer(), "CheckConsistencyForClientOffer returned an error")
 
 	test.Assert(t, ValidChallenge(ChallengeTypeHTTP01), "Refused valid challenge")

commit 83aafd18842e093483d6701b92419ca8f7f1855b

Address #4476 issues (#4504)
Addresses two issues introduced in #4476:
* Keep setting the V2 field in modelToAuthzPB so RPCs returned from new components to old don't cause panics
* Don't return expired orders from the SA, so that users requesting old orders that contain old style authorizations don't cause breakage in the RA

sa/model.go
@@ -670,7 +670,9 @@ func modelToAuthzPB(am *authz2Model) (*corepb.Authorization, error) {
 	expires := am.Expires.UTC().UnixNano()
 	id := fmt.Sprintf("%d", am.ID)
 	status := uintToStatus[am.Status]
+	v2 := true
 	pb := &corepb.Authorization{
+		V2:             &v2,
 		Id:             &id,
 		Status:         &status,
 		Identifier:     &am.IdentifierValue,

sa/sa.go
@@ -1470,6 +1470,11 @@ func (ssa *SQLStorageAuthority) GetOrder(ctx context.Context, req *sapb.OrderReq
 	if err != nil {
 		return nil, err
 	}
+	orderExp := time.Unix(0, *order.Expires)
+	if orderExp.Before(ssa.clk.Now()) {
+		return nil, berrors.NotFoundError("no order found for ID %d", *req.Id)
+	}
+
 	v1AuthzIDs, v2AuthzIDs, err := ssa.authzForOrder(ctx, *order.Id)
 	if err != nil {
 		return nil, err

sa/sa_test.go
@@ -2290,13 +2290,6 @@ func TestStatusForOrder(t *testing.T) {
 			ExpectedStatus:   string(core.StatusDeactivated),
 		},
 		{
-			Name:             "Order that has expired and references a purged expired authz",
-			OrderExpires:     alreadyExpired.UnixNano(),
-			OrderNames:       []string{"missing.your.order.is.up"},
-			AuthorizationIDs: []string{"this does not exist"},
-			ExpectedStatus:   string(core.StatusInvalid),
-		},
-		{
 			Name:             "Order with a pending authz",
 			OrderNames:       []string{"valid.your.order.is.up", "pending.your.order.is.up"},
 			AuthorizationIDs: []string{validAuthz.ID, pendingAuthz.ID},
@@ -2746,7 +2739,9 @@ func TestNewAuthorizations2(t *testing.T) {
 	expires := fc.Now().Add(time.Hour).UTC().UnixNano()
 	challType := string(core.ChallengeTypeDNS01)
 	tokenA := "YXNkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+	v2 := true
 	apbA := &corepb.Authorization{
+		V2:             &v2,
 		Identifier:     &ident,
 		RegistrationID: &reg.ID,
 		Status:         &pending,
@@ -2761,6 +2756,7 @@ func TestNewAuthorizations2(t *testing.T) {
 	}
 	tokenB := "ZmdoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 	apbB := &corepb.Authorization{
+		V2:             &v2,
 		Identifier:     &ident,
 		RegistrationID: &reg.ID,
 		Status:         &pending,
@@ -3392,4 +3388,25 @@ func TestDisableAuthz2Orders(t *testing.T) {
 	})
 	test.AssertError(t, err, "GetOrder didn't fail with DisableAuthz2Orders enabled")
 	test.Assert(t, berrors.Is(err, berrors.NotFound), "GetOrder error was not NotFound")
+}
+
+func TestGetOrderExpired(t *testing.T) {
+	sa, fc, cleanUp := initSA(t)
+	defer cleanUp()
+
+	fc.Add(time.Hour * 5)
+	reg := satest.CreateWorkingRegistration(t, sa)
+	exp := fc.Now().Add(-time.Hour).UnixNano()
+	order, err := sa.NewOrder(context.Background(), &corepb.Order{
+		RegistrationID:   &reg.ID,
+		Expires:          &exp,
+		Names:            []string{"example.com"},
+		V2Authorizations: []int64{666},
+	})
+	test.AssertNotError(t, err, "NewOrder failed")
+	_, err = sa.GetOrder(context.Background(), &sapb.OrderRequest{
+		Id: order.Id,
+	})
+	test.AssertError(t, err, "GetOrder didn't fail for an expired order")
+	test.Assert(t, berrors.Is(err, berrors.NotFound), "GetOrder error wasn't of type NotFound")
 }