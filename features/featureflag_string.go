f24fd0dfc842764f475e010d16c03b3f7728e519
Roland Bracewell Shoemaker <rolandshoemaker@gmail.com>
2019-11-14 15:23:48 -0800

    Cleanup leftovers from PrecertificateOCSP deprecation (#4551)
    Cleans up a few things that were left out of #4465.

--- features/featureflag_string.go
+++ features/featureflag_string.go
@@ -22,27 +22,27 @@ func _() {
 	_ = x[DisableAuthz2Orders-11]
 	_ = x[EarlyOrderRateLimit-12]
 	_ = x[FasterGetOrderForNames-13]
-	_ = x[CAAValidationMethods-14]
-	_ = x[CAAAccountURI-15]
-	_ = x[HeadNonceStatusOK-16]
-	_ = x[EnforceMultiVA-17]
-	_ = x[MultiVAFullResults-18]
-	_ = x[RemoveWFE2AccountID-19]
-	_ = x[CheckRenewalFirst-20]
-	_ = x[MandatoryPOSTAsGET-21]
-	_ = x[AllowV1Registration-22]
-	_ = x[ParallelCheckFailedValidation-23]
-	_ = x[DeleteUnusedChallenges-24]
-	_ = x[V1DisableNewValidations-25]
-	_ = x[PrecertificateOCSP-26]
+	_ = x[PrecertificateOCSP-14]
+	_ = x[CAAValidationMethods-15]
+	_ = x[CAAAccountURI-16]
+	_ = x[HeadNonceStatusOK-17]
+	_ = x[EnforceMultiVA-18]
+	_ = x[MultiVAFullResults-19]
+	_ = x[RemoveWFE2AccountID-20]
+	_ = x[CheckRenewalFirst-21]
+	_ = x[MandatoryPOSTAsGET-22]
+	_ = x[AllowV1Registration-23]
+	_ = x[ParallelCheckFailedValidation-24]
+	_ = x[DeleteUnusedChallenges-25]
+	_ = x[V1DisableNewValidations-26]
 	_ = x[PrecertificateRevocation-27]
 	_ = x[StripDefaultSchemePort-28]
 	_ = x[GetAuthorizationsPerf-29]
 }
 
-const _FeatureFlag_name = "unusedPerformValidationRPCACME13KeyRolloverSimplifiedVAHTTPTLSSNIRevalidationAllowRenewalFirstRLSetIssuedNamesRenewalBitFasterRateLimitProbeCTLogsRevokeAtRANewAuthorizationSchemaDisableAuthz2OrdersEarlyOrderRateLimitFasterGetOrderForNamesCAAValidationMethodsCAAAccountURIHeadNonceStatusOKEnforceMultiVAMultiVAFullResultsRemoveWFE2AccountIDCheckRenewalFirstMandatoryPOSTAsGETAllowV1RegistrationParallelCheckFailedValidationDeleteUnusedChallengesV1DisableNewValidationsPrecertificateOCSPPrecertificateRevocationStripDefaultSchemePortGetAuthorizationsPerf"
+const _FeatureFlag_name = "unusedPerformValidationRPCACME13KeyRolloverSimplifiedVAHTTPTLSSNIRevalidationAllowRenewalFirstRLSetIssuedNamesRenewalBitFasterRateLimitProbeCTLogsRevokeAtRANewAuthorizationSchemaDisableAuthz2OrdersEarlyOrderRateLimitFasterGetOrderForNamesPrecertificateOCSPCAAValidationMethodsCAAAccountURIHeadNonceStatusOKEnforceMultiVAMultiVAFullResultsRemoveWFE2AccountIDCheckRenewalFirstMandatoryPOSTAsGETAllowV1RegistrationParallelCheckFailedValidationDeleteUnusedChallengesV1DisableNewValidationsPrecertificateRevocationStripDefaultSchemePortGetAuthorizationsPerf"
 
-var _FeatureFlag_index = [...]uint16{0, 6, 26, 43, 59, 77, 96, 120, 135, 146, 156, 178, 197, 216, 238, 258, 271, 288, 302, 320, 339, 356, 374, 393, 422, 444, 467, 485, 509, 531, 552}
+var _FeatureFlag_index = [...]uint16{0, 6, 26, 43, 59, 77, 96, 120, 135, 146, 156, 178, 197, 216, 238, 256, 276, 289, 306, 320, 338, 357, 374, 392, 411, 440, 462, 485, 509, 531, 552}
 
 func (i FeatureFlag) String() string {
 	if i < 0 || i >= FeatureFlag(len(_FeatureFlag_index)-1) {
