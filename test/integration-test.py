36e504f21f0438d4ba9d2ec4341d848f7e836318
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2019-11-19 10:44:24 -0800

    integration: log slow queries and check for them (#4554)
    I couldn't get this to work cleanly with
    `--log-queries-not-using-indexes` because a couple of queries show up
    during integration test runs, seemingly because the tables involved are
    small enough that the optimizer finds it faster to skip the index.
    
    Some possible followups:
     - Allow list those queries, or
     - Preload the DB with a certain number of certificates before the start
       of testing.

--- test/integration-test.py
+++ test/integration-test.py
@@ -308,8 +308,21 @@ def main():
     if not startservers.check():
         raise Exception("startservers.check failed")
 
+    check_slow_queries()
+
     global exit_status
     exit_status = 0
+
+def check_slow_queries():
+    """Checks that we haven't run any slow queries during the integration test.
+
+    This depends on flags set on mysqld in docker-compose.yml.
+    """
+    output = run("mysql -h boulder-mysql -D boulder_sa_integration -e " +
+        "'select * from mysql.slow_log where user_host not like \"test_setup%\" \G'")
+    if len(output) > 0:
+        print(output)
+        raise Exception("Found slow queries in the slow query log")
 
 def run_chisel(test_case_filter):
     for key, value in inspect.getmembers(v1_integration):
