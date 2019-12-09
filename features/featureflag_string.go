33ad1b90a10bb44c684b736809e874f5e115de2d
Roland Shoemaker <rolandshoemaker@gmail.com>
2019-12-09 12:38:48 -0800

    Remove deprecated features

--- features/featureflag_string.go
+++ features/featureflag_string.go
@@ -9,42 +9,27 @@ func _() {
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
 	_ = x[unused-0]
-	_ = x[PerformValidationRPC-1]
-	_ = x[ACME13KeyRollover-2]
-	_ = x[SimplifiedVAHTTP-3]
-	_ = x[TLSSNIRevalidation-4]
-	_ = x[AllowRenewalFirstRL-5]
-	_ = x[SetIssuedNamesRenewalBit-6]
-	_ = x[FasterRateLimit-7]
-	_ = x[ProbeCTLogs-8]
-	_ = x[RevokeAtRA-9]
-	_ = x[NewAuthorizationSchema-10]
-	_ = x[DisableAuthz2Orders-11]
-	_ = x[EarlyOrderRateLimit-12]
-	_ = x[FasterGetOrderForNames-13]
-	_ = x[PrecertificateOCSP-14]
-	_ = x[GetAuthorizationsPerf-15]
-	_ = x[CAAValidationMethods-16]
-	_ = x[CAAAccountURI-17]
-	_ = x[HeadNonceStatusOK-18]
-	_ = x[EnforceMultiVA-19]
-	_ = x[MultiVAFullResults-20]
-	_ = x[RemoveWFE2AccountID-21]
-	_ = x[CheckRenewalFirst-22]
-	_ = x[MandatoryPOSTAsGET-23]
-	_ = x[AllowV1Registration-24]
-	_ = x[ParallelCheckFailedValidation-25]
-	_ = x[DeleteUnusedChallenges-26]
-	_ = x[V1DisableNewValidations-27]
-	_ = x[PrecertificateRevocation-28]
-	_ = x[StripDefaultSchemePort-29]
-	_ = x[StoreIssuerInfo-30]
-	_ = x[WriteIssuedNamesPrecert-31]
+	_ = x[CAAValidationMethods-1]
+	_ = x[CAAAccountURI-2]
+	_ = x[HeadNonceStatusOK-3]
+	_ = x[EnforceMultiVA-4]
+	_ = x[MultiVAFullResults-5]
+	_ = x[RemoveWFE2AccountID-6]
+	_ = x[CheckRenewalFirst-7]
+	_ = x[MandatoryPOSTAsGET-8]
+	_ = x[AllowV1Registration-9]
+	_ = x[ParallelCheckFailedValidation-10]
+	_ = x[DeleteUnusedChallenges-11]
+	_ = x[V1DisableNewValidations-12]
+	_ = x[PrecertificateRevocation-13]
+	_ = x[StripDefaultSchemePort-14]
+	_ = x[StoreIssuerInfo-15]
+	_ = x[WriteIssuedNamesPrecert-16]
 }
 
-const _FeatureFlag_name = "unusedPerformValidationRPCACME13KeyRolloverSimplifiedVAHTTPTLSSNIRevalidationAllowRenewalFirstRLSetIssuedNamesRenewalBitFasterRateLimitProbeCTLogsRevokeAtRANewAuthorizationSchemaDisableAuthz2OrdersEarlyOrderRateLimitFasterGetOrderForNamesPrecertificateOCSPGetAuthorizationsPerfCAAValidationMethodsCAAAccountURIHeadNonceStatusOKEnforceMultiVAMultiVAFullResultsRemoveWFE2AccountIDCheckRenewalFirstMandatoryPOSTAsGETAllowV1RegistrationParallelCheckFailedValidationDeleteUnusedChallengesV1DisableNewValidationsPrecertificateRevocationStripDefaultSchemePortStoreIssuerInfoWriteIssuedNamesPrecert"
+const _FeatureFlag_name = "unusedCAAValidationMethodsCAAAccountURIHeadNonceStatusOKEnforceMultiVAMultiVAFullResultsRemoveWFE2AccountIDCheckRenewalFirstMandatoryPOSTAsGETAllowV1RegistrationParallelCheckFailedValidationDeleteUnusedChallengesV1DisableNewValidationsPrecertificateRevocationStripDefaultSchemePortStoreIssuerInfoWriteIssuedNamesPrecert"
 
-var _FeatureFlag_index = [...]uint16{0, 6, 26, 43, 59, 77, 96, 120, 135, 146, 156, 178, 197, 216, 238, 256, 277, 297, 310, 327, 341, 359, 378, 395, 413, 432, 461, 483, 506, 530, 552, 567, 590}
+var _FeatureFlag_index = [...]uint16{0, 6, 26, 39, 56, 70, 88, 107, 124, 142, 161, 190, 212, 235, 259, 281, 296, 319}
 
 func (i FeatureFlag) String() string {
 	if i < 0 || i >= FeatureFlag(len(_FeatureFlag_index)-1) {
