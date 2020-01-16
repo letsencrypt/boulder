f4b07407e7c65d5f413c41ab870d102c6654f3b2
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-01-15 19:10:39 -0800

    Add better test output.

--- test/integration/authz_test.go
+++ test/integration/authz_test.go
@@ -47,5 +47,5 @@ func TestValidAuthzExpires(t *testing.T) {
 	// The authz should have the expected expiry date
 	expectedExpires := time.Now().AddDate(0, 0, validAuthorizationLifetime).Round(time.Minute)
 	actualExpires := authzOb.Expires.Round(time.Minute)
-	test.AssertEquals(t, expectedExpires.Equal(actualExpires), true)
+	test.AssertEquals(t, expectedExpires, actualExpires)
 }
