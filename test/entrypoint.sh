f417a3e1cea43f25a4f23368d2f8494e637cf54f
Brad Warren <bmw@eff.org>
2020-04-07 10:56:55 -0700

    Don't activate venv.

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -56,7 +56,6 @@ if [[ $# -eq 0 ]]; then
     # the activate script touches PS1, which is undefined, so we have to relax
     # the "fail on undefined" setting here.
     set +u
-    source ${CERTBOT_PATH:-/certbot}/${VENV_NAME:-venv3}/bin/activate
     exec python3 ./start.py
 fi
 
