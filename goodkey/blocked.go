b99667f4275fa8df2f6f9e0ab84b6c16642b4a55
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-04-06 17:47:24 -0700

    Block keys using hex(sha256(spki)).
    In addition to base64(sha256(spki)).

--- goodkey/blocked.go
+++ goodkey/blocked.go
@@ -2,6 +2,9 @@ package goodkey
 
 import (
 	"crypto"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
 	"errors"
 	"io/ioutil"
 
@@ -10,18 +13,22 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-// blockedKeys is a type for maintaining a map of Base64 encoded SHA256 hashes
+type keyHash [32]byte
+
+// blockedKeys is a type for maintaining a map of SHA256 hashes
 // of SubjectPublicKeyInfo's that should be considered blocked.
 // blockedKeys are created by using loadBlockedKeysList.
-type blockedKeys map[string]bool
+type blockedKeys map[keyHash]bool
+
+var ErrWrongDecodedSize = errors.New("not enough bytes decoded for sha256 hash")
 
 // blocked checks if the given public key is considered administratively
-// blocked based on a Base64 encoded SHA256 hash of the SubjectPublicKeyInfo.
+// blocked based on a SHA256 hash of the SubjectPublicKeyInfo.
 // Important: blocked should not be called except on a blockedKeys instance
 // returned from loadBlockedKeysList.
 // function should not be used until after `loadBlockedKeysList` has returned.
 func (b blockedKeys) blocked(key crypto.PublicKey) (bool, error) {
-	hash, err := core.KeyDigest(key)
+	b64Hash, err := core.KeyDigest(key)
 	if err != nil {
 		// the bool result should be ignored when err is != nil but to be on the
 		// paranoid side return true anyway so that a key we can't compute the
@@ -29,11 +36,19 @@ func (b blockedKeys) blocked(key crypto.PublicKey) (bool, error) {
 		// err result.
 		return true, err
 	}
+	var hash keyHash
+	n, err := base64.StdEncoding.Decode(hash[:], []byte(b64Hash))
+	if err != nil {
+		return true, err
+	}
+	if n != sha256.Size {
+		return true, ErrWrongDecodedSize
+	}
 	return b[hash], nil
 }
 
 // loadBlockedKeysList creates a blockedKeys object that can be used to check if
-// a key is blocked. It creates a lookup map from a list of Base64 encoded
+// a key is blocked. It creates a lookup map from a list of
 // SHA256 hashes of SubjectPublicKeyInfo's in the input YAML file
 // with the expected format:
 //
@@ -52,18 +67,38 @@ func loadBlockedKeysList(filename string) (*blockedKeys, error) {
 	}
 
 	var list struct {
-		BlockedHashes []string `yaml:"blocked"`
+		BlockedHashes    []string `yaml:"blocked"`
+		BlockedHashesHex []string `yaml:"blockedHashesHex"`
 	}
 	if err := yaml.Unmarshal(yamlBytes, &list); err != nil {
 		return nil, err
 	}
 
-	if len(list.BlockedHashes) == 0 {
+	if len(list.BlockedHashes) == 0 && len(list.BlockedHashesHex) == 0 {
 		return nil, errors.New("no blocked hashes in YAML")
 	}
 
-	blockedKeys := make(blockedKeys, len(list.BlockedHashes))
-	for _, hash := range list.BlockedHashes {
+	blockedKeys := make(blockedKeys, len(list.BlockedHashes)+len(list.BlockedHashesHex))
+	for _, b64Hash := range list.BlockedHashes {
+		var hash keyHash
+		n, err := base64.StdEncoding.Decode(hash[:], []byte(b64Hash))
+		if err != nil {
+			return nil, err
+		}
+		if n != sha256.Size {
+			return nil, ErrWrongDecodedSize
+		}
+		blockedKeys[hash] = true
+	}
+	for _, hexHash := range list.BlockedHashesHex {
+		var hash keyHash
+		n, err := hex.Decode(hash[:], []byte(hexHash))
+		if err != nil {
+			return nil, err
+		}
+		if n != sha256.Size {
+			return nil, ErrWrongDecodedSize
+		}
 		blockedKeys[hash] = true
 	}
 	return &blockedKeys, nil
