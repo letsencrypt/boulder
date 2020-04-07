aad43e46883e0ff03bbfa1baff0188fe29494d88
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-04-07 11:35:42 -0700

    Fix entrypoint.sh / docker-compose up. (#4747)
    We no longer use virtualenv; we just install our Python dependencies
    globally.

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -53,10 +53,6 @@ if [ -n "${PKCS11_PROXY_SOCKET:-}" ]; then
 fi
 
 if [[ $# -eq 0 ]]; then
-    # the activate script touches PS1, which is undefined, so we have to relax
-    # the "fail on undefined" setting here.
-    set +u
-    source ${CERTBOT_PATH:-/certbot}/${VENV_NAME:-venv3}/bin/activate
     exec python3 ./start.py
 fi
 
