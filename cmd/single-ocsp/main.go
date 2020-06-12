7f3c0f61a378f031dbc5934939076fd291e39136
Roland Bracewell Shoemaker <rolandshoemaker@gmail.com>
2020-06-11 17:11:29 -0700

    cmd/single-ocsp: validate issuer/responder are valid to sign the response (#4855)
    Fixes #4853

--- cmd/single-ocsp/main.go
+++ cmd/single-ocsp/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -84,6 +85,33 @@ func readFiles(issuerFileName, responderFileName, targetFileName, pkcs11FileName
 	return
 }
 
+func signerValidForResp(issuer, responder *x509.Certificate, thisUpdate, nextUpdate time.Time) error {
+	if !issuer.Equal(responder) {
+		if err := responder.CheckSignatureFrom(issuer); err != nil {
+			return fmt.Errorf("invalid signature on responder from issuer: %s", err)
+		}
+
+		gotOCSPEKU := false
+		for _, eku := range responder.ExtKeyUsage {
+			if eku == x509.ExtKeyUsageOCSPSigning {
+				gotOCSPEKU = true
+				break
+			}
+		}
+		if !gotOCSPEKU {
+			return errors.New("responder certificate doesn't contain OCSPSigning extended key usage")
+		}
+	}
+
+	if thisUpdate.Before(responder.NotBefore) {
+		return errors.New("thisUpdate is before responder certificate's notBefore")
+	} else if nextUpdate.After(responder.NotAfter) {
+		return errors.New("nextUpdate is after responder certificate's notAfter")
+	}
+
+	return nil
+}
+
 func main() {
 	issuerFile := flag.String("issuer", "", "Issuer certificate (PEM)")
 	responderFile := flag.String("responder", "", "OCSP responder certificate (PEM)")
@@ -107,8 +135,15 @@ func main() {
 	nextUpdate, err := time.Parse(time.RFC3339, *nextUpdateString)
 	cmd.FailOnError(err, "Parsing nextUpdate flag")
 
+	if nextUpdate.Before(thisUpdate) {
+		cmd.Fail("nextUpdate must be after thisUpdate")
+	}
+
 	issuer, responder, target, pkcs11, err := readFiles(*issuerFile, *responderFile, *targetFile, *pkcs11File)
 	cmd.FailOnError(err, "Failed to read files")
+
+	err = signerValidForResp(issuer, responder, thisUpdate, nextUpdate)
+	cmd.FailOnError(err, "Issuer/Responder are not valid for response")
 
 	// Instantiate the private key from PKCS11
 	priv, err := pkcs11key.New(pkcs11.Module, pkcs11.TokenLabel, pkcs11.PIN, responder.PublicKey)
