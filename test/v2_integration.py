dee757c057ca140ecb077977da71fe1d445b1dcc
orangepizza <abnoeh@mail.com>
2020-07-09 02:57:17 +0900

    Remove multiva exception list code (#4933)
    Fixes #4931

--- test/v2_integration.py
+++ test/v2_integration.py
@@ -22,7 +22,7 @@ from helpers import *
 
 from acme import errors as acme_errors
 
-from acme.messages import Status, CertificateRequest, Directory, RegistrationResource
+from acme.messages import Status, CertificateRequest, Directory
 from acme import crypto_util as acme_crypto_util
 from acme import client as acme_client
 from acme import messages
@@ -774,7 +774,7 @@ def test_only_return_existing_reg():
     email = "test@not-example.com"
     client.new_account(messages.NewRegistration.from_data(email=email,
             terms_of_service_agreed=True))
-    
+
     client = chisel2.uninitialized_client(key=client.net.key)
     class extendedAcct(dict):
         def json_dumps(self, indent=None):
@@ -806,7 +806,7 @@ def BouncerHTTPRequestHandler(redirect, guestlist):
     spot for that UA it will be redirected to the real server and the
     guestlist will be decremented. Once the guestlist spots for a UA are
     expended requests will get a bogus result and have to stand outside in the
-    cold 
+    cold
     """
     class BouncerHandler(BaseHTTPRequestHandler):
         def __init__(self, *args, **kwargs):
@@ -851,19 +851,15 @@ def wait_for_server(addr):
             pass
         time.sleep(0.5)
 
-def multiva_setup(client, guestlist, domain=None):
+def multiva_setup(client, guestlist):
     """
     Setup a testing domain and backing multiva server setup. This will block
     until the server is ready. The returned cleanup function should be used to
     stop the server. The first bounceFirst requests to the server will be sent
     to the real challtestsrv for a good answer, the rest will get a bad
-    answer. If no explicit testing domain is provided then one is randomly
-    chosen with random_domain().
+    answer. Domain name is randomly chosen with random_domain().
     """
-    if domain is None:
-        hostname = random_domain()
-    else:
-        hostname = domain
+    hostname = random_domain()
 
     csr_pem = chisel2.make_csr([hostname])
     order = client.new_order(csr_pem)
@@ -1000,109 +996,6 @@ def test_http_multiva_threshold_fail():
     if not httpChall.error.detail.startswith("During secondary validation: "):
         raise(Exception("expected 'During secondary validation' problem detail, found {0}".format(httpChall.error.detail)))
 
-def test_http_multiva_threshold_fail_domain_disabled():
-    client = chisel2.make_client()
-
-    # Configure a guestlist that will fail the multiVA threshold test by
-    # only allowing the primary VA.
-    guestlist = {"boulder": 1}
-
-    # Explicitly use a domain name that exists in
-    # `test/example-multiva-policy.yaml`'s disabledDomains list
-    domain = "brokenmultiva.letsencrypt.org"
-
-    _, cleanup = multiva_setup(client, guestlist, domain)
-
-    # We do not expect any errors, even though the guestlist ensured multi-va
-    # failures, because the domain was in the multi VA policy disabledDomains
-    # list.
-    try:
-        chisel2.auth_and_issue([domain], client=client, chall_type="http-01")
-    finally:
-        cleanup()
-
-def test_http_multiva_threshold_fail_account_disabled():
-    # Create an ACME account
-    client = chisel2.make_client()
-
-    # Find the numeric ID it was assigned by the ACME server
-    acctURI = client.net.account.uri
-    if len(acctURI.split("/")) < 1:
-        raise(Exception("invalid account URI for newly registered account: {0}".format(acctURI)))
-    acctID = acctURI.split("/")[-1:][0]
-
-    def run_query(query):
-        command=["mysql",
-            "-h", "bmysql",
-            "-u", "root",
-            "--password=",
-            "-e", query,
-            "boulder_sa_integration",
-            ]
-        subprocess.check_call(command, shell=False, stderr=subprocess.STDOUT)
-
-    def flip_ids(oldID, newID):
-        """
-        flip_ids changes a registrations ID from one value to another. Note that
-        in order for this to succeed all other tables with foreign key
-        constraints on the registration.ID field must be adjusted or otherwise
-        dealt with.
-        """
-        run_query("UPDATE registrations SET id={0} WHERE id={1}".format(newID, oldID))
-
-    def remove_certs(newID):
-        """
-        remove_certs deletes rows created while the account registration ID was changed.
-        We need to delete these rows so that the original account registration
-        ID can be restored without violating the foreign key constraints on the
-        certs, precerts and serials tables. Updating the registrationID of these
-        rows first is difficult because the new value (the original ID) doesn't
-        exist in the registrations table yet. The best solution would be doing
-        all of this in one transaction but we're already deep in hacky
-        integration test yak shaving at this point...
-        """
-        run_query("DELETE FROM certificates WHERE registrationID={0}".format(newID))
-        run_query("DELETE FROM precertificates WHERE registrationID={0}".format(newID))
-        run_query("DELETE FROM serials WHERE registrationID={0}".format(newID))
-
-    try:
-        # Update the account ID in the database to match an ID that exists in
-        # `test/example-multi-va-policy.yaml`'s disabledAccounts list. We do this
-        # with direct DB access because the alternative is hackish rewriting of the
-        # policy YAML file at runtime (especially since the reload event can't be
-        # easily detected). This approach is _also_ hackish, but marginally less so.
-        newID=99991337
-        flip_ids(acctID, newID)
-
-        # Update the in-memory account ID for the client instance to match
-        client.net.account = RegistrationResource(
-                body=client.net.account.body,
-                uri=acctURI.replace(acctID, str(newID)),
-                terms_of_service=client.net.account.terms_of_service)
-
-        # Configure a guestlist that will fail the multiVA threshold test by
-        # only allowing the primary VA.
-        guestlist = {"boulder": 1}
-
-        # Setup for a random domain name
-        domain, cleanup = multiva_setup(client, guestlist, domain=None)
-
-        # We do not expect any errors, even though the guestlist ensured multi-va
-        # failures, because the client was set up with an account key corresponding
-        # to a multi VA policy disabledAccount ID.
-        chisel2.auth_and_issue([domain], client=client, chall_type="http-01")
-    finally:
-        cleanup()
-        # Remove certificates and related resources issued by the
-        # fixed example-multi-va-policy.yaml account ID. This avoids foreign key
-        # constraints being broken when we flip_ids next.
-        remove_certs(newID)
-        # Change the account ID back to the old account ID. This will prevent
-        # duplicate key errors when the integration test is run again and tries
-        # to update a different newly created account to the fixed ID from the
-        # example-multi-va-policy.yaml file.
-        flip_ids(newID, acctID)
-
 class FakeH2ServerHandler(socketserver.BaseRequestHandler):
     """
     FakeH2ServerHandler is a TCP socket handler that writes data representing an
@@ -1184,7 +1077,7 @@ def test_new_order_policy_errs():
     client = chisel2.make_client(None)
 
     # 'in-addr.arpa' is present in `test/hostname-policy.yaml`'s
-    # HighRiskBlockedNames list. 
+    # HighRiskBlockedNames list.
     csr_pem = chisel2.make_csr(["out-addr.in-addr.arpa", "between-addr.in-addr.arpa"])
 
     # With two policy blocked names in the order we expect to get back a top
