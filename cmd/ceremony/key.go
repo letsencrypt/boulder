62eae60711d7ad771267f91b84f9c657cf2a88ff
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-07-23 16:55:43 -0700

    Check for existing objects before generating a key. (#4981)
    We only expect to want one key per slot/token.
    
    Refactor key_test somewhat to split up cases, and add a new test case.

--- cmd/ceremony/key.go
+++ cmd/ceremony/key.go
@@ -54,9 +54,13 @@ type keyInfo struct {
 }
 
 func generateKey(ctx pkcs11helpers.PKCtx, session pkcs11.SessionHandle, label string, outputPath string, config keyGenConfig) (*keyInfo, error) {
+	_, err := pkcs11helpers.FindObject(ctx, session, []*pkcs11.Attribute{})
+	if err != pkcs11helpers.ErrNoObject {
+		return nil, fmt.Errorf("expected no objects in slot for key storage. got error: %s", err)
+	}
+
 	var pubKey crypto.PublicKey
 	var keyID []byte
-	var err error
 	switch config.Type {
 	case "rsa":
 		pubKey, keyID, err = rsaGenerate(ctx, session, label, config.RSAModLength, rsaExp)
