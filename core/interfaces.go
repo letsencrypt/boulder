d24aac00f8ad9fda6411aab09f9ddb7702803d19
Aaron Gable <aaron@letsencrypt.org>
2020-07-29 09:42:22 -0700

    Update VA interface to consume and return protos

--- core/interfaces.go
+++ core/interfaces.go
@@ -16,6 +16,7 @@ import (
 	rapb "github.com/letsencrypt/boulder/ra/proto"
 	"github.com/letsencrypt/boulder/revocation"
 	sapb "github.com/letsencrypt/boulder/sa/proto"
+	vapb "github.com/letsencrypt/boulder/va/proto"
 )
 
 // A WebFrontEnd object supplies methods that can be hooked into
@@ -90,6 +91,10 @@ type RegistrationAuthority interface {
 	// [AdminRevoker]
 	AdministrativelyRevokeCertificate(ctx context.Context, cert x509.Certificate, code revocation.Reason, adminName string) error
 }
+
+// ValidationAuthority defines the public interface for the Boulder VA
+// TODO(#4956): Remove this unnecessary type alias.
+type ValidationAuthority vapb.VAServer
 
 // CertificateAuthority defines the public interface for the Boulder CA
 type CertificateAuthority interface {
