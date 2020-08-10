e5630eb8ba729907e161823d1b9487af997106b6
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2020-08-10 15:40:08 -0700

    Don't require OCSP URL for intermediates.
    Per ballot SC30, assuming the review period passes with no objections,
    OCSP is no longer required on intermediates.

--- cmd/ceremony/cert.go
+++ cmd/ceremony/cert.go
@@ -102,9 +102,6 @@ func (profile *certProfile) verifyProfile(ct certType) error {
 	}
 
 	if ct == intermediateCert {
-		if profile.OCSPURL == "" {
-			return errors.New("ocsp-url is required for intermediates")
-		}
 		if profile.CRLURL == "" {
 			return errors.New("crl-url is required for intermediates")
 		}
