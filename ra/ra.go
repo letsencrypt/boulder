63eafa4a3b3c736b2ef4844d71a125036b1c2faa
Samantha <hello@entropy.cat>
2020-10-26 15:37:00 -0700

    ra: replacing error assertions with errors.As (#5140)
    Part of #5010

--- ra/ra.go
+++ ra/ra.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/x509"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
 	"net/url"
@@ -864,7 +865,8 @@ func (ra *RegistrationAuthorityImpl) recheckCAA(ctx context.Context, authzs []*c
 		// If the result had a CAA boulder error, construct a suberror with the
 		// identifier from the authorization that was checked.
 		if err := recheckResult.err; err != nil {
-			if bErr, _ := err.(*berrors.BoulderError); berrors.Is(err, berrors.CAA) {
+			var bErr *berrors.BoulderError
+			if errors.As(err, &bErr) && bErr.Type == berrors.CAA {
 				subErrors = append(subErrors, berrors.SubBoulderError{
 					Identifier:   recheckResult.authz.Identifier,
 					BoulderError: bErr})
