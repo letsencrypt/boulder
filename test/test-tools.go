238c7369f1283302038d0d87e21b02649bd238ad
Aaron Gable <aaron@letsencrypt.org>
2020-11-05 13:33:33 -0800

    Change error formatting

--- test/test-tools.go
+++ test/test-tools.go
@@ -56,7 +56,7 @@ func AssertError(t *testing.T, err error, message string) {
 func AssertErrorWraps(t *testing.T, err error, target error) {
 	t.Helper()
 	if !errors.As(err, &target) {
-		t.Fatalf("error does not wrap expected error type: %+v ∌ %+T", err, target)
+		t.Fatalf("error does not unwrap to expected error type: %q !> %+T", err.Error(), target)
 	}
 }
 
