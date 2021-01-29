f2d7713cb089c659c97bbfdfecf6a2c6c51131ee
Aaron Gable <aaron@letsencrypt.org>
2021-01-29 08:28:12 -0800

    CA: Create ECDSA issuance allowlist
    Currently, the CA is configured with a set of `internalIssuer`s,
    and a mapping of public key algorithms (e.g. `x509.RSA`) to which
    internalIssuer to use. In operation today, we use the same issuer
    for all kinds of public key algorithms. In the future, we will use
    different issuers for different algorithms (in particular, we will
    use R3 to issue for RSA keys, and E1 to issue for ECDSA keys). But
    we want to roll that out slowly, continuing to use our RSA issuer
    to issue for all types of public keys, except for ECDSA keys which
    are presented by a specific set of allowed accounts.
    
    This change adds a new config field to the CA, which lets us specify
    a small list of registration IDs which are allowed to have issuance
    from our ECDSA issuer. If the config list is empty, then all accounts
    are allowed. The CA checks to see if the key being issued for is
    ECDSA: if it is, it then checks to make sure that the associated
    registration ID is in the allowlist (or that the allowlist is empty).
    If the account is not allowed, it then overrides the issuance algoriths
    to use RSA instead, mimicking our old behavior.

--- ca/ca.go
+++ ca/ca.go
@@ -130,6 +130,7 @@ type CertificateAuthorityImpl struct {
 	sa                 certificateStorage
 	pa                 core.PolicyAuthority
 	issuers            issuerMaps
+	ecdsaAllowedRegIDs map[int64]bool
 	cfsslRSAProfile    string
 	cfsslECDSAProfile  string
 	prefix             int // Prepended to the serial number
@@ -257,6 +258,7 @@ func NewCertificateAuthorityImpl(
 	cfsslECDSAProfile string,
 	cfsslIssuers []Issuer,
 	boulderIssuers []*issuance.Issuer,
+	ecdsaAllowedRegIDs []int64,
 	certExpiry time.Duration,
 	certBackdate time.Duration,
 	serialPrefix int,
@@ -322,6 +324,11 @@ func NewCertificateAuthorityImpl(
 		}
 	}
 
+	ecdsaAllowedRegIDsMap := make(map[int64]bool, len(ecdsaAllowedRegIDs))
+	for _, regID := range ecdsaAllowedRegIDs {
+		ecdsaAllowedRegIDsMap[regID] = true
+	}
+
 	csrExtensionCount := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "csr_extensions",
@@ -371,6 +378,7 @@ func NewCertificateAuthorityImpl(
 		issuers:            issuers,
 		cfsslRSAProfile:    cfsslRSAProfile,
 		cfsslECDSAProfile:  cfsslECDSAProfile,
+		ecdsaAllowedRegIDs: ecdsaAllowedRegIDsMap,
 		validityPeriod:     certExpiry,
 		backdate:           certBackdate,
 		prefix:             serialPrefix,
@@ -776,7 +784,14 @@ func (ca *CertificateAuthorityImpl) issuePrecertificateInner(ctx context.Context
 	var issuer *internalIssuer
 	var ok bool
 	if issueReq.IssuerNameID == 0 {
-		issuer, ok = ca.issuers.byAlg[csr.PublicKeyAlgorithm]
+		// Use the issuer which corresponds to the algorithm of the public key
+		// contained in the CSR, unless we have an allowlist of registration IDs
+		// for ECDSA, in which case switch all not-allowed accounts to RSA issuance.
+		alg := csr.PublicKeyAlgorithm
+		if alg == x509.ECDSA && len(ca.ecdsaAllowedRegIDs) != 0 && !ca.ecdsaAllowedRegIDs[issueReq.RegistrationID] {
+			alg = x509.RSA
+		}
+		issuer, ok = ca.issuers.byAlg[alg]
 		if !ok {
 			return nil, nil, berrors.InternalServerError("no issuer found for public key algorithm %s", csr.PublicKeyAlgorithm)
 		}
