066ed3427ed2daf03d33fe370288947d159bc567
Andrew Gabbitas <agabbitas@letsencrypt.org>
2021-02-26 13:59:47 -0700

    Add GitHub actions workflow for Boulder CI tests (#5308)
    - Add GitHub actions workflow for Boulder CI tests in parity with Travis
      CI except the coverage test.
    - Change boulder-tools docker image to push to a static docker repo
      instead of creating a new one each time. Use docker version tags and git
      hash to identify go versions in the repo.
    - Change docker-compose to pull from the static boulder-tools repo. This
      breaks using the TRAVIS_GO_VERSION env variable to pull the docker image, but
      the default will still work with intent of decommissioning Travis-CI for
      GitHub CI.
    
    Fix: #5289

--- test/boulder-tools/tag_and_upload.sh
+++ test/boulder-tools/tag_and_upload.sh
@@ -3,13 +3,14 @@
 cd $(dirname $0)
 
 DATESTAMP=$(date +%Y-%m-%d)
-BASE_TAG_NAME="letsencrypt/boulder-tools"
+DOCKER_REPO="letsencrypt/boulder-tools"
+
 GO_VERSIONS=( "1.15.7" )
 
 # Build a tagged image for each GO_VERSION
 for GO_VERSION in "${GO_VERSIONS[@]}"
 do
-  TAG_NAME="$BASE_TAG_NAME-go$GO_VERSION:$DATESTAMP"
+  TAG_NAME="$DOCKER_REPO:go${GO_VERSION}_${DATESTAMP}"
   echo "Building boulder-tools image $TAG_NAME"
 
   # Build the docker image using the templated Dockerfile, tagging it with
@@ -26,9 +27,9 @@ docker login
 # Upload a tagged image for each GO_VERSION
 for GO_VERSION in "${GO_VERSIONS[@]}"
 do
-  TAG_NAME="$BASE_TAG_NAME-go$GO_VERSION:$DATESTAMP"
-  echo "Pushing $TAG_NAME to Dockerhub"
-  docker push $TAG_NAME
+  TAG_NAME="$DOCKER_REPO:go${GO_VERSION}_${DATESTAMP}"
+  echo "Pushing ${TAG_NAME} to Dockerhub"
+  docker push ${TAG_NAME}
 done
 
 # TODO(@cpu): Figure out a `sed` for updating the date in `docker-compose.yml`'s
