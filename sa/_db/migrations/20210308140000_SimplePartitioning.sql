636a71a0995aadaa9aa1672f28c0481469909339
Aaron Gable <aaron@letsencrypt.org>
2021-03-09 15:13:52 -0800

    Add orders, orderFqdnSets, and requestedNames

--- sa/_db/migrations/20210308140000_SimplePartitioning.sql
+++ sa/_db/migrations/20210308140000_SimplePartitioning.sql
@@ -5,7 +5,7 @@
 -- These partition statements all set a start value of 0 because these
 -- commands are run against an test database with little to no data in it.
 
-ALTER TABLE authz2 DROP INDEX token;
+ALTER TABLE authz2 DROP INDEX IF EXISTS token;
 ALTER TABLE authz2 PARTITION BY RANGE(id) (
      PARTITION p_start VALUES LESS THAN MAXVALUE);
 
@@ -16,6 +16,19 @@ ALTER TABLE certificates PARTITION BY RANGE(id) (
 
 ALTER TABLE fqdnSets DROP INDEX IF EXISTS serial, ADD INDEX serial (serial);
 ALTER TABLE fqdnSets PARTITION BY RANGE(id) (
+    PARTITION p_start VALUES LESS THAN MAXVALUE);
+
+ALTER TABLE orderFqdnSets DROP FOREIGN KEY IF EXISTS orderFqdnSets_orderID_orders;
+ALTER TABLE orderFqdnSets DROP FOREIGN KEY IF EXISTS orderFqdnSets_registrationID_registrations;
+ALTER TABLE orderFqdnSets PARTITION BY RANGE (id) (
+    PARTITION p_start VALUES LESS THAN MAXVALUE);
+
+-- Comes before orders, to remove the foreign key before partitioning orders.
+ALTER TABLE requestedNames DROP FOREIGN KEY IF EXISTS orderID_orders;
+ALTER TABLE requestedNames PARTITION BY RANGE (id) (
+    PARTITION p_start VALUES LESS THAN MAXVALUE);
+
+ALTER TABLE orders PARTITION BY RANGE (id) (
     PARTITION p_start VALUES LESS THAN MAXVALUE);
 
 ALTER TABLE precertificates DROP FOREIGN KEY IF EXISTS regId_precertificates;
@@ -29,4 +42,7 @@ ALTER TABLE precertificates PARTITION BY RANGE(id) (
 ALTER TABLE authz2 REMOVE PARTITIONING;
 ALTER TABLE certificates REMOVE PARTITIONING;
 ALTER TABLE fqdnSets REMOVE PARTITIONING;
+ALTER TABLE orderFqdnSets REMOVE PARTITIONING;
+ALTER TABLE orders REMOVE PARTITIONING;
 ALTER TABLE precertificates REMOVE PARTITIONING;
+ALTER TABLE requestedNames REMOVE PARTITIONING;
