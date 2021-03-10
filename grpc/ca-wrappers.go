165dc03438ac7371679e1e8ad1ad0e29c5647b7a
Aaron Gable <aaron@letsencrypt.org>
2021-03-10 15:35:14 -0800

    Remove ca gRPC server wrapper

--- grpc/ca-wrappers.go
+++ grpc/ca-wrappers.go
@@ -10,7 +10,6 @@ import (
 	"context"
 
 	capb "github.com/letsencrypt/boulder/ca/proto"
-	"github.com/letsencrypt/boulder/core"
 	corepb "github.com/letsencrypt/boulder/core/proto"
 	"google.golang.org/grpc"
 )
@@ -45,25 +44,4 @@ func NewOCSPGeneratorClient(inner capb.OCSPGeneratorClient) *OCSPGeneratorClient
 
 func (ogc OCSPGeneratorClientWrapper) GenerateOCSP(ctx context.Context, req *capb.GenerateOCSPRequest, _ ...grpc.CallOption) (*capb.OCSPResponse, error) {
 	return ogc.inner.GenerateOCSP(ctx, req)
-}
-
-// CertificateAuthorityServerWrapper is the gRPC version of a core.CertificateAuthority server
-type CertificateAuthorityServerWrapper struct {
-	inner core.CertificateAuthority
-}
-
-func NewCertificateAuthorityServer(inner core.CertificateAuthority) *CertificateAuthorityServerWrapper {
-	return &CertificateAuthorityServerWrapper{inner}
-}
-
-func (cas *CertificateAuthorityServerWrapper) IssuePrecertificate(ctx context.Context, request *capb.IssueCertificateRequest) (*capb.IssuePrecertificateResponse, error) {
-	return cas.inner.IssuePrecertificate(ctx, request)
-}
-
-func (cas *CertificateAuthorityServerWrapper) IssueCertificateForPrecertificate(ctx context.Context, req *capb.IssueCertificateForPrecertificateRequest) (*corepb.Certificate, error) {
-	return cas.inner.IssueCertificateForPrecertificate(ctx, req)
-}
-
-func (cas *CertificateAuthorityServerWrapper) GenerateOCSP(ctx context.Context, req *capb.GenerateOCSPRequest) (*capb.OCSPResponse, error) {
-	return cas.inner.GenerateOCSP(ctx, req)
 }
