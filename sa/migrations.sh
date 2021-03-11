ed2e3a2968b807ce17a558f590e21bcf0f532b11
beautifulentropy <hello@entropy.cat>
2021-03-11 12:57:45 -0800

    Squashing Fixes

--- sa/migrations.sh
+++ sa/migrations.sh
@@ -8,11 +8,13 @@ fi
 
 # posix compliant escape sequence
 esc=$'\033'"["
+res="${esc}0m"
 
 #
 # Defaults
 #
 DB_NEXT_PATH="_db-next/migrations"
+DB_PATH="_db/migrations"
 OUTCOME="ERROR"
 PROMOTE=()
 RUN=()
@@ -21,71 +23,92 @@ RUN=()
 # Print Functions
 #
 function print_outcome() {
-  if [ "$OUTCOME" == OK ]
+  if [ "${OUTCOME}" == OK ]
   then
-    echo -e "${esc}0;32;1m""$OUTCOME""${esc}0m"
+    echo -e "${esc}0;32;1m${OUTCOME}${res}"
   else
-    echo -e "${esc}0;31;1m""$OUTCOME""${esc}0m"
+    echo -e "${esc}0;31;1m${OUTCOME}${res}"
   fi
 }
 
 function print_usage_exit() {
-  echo "$USAGE"
+  echo "${USAGE}"
   exit 0
 }
 
+# newline + bold magenta
 function print_heading() {
   echo
-  echo -e "${esc}0;34;1m"$1"${esc}0m"
+  echo -e "${esc}0;34;1m${1}${res}"
 }
 
-function print_from() {
-  echo -e "from: ${esc}0;36;1m"$1"${esc}0m"
+# bold cyan
+function print_moving() {
+  local src=${1}
+  local dest=${2}
+  echo -e "moving:    ${esc}0;36;1m${src}${res}"
+  echo -e "to:        ${esc}0;32;1m${dest}${res}"
 }
 
-function print_to() {
-  echo -e "to:   ${esc}0;32;1m"$1"${esc}0m"
+# bold yellow
+function print_unlinking() {
+  echo -e "unlinking: ${esc}0;33;1m${1}${res}"
 }
 
-#
-# CLI Helper Functions
-#
-function check_arg() {
-  if [ -z "$OPTARG" ]
-  then
-    exit_msg "No arg for --$OPT option, use: -h for help">&2
-  fi
+# bold magenta
+function print_linking () {
+  local from=${1}
+  local to=${2}
+  echo -e "linking:   ${esc}0;35;1m${from} ->${res}"
+  echo -e "to:        ${esc}0;39;1m${to}${res}"
+}
+
+function print_migrations(){
+  iter=1
+  for file in "${migrations[@]}"
+  do
+    echo "${iter}) $(basename -- ${file})"
+    iter=$(expr "${iter}" + 1)
+  done
 }
 
 function exit_msg() {
   # complain to STDERR and exit with error
-  echo "$*" >&2
+  echo "${*}" >&2
   exit 2
 }
 
 #
 # Utility Functions
 #
-function get_migrations() {
-  migrations=( $(find $DB_NEXT_PATH -mindepth 1 -maxdepth 1 -not -path '*/\.*' -type "$1"  2> /dev/null | sort) )
-  if [ -z "${migrations[@]+x}" ]
-  then
-    echo "There are no migrations at path: "\"$DB_NEXT_PATH\"""
-    exit 1
+function get_promotable_migrations() {
+  local migrations=()
+  for file in "${DB_NEXT_PATH}"/*.sql; do
+    [[ -f "${file}" && ! -L "${file}" ]] || continue
+    migrations+=("${file}")
+  done
+  if [[ "${migrations[@]}" ]]; then
+    echo "${migrations[@]}"
+  else
+    exit_msg "There are no promotable migrations at path: "\"${DB_NEXT_PATH}\"""
   fi
 }
 
-function print_migrations(){
-  iter=1
-  for mig_file in "${migrations[@]}"
-  do
-    echo ""$iter") $(basename -- "${mig_file}")"
-    iter=$(expr "$iter" + 1)
+function get_demotable_migrations() {
+  local migrations=()
+  for file in "${DB_NEXT_PATH}"/*.sql; do
+    [[ -L "${file}" ]] || continue
+    migrations+=("${file}")
   done
+  if [[ "${migrations[@]}" ]]; then
+    echo "${migrations[@]}"
+  else
+    exit_msg "There are no demotable migrations at path: "\"${DB_NEXT_PATH}\"""
+  fi
 }
 
 #
-# Main CLI Parser
+# CLI Parser
 #
 USAGE="$(cat -- <<-EOM
 
@@ -112,13 +135,13 @@ while getopts nchpd-: OPT; do
     OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
     OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
   fi
-  case "$OPT" in
+  case "${OPT}" in
     n | list-next )           RUN+=("list_next") ;;
     c | list-current )        RUN+=("list_current") ;;
     p | promote )             RUN+=("promote") ;;
     d | demote )              RUN+=("demote") ;;
     h | help )                print_usage_exit ;;
-    ??* )                     exit_msg "Illegal option --$OPT" ;;  # bad long option
+    ??* )                     exit_msg "Illegal option --${OPT}" ;;  # bad long option
     ? )                       exit 2 ;;  # bad short option (error reported via getopts)
   esac
 done
@@ -128,82 +151,84 @@ shift $((OPTIND-1)) # remove parsed opts and args from $@ list
 trap "print_outcome" EXIT
 
 STEP="list_next"
-if [[ "${RUN[@]}" =~ "$STEP" ]] ; then
+if [[ "${RUN[@]}" =~ "${STEP}" ]] ; then
   print_heading "Next Schemas"
-  get_migrations "f"
-  print_migrations
+  migrations=($(get_promotable_migrations))
+  print_migrations "${migrations[@]}"
 fi
 
 STEP="list_current"
-if [[ "${RUN[@]}" =~ "$STEP" ]] ; then
+if [[ "${RUN[@]}" =~ "${STEP}" ]] ; then
   print_heading "Current Schemas"
-  get_migrations "l"
-  print_migrations
+  migrations=($(get_demotable_migrations))
+  print_migrations "${migrations[@]}"
 fi
 
 STEP="promote"
-if [[ "${RUN[@]}" =~ "$STEP" ]] ; then
+if [[ "${RUN[@]}" =~ "${STEP}" ]] ; then
   print_heading "Promote Schema"
-  get_migrations "f"
+  migrations=($(get_promotable_migrations))
   declare -a mig_index=()
   declare -A mig_file=()
   for i in "${!migrations[@]}"; do
-    mig_index[$i]="${migrations[$i]%% *}"
-    mig_file[${mig_index[$i]}]="${migrations[$i]#* }"
+    mig_index["$i"]="${migrations[$i]%% *}"
+    mig_file["${mig_index[$i]}"]="${migrations[$i]#* }"
   done
 
   promote=""
   PS3='Which schema would you like to promote? (q to cancel): '
   
   select opt in "${mig_index[@]}"; do
-    case "$opt" in
+    case "${opt}" in
       "") echo "Invalid option or cancelled, exiting..." ; break ;;
-      *)  promote="${mig_file[$opt]}" ; break ;;
+      *)  mig_file_path="${mig_file[$opt]}" ; break ;;
     esac
   done
-  if [ ! -z "$promote" ]
+  if [[ "${mig_file_path}" ]]
   then
-    schema_name="$(basename -- "$promote")"
-    promoted_path="_db/migrations/"$schema_name""
-
     print_heading "Promoting Schema"
-    print_from "$promote"
-    print_to "$promoted_path"
-    mv "$promote" "$promoted_path"
-    ln -s "$(realpath --relative-to="$DB_NEXT_PATH" "$promoted_path")" "$DB_NEXT_PATH"
+    promote_mig_name="$(basename -- "${mig_file_path}")"
+    promoted_mig_file_path="${DB_PATH}/${promote_mig_name}"
+    symlink_relpath="$(realpath --relative-to=${DB_NEXT_PATH} ${promoted_mig_file_path})"
+
+    print_moving "${mig_file_path}" "${promoted_mig_file_path}"
+    mv "${mig_file_path}" "${promoted_mig_file_path}"
+    
+    print_linking "${mig_file_path}" "${symlink_relpath}"
+    ln -s "${symlink_relpath}" "${DB_NEXT_PATH}"
   fi
 fi
 
 STEP="demote"
-if [[ "${RUN[@]}" =~ "$STEP" ]] ; then
+if [[ "${RUN[@]}" =~ "${STEP}" ]] ; then
   print_heading "Demote Schema"
-  get_migrations "l"
+  migrations=($(get_demotable_migrations))
   declare -a mig_index=()
   declare -A mig_file=()
   for i in "${!migrations[@]}"; do
-    mig_index[$i]="${migrations[$i]%% *}"
-    mig_file[${mig_index[$i]}]="${migrations[$i]#* }"
+    mig_index["$i"]="${migrations[$i]%% *}"
+    mig_file["${mig_index[$i]}"]="${migrations[$i]#* }"
   done
 
-  demote=""
+  demote_mig=""
   PS3='Which schema would you like to demote? (q to cancel): '
   
   select opt in "${mig_index[@]}"; do
-    case "$opt" in
+    case "${opt}" in
       "") echo "Invalid option or cancelled, exiting..." ; break ;;
-      *)  demote="${mig_file[$opt]}" ; break ;;
+      *)  mig_link_path="${mig_file[$opt]}" ; break ;;
     esac
   done
-  if [ ! -z "$demote" ]
+  if [[ "${mig_link_path}" ]]
   then
-    schema_name="$(basename -- "$demote")"
-    promoted_path="_db/migrations/"$schema_name""
-
     print_heading "Demoting Schema"
-    print_from "_db/migrations/"$schema_name""
-    print_to "$demote"
-    rm "$demote"
-    mv "$promoted_path" "$demote"
+    demote_mig_name="$(basename -- "${mig_link_path}")"
+    demote_mig_from="${DB_PATH}/${demote_mig_name}"
+
+    print_unlinking "${mig_link_path}"
+    rm "${mig_link_path}"
+    print_moving "${demote_mig_from}" "${mig_link_path}"
+    mv "${demote_mig_from}" "${mig_link_path}"
   fi
 fi
 
--- test/create_db.sh
+++ test/create_db.sh
@@ -5,32 +5,54 @@ source test/db-common.sh
 
 # posix compliant escape sequence
 esc=$'\033'"["
+res="${esc}0m"
+
 
 function print_heading() {
   echo
-  echo -e "${esc}0;34;1m"$1"${esc}0m"
+  # newline + bold magenta
+  echo -e "${esc}0;34;1m${1}${res}"
+}
+
+function exit_msg() {
+  # complain to STDERR and exit with error
+  echo "${*}" >&2
+  exit 2
 }
 
 function get_migrations() {
-  migrations=( $(find "$dbpath"/migrations -mindepth 1 -maxdepth 1 -not -path '*/\.*'  2> /dev/null | sort) )
+  local db_schemas_path="${1}"
+  local migrations=()
+  for file in "${db_schemas_path}"/*.sql; do
+    [[ -f "${file}" ]] || continue
+    migrations+=("${file}")
+  done
+  if [[ "${migrations[@]}" ]]; then
+    echo "${migrations[@]}"
+  else
+    exit_msg "There are no migrations at path: "\"${db_schemas_path}\"""
+  fi
 }
 
 function create_empty_db() {
+  local db="${1}"
+  local dbconn="${2}"
   create_script="drop database if exists \`${db}\`; create database if not exists \`${db}\`;"
-  mysql $dbconn -e "$create_script" || die "unable to create ${db}"
-  echo "Created empty "$db" database"
-}
-
-function get_container_version() {
-  container_version="$(goose -path="$dbpath" -env="$dbenv" dbversion | sed 's/goose: dbversion //')"
+  mysql ${dbconn} -e "${create_script}" || die "unable to create ${db}"
+  echo "created empty "$db" database"
 }
 
 function apply_migrations() {
-  if [ ! -z "${migrations[@]+x}" ]
+  local migrations="${1}"
+  local dbpath="${2}"
+  local dbenv="${3}"
+  local db="${4}"
+  if [[ "${migrations[@]}" ]]
   then
-    goose -path="$dbpath" -env=$dbenv up || die "unable to migrate "${db}" with "$dbpath""
+    echo "applying migrations from ${db_mig_path}"
+    goose -path="${dbpath}" -env="${dbenv}" up || die "unable to migrate ${db} with ${dbpath}"
   else
-    echo "no migrations at "$dbpath""
+    echo "no migrations at ${dbpath}"
   fi
 }
 
@@ -52,48 +74,51 @@ mysql $dbconn -e "SET GLOBAL max_connections = 500;"
 
 for dbenv in $DBENVS; do
   db="boulder_sa_${dbenv}"
-
-  print_heading "Checking if "$db" exists"
-  if mysql $dbconn -e 'show databases;' | grep $db > /dev/null; then
-    echo ""$db" already exists - skipping create"
+  print_heading "Checking if ${db} exists"
+  if mysql ${dbconn} -e 'show databases;' | grep "${db}" > /dev/null; then
+    echo "${db} already exists - skipping create"
   else
-    echo ""$db" doesn't exist - creating"
-    create_empty_db
+    echo "${db} doesn't exist - creating"
+    create_empty_db "${db}" "${dbconn}"
   fi
 
-  # determine which db_path to use
-  if [[ "$BOULDER_CONFIG_DIR" = "test/config-next" ]]
+  # Determine which $dbpath and $db_mig_path to use.
+  if [[ "${BOULDER_CONFIG_DIR}" == "test/config-next" ]]
   then
-    dbpath="./sa/_db-next/"
-    migrations="./sa/_db-next/migrations"
+    dbpath="./sa/_db-next"
   else
-    dbpath="./sa/_db/"
-    migrationspath="./sa/_db/migrations"
+    dbpath="./sa/_db"
   fi
+  db_mig_path="${dbpath}/migrations"
 
-  # populate list of migration files for $dbpath
-  get_migrations
+  # Populate an array with schema files present at $dbpath.
+  migrations=($(get_migrations "${db_mig_path}"))
 
-  # goose up to the latest schema present
-  apply_migrations
+  # Goose up, this will work if there are schema files present at
+  # $dbpath with a newer timestamp than the current goose dbversion.
+  apply_migrations "${migrations}" "${dbpath}" "${dbenv}" "${db}"
 
-  # latest schema is the last in the array of 
-  latest_schema="$(basename -- "${migrations[-1]}")"
+  # The (actual) latest migration should always be the last file or
+  # symlink at $db_mig_path.
+  latest_mig_path_filename="$(basename -- "${migrations[-1]}")"
 
-  # latest version is the timestamp contained in the first 14 characters
-  # of the latest filename
-  latest_version="${latest_schema:0:14}"
+  # Goose's dbversion is the timestamp (first 14 characters) of the file
+  # that it last migrated to. We can figure out which goose dbversion we
+  # should be on by parsing the timestamp of the latest file at
+  # $db_mig_path.
+  latest_db_mig_version="${latest_mig_path_filename:0:14}"
   
-  # get the version of the db running in the container
-  get_container_version
+  # Ask Goose the timestamp (dbversion) our database is currently
+  # migrated to.
+  goose_dbversion="$(goose -path=${dbpath} -env=${dbenv} dbversion | sed 's/goose: dbversion //')"
 
-  # if the container_version does not match the latest schema, trigger
-  # recreate
-  if [ $latest_version != $container_version ]; then
-    print_heading "Detected schema mismatch"
-    echo "dropping and recreating from schema at "$migrationspath""
-    create_empty_db
-    apply_migrations
+  # If the $goose_dbversion does not match the $latest_in_db_mig_path,
+  # trigger recreate
+  if [[ "${latest_db_mig_version}" != "${goose_dbversion}" ]]; then
+    print_heading "Detected latest migration version mismatch"
+    echo "dropping and recreating from migrations at ${db_mig_path}"
+    create_empty_db "${db}" "${dbconn}"
+    apply_migrations "${migrations}" "${dbpath}" "${dbenv}" "${db}"
   fi
 
   # With MYSQL_CONTAINER, patch the GRANT statements to
