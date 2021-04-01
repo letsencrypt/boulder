afbc86331e80d9affad129b9a6dc06cd073b8326
beautifulentropy <hello@entropy.cat>
2021-04-01 16:54:21 -0700

    publisher: count errors by type of request

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -166,6 +166,7 @@ type ctSubmissionRequest struct {
 type pubMetrics struct {
 	submissionLatency *prometheus.HistogramVec
 	probeLatency      *prometheus.HistogramVec
+	countErrors       *prometheus.CounterVec
 }
 
 func initMetrics(stats prometheus.Registerer) *pubMetrics {
@@ -189,10 +190,16 @@ func initMetrics(stats prometheus.Registerer) *pubMetrics {
 	)
 	stats.MustRegister(probeLatency)
 
-	return &pubMetrics{
-		submissionLatency: submissionLatency,
-		probeLatency:      probeLatency,
-	}
+	countErrors := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "ct_errors_count",
+			Help: "Count of errors by type",
+		},
+		[]string{"type"},
+	)
+	stats.MustRegister(countErrors)
+
+	return &pubMetrics{submissionLatency, probeLatency, countErrors}
 }
 
 // Impl defines a Publisher
@@ -310,6 +317,11 @@ func (pub *Impl) singleLogSubmit(
 			"status":      status,
 			"http_status": httpStatus,
 		}).Observe(took)
+		if isPrecert {
+			pub.metrics.countErrors.WithLabelValues("precert").Inc()
+		} else {
+			pub.metrics.countErrors.WithLabelValues("final").Inc()
+		}
 		return nil, err
 	}
 	pub.metrics.submissionLatency.With(prometheus.Labels{
