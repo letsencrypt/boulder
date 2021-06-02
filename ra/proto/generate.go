12f1d8ceb8563f3862449d9fef4194b50c4ea219
Aaron Gable <aaron@letsencrypt.org>
2021-06-02 15:35:02 -0700

    Replace generate.go files with symlinks

--- ra/proto/generate.go
+++ ra/proto/generate.go
@@ -1,3 +1,1 @@
-package proto
-
-//go:generate sh -c "cd ../.. && protoc -I ra/proto/ -I . --go_out=ra/proto --go-grpc_out=ra/proto --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative ra/proto/ra.proto"
+../../grpc/gen.go.tmpl
--- sa/proto/generate.go
+++ sa/proto/generate.go
@@ -1,3 +1,1 @@
-package proto
-
-//go:generate sh -c "cd ../.. && protoc -I sa/proto/ -I . --go_out=sa/proto --go-grpc_out=sa/proto --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative sa/proto/sa.proto"
+../../grpc/gen.go.tmpl
