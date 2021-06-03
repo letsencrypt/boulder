15d7af653baeb09445b90bb4d522f58e0c26a582
Aaron Gable <aaron@letsencrypt.org>
2021-06-03 11:08:55 -0700

    Undo grpc test changes

--- grpc/errors_test.go
+++ grpc/errors_test.go
@@ -11,18 +11,19 @@ import (
 
 	"github.com/jmhodges/clock"
 	berrors "github.com/letsencrypt/boulder/errors"
-	testpb "github.com/letsencrypt/boulder/grpc/proto"
+	"github.com/letsencrypt/boulder/grpc/test_proto"
+	testproto "github.com/letsencrypt/boulder/grpc/test_proto"
 	"github.com/letsencrypt/boulder/identifier"
 	"github.com/letsencrypt/boulder/metrics"
 	"github.com/letsencrypt/boulder/test"
 )
 
 type errorServer struct {
-	testpb.UnimplementedChillerServer
+	test_proto.UnimplementedChillerServer
 	err error
 }
 
-func (s *errorServer) Chill(_ context.Context, _ *testpb.Time) (*testpb.Time, error) {
+func (s *errorServer) Chill(_ context.Context, _ *testproto.Time) (*testproto.Time, error) {
 	return nil, s.err
 }
 
@@ -32,7 +33,7 @@ func TestErrorWrapping(t *testing.T) {
 	ci := clientInterceptor{time.Second, NewClientMetrics(metrics.NoopRegisterer), clock.NewFake()}
 	srv := grpc.NewServer(grpc.UnaryInterceptor(si.intercept))
 	es := &errorServer{}
-	testpb.RegisterChillerServer(srv, es)
+	testproto.RegisterChillerServer(srv, es)
 	lis, err := net.Listen("tcp", "127.0.0.1:")
 	test.AssertNotError(t, err, "Failed to create listener")
 	go func() { _ = srv.Serve(lis) }()
@@ -44,10 +45,10 @@ func TestErrorWrapping(t *testing.T) {
 		grpc.WithUnaryInterceptor(ci.intercept),
 	)
 	test.AssertNotError(t, err, "Failed to dial grpc test server")
-	client := testpb.NewChillerClient(conn)
+	client := testproto.NewChillerClient(conn)
 
 	es.err = berrors.MalformedError("yup")
-	_, err = client.Chill(context.Background(), &testpb.Time{})
+	_, err = client.Chill(context.Background(), &testproto.Time{})
 	test.Assert(t, err != nil, fmt.Sprintf("nil error returned, expected: %s", err))
 	test.AssertDeepEquals(t, err, es.err)
 
@@ -63,7 +64,7 @@ func TestSubErrorWrapping(t *testing.T) {
 	ci := clientInterceptor{time.Second, NewClientMetrics(metrics.NoopRegisterer), clock.NewFake()}
 	srv := grpc.NewServer(grpc.UnaryInterceptor(si.intercept))
 	es := &errorServer{}
-	testpb.RegisterChillerServer(srv, es)
+	testproto.RegisterChillerServer(srv, es)
 	lis, err := net.Listen("tcp", "127.0.0.1:")
 	test.AssertNotError(t, err, "Failed to create listener")
 	go func() { _ = srv.Serve(lis) }()
@@ -75,7 +76,7 @@ func TestSubErrorWrapping(t *testing.T) {
 		grpc.WithUnaryInterceptor(ci.intercept),
 	)
 	test.AssertNotError(t, err, "Failed to dial grpc test server")
-	client := testpb.NewChillerClient(conn)
+	client := testproto.NewChillerClient(conn)
 
 	subErrors := []berrors.SubBoulderError{
 		{
@@ -92,7 +93,7 @@ func TestSubErrorWrapping(t *testing.T) {
 		Detail: "malformed chill req",
 	}).WithSubErrors(subErrors)
 
-	_, err = client.Chill(context.Background(), &testpb.Time{})
+	_, err = client.Chill(context.Background(), &testproto.Time{})
 	test.Assert(t, err != nil, fmt.Sprintf("nil error returned, expected: %s", err))
 	test.AssertDeepEquals(t, err, es.err)
 }
