21bebd26fc10da4ff25c68a4190a9cdeee6f64b2
Samantha Frank <hello@entropy.cat>
2021-06-23 12:27:08 -0700

    Addressing comments

--- sa/model_test.go
+++ sa/model_test.go
@@ -1,25 +1,20 @@
 package sa
 
 import (
-	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/base64"
-	"encoding/json"
 	"math/big"
-	"net"
 	"testing"
 	"time"
 
 	"github.com/jmhodges/clock"
 	"github.com/letsencrypt/boulder/db"
 	"github.com/letsencrypt/boulder/grpc"
-	"github.com/letsencrypt/boulder/metrics"
 	"github.com/letsencrypt/boulder/probs"
 	"github.com/letsencrypt/boulder/test/vars"
-	jose "gopkg.in/square/go-jose.v2"
 
 	"github.com/letsencrypt/boulder/core"
 	corepb "github.com/letsencrypt/boulder/core/proto"
@@ -246,16 +241,16 @@ func TestCerficatesTableContainsDuplicateSerials(t *testing.T) {
 
 	serialString := core.SerialToString(big.NewInt(1337))
 
-	// Register and insert a certificate with a serial of `1337`.
-	testCtx.setupCertificate1337A(t)
+	// Insert a certificate with a serial of `1337`.
+	testCtx.insertCertificate(t, "1337.com", 1337, 1)
 
 	// This should return the certificate that we just inserted.
 	_, err := SelectCertificate(testCtx.db, serialString)
 	test.AssertNotError(t, err, "received an error for a valid query")
 
-	// Register and insert a certificate with a serial of `1337` but for a
-	// different hostname.
-	testCtx.setupCertificate1337B(t)
+	// Insert a certificate with a serial of `1337` but for a different
+	// hostname.
+	testCtx.insertCertificate(t, "1337.net", 1337, 1)
 
 	// With a duplicate being present, this should error.
 	_, err = SelectCertificate(testCtx.db, serialString)
@@ -264,51 +259,25 @@ func TestCerficatesTableContainsDuplicateSerials(t *testing.T) {
 
 type testCtx struct {
 	db      *db.WrappedMap
-	ssa     core.StorageAdder
+	ssa     *SQLStorageAuthority
 	cleanUp func()
-}
-
-func (c testCtx) registerCertificate(t *testing.T, emailRaw string, jsonKey []byte, regID int64) {
-	var key jose.JSONWebKey
-	err := json.Unmarshal(jsonKey, &key)
-	test.AssertNotError(t, err, "Failed to unmarshal public JWK")
-
-	var reg core.Registration
-	email := "mailto:" + emailRaw
-	reg = core.Registration{
-		ID: regID,
-		Contact: &[]string{
-			email,
-		},
-		Key:       &key,
-		InitialIP: net.ParseIP("127.0.0.1"),
-	}
-	ctx := context.Background()
-	_, err = c.ssa.NewRegistration(ctx, reg)
-	test.AssertNotError(t, err, "Couldn't complete registration")
+	fc      clock.FakeClock
 }
 
 func (ctx testCtx) insertCertificate(t *testing.T, hostname string, serial, regID int64) {
 	serialBigInt := big.NewInt(serial)
 	serialString := core.SerialToString(serialBigInt)
 
-	fc := newFakeClock(t)
-
 	rawCert := x509.Certificate{
 		Subject: pkix.Name{
 			CommonName: "leet",
 		},
-		NotAfter:     fc.Now().Add(30 * 24 * time.Hour),
+		NotAfter:     ctx.fc.Now().Add(30 * 24 * time.Hour),
 		DNSNames:     []string{hostname},
 		SerialNumber: serialBigInt,
 	}
 
-	n := bigIntFromB64("n4EPtAOCc9AlkeQHPzHStgAbgs7bTZLwUBZdR8_KuKPEHLd4rHVTeT-O-XV2jRojdNhxJWTDvNd7nqQ0VEiZQHz_AJmSCpMaJMRBSFKrKb2wqVwGU_NsYOYL-QtiWN2lbzcEe6XC0dApr5ydQLrHqkHHig3RBordaZ6Aj-oBHqFEHYpPe7Tpe-OfVfHd1E6cS6M1FZcD1NNLYD5lFHpPI9bTwJlsde3uhGqC0ZCuEHg8lhzwOHrtIQbS0FVbb9k3-tVTU4fg_3L_vniUFAKwuCLqKnS2BYwdq_mzSnbLY7h_qixoR7jig3__kRhuaxwUkRz5iaiQkqgc5gHdrNP5zw==")
-	e := intFromB64("AQAB")
-	d := bigIntFromB64("bWUC9B-EFRIo8kpGfh0ZuyGPvMNKvYWNtB_ikiH9k20eT-O1q_I78eiZkpXxXQ0UTEs2LsNRS-8uJbvQ-A1irkwMSMkK1J3XTGgdrhCku9gRldY7sNA_AKZGh-Q661_42rINLRCe8W-nZ34ui_qOfkLnK9QWDDqpaIsA-bMwWWSDFu2MUBYwkHTMEzLYGqOe04noqeq1hExBTHBOBdkMXiuFhUq1BU6l-DqEiWxqg82sXt2h-LMnT3046AOYJoRioz75tSUQfGCshWTBnP5uDjd18kKhyv07lhfSJdrPdM5Plyl21hsFf4L_mHCuoFau7gdsPfHPxxjVOcOpBrQzwQ==")
-	p := bigIntFromB64("uKE2dh-cTf6ERF4k4e_jy78GfPYUIaUyoSSJuBzp3Cubk3OCqs6grT8bR_cu0Dm1MZwWmtdqDyI95HrUeq3MP15vMMON8lHTeZu2lmKvwqW7anV5UzhM1iZ7z4yMkuUwFWoBvyY898EXvRD-hdqRxHlSqAZ192zB3pVFJ0s7pFc=")
-	q := bigIntFromB64("uKE2dh-cTf6ERF4k4e_jy78GfPYUIaUyoSSJuBzp3Cubk3OCqs6grT8bR_cu0Dm1MZwWmtdqDyI95HrUeq3MP15vMMON8lHTeZu2lmKvwqW7anV5UzhM1iZ7z4yMkuUwFWoBvyY898EXvRD-hdqRxHlSqAZ192zB3pVFJ0s7pFc=")
-	testKey := rsa.PrivateKey{PublicKey: rsa.PublicKey{N: n, E: e}, D: d, Primes: []*big.Int{p, q}}
+	testKey := makeKey()
 
 	certDer, _ := x509.CreateCertificate(rand.Reader, &rawCert, &rawCert, &testKey.PublicKey, &testKey)
 	cert := &core.Certificate{
@@ -321,44 +290,12 @@ func (ctx testCtx) insertCertificate(t *testing.T, hostname string, serial, regI
 	test.AssertNotError(t, err, "Couldn't insert certificate")
 }
 
-// setupCertificate1337A registers and inserts a record into the `certificates`
-// table with a domain of `1337.com`, `registrationID` of `1` and a `serial` of
-// `1337`.
-func (c testCtx) setupCertificate1337A(t *testing.T) {
-	jsonKey := []byte(`
-{
-  "kty":"RSA",
-  "n":"0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
-  "e":"AQAB"
-}`)
-	c.registerCertificate(t, "test@example.com", jsonKey, 1)
-	c.insertCertificate(t, "1337.com", 1337, 1)
-}
-
-// setupCertificate1337B registers and inserts a record into the `certificates`
-// table with a domain of `1337.net`, `registrationID` of `1` and a `serial` of
-// `1337`.
-func (c testCtx) setupCertificate1337B(t *testing.T) {
-	jsonKey := []byte(`
-{
-  "kty":"RSA",
-  "n":"z8bp-jPtHt4lKBqepeKF28g_QAEOuEsCIou6sZ9ndsQsEjxEOQxQ0xNOQezsKa63eogw8YS3vzjUcPP5BJuVzfPfGd5NVUdT-vSSwxk3wvk_jtNqhrpcoG0elRPQfMVsQWmxCAXCVRz3xbcFI8GTe-syynG3l-g1IzYIIZVNI6jdljCZML1HOMTTW4f7uJJ8mM-08oQCeHbr5ejK7O2yMSSYxW03zY-Tj1iVEebROeMv6IEEJNFSS4yM-hLpNAqVuQxFGetwtwjDMC1Drs1dTWrPuUAAjKGrP151z1_dE74M5evpAhZUmpKv1hY-x85DC6N0hFPgowsanmTNNiV75w",
-  "e":"AQAB"
-}`)
-	c.registerCertificate(t, "test@example.com", jsonKey, 1)
-	c.insertCertificate(t, "1337.net", 1337, 1)
-}
-
 func setup(t *testing.T) testCtx {
 	dbMap, err := NewDbMap(vars.DBConnSAFullPerms, DbSettings{})
 	test.AssertNotError(t, err, "Couldn't create database connection")
 
-	cleanUp := test.ResetSATestDatabase(t)
-	fc := newFakeClock(t)
-
-	ssa, err := NewSQLStorageAuthority(dbMap, fc, log, metrics.NoopRegisterer, 1)
-	test.AssertNotError(t, err, "Unable to create SQLStorageAuthority")
-	return testCtx{dbMap, ssa, cleanUp}
+	ssa, fc, cleanUp := initSA(t)
+	return testCtx{dbMap, ssa, cleanUp, fc}
 }
 
 func bigIntFromB64(b64 string) *big.Int {
@@ -372,11 +309,11 @@ func intFromB64(b64 string) int {
 	return int(bigIntFromB64(b64).Int64())
 }
 
-func newFakeClock(t *testing.T) clock.FakeClock {
-	const fakeTimeFormat = "2006-01-02T15:04:05.999999999Z"
-	ft, err := time.Parse(fakeTimeFormat, fakeTimeFormat)
-	test.AssertNotError(t, err, "Couldn't create fake clock")
-	fc := clock.NewFake()
-	fc.Set(ft.UTC())
-	return fc
+func makeKey() rsa.PrivateKey {
+	n := bigIntFromB64("n4EPtAOCc9AlkeQHPzHStgAbgs7bTZLwUBZdR8_KuKPEHLd4rHVTeT-O-XV2jRojdNhxJWTDvNd7nqQ0VEiZQHz_AJmSCpMaJMRBSFKrKb2wqVwGU_NsYOYL-QtiWN2lbzcEe6XC0dApr5ydQLrHqkHHig3RBordaZ6Aj-oBHqFEHYpPe7Tpe-OfVfHd1E6cS6M1FZcD1NNLYD5lFHpPI9bTwJlsde3uhGqC0ZCuEHg8lhzwOHrtIQbS0FVbb9k3-tVTU4fg_3L_vniUFAKwuCLqKnS2BYwdq_mzSnbLY7h_qixoR7jig3__kRhuaxwUkRz5iaiQkqgc5gHdrNP5zw==")
+	e := intFromB64("AQAB")
+	d := bigIntFromB64("bWUC9B-EFRIo8kpGfh0ZuyGPvMNKvYWNtB_ikiH9k20eT-O1q_I78eiZkpXxXQ0UTEs2LsNRS-8uJbvQ-A1irkwMSMkK1J3XTGgdrhCku9gRldY7sNA_AKZGh-Q661_42rINLRCe8W-nZ34ui_qOfkLnK9QWDDqpaIsA-bMwWWSDFu2MUBYwkHTMEzLYGqOe04noqeq1hExBTHBOBdkMXiuFhUq1BU6l-DqEiWxqg82sXt2h-LMnT3046AOYJoRioz75tSUQfGCshWTBnP5uDjd18kKhyv07lhfSJdrPdM5Plyl21hsFf4L_mHCuoFau7gdsPfHPxxjVOcOpBrQzwQ==")
+	p := bigIntFromB64("uKE2dh-cTf6ERF4k4e_jy78GfPYUIaUyoSSJuBzp3Cubk3OCqs6grT8bR_cu0Dm1MZwWmtdqDyI95HrUeq3MP15vMMON8lHTeZu2lmKvwqW7anV5UzhM1iZ7z4yMkuUwFWoBvyY898EXvRD-hdqRxHlSqAZ192zB3pVFJ0s7pFc=")
+	q := bigIntFromB64("uKE2dh-cTf6ERF4k4e_jy78GfPYUIaUyoSSJuBzp3Cubk3OCqs6grT8bR_cu0Dm1MZwWmtdqDyI95HrUeq3MP15vMMON8lHTeZu2lmKvwqW7anV5UzhM1iZ7z4yMkuUwFWoBvyY898EXvRD-hdqRxHlSqAZ192zB3pVFJ0s7pFc=")
+	return rsa.PrivateKey{PublicKey: rsa.PublicKey{N: n, E: e}, D: d, Primes: []*big.Int{p, q}}
 }
