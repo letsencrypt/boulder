0e6d01feed9b935856b1fb0538f6bae756104f83
Andrew Gabbitas <agabbitas@letsencrypt.org>
2021-07-30 14:04:56 -0600

    Fix ra tests
    Remove one of the tests in TestNewCertificate that checks for mismatched
    case. It is no longer trivial to change the CSR common name as was being
    done in this test. There is plenty of coverage for this test case in the
    ACMEv2 code so it is being removed here.

--- ra/ra_test.go
+++ ra/ra_test.go
@@ -1166,15 +1166,9 @@ func TestAuthorizationRequired(t *testing.T) {
 	exp := ra.clk.Now().Add(365 * 24 * time.Hour)
 	_ = createFinalizedAuthorization(t, sa, "not-example.com", exp, "valid")
 
-	// ExampleCSR requests not-example.com and www.not-example.com,
-	// but the authorization only covers not-example.com
-	certRequest := core.CertificateRequest{
-		CSR: ExampleCSR,
-	}
-
 	_, err := ra.NewCertificate(ctx,
 		&rapb.NewCertificateRequest{
-			Csr:          certRequest.Bytes,
+			Csr:          ExampleCSR.Raw,
 			RegID:        1,
 			IssuerNameID: 1,
 		})
@@ -1190,33 +1184,18 @@ func TestNewCertificate(t *testing.T) {
 	_ = createFinalizedAuthorization(t, sa, "www.not-example.com", exp, "valid")
 
 	// Check that we fail if the CSR signature is invalid
-	ExampleCSR.Signature[0]++
-
+	ExampleCSR.Raw[len(ExampleCSR.Raw)-1]++
+	fmt.Println(ExampleCSR.Subject.CommonName)
+	fmt.Println(ExampleCSR.DNSNames)
 	_, err := ra.NewCertificate(ctx,
 		&rapb.NewCertificateRequest{
 			Csr:          ExampleCSR.Raw,
 			RegID:        Registration.ID,
 			IssuerNameID: 1,
 		})
-	ExampleCSR.Signature[0]--
+	ExampleCSR.Raw[len(ExampleCSR.Raw)-1]--
 	test.AssertError(t, err, "Failed to check CSR signature")
-
-	// Check that we don't fail on case mismatches
-	ExampleCSR.Subject.CommonName = "www.NOT-example.com"
-
-	certPB, err := ra.NewCertificate(ctx,
-		&rapb.NewCertificateRequest{
-			Csr:          ExampleCSR.Raw,
-			RegID:        Registration.ID,
-			IssuerNameID: 1,
-		})
-	test.AssertNotError(t, err, "Failed to issue certificate")
-	cert, err := bgrpc.PBToCert(certPB)
-	test.AssertNotError(t, err, "Failed to deserialize cert")
-	cert2, _ := x509.ParseCertificate(cert.DER)
-	fmt.Println(cert2.Subject)
-	_, err = x509.ParseCertificate(cert.DER)
-	test.AssertNotError(t, err, "Failed to parse certificate")
+	test.AssertEquals(t, err.Error(), "invalid signature on CSR")
 }
 
 func TestAuthzRateLimiting(t *testing.T) {
