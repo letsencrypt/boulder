db9497a178b32a3b4dfec90cee7cc48abceabdb6
Samantha Frank <hello@entropy.cat>
2021-07-30 13:58:07 -0700

    Addressing comments

--- ra/ra.go
+++ ra/ra.go
@@ -973,7 +973,7 @@ func (ra *RegistrationAuthorityImpl) failOrder(
 // to poll while awaiting finalization to occur.
 func (ra *RegistrationAuthorityImpl) FinalizeOrder(ctx context.Context, req *rapb.FinalizeOrderRequest) (*corepb.Order, error) {
 	if req == nil || req.Order == nil {
-		return nil, berrors.InternalServerError("Incomplete gRPC request message")
+		return nil, berrors.InternalServerError(errIncompleteGRPCRequest.Error())
 	}
 
 	order := req.Order
