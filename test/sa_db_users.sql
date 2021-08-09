788a00b11fb62fb12d4cd066f664a34b994abf7b
Aaron Gable <aaron@letsencrypt.org>
2021-08-09 15:04:30 -0700

    Delete expired-authz-purger2

--- test/sa_db_users.sql
+++ test/sa_db_users.sql
@@ -13,7 +13,6 @@ CREATE USER IF NOT EXISTS 'cert_checker'@'localhost';
 CREATE USER IF NOT EXISTS 'ocsp_update'@'localhost';
 CREATE USER IF NOT EXISTS 'ocsp_update_ro'@'localhost';
 CREATE USER IF NOT EXISTS 'test_setup'@'localhost';
-CREATE USER IF NOT EXISTS 'purger'@'localhost';
 CREATE USER IF NOT EXISTS 'janitor'@'localhost';
 CREATE USER IF NOT EXISTS 'badkeyrevoker'@'localhost';
 
@@ -75,9 +74,6 @@ GRANT SELECT ON fqdnSets TO 'mailer'@'localhost';
 
 -- Cert checker
 GRANT SELECT ON certificates TO 'cert_checker'@'localhost';
-
--- Expired authorization purger
-GRANT SELECT,DELETE ON authz2 TO 'purger'@'localhost';
 
 -- Janitor
 GRANT SELECT,DELETE ON certificates TO 'janitor'@'localhost';
