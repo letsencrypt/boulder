73a877dbbe27510c6a13f33f8335771a7b9f3b43
Aaron Gable <aaron@letsencrypt.org>
2021-08-11 14:08:14 -0700

    FREEZE.unindexed

--- cmd/caa-log-checker/main.go
+++ cmd/caa-log-checker/main.go
@@ -57,16 +57,17 @@ func parseTimestamp(line string) (time.Time, error) {
 
 // loadIssuanceLog processes a single issuance (RA) log file. It returns a map
 // of names to slices of timestamps at which certificates for those names were
-// issued.
-// TODO: plumb through earliest and latest for parity with old implementation.
-func loadIssuanceLog(path string) (map[string][]time.Time, error) {
+// issued. It also returns the earliest and latest timestamps seen, to allow
+// CAA log processing to quickly skip irrelevant entries.
+func loadIssuanceLog(path string) (map[string][]time.Time, time.Time, time.Time, error) {
 	scanner, err := openFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open %q: %w", path, err)
+		return nil, time.Time{}, time.Time{}, fmt.Errorf("failed to open %q: %w", path, err)
 	}
 
 	linesCount := 0
-	issuancesCount := 0
+	earliest := time.Time{}
+	latest := time.Time{}
 
 	issuanceMap := map[string][]time.Time{}
 	for scanner.Scan() {
@@ -78,13 +79,13 @@ func loadIssuanceLog(path string) (map[string][]time.Time, error) {
 			continue
 		}
 		if len(matches) != 2 {
-			return nil, fmt.Errorf("line %d: unexpected number of regex matches", linesCount)
+			return nil, earliest, latest, fmt.Errorf("line %d: unexpected number of regex matches", linesCount)
 		}
 
 		var ie issuanceEvent
 		err := json.Unmarshal([]byte(matches[1]), &ie)
 		if err != nil {
-			return nil, fmt.Errorf("line %d: failed to unmarshal JSON: %w", linesCount, err)
+			return nil, earliest, latest, fmt.Errorf("line %d: failed to unmarshal JSON: %w", linesCount, err)
 		}
 
 		// Populate the issuance time from the syslog timestamp, rather than the
@@ -93,19 +94,24 @@ func loadIssuanceLog(path string) (map[string][]time.Time, error) {
 		// generated on the same system, they should be tightly coupled anyway.
 		ie.issuanceTime, err = parseTimestamp(line)
 		if err != nil {
-			return nil, fmt.Errorf("line %d: failed to parse timestamp: %w", linesCount, err)
+			return nil, earliest, latest, fmt.Errorf("line %d: failed to parse timestamp: %w", linesCount, err)
 		}
 
-		issuancesCount++
+		if earliest.IsZero() || ie.issuanceTime.Before(earliest) {
+			earliest = ie.issuanceTime
+		}
+		if latest.IsZero() || ie.issuanceTime.After(latest) {
+			latest = ie.issuanceTime
+		}
 		for _, name := range ie.Names {
 			issuanceMap[name] = append(issuanceMap[name], ie.issuanceTime)
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		return nil, err
+		return nil, earliest, latest, err
 	}
 
-	return issuanceMap, nil
+	return issuanceMap, earliest, latest, nil
 }
 
 // processCAALog processes a single CAA (VA) log file. It modifies the input map
@@ -113,7 +119,7 @@ func loadIssuanceLog(path string) (map[string][]time.Time, error) {
 // timestamps which are covered by (i.e. less than 8 hours after) a CAA check
 // for that name in the log file. It also prunes any names whose slice of
 // issuance times becomes empty.
-func processCAALog(path string, issuances map[string][]time.Time) error {
+func processCAALog(path string, issuances map[string][]time.Time, earliest time.Time, latest time.Time, tolerance time.Duration) error {
 	scanner, err := openFile(path)
 	if err != nil {
 		return fmt.Errorf("failed to open %q: %w", path, err)
@@ -140,9 +146,15 @@ func processCAALog(path string, issuances map[string][]time.Time) error {
 			return fmt.Errorf("line %d: failed to parse timestamp: %w", linesCount, err)
 		}
 
+		// Don't bother processing rows that definitely fall outside the period we
+		// care about.
+		if checkTime.After(latest) || checkTime.Before(earliest.Add(-8*time.Hour)) {
+			continue
+		}
+
 		// TODO: Only remove covered issuance timestamps if the CAA check actually
 		// said that we're allowed to issue (i.e. had "Valid for issuance: true").
-		issuances[name] = removeCoveredTimestamps(issuances[name], checkTime)
+		issuances[name] = removeCoveredTimestamps(issuances[name], checkTime, tolerance)
 		if len(issuances[name]) == 0 {
 			delete(issuances, name)
 		}
@@ -154,7 +166,7 @@ func processCAALog(path string, issuances map[string][]time.Time) error {
 			labels := strings.Split(name, ".")
 			for i := 1; i < len(labels)-1; i++ {
 				tailName := strings.Join(labels[i:], ".")
-				issuances[tailName] = removeCoveredTimestamps(issuances[tailName], checkTime)
+				issuances[tailName] = removeCoveredTimestamps(issuances[tailName], checkTime, tolerance)
 				if len(issuances[tailName]) == 0 {
 					delete(issuances, tailName)
 				}
@@ -167,15 +179,14 @@ func processCAALog(path string, issuances map[string][]time.Time) error {
 
 // removeCoveredTimestamps returns a new slice of timestamps which contains all
 // timestamps that are *not* within 8 hours after the input timestamp.
-// TODO: plumb through time-tolerance to account for slight slop.
-func removeCoveredTimestamps(timestamps []time.Time, cover time.Time) []time.Time {
+func removeCoveredTimestamps(timestamps []time.Time, cover time.Time, tolerance time.Duration) []time.Time {
 	r := make([]time.Time, len(timestamps))
 	for _, ts := range timestamps {
 		// Copy the timestamp into the results slice if it is before the covering
 		// timestamp, or more than 8 hours after the covering timestamp (i.e. if
 		// it is *not* covered by the covering timestamp).
 		diff := ts.Sub(cover)
-		if diff < 0 || diff > 8*time.Hour {
+		if diff < -tolerance || diff > 8*time.Hour+tolerance {
 			ts := ts
 			r = append(r, ts)
 		}
@@ -208,10 +219,8 @@ func main() {
 	raLog := flag.String("ra-log", "", "Path to a single boulder-ra log file")
 	vaLogs := flag.String("va-logs", "", "List of paths to boulder-va logs, separated by commas")
 	timeTolerance := flag.Duration("time-tolerance", 0, "How much slop to allow when comparing timestamps for ordering")
-	earliestFlag := flag.String("earliest", "", "Day at which to start checking issuances "+
-		"(inclusive). Formatted like '20060102' Optional. If specified, -latest is required.")
-	latestFlag := flag.String("latest", "", "Day at which to stop checking issuances "+
-		"(exclusive). Formatted like '20060102'. Optional. If specified, -earliest is required.")
+	earliestFlag := flag.String("earliest", "", "Deprecated.")
+	latestFlag := flag.String("latest", "", "Deprecated.")
 
 	flag.Parse()
 
@@ -222,6 +231,7 @@ func main() {
 	var earliest time.Time
 	var latest time.Time
 	if *earliestFlag != "" || *latestFlag != "" {
+		fmt.Printf("The -earliest and -lastest flags are deprecated and ignored.")
 		if *earliestFlag == "" || *latestFlag == "" {
 			cmd.Fail("-earliest and -latest must be both set or both unset")
 		}
@@ -242,13 +252,13 @@ func main() {
 	})
 
 	// Build a map from hostnames to times at which those names were issued for.
-	issuanceMap, err := loadIssuanceLog(*raLog)
+	issuanceMap, earliest, latest, err := loadIssuanceLog(*raLog)
 	cmd.FailOnError(err, "failed to load issuance logs")
 
 	// Try to pare the issuance map down to nothing by removing every entry which
 	// is covered by a CAA check.
 	for _, vaLog := range strings.Split(*vaLogs, ",") {
-		err = processCAALog(vaLog, issuanceMap)
+		err = processCAALog(vaLog, issuanceMap, earliest, latest, timeTolerance)
 		cmd.FailOnError(err, "failed to process CAA checking logs")
 	}
 
