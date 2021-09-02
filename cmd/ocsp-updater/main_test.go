44e15a5e6ddea6f8164f87cca1bf5ff10d1d579f
J.C. Jones <jc@insufficient.coffee>
2021-09-02 11:11:57 -0700

    Fixup: unit test should deepEquals

--- cmd/ocsp-updater/main_test.go
+++ cmd/ocsp-updater/main_test.go
@@ -616,17 +616,17 @@ func TestAppendShardList(t *testing.T) {
 	u, err := mkNewUpdaterWithStrings(t, strings.Fields("0 1"))
 	test.AssertNotError(t, err, "mkNewUpdaterWithStrings failed")
 	list = u.appendShardList(list)
-	test.AssertEquals(t, list, []interface{}{"0", "1"})
+	test.AssertDeepEquals(t, list, []interface{}{"0", "1"})
 
 	list = make([]interface{}, 0)
 	u, err = mkNewUpdaterWithStrings(t, strings.Fields("f"))
 	test.AssertNotError(t, err, "mkNewUpdaterWithStrings failed")
 	list = u.appendShardList(list)
-	test.AssertEquals(t, list, []interface{}{"f"})
+	test.AssertDeepEquals(t, list, []interface{}{"f"})
 
 	list = make([]interface{}, 0)
 	u, err = mkNewUpdaterWithStrings(t, strings.Fields("0 1 2 3 4 5 6 7 8 9 a b c d e f"))
 	test.AssertNotError(t, err, "mkNewUpdaterWithStrings failed")
 	list = u.appendShardList(list)
-	test.AssertEquals(t, list, []interface{}{"0", "1"})
+	test.AssertDeepEquals(t, list, []interface{}{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"})
 }
