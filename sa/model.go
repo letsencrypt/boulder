132d89d786db85c870e7413355e6a6e2deb8ad99
Samantha Frank <hello@entropy.cat>
2021-09-15 12:55:25 -0400

    WIP

--- sa/model.go
+++ sa/model.go
@@ -110,17 +110,22 @@ func SelectCertificates(s db.Selector, q string, args map[string]interface{}) ([
 	return models, err
 }
 
-func certStatusMetadataFields() []string {
+// CertStatusMetadataFields returns a slice of column names for rows in the
+// certificateStatus table.
+func CertStatusMetadataFields() []string {
 	return []string{"serial", "status", "ocspLastUpdated", "revokedDate", "revokedReason", "lastExpirationNagSent", "notAfter", "isExpired", "issuerID"}
 }
 
+// TODO(#5655) Remove once #5642 has been deployed to staging and production.
 func certStatusMetadataFieldsSelect(restOfQuery string) string {
-	fields := strings.Join(certStatusMetadataFields(), ",")
+	fields := strings.Join(CertStatusMetadataFields(), ",")
 	return fmt.Sprintf("SELECT %s FROM certificateStatus %s", fields, restOfQuery)
 }
 
 // SelectCertificateStatusMetadata selects all non-OCSPResponse fields of
 // multiple certificate status objects.
+//
+// TODO(#5655) Remove once #5642 has been deployed to staging and production.
 func SelectCertificateStatusMetadata(s db.Selector, q string, args ...interface{}) ([]core.CertificateStatus, error) {
 	var models []core.CertificateStatus
 	_, err := s.Select(
@@ -132,7 +137,7 @@ func SelectCertificateStatusMetadata(s db.Selector, q string, args ...interface{
 }
 
 func certStatusFields() []string {
-	return append(certStatusMetadataFields(), "ocspResponse")
+	return append(CertStatusMetadataFields(), "ocspResponse")
 }
 
 func certStatusFieldsSelect(restOfQuery string) string {
