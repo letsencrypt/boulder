289308bf39a7fcdfa5cf2bc9abca18c5d457f9ce
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2021-10-18 10:45:42 -0700

    Fix bash script correctness.

--- link.sh
+++ link.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Symlink the various boulder subcommands into place.
 #
-BINDIR=$PWD/bin
-for n in `${BINDIR}/boulder --list` ; do
+BINDIR="$PWD/bin"
+for n in `"${BINDIR}/boulder" --list` ; do
   ln -sf boulder "${BINDIR}/$n"
 done
