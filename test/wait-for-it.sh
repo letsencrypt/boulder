acb15fb772d9558eb3bd36050f4b3a6e7b4b9595
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2021-11-02 17:46:35 -0700

    Quiet the output of wait-for-it
    When wait-for-it is trying to connect and failing, bash emits errors on
    stderr. This captures those errors and sends them to /dev/null.
    
    This also replaces an internal wait_tcp_port function inside
    entrypoint.sh with a call to wait-for-it.sh.

--- test/wait-for-it.sh
+++ test/wait-for-it.sh
@@ -8,7 +8,7 @@ wait_tcp_port() {
     # see http://tldp.org/LDP/abs/html/devref1.html for description of this syntax.
     local max_tries="40"
     for n in `seq 1 "${max_tries}"` ; do
-      if exec 6<>/dev/tcp/"${host}"/"${port}"; then
+      if { exec 6<>/dev/tcp/"${host}"/"${port}" ; } 2>/dev/null ; then
         break
       else
         echo "$(date) - still trying to connect to ${host}:${port}"
