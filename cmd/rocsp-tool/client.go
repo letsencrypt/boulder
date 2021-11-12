9820972998fc75f039677d3c240e2410f723c72e
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2021-11-11 17:06:26 -0800

    Split rocsp-tool into files, add some tests

--- cmd/rocsp-tool/client.go
+++ cmd/rocsp-tool/client.go
@@ -1,97 +1,24 @@
 package notmain
 
 import (
-	"bytes"
 	"context"
-	"crypto/x509/pkix"
 	"database/sql"
-	"encoding/asn1"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"strings"
-	"sync"
 	"sync/atomic"
 	"time"
 
 	"github.com/jmhodges/clock"
 	capb "github.com/letsencrypt/boulder/ca/proto"
 	"github.com/letsencrypt/boulder/core"
-	"github.com/letsencrypt/boulder/issuance"
 	"github.com/letsencrypt/boulder/rocsp"
 	"github.com/letsencrypt/boulder/sa"
 	"github.com/letsencrypt/boulder/test/ocsp/helper"
 	"golang.org/x/crypto/ocsp"
 )
-
-type shortIDIssuer struct {
-	*issuance.Certificate
-	subject      pkix.RDNSequence
-	shortID      byte
-	issuerID     issuance.IssuerID
-	issuerNameID issuance.IssuerNameID
-}
-
-func loadIssuers(input map[string]int) ([]shortIDIssuer, error) {
-	var issuers []shortIDIssuer
-	for issuerFile, shortID := range input {
-		if shortID > 255 || shortID < 0 {
-			return nil, fmt.Errorf("invalid shortID %d (must be byte)", shortID)
-		}
-		cert, err := issuance.LoadCertificate(issuerFile)
-		if err != nil {
-			return nil, fmt.Errorf("reading issuer: %w", err)
-		}
-		var subject pkix.RDNSequence
-		_, err = asn1.Unmarshal(cert.Certificate.RawSubject, &subject)
-		if err != nil {
-			return nil, fmt.Errorf("parsing issuer.RawSubject: %w", err)
-		}
-		var shortID byte = byte(shortID)
-		for _, issuer := range issuers {
-			if issuer.shortID == shortID {
-				return nil, fmt.Errorf("duplicate shortID in config file: %d (for %q and %q)", shortID, issuer.subject, subject)
-			}
-			if !issuer.IsCA {
-				return nil, fmt.Errorf("certificate for %q is not a CA certificate", subject)
-			}
-		}
-		issuers = append(issuers, shortIDIssuer{
-			Certificate:  cert,
-			subject:      subject,
-			shortID:      shortID,
-			issuerID:     cert.ID(),
-			issuerNameID: cert.NameID(),
-		})
-	}
-	return issuers, nil
-}
-
-func findIssuerByID(longID int64, issuers []shortIDIssuer) (*shortIDIssuer, error) {
-	for _, iss := range issuers {
-		if iss.issuerNameID == issuance.IssuerNameID(longID) || iss.issuerID == issuance.IssuerID(longID) {
-			return &iss, nil
-		}
-	}
-	return nil, fmt.Errorf("no issuer found for an ID in certificateStatus: %d", longID)
-}
-
-func findIssuerByName(resp *ocsp.Response, issuers []shortIDIssuer) (*shortIDIssuer, error) {
-	var responder pkix.RDNSequence
-	_, err := asn1.Unmarshal(resp.RawResponderName, &responder)
-	if err != nil {
-		return nil, fmt.Errorf("parsing resp.RawResponderName: %w", err)
-	}
-	var responders strings.Builder
-	for _, issuer := range issuers {
-		fmt.Fprintf(&responders, "%s\n", issuer.subject)
-		if bytes.Equal(issuer.RawSubject, resp.RawResponderName) {
-			return &issuer, nil
-		}
-	}
-	return nil, fmt.Errorf("no issuer found matching OCSP response for %s. Available issuers:\n%s\n", responder, responders.String())
-}
 
 type client struct {
 	issuers       []shortIDIssuer
@@ -99,56 +26,6 @@ type client struct {
 	db            *sql.DB // optional
 	ocspGenerator capb.OCSPGeneratorClient
 	clk           clock.Clock
-}
-
-type inflight struct {
-	sync.RWMutex
-	items map[uint64]struct{}
-}
-
-func newInflight() *inflight {
-	return &inflight{
-		items: make(map[uint64]struct{}),
-	}
-}
-
-func (i *inflight) add(n uint64) {
-	i.Lock()
-	defer i.Unlock()
-	i.items[n] = struct{}{}
-}
-
-func (i *inflight) remove(n uint64) {
-	i.Lock()
-	defer i.Unlock()
-	delete(i.items, n)
-}
-
-func (i *inflight) len() int {
-	i.RLock()
-	defer i.RUnlock()
-	return len(i.items)
-}
-
-// min returns the numerically smallest key inflight. If nothing is inflight,
-// it returns 0. Note: this takes O(n) time in the number of keys and should
-// be called rarely.
-func (i *inflight) min() uint64 {
-	i.RLock()
-	defer i.RUnlock()
-	if len(i.items) == 0 {
-		return 0
-	}
-	var min uint64
-	for k := range i.items {
-		if min == 0 {
-			min = k
-		}
-		if k < min {
-			min = k
-		}
-	}
-	return min
 }
 
 // processResult represents the result of attempting to sign and store status
--- cmd/rocsp-tool/client_test.go
+++ cmd/rocsp-tool/client_test.go
@@ -1,80 +1,66 @@
 package notmain
 
 import (
-	"encoding/hex"
-	"strings"
+	"context"
+	"io/ioutil"
 	"testing"
+	"time"
 
+	"github.com/go-redis/redis/v8"
+	"github.com/jmhodges/clock"
+	"github.com/letsencrypt/boulder/cmd"
+	"github.com/letsencrypt/boulder/rocsp"
 	"github.com/letsencrypt/boulder/test"
-	"golang.org/x/crypto/ocsp"
 )
 
-func TestLoadIssuers(t *testing.T) {
-	input := map[string]int{
-		"../../test/hierarchy/int-e1.cert.pem": 23,
-		"../../test/hierarchy/int-r3.cert.pem": 99,
+func makeClient() (*rocsp.WritingClient, clock.Clock) {
+	CACertFile := "../test/redis-tls/minica.pem"
+	CertFile := "../test/redis-tls/boulder/cert.pem"
+	KeyFile := "../test/redis-tls/boulder/key.pem"
+	tlsConfig := cmd.TLSConfig{
+		CACertFile: &CACertFile,
+		CertFile:   &CertFile,
+		KeyFile:    &KeyFile,
 	}
-	output, err := loadIssuers(input)
+	tlsConfig2, err := tlsConfig.Load()
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
 
-	var e1 *shortIDIssuer
-	var r3 *shortIDIssuer
-
-	for i, v := range output {
-		if strings.Contains(v.Certificate.Subject.String(), "E1") {
-			e1 = &output[i]
-		}
-		if strings.Contains(v.Certificate.Subject.String(), "R3") {
-			r3 = &output[i]
-		}
-	}
-
-	test.AssertEquals(t, e1.Subject.String(), "CN=(TEST) Elegant Elephant E1,O=Boulder Test,C=XX")
-	test.AssertEquals(t, r3.Subject.String(), "CN=(TEST) Radical Rhino R3,O=Boulder Test,C=XX")
-	test.AssertEquals(t, e1.shortID, uint8(23))
-	test.AssertEquals(t, r3.shortID, uint8(99))
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:     []string{"10.33.33.2:4218"},
+		Username:  "unittest-rw",
+		Password:  "824968fa490f4ecec1e52d5e34916bdb60d45f8d",
+		TLSConfig: tlsConfig2,
+	})
+	clk := clock.NewFake()
+	return rocsp.NewWritingClient(rdb, 500*time.Millisecond, clk), clk
 }
 
-func TestFindIssuer(t *testing.T) {
-	input := map[string]int{
-		"../../test/hierarchy/int-e1.cert.pem": 23,
+func TestStoreResponse(t *testing.T) {
+	redisClient, clk := makeClient()
+
+	response, err := ioutil.ReadFile("testdata/ocsp.response")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	issuers, err := loadIssuers(map[string]int{
 		"../../test/hierarchy/int-r3.cert.pem": 99,
-	}
-	issuers, err := loadIssuers(input)
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	elephant, err := hex.DecodeString("3049310b300906035504061302585831153013060355040a130c426f756c6465722054657374312330210603550403131a28544553542920456c6567616e7420456c657068616e74204531")
-	if err != nil {
-		t.Fatal(err)
-	}
-	rhino, err := hex.DecodeString("3046310b300906035504061302585831153013060355040a130c426f756c64657220546573743120301e06035504031317285445535429205261646963616c205268696e6f205233")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ocspResp := &ocsp.Response{
-		RawResponderName: elephant,
+	cl := client{
+		issuers:       issuers,
+		redis:         redisClient,
+		db:            nil,
+		ocspGenerator: nil,
+		clk:           clk,
 	}
 
-	issuer, err := findIssuerByName(ocspResp, issuers)
-	if err != nil {
-		t.Fatalf("couldn't find issuer: %s", err)
-	}
-
-	test.AssertEquals(t, issuer.shortID, uint8(23))
-
-	ocspResp = &ocsp.Response{
-		RawResponderName: rhino,
-	}
-
-	issuer, err = findIssuerByName(ocspResp, issuers)
-	if err != nil {
-		t.Fatalf("couldn't find issuer: %s", err)
-	}
-
-	test.AssertEquals(t, issuer.shortID, uint8(99))
+	ttl := time.Hour
+	err = cl.storeResponse(context.Background(), response, &ttl)
+	test.AssertNotError(t, err, "storing response")
 }
