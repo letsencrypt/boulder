3fb75d29bbec67a58a822c0c3e29ad01ec6aa5a8
Samantha Frank <hello@entropy.cat>
2021-12-07 22:38:05 -0800

    test: Add support for multi-arch docker images

--- test/boulder-tools/tag_and_upload.sh
+++ test/boulder-tools/tag_and_upload.sh
@@ -7,29 +7,25 @@ DOCKER_REPO="letsencrypt/boulder-tools"
 
 GO_VERSIONS=( "1.17" )
 
-# Build a tagged image for each GO_VERSION
-for GO_VERSION in "${GO_VERSIONS[@]}"
-do
-  TAG_NAME="$DOCKER_REPO:go${GO_VERSION}_${DATESTAMP}"
-  echo "Building boulder-tools image $TAG_NAME"
-
-  # Build the docker image using the templated Dockerfile, tagging it with
-  # TAG_NAME
-  docker build . \
-    -t $TAG_NAME \
-    --build-arg "GO_VERSION=${GO_VERSION}"
-done
-
-# Log in once now that images are ready to upload
-echo "Images ready, please login to allow Dockerhub push"
+echo "Please login to allow push to DockerHub"
 docker login
 
-# Upload a tagged image for each GO_VERSION
+# Build and push a tagged image for each GO_VERSION.
 for GO_VERSION in "${GO_VERSIONS[@]}"
 do
-  TAG_NAME="$DOCKER_REPO:go${GO_VERSION}_${DATESTAMP}"
-  echo "Pushing ${TAG_NAME} to Dockerhub"
-  docker push ${TAG_NAME}
+  TAG_NAME="${DOCKER_REPO}:go${GO_VERSION}_${DATESTAMP}"
+  echo "Building boulder-tools image ${TAG_NAME}"
+  
+  # create a docker buildx node for cross-compilation.
+  docker buildx create --use --name=cross
+  
+  # build, tag, and push the image.
+  docker buildx build --build-arg "GO_VERSION=${GO_VERSION}" \
+    --push --tag "${TAG_NAME}" \
+    --platform=linux/amd64,linux/arm64 .
+  
+  # delete the docker buildx node.
+  docker buildx rm cross
 done
 
 # TODO(@cpu): Figure out a `sed` for updating the date in `docker-compose.yml`'s
