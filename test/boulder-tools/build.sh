2385698d8fdfaed4343da1aa56f1da5d0ede5c67
Samantha <hello@entropy.cat>
2021-12-16 14:13:41 -0800

    Fixing go install and bumping images

--- test/boulder-tools/build.sh
+++ test/boulder-tools/build.sh
@@ -47,7 +47,7 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1.0
 go install bitbucket.org/liamstask/goose/cmd/goose@latest
 go install golang.org/x/tools/cmd/stringer@latest
-go install github.com/letsencrypt/pebble/cmd/pebble-challtestsrv@latest
+go install github.com/letsencrypt/pebble/cmd/pebble-challtestsrv@master
 
 go clean -cache
 go clean -modcache
