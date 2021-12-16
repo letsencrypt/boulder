9c21187666e891bc0328102b341fe37366bc2136
Samantha <hello@entropy.cat>
2021-12-15 18:19:39 -0800

    test: tag and upload script

--- test/boulder-tools/tag_and_upload.sh
+++ test/boulder-tools/tag_and_upload.sh
@@ -10,18 +10,18 @@ GO_VERSIONS=( "1.17" "1.17.5" )
 echo "Please login to allow push to DockerHub"
 docker login
 
+# create a docker buildx node for cross-compilation.
+docker buildx create --use --name=cross
+
+# on EXIT, delete the docker buildx node if it exists.
+trap "if [ $(docker buildx ls | grep 'cross' | wc -l) != 0 ]; then docker buildx rm cross; fi" EXIT
+
 # Build and push a tagged image for each GO_VERSION.
 for GO_VERSION in "${GO_VERSIONS[@]}"
 do
   TAG_NAME="${DOCKER_REPO}:go${GO_VERSION}_${DATESTAMP}"
   echo "Building boulder-tools image ${TAG_NAME}"
-  
-  # create a docker buildx node for cross-compilation.
-  docker buildx create --use --name=cross
 
-  # on EXIT, delete the docker buildx node.
-  trap "docker buildx rm cross" EXIT
-  
   # build, tag, and push the image.
   docker buildx build --build-arg "GO_VERSION=${GO_VERSION}" \
     --push --tag "${TAG_NAME}" \
