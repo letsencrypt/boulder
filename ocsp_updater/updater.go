033d2d623a0fb9a2fdec56945ae9e6b8e3df9ecf
Andrew Gabbitas <agabbitas@letsencrypt.org>
2022-01-07 21:55:10 -0700

    Fix race in updater writing to redis
    Fix race condition where if an ocsp response is stored in mysql before
    redis, the redis storage goroutine will be canceled.
    
    Fixes #5866

--- ocsp_updater/updater.go
+++ ocsp_updater/updater.go
@@ -93,6 +93,8 @@ type OCSPUpdater struct {
 	// these requests in parallel allows us to get higher total throughput.
 	parallelGenerateOCSPRequests int
 
+	redisTimeout time.Duration
+
 	stalenessHistogram   prometheus.Histogram
 	genStoreHistogram    prometheus.Histogram
 	generatedCounter     *prometheus.CounterVec
@@ -213,6 +215,7 @@ func New(
 		backoffFactor:                config.SignFailureBackoffFactor,
 		serialSuffixes:               serialSuffixes,
 		queryBody:                    queryBody.String(),
+		redisTimeout:                 config.Redis.Timeout.Duration,
 	}
 
 	return &updater, nil
@@ -330,21 +333,32 @@ func (updater *OCSPUpdater) generateResponse(ctx context.Context, status sa.Cert
 
 // storeResponse stores a given CertificateStatus in the database.
 func (updater *OCSPUpdater) storeResponse(ctx context.Context, status *sa.CertStatusMetadata) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	// Create a context to set a deadline for the goroutine that stores
+	// the response in redis. Set the timeout to one second longer than
+	// the configured redis timeout to give redis a chance to return a
+	// timeout error first. This context is necessary because we don't
+	// want to wait to confirm a write to redis (best effort), which
+	// causes a race with the Tick() context cancelation if the parent
+	// context is used. When writing to redis is the primary storage
+	// source we can change to use the parent context.
+	ctx2, cancel := context.WithTimeout(context.Background(), updater.redisTimeout+time.Second)
 
+	// If a redis client is configured, try to store the response in redis.
 	if updater.rocspClient != nil {
 		go func() {
+			defer cancel()
 			ttl := status.NotAfter.Sub(updater.clk.Now())
 			shortIssuerID, err := rocsp_config.FindIssuerByID(status.IssuerID, updater.issuers)
 			if err != nil {
 				updater.storedRedisCounter.WithLabelValues("missing issuer").Inc()
 				return
 			}
-			err = updater.rocspClient.StoreResponse(ctx, status.OCSPResponse, shortIssuerID.ShortID(), ttl)
+			err = updater.rocspClient.StoreResponse(ctx2, status.OCSPResponse, shortIssuerID.ShortID(), ttl)
 			if err != nil {
 				if errors.Is(err, context.Canceled) {
 					updater.storedRedisCounter.WithLabelValues("canceled").Inc()
+				} else if errors.Is(err, context.DeadlineExceeded) {
+					updater.storedRedisCounter.WithLabelValues("deadlineExceeded").Inc()
 				} else {
 					updater.storedRedisCounter.WithLabelValues("failed").Inc()
 				}
@@ -352,6 +366,8 @@ func (updater *OCSPUpdater) storeResponse(ctx context.Context, status *sa.CertSt
 				updater.storedRedisCounter.WithLabelValues("success").Inc()
 			}
 		}()
+	} else {
+		defer cancel()
 	}
 	// Update the certificateStatus table with the new OCSP response, the status
 	// WHERE is used make sure we don't overwrite a revoked response with a one
