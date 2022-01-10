47377915f9c2d98b3dea7e7d0e92fc4e6b3d7871
Andrew Gabbitas <agabbitas@letsencrypt.org>
2022-01-10 16:22:55 -0700

    Add latency metrics to rocsp (#5865)
    Measure and export latency histogram metrics for ocsp storage and lookup
    operations in redis.
    
    Fixes #5832

--- rocsp/rocsp.go
+++ rocsp/rocsp.go
@@ -68,15 +68,21 @@ func MakeMetadataKey(serial string) string {
 
 // Client represents a read-only Redis client.
 type Client struct {
-	rdb     *redis.ClusterClient
-	timeout time.Duration
-	clk     clock.Clock
-	rdc     metricsCollector
+	rdb        *redis.ClusterClient
+	timeout    time.Duration
+	clk        clock.Clock
+	rdc        metricsCollector
+	getLatency *prometheus.HistogramVec
 }
 
 // NewClient creates a Client. The timeout applies to all requests, though a shorter timeout can be
 // applied on a per-request basis using context.Context.
-func NewClient(rdb *redis.ClusterClient, timeout time.Duration, clk clock.Clock, stats prometheus.Registerer) *Client {
+func NewClient(
+	rdb *redis.ClusterClient,
+	timeout time.Duration,
+	clk clock.Clock,
+	stats prometheus.Registerer,
+) *Client {
 	dbc := metricsCollector{rdb: rdb}
 
 	labels := prometheus.Labels{"address": rdb.Options().Addrs[0], "user": rdb.Options().Username}
@@ -104,25 +110,42 @@ func NewClient(rdb *redis.ClusterClient, timeout time.Duration, clk clock.Clock,
 		"redis_stale_conns",
 		"Number of stale connections removed from the pool.",
 		nil, labels)
-
 	stats.MustRegister(dbc)
+	getLatency := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "rocsp_get_latency",
+			Help: "Histogram of latencies of rocsp.GetResponse and rocsp.GetMetadata calls with result and method labels",
+		},
+		[]string{"result", "method"},
+	)
+	stats.MustRegister(getLatency)
 
 	return &Client{
-		rdb:     rdb,
-		timeout: timeout,
-		clk:     clk,
-		rdc:     dbc,
+		rdb:        rdb,
+		timeout:    timeout,
+		clk:        clk,
+		rdc:        dbc,
+		getLatency: getLatency,
 	}
 }
 
 // WritingClient represents a Redis client that can both read and write.
 type WritingClient struct {
 	*Client
+	storeResponseLatency *prometheus.HistogramVec
 }
 
 // NewWritingClient creates a WritingClient.
 func NewWritingClient(rdb *redis.ClusterClient, timeout time.Duration, clk clock.Clock, stats prometheus.Registerer) *WritingClient {
-	return &WritingClient{NewClient(rdb, timeout, clk, stats)}
+	storeResponseLatency := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "rocsp_store_response_latency",
+			Help: "Histogram of latencies of rocsp.StoreResponse calls with result labels",
+		},
+		[]string{"result"},
+	)
+	stats.MustRegister(storeResponseLatency)
+	return &WritingClient{NewClient(rdb, timeout, clk, stats), storeResponseLatency}
 }
 
 // StoreResponse parses the given bytes as an OCSP response, and stores it into
@@ -131,6 +154,7 @@ func NewWritingClient(rdb *redis.ClusterClient, timeout time.Duration, clk clock
 // same across OCSP components. Returns error if the OCSP response fails to
 // parse.
 func (c *WritingClient) StoreResponse(ctx context.Context, respBytes []byte, shortIssuerID byte, ttl time.Duration) error {
+	start := c.clk.Now()
 	ctx, cancel := context.WithTimeout(ctx, c.timeout)
 	defer cancel()
 
@@ -164,9 +188,17 @@ func (c *WritingClient) StoreResponse(ctx context.Context, respBytes []byte, sho
 		return nil
 	}, metadataKey, responseKey)
 	if err != nil {
+		state := "failed"
+		if errors.Is(err, context.DeadlineExceeded) {
+			state = "deadlineExceeded"
+		} else if errors.Is(err, context.Canceled) {
+			state = "canceled"
+		}
+		c.storeResponseLatency.With(prometheus.Labels{"result": state}).Observe(time.Since(start).Seconds())
 		return fmt.Errorf("transaction failed: %w", err)
 	}
 
+	c.storeResponseLatency.With(prometheus.Labels{"result": "success"}).Observe(time.Since(start).Seconds())
 	return nil
 }
 
@@ -174,6 +206,7 @@ func (c *WritingClient) StoreResponse(ctx context.Context, respBytes []byte, sho
 // Returns error if the OCSP response fails to parse.
 // Does not check the metadata field.
 func (c *Client) GetResponse(ctx context.Context, serial string) ([]byte, error) {
+	start := c.clk.Now()
 	ctx, cancel := context.WithTimeout(ctx, c.timeout)
 	defer cancel()
 
@@ -184,16 +217,27 @@ func (c *Client) GetResponse(ctx context.Context, serial string) ([]byte, error)
 		// go-redis `Get` returns redis.Nil error when key does not exist. In
 		// that case return a `ErrRedisNotFound` error.
 		if errors.Is(err, redis.Nil) {
+			c.getLatency.With(prometheus.Labels{"result": "notFound", "method": "GetResponse"}).Observe(time.Since(start).Seconds())
 			return nil, ErrRedisNotFound
 		}
+
+		state := "failed"
+		if errors.Is(err, context.DeadlineExceeded) {
+			state = "deadlineExceeded"
+		} else if errors.Is(err, context.Canceled) {
+			state = "canceled"
+		}
+		c.getLatency.With(prometheus.Labels{"result": state, "method": "GetResponse"}).Observe(time.Since(start).Seconds())
 		return nil, fmt.Errorf("getting response: %w", err)
 	}
 
+	c.getLatency.With(prometheus.Labels{"result": "success", "method": "GetResponse"}).Observe(time.Since(start).Seconds())
 	return []byte(resp), nil
 }
 
 // GetMetadata fetches the metadata for the given serial number.
 func (c *Client) GetMetadata(ctx context.Context, serial string) (*Metadata, error) {
+	start := c.clk.Now()
 	ctx, cancel := context.WithTimeout(ctx, c.timeout)
 	defer cancel()
 
@@ -204,13 +248,25 @@ func (c *Client) GetMetadata(ctx context.Context, serial string) (*Metadata, err
 		// go-redis `Get` returns redis.Nil error when key does not exist. In
 		// that case return a `ErrRedisNotFound` error.
 		if errors.Is(err, redis.Nil) {
+			c.getLatency.With(prometheus.Labels{"result": "notFound", "method": "GetMetadata"}).Observe(time.Since(start).Seconds())
 			return nil, ErrRedisNotFound
 		}
+
+		state := "failed"
+		if errors.Is(err, context.DeadlineExceeded) {
+			state = "deadlineExceeded"
+		} else if errors.Is(err, context.Canceled) {
+			state = "canceled"
+		}
+		c.getLatency.With(prometheus.Labels{"result": state, "method": "GetMetadata"}).Observe(time.Since(start).Seconds())
 		return nil, fmt.Errorf("getting metadata: %w", err)
 	}
 	metadata, err := UnmarshalMetadata([]byte(resp))
 	if err != nil {
+		c.getLatency.With(prometheus.Labels{"result": "failed", "method": "GetMetadata"}).Observe(time.Since(start).Seconds())
 		return nil, fmt.Errorf("unmarshaling metadata: %w", err)
 	}
+
+	c.getLatency.With(prometheus.Labels{"result": "success", "method": "GetMetadata"}).Observe(time.Since(start).Seconds())
 	return &metadata, nil
 }
