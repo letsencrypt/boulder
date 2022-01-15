ad0e56ec4a7bc98938bb3fbdb55031cdb9954c34
Aaron Gable <aaron@letsencrypt.org>
2022-01-14 16:22:24 -0800

    Remove test coverage on go1.17 (#5882)

--- test/boulder-tools/tag_and_upload.sh
+++ test/boulder-tools/tag_and_upload.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 DATESTAMP=$(date +%Y-%m-%d)
 DOCKER_REPO="letsencrypt/boulder-tools"
 
-GO_VERSIONS=( "1.17" "1.17.5" )
+GO_VERSIONS=( "1.17.5" )
 
 echo "Please login to allow push to DockerHub"
 docker login
