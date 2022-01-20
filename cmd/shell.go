564fd44ee4892b82bfe51140f6bb944e3c806734
Samantha <hello@entropy.cat>
2022-01-20 12:20:57 -0800

    cmd: Make msg optional for FailOnError

--- cmd/shell.go
+++ cmd/shell.go
@@ -221,12 +221,18 @@ func Fail(msg string) {
 }
 
 // FailOnError exits and prints an error message, but only if we encountered
-// a problem and err != nil
+// a problem and err != nil. err is required but msg can be "".
 func FailOnError(err error, msg string) {
-	if err != nil {
-		msg := fmt.Sprintf("%s: %s", msg, err)
-		Fail(msg)
+	if err == nil {
+		return
 	}
+	if msg != "" {
+		Fail(fmt.Sprintf("%s: %s", msg, err))
+	}
+	if msg == "" {
+		Fail(err.Error())
+	}
+
 }
 
 // ReadConfigFile takes a file path as an argument and attempts to
