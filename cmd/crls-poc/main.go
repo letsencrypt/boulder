4ea3723d8ac502433e3c172e19154740c2f5eeb5
Aaron Gable <aaron@letsencrypt.org>
2022-03-11 15:28:32 -0800

    Better benchmarking

--- cmd/crls-poc/main.go
+++ cmd/crls-poc/main.go
@@ -124,71 +124,73 @@ func main() {
 	// statuses across, then read all the bytes back. Not using a goroutine
 	// because we know the server side is synchronous and won't start sending
 	// responses until the input stream is complete.
-	stream, err := cc.GenerateCRL(context.Background())
-	cmd.FailOnError(err, "Failed to create gRPC stream")
-
-	err = stream.Send(&crlpb.GenerateCRLRequest{
-		Payload: &crlpb.GenerateCRLRequest_Metadata{
-			Metadata: &crlpb.CRLMetadata{
-				IssuerNameID: int64(issuers[0].Cert.NameID()),
-				ThisUpdate:   clk.Now().UnixNano(),
-			},
-		},
-	})
-	cmd.FailOnError(err, "Failed to send metadata")
-
-	numEntries := 100_000
-
-	for i := 0; i < numEntries; i++ {
-		var serialBytes [16]byte
-		_, _ = rand.Read(serialBytes[:])
-		serial := big.NewInt(0).SetBytes(serialBytes[:])
-		serialString := core.SerialToString(serial)
-
-		reason := int32(mrand.Intn(10))
-
-		ninetyDays := time.Duration(90 * 24 * time.Hour)
-		earliest := clk.Now().Add(-ninetyDays).UnixNano()
-		revokedAt := time.Unix(0, earliest+mrand.Int63n(ninetyDays.Nanoseconds())).UnixNano()
+	numShards := 100
+	for i := 0; i < numShards; i++ {
+		start := time.Now()
+		stream, err := cc.GenerateCRL(context.Background())
+		cmd.FailOnError(err, "Failed to create gRPC stream")
 
 		err = stream.Send(&crlpb.GenerateCRLRequest{
-			Payload: &crlpb.GenerateCRLRequest_Entry{
-				Entry: &crlpb.CRLEntry{
-					Serial:    serialString,
-					Reason:    reason,
-					RevokedAt: revokedAt,
+			Payload: &crlpb.GenerateCRLRequest_Metadata{
+				Metadata: &crlpb.CRLMetadata{
+					IssuerNameID: int64(issuers[0].Cert.NameID()),
+					ThisUpdate:   clk.Now().UnixNano(),
 				},
 			},
 		})
-		cmd.FailOnError(err, "Failed to send crl entry")
-	}
-	err = stream.CloseSend()
-	cmd.FailOnError(err, "Failed to close send stream")
+		cmd.FailOnError(err, "Failed to send metadata")
 
-	crlBytes := make([]byte, 0)
-	for {
-		out, err := stream.Recv()
-		if err != nil {
-			if err == io.EOF {
-				break
+		numEntries := 100_000
+		for j := 0; j < numEntries; j++ {
+			var serialBytes [16]byte
+			_, _ = rand.Read(serialBytes[:])
+			serial := big.NewInt(0).SetBytes(serialBytes[:])
+			serialString := core.SerialToString(serial)
+
+			reason := int32(mrand.Intn(10))
+
+			ninetyDays := time.Duration(90 * 24 * time.Hour)
+			earliest := clk.Now().Add(-ninetyDays).UnixNano()
+			revokedAt := time.Unix(0, earliest+mrand.Int63n(ninetyDays.Nanoseconds())).UnixNano()
+
+			err = stream.Send(&crlpb.GenerateCRLRequest{
+				Payload: &crlpb.GenerateCRLRequest_Entry{
+					Entry: &crlpb.CRLEntry{
+						Serial:    serialString,
+						Reason:    reason,
+						RevokedAt: revokedAt,
+					},
+				},
+			})
+			cmd.FailOnError(err, "Failed to send crl entry")
+		}
+		err = stream.CloseSend()
+		cmd.FailOnError(err, "Failed to close send stream")
+
+		crlBytes := make([]byte, 0)
+		for {
+			out, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				cmd.FailOnError(err, "Failed to read from response stream")
 			}
-			cmd.FailOnError(err, "Failed to read from response stream")
+
+			crlBytes = append(crlBytes, out.Chunk...)
 		}
 
-		crlBytes = append(crlBytes, out.Chunk...)
+		crl, err := x509.ParseDERCRL(crlBytes)
+		cmd.FailOnError(err, "Failed to parse CRL bytes")
+
+		err = issuers[0].Cert.CheckCRLSignature(crl)
+		cmd.FailOnError(err, "Failed to validate CRL signature")
+
+		if len(crl.TBSCertList.RevokedCertificates) != numEntries {
+			cmd.Fail("Got wrong number of entries back in CRL")
+		}
+
+		elapsed := time.Since(start)
+		logger.Warningf("Shard %03d took %02.02fs", i, elapsed.Seconds())
 	}
-
-	crl, err := x509.ParseDERCRL(crlBytes)
-	cmd.FailOnError(err, "Failed to parse CRL bytes")
-
-	err = issuers[0].Cert.CheckCRLSignature(crl)
-	cmd.FailOnError(err, "Failed to validate CRL signature")
-
-	if len(crl.TBSCertList.RevokedCertificates) != numEntries {
-		cmd.Fail("Got wrong number of entries back in CRL")
-	}
-
-	// for _, entry := range crl.TBSCertList.RevokedCertificates {
-	// 	fmt.Printf("%s: %s\n", core.SerialToString(entry.SerialNumber), entry.RevocationTime)
-	// }
 }
