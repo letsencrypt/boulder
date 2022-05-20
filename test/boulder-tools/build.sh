9b4ca235dd3fbf1f6f271a2bb82f87a0c51e2624
Aaron Gable <aaron@letsencrypt.org>
2022-05-20 14:24:01 -0700

    Update boulder-tools dependencies (#6129)
    Update:
    - golangci-lint from v1.42.1 to v1.46.2
    - protoc from v3.15.6 to v3.20.1
    - protoc-gen-go from v1.26.0 to v1.28.0
    - protoc-gen-go-grpc from v1.1.0 to v1.2.0
    - fpm from v1.14.0 to v1.14.2
    
    Also remove a reference to go1.17.9 from one last place.
    
    This does result in updating all of our generated .pb.go files, but only
    to update the version number embedded in each file's header.
    
    Fixes #6123

--- test/boulder-tools/build.sh
+++ test/boulder-tools/build.sh
@@ -30,21 +30,21 @@ then
   PROTO_ARCH=aarch_64
 fi
 
-curl -L https://github.com/google/protobuf/releases/download/v3.15.6/protoc-3.15.6-linux-"${PROTO_ARCH}".zip -o /tmp/protoc.zip
+curl -L https://github.com/google/protobuf/releases/download/v3.20.1/protoc-3.20.1-linux-"${PROTO_ARCH}".zip -o /tmp/protoc.zip
 unzip /tmp/protoc.zip -d /usr/local/protoc
 
 # Override default GOBIN and GOCACHE
 export GOBIN=/usr/local/bin GOCACHE=/tmp/gocache
 
 # Install golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $GOBIN v1.42.1
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $GOBIN v1.46.2
 
 # Install protobuf and testing/dev tools.
 # Note: The version of golang/protobuf is partially tied to the version of grpc
 # used by Boulder overall. Updating it may require updating the grpc version
 # and vice versa.
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1.0
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
 go install bitbucket.org/liamstask/goose/cmd/goose@latest
 go install golang.org/x/tools/cmd/stringer@latest
 go install github.com/letsencrypt/pebble/cmd/pebble-challtestsrv@master
@@ -54,9 +54,13 @@ go clean -modcache
 
 pip3 install -r /tmp/requirements.txt
 
-gem install --no-document -v 1.14.0 fpm
+gem install --no-document -v 1.14.2 fpm
 
 apt-get autoremove -y libssl-dev ruby-dev cmake pkg-config libtool autoconf automake
 apt-get clean -y
+
+# Tell git to trust the directory where the boulder repo volume is mounted
+# by docker-compose.
+git config --global --add safe.directory /boulder
 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
