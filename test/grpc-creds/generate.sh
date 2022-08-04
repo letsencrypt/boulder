afeff9a4dd674f010ccdec0181ca371561e0ca8d
Samantha <hello@entropy.cat>
2022-08-04 12:42:19 -0700

    Adjust generate.sh to issue certs with IP SANs for SA

--- test/grpc-creds/generate.sh
+++ test/grpc-creds/generate.sh
@@ -10,10 +10,16 @@ command -v minica >/dev/null 2>&1 || {
 }
 
 for SERVICE in admin-revoker expiration-mailer ocsp-updater ocsp-responder \
-  orphan-finder wfe akamai-purger nonce bad-key-revoker crl-updater health-checker; do
+  orphan-finder wfe akamai-purger bad-key-revoker crl-updater health-checker; do
   minica -domains "${SERVICE}.boulder"
 done
 
-for SERVICE in publisher ra ca sa va ; do
-  minica -domains "${SERVICE}.boulder,${SERVICE}1.boulder,${SERVICE}2.boulder"
+NEEDIPSANS=( "sa" )
+for SERVICE in publisher nonce ra ca sa va ; do
+  if [[ "${NEEDIPSANS[@]}" =~ "${SERVICE}" ]]; then
+    minica -domains "${SERVICE}.boulder,${SERVICE}1.boulder,${SERVICE}2.boulder" \
+      -ip-addresses "10.77.77.77,10.88.88.88"
+  else
+    minica -domains "${SERVICE}.boulder,${SERVICE}1.boulder,${SERVICE}2.boulder"
+  fi
 done
