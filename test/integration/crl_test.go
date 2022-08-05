824f9dfd5dc9288502a5fcc81863d273b09d392f
Aaron Gable <aaron@letsencrypt.org>
2022-08-05 16:29:57 -0700

    Review comments

--- test/integration/crl_test.go
+++ test/integration/crl_test.go
@@ -18,18 +18,13 @@ import (
 
 // runUpdater executes the crl-updater binary with the -runOnce flag, and
 // returns when it completes.
-func runUpdater(t *testing.T) {
+func runUpdater(t *testing.T, configFile string) {
 	t.Helper()
 
-	config_dir := os.Getenv("BOULDER_CONFIG_DIR")
-	if config_dir == "" {
-		t.Fatal("couldn't determine configuration directory")
-	}
-
-	bin_path, err := filepath.Abs("bin/crl-updater")
+	binPath, err := filepath.Abs("bin/crl-updater")
 	test.AssertNotError(t, err, "computing crl-updater path")
 
-	c := exec.Command(bin_path, "-config", path.Join(config_dir, "crl-updater.json"), "-debug-addr", ":8022", "-runOnce")
+	c := exec.Command(binPath, "-config", configFile, "-debug-addr", ":8022", "-runOnce")
 	err = c.Run()
 	test.AssertNotError(t, err, "failed to run crl-updater")
 }
@@ -40,7 +35,8 @@ func runUpdater(t *testing.T) {
 func TestCRLPipeline(t *testing.T) {
 	// The crl-updater and crl-storer are not yet deployed in Prod, so only run
 	// this test in config-next.
-	if !strings.Contains(os.Getenv("BOULDER_CONFIG_DIR"), "config-next") {
+	configDir := os.Getenv("BOULDER_CONFIG_DIR")
+	if !strings.Contains(configDir, "config-next") {
 		return
 	}
 
@@ -48,17 +44,16 @@ func TestCRLPipeline(t *testing.T) {
 	os.Setenv("DIRECTORY", "http://boulder:4001/directory")
 	client, err := makeClient()
 	test.AssertNotError(t, err, "creating acme client")
+	configFile := path.Join(configDir, "crl-updater.json")
 
 	// Issue a test certificate and save its serial number.
 	res, err := authAndIssue(client, nil, []string{random_domain()})
-	if err != nil || len(res.certs) < 1 {
-		t.Fatal("failed to create test certificate")
-	}
+	test.AssertNotError(t, err, "failed to create test certificate")
 	cert := res.certs[0]
 	serial := core.SerialToString(cert.SerialNumber)
 
 	// Confirm that the cert does not yet show up as revoked in the CRLs.
-	runUpdater(t)
+	runUpdater(t, configFile)
 	resp, err := http.Get("http://localhost:7890/query?serial=" + serial)
 	test.AssertNotError(t, err, "s3-test-srv GET /query failed")
 	test.AssertEquals(t, resp.StatusCode, 404)
@@ -74,7 +69,7 @@ func TestCRLPipeline(t *testing.T) {
 	test.AssertEquals(t, resp.StatusCode, 200)
 
 	// Confirm that the cert now *does* show up in the CRLs.
-	runUpdater(t)
+	runUpdater(t, configFile)
 	resp, err = http.Get("http://localhost:7890/query?serial=" + serial)
 	test.AssertNotError(t, err, "s3-test-srv GET /query failed")
 	test.AssertEquals(t, resp.StatusCode, 200)
