15df60d5db5adcd6e875d2d8056867f2131ba629
Aaron Gable <aaron@letsencrypt.org>
2022-08-23 09:27:32 -0700

    Fix autoformat

--- wfe2/wfe_test.go
+++ wfe2/wfe_test.go
@@ -3189,7 +3189,7 @@ func TestPrepAuthzForDisplay(t *testing.T) {
 		Identifier:     identifier.DNSIdentifier("*.example.com"),
 		Challenges: []core.Challenge{
 			{
-				Type:                     "dns",
+				Type: "dns",
 				ProvidedKeyAuthorization: "	🔑",
 			},
 		},
