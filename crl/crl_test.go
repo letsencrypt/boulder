30b9a0225fd5fa018de25770d555f2e943e5b6dd
Aaron Gable <aaron@letsencrypt.org>
2022-09-14 12:37:48 -0700

    Fix TestId

--- crl/crl_test.go
+++ crl/crl_test.go
@@ -11,8 +11,7 @@ import (
 
 func TestId(t *testing.T) {
 	thisUpdate := time.Now()
-	out, err := Id(1337, Number(thisUpdate), 1)
-	test.AssertNotError(t, err, "Failed to create CRLId")
+	out := Id(1337, Number(thisUpdate), 1)
 	expectCRLId := fmt.Sprintf("{\"issuerID\":1337,\"crlNumber\":%d,\"shardIdx\":1}", big.NewInt(thisUpdate.UnixNano()))
 	test.AssertEquals(t, string(out), expectCRLId)
 }
