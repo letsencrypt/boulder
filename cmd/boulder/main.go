1d16ff9b00bc3c0b521189eed92e5f494ce27653
Matthew McPherrin <mattm@letsencrypt.org>
2022-10-06 14:21:47 -0400

    Add support for subcommands to "boulder" command (#6426)
    Boulder builds a single binary which is symlinked to the different binary names, which are included in its releases.
    However, requiring symlinks isn't always convenient.
    
    This change makes the base `boulder` command usable as any of the other binary names.  If the binary is invoked as boulder, runs the second argument as the command name.  It shifts off the `boulder` from os.Args so that all the existing argument parsing can remain unchanged.
    
    This uses the subcommand versions in integration tests, which I think is important to verify this change works, however we can debate whether or not that should be merged, since we're using the symlink method in production, that's what we want to test.
    
    Issue #6362 suggests we want to move to a more fully-featured command-line parsing library that has proper subcommand support. This fixes one fragment of that, by providing subcommands, but is definitely nowhere near as nice as it could be with a more fully fleshed out library.  Thus this change takes a minimal-touch approach to this change, since we know a larger refactoring is coming.

--- cmd/boulder/main.go
+++ cmd/boulder/main.go
@@ -42,14 +42,23 @@ func main() {
 
 func init() {
 	cmd.RegisterCommand("boulder", func() {
-		if len(os.Args) > 1 && os.Args[1] == "--list" {
-			for _, c := range cmd.AvailableCommands() {
-				if c != "boulder" {
-					fmt.Println(c)
-				}
+		if len(os.Args) <= 1 {
+			fmt.Fprintf(os.Stderr, "Call with --list to list available subcommands. Run them like boulder <subcommand>.\n")
+			return
+		}
+		subcommand := cmd.LookupCommand(os.Args[1])
+		if subcommand == nil {
+			fmt.Fprintf(os.Stderr, "Unknown subcommand '%s'.\n", os.Args[1])
+			return
+		}
+		os.Args = os.Args[1:]
+		subcommand()
+	})
+	cmd.RegisterCommand("--list", func() {
+		for _, c := range cmd.AvailableCommands() {
+			if c != "boulder" && c != "--list" {
+				fmt.Println(c)
 			}
-		} else {
-			fmt.Fprintf(os.Stderr, "Call with --list to list available subcommands. Symlink and run as a subcommand to run that subcommand.\n")
 		}
 	})
 }
--- test/integration-test.py
+++ test/integration-test.py
@@ -54,7 +54,7 @@ def test_single_ocsp():
        This is a non-API test.
     """
     p = subprocess.Popen(
-        ["./bin/ocsp-responder", "--config", "test/issuer-ocsp-responder.json"])
+        ["./bin/boulder", "ocsp-responder", "--config", "test/issuer-ocsp-responder.json"])
     waitport(4003, ' '.join(p.args))
 
     # Verify that the static OCSP responder, which answers with a
@@ -218,7 +218,7 @@ def check_balance():
                 % address)
 
 def run_cert_checker():
-    run(["./bin/cert-checker", "-config", "%s/cert-checker.json" % config_dir])
+    run(["./bin/boulder", "cert-checker", "-config", "%s/cert-checker.json" % config_dir])
 
 if __name__ == "__main__":
     main()
