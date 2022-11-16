4b37f4e7f041813fbddf9e51c615c8edcef506a1
Lena Underwood <lunderwo@macalester.edu>
2022-11-16 13:00:48 -0500

    reorganize

--- observer/probers/tls/tls.go
+++ observer/probers/tls/tls.go
@@ -60,46 +60,65 @@ func isOCSPRevoked(cert, issuer *x509.Certificate) bool {
 	return true
 }
 
-func getExpiredRootInfo() (bool, time.Duration) {
-	return true, time.Minute
+func (p TLSProbe) getExpiredCertInfo() (string, string, time.Duration) {
+	conf := &tls.Config{InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", p.url+":443", conf)
+	if err != nil {
+		return "", "", 0
+	}
+	peers := conn.ConnectionState().PeerCertificates
+	expiry_date := peers[0].NotAfter
+	root := peers[len(peers)-1].Issuer
+	return root.CommonName, root.Organization[0], time.Since(expiry_date)
 }
 
 // Probe performs the configured TLS protocol.
 // Return true if both root AND response are the expected values, otherwise false
 // Export time to cert expiry as Prometheus metric
 func (p TLSProbe) Probe(timeout time.Duration) (bool, time.Duration) {
-	expected_root, expected_response := false, false
+	is_expected_root, is_expected_response := false, false
+	root_cn, root_o := "", ""
+	var time_to_expiry time.Duration
+	var time_since_expiry time.Duration
 
 	conf := &tls.Config{}
 	start := time.Now()
 	conn, err := tls.Dial("tcp", p.url+":443", conf)
 	if err != nil {
+		// check expired or return unknown
 		certInvalidErr := x509.CertificateInvalidError{}
 		if errors.As(err, &certInvalidErr) && certInvalidErr.Reason == x509.Expired {
 			if p.response == "expired" {
-				expected_response = true
-			} //need a way to still verify root and get time since expiry
+				is_expected_response = true
+			}
+			// incorrect response, but check the rest anyways
+			root_cn, root_o, time_since_expiry = p.getExpiredCertInfo()
 		}
+		// return unknown
 	} else {
+		// check valid, revoked, or unknown
 		defer conn.Close()
 		chains := conn.ConnectionState().VerifiedChains
-		for _, chain := range chains {
-			root_cert := chain[len(chain)-1]
-			if p.root == fmt.Sprintf("/O=%s/CN=%s", root_cert.Issuer.Organization[0], root_cert.Issuer.CommonName) {
-				expected_root = true
-				break
-			}
-		}
-		end_cert, issuer := chains[0][0], chains[0][1]
-		time_to_expiry := time.Until(end_cert.NotAfter)
+		end_cert, issuer, root_cert := chains[0][0], chains[0][1], chains[0][len(chains[0])-1].Issuer
+		time_to_expiry = time.Until(end_cert.NotAfter)
+		root_cn, root_o = root_cert.CommonName, root_cert.Organization[0]
 
 		is_revoked := isOCSPRevoked(end_cert, issuer)
 		if (p.response == "revoked" && is_revoked) || (p.response == "valid" && !is_revoked) {
-			expected_response = true
+			is_expected_response = true
 		}
+	}
 
+	// check root
+	if p.root == fmt.Sprintf("/O=%s/CN=%s", root_o, root_cn) {
+		is_expected_root = true
+	}
+	// export time to (or since) expiration
+	if time_since_expiry != start.Sub(start) {
+		p.certExpiry.WithLabelValues(p.url).Set(-time_since_expiry.Seconds())
+	} else {
 		p.certExpiry.WithLabelValues(p.url).Set(time_to_expiry.Seconds())
 	}
 
-	return expected_root && expected_response, time.Since(start)
+	return is_expected_root && is_expected_response, time.Since(start)
 }
