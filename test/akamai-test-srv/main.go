e6dabce0d77e899c5ef1667a28695b07ede5febe
Aaron Gable <aaron@letsencrypt.org>
2022-12-15 12:26:00 -0800

    Fix gosec lints

--- test/akamai-test-srv/main.go
+++ test/akamai-test-srv/main.go
@@ -92,6 +92,9 @@ func main() {
 		w.Write(resp)
 	})
 
+	// The gosec linter complains that timeouts cannot be set here. That's fine,
+	// because this is test-only code.
+	////nolint:gosec
 	go log.Fatal(http.ListenAndServe(*listenAddr, nil))
 	cmd.CatchSignals(nil, nil)
 }
