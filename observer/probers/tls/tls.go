da68802431354281c84962b563c3dd160cb62075
Lena Underwood <lena@letsencrypt.org>
2023-01-19 10:51:48 -0500

    initialize intermediates in opts

--- observer/probers/tls/tls.go
+++ observer/probers/tls/tls.go
@@ -100,7 +100,8 @@ func (p TLSProbe) probeExpired(timeout time.Duration) bool {
 		InsecureSkipVerify: true,
 		VerifyConnection: func(cs tls.ConnectionState) error {
 			opts := x509.VerifyOptions{
-				CurrentTime: cs.PeerCertificates[0].NotAfter,
+				CurrentTime:   cs.PeerCertificates[0].NotAfter,
+				Intermediates: x509.NewCertPool(),
 			}
 			_, err := cs.PeerCertificates[0].Verify(opts)
 			return err
