e6c664363cf789d2574657a655642da111319a29
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2023-02-14 14:01:12 -0800

    Speed up builds of boulder-tools images.
    Only build arm64 images for one version of Go.
    
    Use a separate RUN command for installing Go, and the Go dependencies.
    This allows reusing a cached layer for the installation of apt
    dependencies, protoc, python-acme, and fpm.
    
    Remove apt dependency on pkg-config, libtool, autoconf, and automake.

--- test/boulder-tools/tag_and_upload.sh
+++ test/boulder-tools/tag_and_upload.sh
@@ -7,18 +7,24 @@ cd $(dirname $0)
 DATESTAMP=$(date +%Y-%m-%d)
 DOCKER_REPO="letsencrypt/boulder-tools"
 
-# When updating these GO_VERSIONS, please also update
+# These versions are only built for platforms that we run in CI.
+# When updating these GO_CI_VERSIONS, please also update
 # .github/workflows/release.yml,
 # .github/workflows/try-release.yml if appropriate,
-# .github/workflows/boulder-ci.yml with the new container tag.
-GO_VERSIONS=( "1.19.5" "1.19.6" "1.20.1" )
+# and .github/workflows/boulder-ci.yml with the new container tag.
+GO_CI_VERSIONS=( "1.19.5" "1.19.6" "1.20.1" )
+# These versions are built for both platforms that boulder devs use.
+# When updating GO_DEV_VERSIONS, please also update
+# ../../docker-compose.yml's default Go version.
+GO_DEV_VERSIONS=( "1.20.1" )
 
 echo "Please login to allow push to DockerHub"
 docker login
 
-# Build and push a tagged image for each GO_VERSION.
-for GO_VERSION in "${GO_VERSIONS[@]}"
-do
+# Usage: build_and_push_image $GO_VERSION $PLATFORMS
+build_and_push_image() {
+  GO_VERSION="$1"
+  PLATFORMS="$2"
   TAG_NAME="${DOCKER_REPO}:go${GO_VERSION}_${DATESTAMP}"
   echo "Building boulder-tools image ${TAG_NAME}"
 
@@ -26,7 +32,18 @@ do
   docker buildx build --build-arg "GO_VERSION=${GO_VERSION}" \
     --progress plain \
     --push --tag "${TAG_NAME}" \
-    --platform=linux/amd64,linux/arm64 .
+    --platform "${PLATFORMS}" \
+    .
+}
+
+for GO_VERSION in "${GO_CI_VERSIONS[@]}"
+do
+  build_and_push_image $GO_VERSION linux/amd64
+done
+
+for GO_VERSION in "${GO_DEV_VERSIONS[@]}"
+do
+  build_and_push_image $GO_VERSION linux/amd64,linux/arm64
 done
 
 # TODO(@cpu): Figure out a `sed` for updating the date in `docker-compose.yml`'s
