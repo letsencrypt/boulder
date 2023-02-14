342d2efc9f4c7175777fc3a8f9cf320d7fdf2ac6
Phil Porada <philporada@gmail.com>
2023-02-14 14:45:00 -0500

    Add go 1.19.6 and 1.20.1 to container building array. Update docker-compose.yml with a fresh build timestamp.

--- test/boulder-tools/tag_and_upload.sh
+++ test/boulder-tools/tag_and_upload.sh
@@ -8,9 +8,10 @@ DATESTAMP=$(date +%Y-%m-%d)
 DOCKER_REPO="letsencrypt/boulder-tools"
 
 # When updating these GO_VERSIONS, please also update
-# .github/workflows/release.yml and
-# .github/workflows/try-release.yml if appropriate.
-GO_VERSIONS=( "1.19.5" "1.20" )
+# .github/workflows/release.yml,
+# .github/workflows/try-release.yml if appropriate,
+# .github/workflows/boulder-ci.yml with the new container tag.
+GO_VERSIONS=( "1.19.5" "1.19.6" "1.21" )
 
 echo "Please login to allow push to DockerHub"
 docker login
@@ -28,5 +29,6 @@ do
     --platform=linux/amd64,linux/arm64 .
 done
 
-# TODO(@cpu): Figure out a `sed` for updating the date in `docker-compose.yml`'s
-# `image` lines with $DATESTAMP
+echo "Updating the docker-compose.yml BOULDER_TOOLS_TAG date stamp"
+OLD_DATESTAMP=$(grep 'BOULDER_TOOLS_TAG' docker-compose.yml | awk -F':' '{print $4}' | awk -F'_' '{print $2}' | sed 's/}$//')
+sed -i '' "s/${OLD_DATESTAMP}/${DATESTAMP}/" docker-compose.yml
