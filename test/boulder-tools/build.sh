f662332bcfe551e7cf3581199b1b0ec6d0c6bd43
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2023-02-16 09:35:39 -0800

    Speed up builds of boulder-tools images. (#6663)
    Only build arm64 images for one version of Go.
    
    Split build.sh into two scripts: build.sh (which installs apt and
    Python) and install-go.sh (which installs a specific Go version and Go
    dependencies). This allows reusing a cached layer for the build.sh step
    across multiple Go versions.
    
    Remove installation of fpm from build.sh. This is no longer needed since
    #6669 and allows us to get rid of `rpm`, `ruby`, and `ruby-dev`.
    
    Remove apt dependency on pkg-config, libtool, autoconf, and automake.
    These were introduced in
    https://github.com/letsencrypt/boulder/pull/4832 but aren't needed
    anymore because we don't build softhsm2 ourselves (we get it from apt).
    
    Remove apt dependency on cmake, libssl-dev, and openssl. I'm not totally
    sure what these were needed for but they're not needed anymore.
    
    Running this locally on my laptop for our current 3 GO_CI_VERSIONS and 1
    GO_DEV_VERSION takes 23 minutes of wall time, dominated by the cross
    build for arm64.

--- test/boulder-tools/build.sh
+++ test/boulder-tools/build.sh
@@ -5,24 +5,14 @@ apt-get update
 # Install system deps
 apt-get install -y --no-install-recommends \
   mariadb-client-core-10.3 \
-  rpm \
-  ruby \
-  ruby-dev \
   rsyslog \
   build-essential \
-  cmake \
-  libssl-dev \
   opensc \
   unzip \
   python3-pip \
   gcc \
   ca-certificates \
-  openssl \
-  softhsm2 \
-  pkg-config \
-  libtool \
-  autoconf \
-  automake
+  softhsm2
 
 PROTO_ARCH=x86_64
 if [ "${TARGETPLATFORM}" = linux/arm64 ]
@@ -33,28 +23,8 @@ fi
 curl -L https://github.com/google/protobuf/releases/download/v3.20.1/protoc-3.20.1-linux-"${PROTO_ARCH}".zip -o /tmp/protoc.zip
 unzip /tmp/protoc.zip -d /usr/local/protoc
 
-# Override default GOBIN and GOCACHE
-export GOBIN=/usr/local/bin GOCACHE=/tmp/gocache
-
-# Install protobuf and testing/dev tools.
-# Note: The version of golang/protobuf is partially tied to the version of grpc
-# used by Boulder overall. Updating it may require updating the grpc version
-# and vice versa.
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.0
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2.0
-go install github.com/rubenv/sql-migrate/...@v1.1.2
-go install golang.org/x/tools/cmd/stringer@latest
-go install github.com/letsencrypt/pebble/cmd/pebble-challtestsrv@master
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.0
-
-go clean -cache
-go clean -modcache
-
 pip3 install -r /tmp/requirements.txt
 
-gem install --no-document -v 1.14.2 fpm
-
-apt-get autoremove -y libssl-dev ruby-dev cmake pkg-config libtool autoconf automake
 apt-get clean -y
 
 # Tell git to trust the directory where the boulder repo volume is mounted
