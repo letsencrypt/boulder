7c3903c0901774239e687d4f1240405c0f6d5765
Matthew McPherrin <mattm@letsencrypt.org>
2023-03-20 09:57:54 -0400

    Update test/format-configs.py
    Co-authored-by: Aaron Gable <aaron@aarongable.com>

--- test/format-configs.py
+++ test/format-configs.py
@@ -5,7 +5,7 @@ import json
 import sys
 
 if len(sys.argv) != 2:
-  print("This program reformats JSON.  It takes a glob pattern as input")
+  print("This program reformats JSON files in-place. You must provide a glob pattern of files to process as its argument.")
 else:
   for cfg in glob.glob(sys.argv[1]):
     with open(cfg, "r") as fr:
