310546a14e04d7a24a78c828817559ff98d32588
Samantha <hello@entropy.cat>
2023-05-12 12:54:31 -0400

    VA: Support discovery of DNS resolvers via Consul (#6869)
    Deprecate `va.DNSResolver` in favor of backwards compatible
    `va.DNSProvider`.
    
    Fixes #6852

--- cmd/config.go
+++ cmd/config.go
@@ -237,8 +237,8 @@ type SyslogConfig struct {
 	SyslogLevel int `validate:"min=-1,max=7"`
 }
 
-// ServiceDomain contains the service and domain name the gRPC client will use
-// to construct a SRV DNS query to lookup backends.
+// ServiceDomain contains the service and domain name the gRPC or bdns provider
+// will use to construct a SRV DNS query to lookup backends.
 type ServiceDomain struct {
 	Service string `validate:"required"`
 	Domain  string `validate:"required"`
@@ -265,8 +265,8 @@ type GRPCClientConfig struct {
 	// the 'Domain' is 'service.consul'. The expected dNSName to be
 	// authenticated in the server certificate would be 'foo.service.consul'.
 	//
-	// Note: The 'proto' field of the SRV record MUST be 'tcp' and the 'port'
-	// field MUST be contain valid port. In a Consul configuration file you
+	// Note: The 'proto' field of the SRV record MUST contain 'tcp' and the
+	// 'port' field MUST be a valid port. In a Consul configuration file you
 	// would specify 'foo.service.consul' as:
 	//
 	// services {
@@ -493,4 +493,53 @@ type OpenTelemetryConfig struct {
 	// ensure that any external API users don't influence our own sampling
 	// decisions.
 	DisableParentSampler bool
+}
+
+// DNSProvider contains the configuration for a DNS provider in the bdns package
+// which supports dynamic reloading of its backends.
+type DNSProvider struct {
+	// DNSAuthority is the single <hostname|IPv4|[IPv6]>:<port> of the DNS
+	// server to be used for resolution of DNS backends. If the address contains
+	// a hostname it will be resolved via the system DNS. If the port is left
+	// unspecified it will default to '53'. If this field is left unspecified
+	// the system DNS will be used for resolution of DNS backends.
+	//
+	// TODO(#6868): Make this field required once 'dnsResolver' is removed from
+	// the boulder-va JSON config in favor of 'dnsProvider'.
+	DNSAuthority string `validate:"omitempty,ip|hostname|hostname_port"`
+
+	// SRVLookup contains the service and domain name used to construct a SRV
+	// DNS query to lookup DNS backends. 'Domain' is required. 'Service' is
+	// optional and will be defaulted to 'dns' if left unspecified.
+	//
+	// Usage: If the resource record is 'unbound.service.consul', then the
+	// 'Service' is 'unbound' and the 'Domain' is 'service.consul'. The expected
+	// dNSName to be authenticated in the server certificate would be
+	// 'unbound.service.consul'. The 'proto' field of the SRV record MUST
+	// contain 'udp' and the 'port' field MUST be a valid port. In a Consul
+	// configuration file you would specify 'unbound.service.consul' as:
+	//
+	// services {
+	//   id      = "unbound-1" // Must be unique
+	//   name    = "unbound"
+	//   address = "10.77.77.77"
+	//   port    = 53
+	//   tags    = ["udp"]
+	// }
+	//
+	// services {
+	//   id      = "unbound-2" // Must be unique
+	//   name    = "unbound"
+	//   address = "10.88.88.88"
+	//   port    = 53
+	//   tags    = ["udp"]
+	// }
+	//
+	// If you've added the above to your Consul configuration file (and reloaded
+	// Consul) then you should be able to resolve the following dig query:
+	//
+	// $ dig @10.55.55.10 -t SRV _unbound._udp.service.consul +short
+	// 1 1 53 0a585858.addr.dc1.consul.
+	// 1 1 53 0a4d4d4d.addr.dc1.consul.
+	SRVLookup ServiceDomain `validate:"required"`
 }
