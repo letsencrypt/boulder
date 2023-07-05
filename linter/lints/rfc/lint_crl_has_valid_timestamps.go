5e379fe38ac5c6b0bd13a0f3fca49de8fe492897
Aaron Gable <aaron@letsencrypt.org>
2023-07-05 13:54:28 -0700

    Rewrite hasValidTimestamps

--- linter/lints/rfc/lint_crl_has_valid_timestamps.go
+++ linter/lints/rfc/lint_crl_has_valid_timestamps.go
@@ -5,33 +5,80 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/letsencrypt/boulder/crl/crl_x509"
+	"github.com/zmap/zcrypto/x509"
 	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/util"
 	"golang.org/x/crypto/cryptobyte"
+	cryptobyte_asn1 "golang.org/x/crypto/cryptobyte/asn1"
 )
 
-// hasValidTimestamps validates encoding of all CRL timestamp values as
-// specified in section 4.1.2.5 of RFC5280. Timestamp values MUST be encoded as
-// either UTCTime or a GeneralizedTime.
-//
-// UTCTime values MUST be expressed in Greenwich Mean Time (Zulu) and MUST
-// include seconds (i.e., times are YYMMDDHHMMSSZ), even where the number of
-// seconds is zero. See:
-// https://www.rfc-editor.org/rfc/rfc5280.html#section-4.1.2.5.1
-//
-// GeneralizedTime values MUST be expressed in Greenwich Mean Time (Zulu) and
-// MUST include seconds (i.e., times are YYYYMMDDHHMMSSZ), even where the number
-// of seconds is zero.  GeneralizedTime values MUST NOT include fractional
-// seconds. See: https://www.rfc-editor.org/rfc/rfc5280.html#section-4.1.2.5.2
-//
-// Conforming applications MUST encode thisUpdate, nextUpdate, and cerficate
-// validity timestamps prior to 2050 as UTCTime and GeneralizedTime there-after.
-// See:
-//   - https://www.rfc-editor.org/rfc/rfc5280.html#section-5.1.2.4
-//   - https://www.rfc-editor.org/rfc/rfc5280.html#section-5.1.2.5
-//   - https://www.rfc-editor.org/rfc/rfc5280.html#section-5.1.2.6
-func hasValidTimestamps(crl *crl_x509.RevocationList) *lint.LintResult {
-	input := cryptobyte.String(crl.RawTBSRevocationList)
+const (
+	utcTimeFormat         = "YYMMDDHHMMSSZ"
+	generalizedTimeFormat = "YYYYMMDDHHMMSSZ"
+)
+
+type crlHasValidTimestamps struct{}
+
+/************************************************
+RFC 5280: 5.1.2.4
+CRL issuers conforming to this profile MUST encode thisUpdate as UTCTime for
+dates through the year 2049. CRL issuers conforming to this profile MUST encode
+thisUpdate as GeneralizedTime for dates in the year 2050 or later. Conforming
+applications MUST be able to process dates that are encoded in either UTCTime or
+GeneralizedTime.
+
+Where encoded as UTCTime, thisUpdate MUST be specified and interpreted as
+defined in Section 4.1.2.5.1. Where encoded as GeneralizedTime, thisUpdate MUST
+be specified and interpreted as defined in Section 4.1.2.5.2.
+
+RFC 5280: 5.1.2.5
+CRL issuers conforming to this profile MUST encode nextUpdate as UTCTime for
+dates through the year 2049. CRL issuers conforming to this profile MUST encode
+nextUpdate as GeneralizedTime for dates in the year 2050 or later. Conforming
+applications MUST be able to process dates that are encoded in either UTCTime or
+GeneralizedTime.
+
+Where encoded as UTCTime, nextUpdate MUST be specified and interpreted as
+defined in Section 4.1.2.5.1. Where encoded as GeneralizedTime, nextUpdate MUST
+be specified and interpreted as defined in Section 4.1.2.5.2.
+
+RFC 5280: 5.1.2.6
+The time for revocationDate MUST be expressed as described in Section 5.1.2.4.
+
+RFC 5280: 4.1.2.5.1
+UTCTime values MUST be expressed in Greenwich Mean Time (Zulu) and MUST include
+seconds (i.e., times are YYMMDDHHMMSSZ), even where the number of seconds is
+zero.
+
+RFC 5280: 4.1.2.5.2
+GeneralizedTime values MUST be expressed in Greenwich Mean Time (Zulu) and MUST
+include seconds (i.e., times are YYYYMMDDHHMMSSZ), even where the number of
+seconds is zero. GeneralizedTime values MUST NOT include fractional seconds.
+************************************************/
+
+func init() {
+	lint.RegisterRevocationListLint(&lint.RevocationListLint{
+		LintMetadata: lint.LintMetadata{
+			Name:          "e_crl_has_valid_timestamps",
+			Description:   "CRL thisUpdate, nextUpdate, and revocationDates must be properly encoded",
+			Citation:      "RFC 5280: 5.1.2.4, 5.1.2.5, and 5.1.2.6",
+			Source:        lint.RFC5280,
+			EffectiveDate: util.RFC5280Date,
+		},
+		Lint: NewCrlHasValidTimestamps,
+	})
+}
+
+func NewCrlHasValidTimestamps() lint.RevocationListLintInterface {
+	return &crlHasValidTimestamps{}
+}
+
+func (l *crlHasValidTimestamps) CheckApplies(c *x509.RevocationList) bool {
+	return true
+}
+
+func (l *crlHasValidTimestamps) Execute(c *x509.RevocationList) *lint.LintResult {
+	input := cryptobyte.String(c.RawTBSRevocationList)
 	lintFail := lint.LintResult{
 		Status:  lint.Error,
 		Details: "Failed to re-parse tbsCertList during linting",
