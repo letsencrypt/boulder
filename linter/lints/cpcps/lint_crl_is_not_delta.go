ff67db9ad13946f8bd720fc8c483a7327644a069
Aaron Gable <aaron@letsencrypt.org>
2023-07-05 14:03:25 -0700

    Rewrite isNotDelta

--- linter/lints/cpcps/lint_crl_is_not_delta.go
+++ linter/lints/cpcps/lint_crl_is_not_delta.go
@@ -1,18 +1,47 @@
 package cpcps
 
 import (
-	"encoding/asn1"
-
-	"github.com/letsencrypt/boulder/crl/crl_x509"
+	"github.com/zmap/zcrypto/encoding/asn1"
+	"github.com/zmap/zcrypto/x509"
 	"github.com/zmap/zlint/v3/lint"
+
+	"github.com/letsencrypt/boulder/linter/lints"
 )
 
-// isNotDelta checks that the CRL is not a Delta CRL. (RFC 5280, Section 5.2.4).
-// There's no requirement against this, but Delta CRLs come with extra
-// requirements we don't want to deal with.
-func isNotDelta(crl *crl_x509.RevocationList) *lint.LintResult {
+type crlIsNotDelta struct{}
+
+/************************************************
+RFC 5280: 5.2.4
+
+Section 5.2.4 defines a Delta CRL, and all the requirements that come with it.
+These requirements are complex and do not serve our purpose, so we ensure that
+we never issue a CRL which could be construed as a Delta CRL.
+************************************************/
+
+func init() {
+	lint.RegisterRevocationListLint(&lint.RevocationListLint{
+		LintMetadata: lint.LintMetadata{
+			Name:          "e_crl_is_not_delta",
+			Description:   "Let's Encrypt does not issue delta CRLs",
+			Citation:      "",
+			Source:        lints.LetsEncryptCPS,
+			EffectiveDate: lints.CPSV33Date,
+		},
+		Lint: NewCrlIsNotDelta,
+	})
+}
+
+func NewCrlIsNotDelta() lint.RevocationListLintInterface {
+	return &crlIsNotDelta{}
+}
+
+func (l *crlIsNotDelta) CheckApplies(c *x509.RevocationList) bool {
+	return true
+}
+
+func (l *crlIsNotDelta) Execute(c *x509.RevocationList) *lint.LintResult {
 	deltaCRLIndicatorOID := asn1.ObjectIdentifier{2, 5, 29, 27} // id-ce-deltaCRLIndicator
-	if getExtWithOID(crl.Extensions, deltaCRLIndicatorOID) != nil {
+	if lints.GetExtWithOID(c.Extensions, deltaCRLIndicatorOID) != nil {
 		return &lint.LintResult{
 			Status:  lint.Notice,
 			Details: "CRL is a Delta CRL",
