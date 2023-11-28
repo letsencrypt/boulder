e59f59c21bbef9b87388013ab88018c061aabf43
Aaron Gable <aaron@letsencrypt.org>
2023-11-28 11:34:04 -0800

    Split issuance.go into issuer.go and cert.go (#7167)
    This will make it easier to add a crl.go, holding functionality similar
    to cert.go, without making any single file overly complex.
    
    This introduces no functionality changes.
    
    Part of https://github.com/letsencrypt/boulder/issues/7159

--- issuance/cert_test.go
+++ issuance/cert_test.go
@@ -4,7 +4,6 @@ import (
 	"crypto"
 	"crypto/dsa"
 	"crypto/ecdsa"
-	"crypto/ed25519"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
@@ -12,71 +11,20 @@ import (
 	"crypto/x509/pkix"
 	"encoding/asn1"
 	"encoding/base64"
-	"fmt"
-	"math/big"
-	"os"
-	"strings"
 	"testing"
 	"time"
 
 	ct "github.com/google/certificate-transparency-go"
 	"github.com/jmhodges/clock"
 
-	"github.com/letsencrypt/boulder/cmd"
-	"github.com/letsencrypt/boulder/config"
-	"github.com/letsencrypt/boulder/core"
 	"github.com/letsencrypt/boulder/ctpolicy/loglist"
 	"github.com/letsencrypt/boulder/linter"
 	"github.com/letsencrypt/boulder/test"
 )
 
-func defaultProfileConfig() ProfileConfig {
-	return ProfileConfig{
-		AllowCommonName:     true,
-		AllowCTPoison:       true,
-		AllowSCTList:        true,
-		AllowMustStaple:     true,
-		MaxValidityPeriod:   config.Duration{Duration: time.Hour},
-		MaxValidityBackdate: config.Duration{Duration: time.Hour},
-	}
-}
-
-func defaultIssuerConfig() IssuerConfig {
-	return IssuerConfig{
-		UseForECDSALeaves: true,
-		UseForRSALeaves:   true,
-		IssuerURL:         "http://issuer-url",
-		OCSPURL:           "http://ocsp-url",
-	}
-}
-
 func defaultProfile() *Profile {
 	p, _ := NewProfile(defaultProfileConfig(), defaultIssuerConfig())
 	return p
-}
-
-var issuerCert *Certificate
-var issuerSigner *ecdsa.PrivateKey
-
-func TestMain(m *testing.M) {
-	tk, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	cmd.FailOnError(err, "failed to generate test key")
-	issuerSigner = tk
-	template := &x509.Certificate{
-		SerialNumber:          big.NewInt(123),
-		BasicConstraintsValid: true,
-		IsCA:                  true,
-		Subject: pkix.Name{
-			CommonName: "big ca",
-		},
-		KeyUsage: x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
-	}
-	issuer, err := x509.CreateCertificate(rand.Reader, template, template, tk.Public(), tk)
-	cmd.FailOnError(err, "failed to generate test issuer")
-	cert, err := x509.ParseCertificate(issuer)
-	cmd.FailOnError(err, "failed to parse test issuer")
-	issuerCert = &Certificate{Certificate: cert}
-	os.Exit(m.Run())
 }
 
 func TestNewProfileNoIssuerURL(t *testing.T) {
@@ -331,92 +279,6 @@ func TestGenerateTemplate(t *testing.T) {
 			test.AssertDeepEquals(t, *template, *tc.expectedTemplate)
 		})
 	}
-}
-
-func TestNewIssuer(t *testing.T) {
-	_, err := NewIssuer(
-		issuerCert,
-		issuerSigner,
-		defaultProfile(),
-		&linter.Linter{},
-		clock.NewFake(),
-	)
-	test.AssertNotError(t, err, "NewIssuer failed")
-}
-
-func TestNewIssuerUnsupportedKeyType(t *testing.T) {
-	_, err := NewIssuer(
-		&Certificate{
-			Certificate: &x509.Certificate{
-				PublicKey: &ed25519.PublicKey{},
-			},
-		},
-		&ed25519.PrivateKey{},
-		defaultProfile(),
-		&linter.Linter{},
-		clock.NewFake(),
-	)
-	test.AssertError(t, err, "NewIssuer didn't fail")
-	test.AssertEquals(t, err.Error(), "unsupported issuer key type")
-}
-
-func TestNewIssuerNoCertSign(t *testing.T) {
-	_, err := NewIssuer(
-		&Certificate{
-			Certificate: &x509.Certificate{
-				PublicKey: &ecdsa.PublicKey{
-					Curve: elliptic.P256(),
-				},
-				KeyUsage: 0,
-			},
-		},
-		issuerSigner,
-		defaultProfile(),
-		&linter.Linter{},
-		clock.NewFake(),
-	)
-	test.AssertError(t, err, "NewIssuer didn't fail")
-	test.AssertEquals(t, err.Error(), "end-entity signing cert does not have keyUsage certSign")
-}
-
-func TestNewIssuerNoDigitalSignature(t *testing.T) {
-	_, err := NewIssuer(
-		&Certificate{
-			Certificate: &x509.Certificate{
-				PublicKey: &ecdsa.PublicKey{
-					Curve: elliptic.P256(),
-				},
-				KeyUsage: x509.KeyUsageCertSign,
-			},
-		},
-		issuerSigner,
-		defaultProfile(),
-		&linter.Linter{},
-		clock.NewFake(),
-	)
-	test.AssertError(t, err, "NewIssuer didn't fail")
-	test.AssertEquals(t, err.Error(), "end-entity ocsp signing cert does not have keyUsage digitalSignature")
-}
-
-func TestNewIssuerOCSPOnly(t *testing.T) {
-	p := defaultProfile()
-	p.useForRSALeaves = false
-	p.useForECDSALeaves = false
-	_, err := NewIssuer(
-		&Certificate{
-			Certificate: &x509.Certificate{
-				PublicKey: &ecdsa.PublicKey{
-					Curve: elliptic.P256(),
-				},
-				KeyUsage: x509.KeyUsageDigitalSignature,
-			},
-		},
-		issuerSigner,
-		p,
-		&linter.Linter{},
-		clock.NewFake(),
-	)
-	test.AssertNotError(t, err, "NewIssuer failed")
 }
 
 func TestIssue(t *testing.T) {
@@ -745,62 +607,6 @@ func TestIssueBadLint(t *testing.T) {
 	test.AssertError(t, err, "Prepare didn't fail")
 	test.AssertErrorIs(t, err, linter.ErrLinting)
 	test.AssertContains(t, err.Error(), "tbsCertificate linting failed: failed lint(s)")
-}
-
-func TestLoadChain_Valid(t *testing.T) {
-	chain, err := LoadChain([]string{
-		"../test/test-ca-cross.pem",
-		"../test/test-root2.pem",
-	})
-	test.AssertNotError(t, err, "Should load valid chain")
-
-	expectedIssuer, err := core.LoadCert("../test/test-ca-cross.pem")
-	test.AssertNotError(t, err, "Failed to load test issuer")
-
-	chainIssuer := chain[0]
-	test.AssertNotNil(t, chainIssuer, "Failed to decode chain PEM")
-
-	test.AssertByteEquals(t, chainIssuer.Raw, expectedIssuer.Raw)
-}
-
-func TestLoadChain_TooShort(t *testing.T) {
-	_, err := LoadChain([]string{"/path/to/one/cert.pem"})
-	test.AssertError(t, err, "Should reject too-short chain")
-}
-
-func TestLoadChain_Unloadable(t *testing.T) {
-	_, err := LoadChain([]string{
-		"does-not-exist.pem",
-		"../test/test-root2.pem",
-	})
-	test.AssertError(t, err, "Should reject unloadable chain")
-
-	_, err = LoadChain([]string{
-		"../test/test-ca-cross.pem",
-		"does-not-exist.pem",
-	})
-	test.AssertError(t, err, "Should reject unloadable chain")
-
-	invalidPEMFile, _ := os.CreateTemp("", "invalid.pem")
-	err = os.WriteFile(invalidPEMFile.Name(), []byte(""), 0640)
-	test.AssertNotError(t, err, "Error writing invalid PEM tmp file")
-	_, err = LoadChain([]string{
-		invalidPEMFile.Name(),
-		"../test/test-root2.pem",
-	})
-	test.AssertError(t, err, "Should reject unloadable chain")
-}
-
-func TestLoadChain_InvalidSig(t *testing.T) {
-	_, err := LoadChain([]string{
-		"../test/test-root2.pem",
-		"../test/test-ca-cross.pem",
-	})
-	test.AssertError(t, err, "Should reject invalid signature")
-	test.Assert(t, strings.Contains(err.Error(), "test-ca-cross.pem"),
-		fmt.Sprintf("Expected error to mention filename, got: %s", err))
-	test.Assert(t, strings.Contains(err.Error(), "signature from \"CN=happy hacker fake CA\""),
-		fmt.Sprintf("Expected error to mention subject, got: %s", err))
 }
 
 func TestIssuanceToken(t *testing.T) {
