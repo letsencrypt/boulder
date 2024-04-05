e285db8cbbf6b6a4e42a5423f7af2c6168db64e2
Phil Porada <philporada@gmail.com>
2024-04-05 16:59:39 -0400

    Initial commit

--- issuance/crl_test.go
+++ issuance/crl_test.go
@@ -2,12 +2,15 @@ package issuance
 
 import (
 	"crypto/x509"
+	"errors"
 	"math/big"
 	"testing"
 	"time"
 
 	"github.com/jmhodges/clock"
 	"github.com/zmap/zlint/v3/lint"
+	"golang.org/x/crypto/cryptobyte"
+	cryptobyte_asn1 "golang.org/x/crypto/cryptobyte/asn1"
 
 	"github.com/letsencrypt/boulder/config"
 	"github.com/letsencrypt/boulder/crl/idp"
@@ -151,4 +154,94 @@ func TestIssueCRL(t *testing.T) {
 	_, err = issuer.IssueCRL(&defaultProfile, &req)
 	test.AssertError(t, err, "crl issuance with no IDP should fail")
 	test.AssertContains(t, err.Error(), "must contain an issuingDistributionPoint")
+
+	emptyEntries := CRLRequest{
+		Number:               big.NewInt(123),
+		Shard:                100,
+		ThisUpdate:           clk.Now().Add(-time.Second),
+		Entries:              []x509.RevocationListEntry{},
+		DeprecatedIDPBaseURL: "http://old.crl.url",
+	}
+
+	res, err = issuer.IssueCRL(&defaultProfile, &emptyEntries)
+	test.AssertNotError(t, err, "parsing crl with no entries")
+	parsedRes, err = x509.ParseRevocationList(res)
+	test.AssertNotError(t, err, "parsing test crl")
+	test.AssertEquals(t, parsedRes.Issuer.CommonName, issuer.Cert.Subject.CommonName)
+	test.AssertDeepEquals(t, parsedRes.Number, big.NewInt(123))
+	err = checkRevocationListEntries(res)
+	test.AssertNotError(t, err, "parsing test crl with my code")
+
+	nilEntries := CRLRequest{
+		Number:               big.NewInt(123),
+		Shard:                100,
+		ThisUpdate:           clk.Now().Add(-time.Second),
+		Entries:              nil,
+		DeprecatedIDPBaseURL: "http://old.crl.url",
+	}
+
+	res, err = issuer.IssueCRL(&defaultProfile, &nilEntries)
+	test.AssertNotError(t, err, "parsing crl with no entries")
+	parsedRes, err = x509.ParseRevocationList(res)
+	test.AssertNotError(t, err, "parsing test crl")
+	test.AssertEquals(t, parsedRes.Issuer.CommonName, issuer.Cert.Subject.CommonName)
+	test.AssertDeepEquals(t, parsedRes.Number, big.NewInt(123))
+}
+
+// checkRevocationListEntries is a hacked up version of x509.ParseRevocationList
+// that only cares about the ASN1 TBS.CertList.revokedCertificates field.
+func checkRevocationListEntries(der []byte) error {
+	rl := &x509.RevocationList{}
+
+	input := cryptobyte.String(der)
+	// we read the SEQUENCE including length and tag bytes so that
+	// we can populate RevocationList.Raw, before unwrapping the
+	// SEQUENCE so it can be operated on
+	if !input.ReadASN1Element(&input, cryptobyte_asn1.SEQUENCE) {
+		return errors.New("x509: malformed crl")
+	}
+	rl.Raw = input
+	if !input.ReadASN1(&input, cryptobyte_asn1.SEQUENCE) {
+		return errors.New("x509: malformed crl")
+	}
+
+	var tbs cryptobyte.String
+	// do the same trick again as above to extract the raw
+	// bytes for Certificate.RawTBSCertificate
+	if !input.ReadASN1Element(&tbs, cryptobyte_asn1.SEQUENCE) {
+		return errors.New("x509: malformed tbs crl")
+	}
+	rl.RawTBSRevocationList = tbs
+	if !tbs.ReadASN1(&tbs, cryptobyte_asn1.SEQUENCE) {
+		return errors.New("x509: malformed tbs crl")
+	}
+
+	// Skip optional version
+	tbs.SkipOptionalASN1(cryptobyte_asn1.INTEGER)
+
+	// Skip the signature
+	tbs.SkipASN1(cryptobyte_asn1.SEQUENCE)
+
+	// Skip the issuer
+	tbs.SkipASN1(cryptobyte_asn1.SEQUENCE)
+
+	// SkipOptionalASN1 is identical to SkipASN1 except that it also does a
+	// peek. We'll handle the non-optional thisUpdate with these double peeks
+	// because there's no harm doing so.
+	skipTime := func(s cryptobyte.String) {
+		switch {
+		case s.PeekASN1Tag(cryptobyte_asn1.UTCTime):
+			s.SkipOptionalASN1(cryptobyte_asn1.UTCTime)
+		case s.PeekASN1Tag(cryptobyte_asn1.GeneralizedTime):
+			s.SkipOptionalASN1(cryptobyte_asn1.GeneralizedTime)
+		}
+	}
+
+	// Skip thisUpdate
+	skipTime(tbs)
+
+	// Skip optional nextUpdate
+	skipTime(tbs)
+
+	return nil
 }
