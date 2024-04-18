c59874ceee8cbfa99a299062a334893206a321d1
Phil Porada <philporada@gmail.com>
2024-04-18 11:14:47 -0400

    Fix unit test

--- cmd/boulder/main_test.go
+++ cmd/boulder/main_test.go
@@ -37,10 +37,17 @@ func TestConfigValidation(t *testing.T) {
 			fileNames = []string{"publisher.json"}
 		case "boulder-ra":
 			fileNames = []string{"ra.json"}
+		// TODO(#5294) After we have separate RVA and VA binaries deployed,
+		// rename boulder-remoteva2 to boulder-remoteva.
 		case "boulder-remoteva":
 			fileNames = []string{
 				"va-remote-a.json",
 				"va-remote-b.json",
+			}
+		case "boulder-remoteva2":
+			fileNames = []string{
+				"remoteva2-a.json",
+				"remoteva2-b.json",
 			}
 		case "boulder-sa":
 			fileNames = []string{"sa.json"}
