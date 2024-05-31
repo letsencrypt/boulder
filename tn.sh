11131f70f3fd6f93f4ce05a75a2c8733b71855ac
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2024-05-31 13:37:58 -0700

    test: bail if bsetup fails

--- tn.sh
+++ tn.sh
@@ -3,12 +3,14 @@
 # Outer wrapper for invoking test.sh with config-next inside docker-compose.
 #
 
+set -o errexit
+
 if type realpath >/dev/null 2>&1 ; then
   cd "$(realpath -- $(dirname -- "$0"))"
 fi
 
 # Generate the test keys and certs necessary for the integration tests.
-docker compose up bsetup
+docker compose run bsetup
 
 # Use a predictable name for the container so we can grab the logs later
 # for use when testing logs analysis tools.
