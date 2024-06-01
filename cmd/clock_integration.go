764f6c2f7afe9aa5fec7022b52cc6b9e1da3e40f
Phil Porada <pgporada@users.noreply.github.com>
2024-05-31 21:34:39 -0400

    cmd: Use non-deprecated clock.New (#7517)
    Switch from using the deprecated `clock.Default()` to `clock.New()`.
    Both return the same value of `systemClock`.

--- cmd/clock_integration.go
+++ cmd/clock_integration.go
@@ -8,10 +8,11 @@ import (
 	"time"
 
 	"github.com/jmhodges/clock"
+
 	blog "github.com/letsencrypt/boulder/log"
 )
 
-// Clock functions similarly to clock.Default(), but the returned value can be
+// Clock functions similarly to clock.New(), but the returned value can be
 // changed using the FAKECLOCK environment variable if the 'integration' build
 // flag is set.
 //
@@ -26,5 +27,6 @@ func Clock() clock.Clock {
 		blog.Get().Infof("Time was set to %v via FAKECLOCK", targetTime)
 		return cl
 	}
-	return clock.Default()
+
+	return clock.New()
 }
