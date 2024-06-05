3786401f107be28f550366bc88c4a3bfc99e9a8b
Aaron Gable <aaron@letsencrypt.org>
2024-06-05 12:14:10 -0700

    Improve redis rate limit construction errors (#7525)
    Change ratelimits.validateIdForName to call the appropriate validate
    function based on the contents of the to-be-validated ID, rather than
    falling back and potentially performing multiple validations.
    
    Previously, if an ID like "12345:bad.domain" was passed into this
    function, it would fail the first validateRegIdDomain validation due to
    having a bad domain name (no TLD), fall back to the simpler
    validateRegId function, and then fail that because it contains a colon.
    This obscured the true reason for the failure. Changing this code to not
    fall back means that the true reason for the id validation failure will
    be exposed in the error message.

--- ratelimits/names.go
+++ ratelimits/names.go
@@ -212,22 +212,22 @@ func validateIdForName(name Name, id string) error {
 		return validateRegId(id)
 
 	case FailedAuthorizationsPerDomainPerAccount:
-		// 'enum:regId:domain' for transaction
-		err := validateRegIdDomain(id)
-		if err == nil {
-			return nil
+		if strings.Contains(id, ":") {
+			// 'enum:regId:domain' for transaction
+			return validateRegIdDomain(id)
+		} else {
+			// 'enum:regId' for overrides
+			return validateRegId(id)
 		}
-		// 'enum:regId' for overrides
-		return validateRegId(id)
 
 	case CertificatesPerDomainPerAccount:
-		// 'enum:regId:domain' for transaction
-		err := validateRegIdDomain(id)
-		if err == nil {
-			return nil
+		if strings.Contains(id, ":") {
+			// 'enum:regId:domain' for transaction
+			return validateRegIdDomain(id)
+		} else {
+			// 'enum:regId' for overrides
+			return validateRegId(id)
 		}
-		// 'enum:regId' for overrides
-		return validateRegId(id)
 
 	case CertificatesPerDomain:
 		// 'enum:domain'
