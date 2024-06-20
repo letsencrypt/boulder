8c324a5e8a8891eb2863c81c727e6a1ad918624b
Phil Porada <pgporada@users.noreply.github.com>
2024-06-20 11:21:46 -0400

    RA: Add UnpauseAccountRequest protobuf message and service (#7537)
    Add the `ra.UnpauseAccount` which takes an `rapb.UnpauseAccountRequest`
    input parameter. The method is just a stub to allow downstream SFE
    development to continue. There is relevant ongoing work in the SA which
    will eventually reside in this stub method.

--- wfe2/wfe_test.go
+++ wfe2/wfe_test.go
@@ -235,6 +235,10 @@ func (ra *MockRegistrationAuthority) DeactivateRegistration(context.Context, *co
 	return &emptypb.Empty{}, nil
 }
 
+func (ra *MockRegistrationAuthority) UnpauseAccount(context.Context, *rapb.UnpauseAccountRequest, ...grpc.CallOption) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
+
 func (ra *MockRegistrationAuthority) NewOrder(ctx context.Context, in *rapb.NewOrderRequest, _ ...grpc.CallOption) (*corepb.Order, error) {
 	created := time.Date(2021, 1, 1, 1, 1, 1, 0, time.UTC)
 	expires := time.Date(2021, 2, 1, 1, 1, 1, 0, time.UTC)
