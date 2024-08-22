0708a6ed9cffc47da4801f24ce54268792c7bf67
Samantha <hello@entropy.cat>
2024-08-22 11:38:42 -0400

    Addressing comments.

--- test/integration/ratelimit_test.go
+++ test/integration/ratelimit_test.go
@@ -46,7 +46,8 @@ func TestCertificatesPerDomain(t *testing.T) {
 		return fmt.Sprintf("%s.%s", hex.EncodeToString(bytes[:]), randomDomain)
 	}
 
-	_, err := authAndIssue(nil, nil, []string{randomSubDomain()}, true)
+	firstSubDomain := randomSubDomain()
+	_, err := authAndIssue(nil, nil, []string{firstSubDomain}, true)
 	test.AssertNotError(t, err, "Failed to issue first certificate")
 
 	_, err = authAndIssue(nil, nil, []string{randomSubDomain()}, true)
@@ -62,38 +63,9 @@ func TestCertificatesPerDomain(t *testing.T) {
 		// Error should be served from legacy rate limits implementation.
 		test.AssertContains(t, err.Error(), fmt.Sprintf("too many certificates already issued for %q", randomDomain))
 	}
-}
 
-func TestRenewalExemption(t *testing.T) {
-	t.Parallel()
-
-	// Issue two certificates for different subdomains under a single domain,
-	// then renew both. With the certificatesPerName limit at 2 per 90 days, and
-	// renewals not exempt, both issuances should succeed. Finally, issue a
-	// certificate for a third subdomain, which should fail due to the limit.
-
-	baseDomain := random_domain()
-
-	_, err := authAndIssue(nil, nil, []string{"www." + baseDomain}, true)
-	test.AssertNotError(t, err, "Failed to issue first certificate")
-
-	_, err = authAndIssue(nil, nil, []string{"www." + baseDomain}, true)
-	test.AssertNotError(t, err, "Failed to issue first renewal")
-
-	_, err = authAndIssue(nil, nil, []string{"blog." + baseDomain}, true)
-	test.AssertNotError(t, err, "Failed to issue second certificate")
-
-	_, err = authAndIssue(nil, nil, []string{"blog." + baseDomain}, true)
-	test.AssertNotError(t, err, "Failed to issue second renewal")
-
-	_, err = authAndIssue(nil, nil, []string{"mail." + baseDomain}, true)
-	test.AssertError(t, err, "Somehow managed to issue third certificate")
-
-	if strings.Contains(os.Getenv("BOULDER_CONFIG_DIR"), "test/config-next") {
-		// Error should be served from key-value rate limits implementation.
-		test.AssertContains(t, err.Error(), fmt.Sprintf("too many certificates (2) already issued for %q in the last 2160h0m0s", baseDomain))
-	} else {
-		// Error should be served from legacy rate limits implementation.
-		test.AssertContains(t, err.Error(), fmt.Sprintf("too many certificates already issued for %q", baseDomain))
-	}
+	// Issue a certificate for the first subdomain, which should succeed because
+	// it's a renewal.
+	_, err = authAndIssue(nil, nil, []string{firstSubDomain}, true)
+	test.AssertNotError(t, err, "Failed to issue renewal certificate")
 }
