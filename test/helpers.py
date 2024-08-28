e7990e0dcfe3be87c637244a177f2ddcd9658981
Aaron Gable <aaron@letsencrypt.org>
2024-08-27 17:24:59 -0700

    Run health-checker with a fake clock, too

--- test/helpers.py
+++ test/helpers.py
@@ -195,9 +195,12 @@ def waitport(port, prog, perTickCheck=None):
                 raise
     raise(Exception("timed out waiting for debug port %d (%s)" % (port, prog)))
 
-def waithealth(prog, port, host_override):
+def waithealth(prog, port, host_override, fakeclock):
+    e = os.environ.copy()
+    if fakeclock:
+        e.setdefault("FAKECLOCK", fakeclock)
     subprocess.check_call([
         './bin/health-checker',
         '-addr', ("localhost:%d" % (port)),
         '-host-override', host_override,
-        '-config', os.path.join(config_dir, 'health-checker.json')])
+        '-config', os.path.join(config_dir, 'health-checker.json')], env=e)
