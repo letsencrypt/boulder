5e4d612b5692111ca75b37152ae09001a2989294
Aaron Gable <aaron@letsencrypt.org>
2024-08-29 16:38:56 -0700

    Add IP identifier type and constructor

--- identifier/identifier.go
+++ identifier/identifier.go
@@ -1,5 +1,9 @@
 // The identifier package defines types for RFC 8555 ACME identifiers.
+// It exists as a separate package to prevent an import loop between the core
+// and probs packages.
 package identifier
+
+import "net/netip"
 
 // IdentifierType is a named string type for registered ACME identifier types.
 // See https://tools.ietf.org/html/rfc8555#section-9.7.7
@@ -8,6 +12,8 @@ type IdentifierType string
 const (
 	// DNS is specified in RFC 8555 for DNS type identifiers.
 	DNS = IdentifierType("dns")
+	// IP is specified in RFC 8738
+	IP = IdentifierType("ip")
 )
 
 // ACMEIdentifier is a struct encoding an identifier that can be validated. The
@@ -28,5 +34,14 @@ func DNSIdentifier(domain string) ACMEIdentifier {
 	return ACMEIdentifier{
 		Type:  DNS,
 		Value: domain,
+	}
+}
+
+// IPIdentifier is a convenience function for creating an ACMEIdentifier with
+// Type IP for a given IP address.
+func IPIdentifier(ip netip.Addr) ACMEIdentifier {
+	return ACMEIdentifier{
+		Type:  IP,
+		Value: ip.StringExpanded(),
 	}
 }
