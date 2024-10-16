e966a9a9ad41293ff05aab12d4b1cf82ac13a930
kruti-s <ksutaria@andrew.cmu.edu>
2024-10-16 14:03:21 -0400

    more changes made

--- ratelimits/limiter_test.go
+++ ratelimits/limiter_test.go
@@ -536,7 +536,7 @@ func TestRateLimitError(t *testing.T) {
 						Burst:  7,
 						Period: config.Duration{Duration: time.Hour},
 					},
-					bucketKey: "4:12345:example.com",
+					bucketKey: "8:12345:example.com",
 				},
 			},
 			expectedErr:     "too many failed authorizations (7) for \"example.com\" in the last 1h0m0s, retry after 1970-01-01 00:00:15 UTC",
