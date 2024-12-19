13eaba0f3cd3d868ab3e1597c1e9f4e1fc329177
Aaron Gable <aaron@letsencrypt.org>
2024-12-19 11:56:34 -0800

    Fix orderModelv2 for nullable profile column

--- sa/sa.go
+++ sa/sa.go
@@ -654,7 +654,7 @@ func (ssa *SQLStorageAuthority) NewOrderAndAuthzs(ctx context.Context, req *sapb
 				RegistrationID:         req.NewOrder.RegistrationID,
 				Expires:                req.NewOrder.Expires.AsTime(),
 				Created:                created,
-				CertificateProfileName: req.NewOrder.CertificateProfileName,
+				CertificateProfileName: &req.NewOrder.CertificateProfileName,
 			}
 			err = tx.Insert(ctx, &omv2)
 			orderID = omv2.ID
@@ -1145,7 +1145,7 @@ func (ssa *SQLStorageAuthority) AddBlockedKey(ctx context.Context, req *sapb.Add
 	if !ok {
 		return nil, errors.New("unknown source")
 	}
-	cols, qs := blockedKeysColumns, "?, ?, ?, ?"
+	/*  */ cols, qs := blockedKeysColumns, "?, ?, ?, ?"
 	vals := []interface{}{
 		req.KeyHash,
 		req.Added.AsTime(),
