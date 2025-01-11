2920875b4f0138e4e810bf5d2b6889b3e9ebd438
James Renken <james@renken.is>
2025-01-10 22:59:30 -0800

    Fix MariaDB syntax

--- sa/db-next/boulder_sa/20250110000000_NullRegistrationsLockCol.sql
+++ sa/db-next/boulder_sa/20250110000000_NullRegistrationsLockCol.sql
@@ -2,9 +2,9 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-ALTER TABLE `registrations` ALTER COLUMN `LockCol` BIGINT(20) DEFAULT 0;
+ALTER TABLE `registrations` MODIFY COLUMN `LockCol` BIGINT(20) DEFAULT 0;
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
-ALTER TABLE `registrations` ALTER COLUMN `LockCol` BIGINT(20) NOT NULL;
+ALTER TABLE `registrations` MODIFY COLUMN `LockCol` BIGINT(20) NOT NULL;
