0eb591fc75fea729b637839ca7248bca99855229
Samantha <hello@entropy.cat>
2025-01-24 14:35:00 -0500

    Remove outdated comments.

--- tn.sh
+++ tn.sh
@@ -12,6 +12,4 @@ fi
 # Generate the test keys and certs necessary for the integration tests.
 docker compose run --rm bsetup
 
-# Use a predictable name for the container so we can grab the logs later
-# for use when testing logs analysis tools.
 exec docker compose -f docker-compose.yml -f docker-compose.next.yml run --rm --name boulder_tests boulder ./test.sh "$@"
