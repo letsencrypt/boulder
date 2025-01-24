5f3a080af51b8a38c627f95df17a4ca3215a7df8
Samantha <hello@entropy.cat>
2025-01-24 14:01:43 -0500

    test: No longer accumlate orphans for each test run

--- t.sh
+++ t.sh
@@ -10,9 +10,8 @@ if type realpath >/dev/null 2>&1 ; then
 fi
 
 # Generate the test keys and certs necessary for the integration tests.
-docker compose run bsetup
+docker compose run --rm bsetup
 
 # Use a predictable name for the container so we can grab the logs later
 # for use when testing logs analysis tools.
-docker rm boulder_tests || true
-exec docker compose run --name boulder_tests boulder ./test.sh "$@"
+exec docker compose run --rm --name boulder_tests boulder ./test.sh "$@"
