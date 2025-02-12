cdff8d1954087bda11fbe45d2e82b0cb27ed2bf2
Samantha <hello@entropy.cat>
2025-02-12 16:39:22 -0500

    email/exporter: Address comments

--- email/exporter.go
+++ email/exporter.go
@@ -36,19 +36,32 @@ var ErrQueueFull = errors.New("email export queue is full")
 
 // ExporterImpl implements the gRPC server and processes email exports.
 type ExporterImpl struct {
-	sync.RWMutex
+	exporterpb.UnsafeExporterServer
 
-	toSend []string
-	client *PardotClient
-	log    blog.Logger
+	sync.RWMutex
+	drainWG sync.WaitGroup
+
+	toSend               []string
+	client               *PardotClient
+	emailsHandledCounter prometheus.Counter
+	log                  blog.Logger
 }
+
+var _ exporterpb.ExporterServer = (*ExporterImpl)(nil)
 
 // NewExporterImpl creates a new ExporterImpl.
 func NewExporterImpl(client *PardotClient, scope prometheus.Registerer, logger blog.Logger) *ExporterImpl {
+	emailsHandledCounter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "email_exporter_emails_handled",
+		Help: "Total number of emails handled by the email exporter",
+	})
+	scope.MustRegister(emailsHandledCounter)
+
 	impl := &ExporterImpl{
-		toSend: make([]string, 0, queueCap),
-		client: client,
-		log:    logger,
+		toSend:               make([]string, 0, queueCap),
+		client:               client,
+		emailsHandledCounter: emailsHandledCounter,
+		log:                  logger,
 	}
 
 	queueGauge := prometheus.NewGaugeFunc(prometheus.GaugeOpts{
@@ -64,10 +77,10 @@ func NewExporterImpl(client *PardotClient, scope prometheus.Registerer, logger b
 	return impl
 }
 
-// UpsertEmails enqueues the provided email addresses. If the queue is near
+// CreateProspects enqueues the provided email addresses. If the queue is near
 // capacity, only enqueues as many emails as can fit. Returns ErrQueueFull if
 // some or all emails were dropped.
-func (impl *ExporterImpl) UpsertEmails(ctx context.Context, req *exporterpb.UpsertEmailsRequest) (*emptypb.Empty, error) {
+func (impl *ExporterImpl) CreateProspects(ctx context.Context, req *exporterpb.CreateProspectsRequest) (*emptypb.Empty, error) {
 	if core.IsAnyNilOrZero(req, req.Emails) {
 		return nil, berrors.InternalServerError("Incomplete UpsertEmails request")
 	}
@@ -76,26 +89,14 @@ func (impl *ExporterImpl) UpsertEmails(ctx context.Context, req *exporterpb.Upse
 	defer impl.Unlock()
 
 	spotsLeft := queueCap - len(impl.toSend)
-	if spotsLeft <= 0 {
+	if spotsLeft < len(req.Emails) {
 		return nil, ErrQueueFull
 	}
-
-	toAdd := req.Emails
-	if len(toAdd) > spotsLeft {
-		toAdd = toAdd[:spotsLeft]
-	}
-
-	impl.toSend = append(impl.toSend, toAdd...)
-
-	if len(toAdd) < len(req.Emails) {
-		impl.log.Errf("Dropped %d emails due to queue capacity", len(req.Emails)-len(toAdd))
-		return nil, ErrQueueFull
-	}
+	impl.toSend = append(impl.toSend, req.Emails...)
 
 	return &emptypb.Empty{}, nil
 }
 
-// takeEmail pops an email from the slice (LIFO).
 func (impl *ExporterImpl) takeEmail() (string, bool) {
 	impl.Lock()
 	defer impl.Unlock()
@@ -111,41 +112,31 @@ func (impl *ExporterImpl) takeEmail() (string, bool) {
 }
 
 // Start begins asynchronous processing of the email queue. When the parent
-// daemonCtx is cancelled switches into a draining mode.
+// daemonCtx is cancelled the queue will be drained and the workers will exit.
 func (impl *ExporterImpl) Start(daemonCtx context.Context) {
 	limiter := rate.NewLimiter(rate.Limit(rateLimit), numWorkers)
-	var wg sync.WaitGroup
 
 	worker := func() {
-		defer wg.Done()
-		draining := false
+		defer impl.drainWG.Done()
 		for {
-			if daemonCtx.Err() != nil {
-				draining = true
+			if daemonCtx.Err() != nil && len(impl.toSend) == 0 {
+				return
 			}
 
-			if draining {
-				err := limiter.Wait(context.Background())
-				if err != nil {
-					// This should never happen, we're using a background
-					// context.
-					impl.log.Errf("While draining: limiter wait error: %s", err)
-				}
-			} else {
-				err := limiter.Wait(daemonCtx)
-				if err != nil {
-					if errors.Is(err, context.Canceled) {
-						draining = true
-						continue
-					}
-					impl.log.Errf("While running: unexpected limiter wait error: %s", err)
+			err := limiter.Wait(daemonCtx)
+			if err != nil {
+				if errors.Is(err, context.Canceled) {
+					// Keep processing emails until the queue is drained.
 					continue
 				}
+				impl.log.Errf("Unexpected limiter wait error: %s", err)
+				continue
 			}
 
 			email, ok := impl.takeEmail()
 			if !ok {
-				if draining {
+				if daemonCtx.Err() != nil {
+					// Exit immediately.
 					return
 				}
 				// No emails to process, avoid busy-waiting.
@@ -153,17 +144,21 @@ func (impl *ExporterImpl) Start(daemonCtx context.Context) {
 				continue
 			}
 
-			err := impl.client.UpsertEmail(email)
+			err = impl.client.CreateProspect(email)
 			if err != nil {
 				impl.log.Errf("Failed to upsert email: %s", err)
 			}
+			impl.emailsHandledCounter.Inc()
 		}
 	}
 
 	for range numWorkers {
-		wg.Add(1)
+		impl.drainWG.Add(1)
 		go worker()
 	}
-	<-daemonCtx.Done()
-	wg.Wait()
+}
+
+// Drain blocks until all workers have finished processing the email queue.
+func (impl *ExporterImpl) Drain() {
+	impl.drainWG.Wait()
 }
