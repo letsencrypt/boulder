890deb6aacd13f96e1331cfe16c20c3d4231d9ca
Aaron Gable <aaron@letsencrypt.org>
2025-03-07 12:47:28 -0800

    Add integration test for no-op account contact update

--- test/integration/account_test.go
+++ test/integration/account_test.go
@@ -48,3 +48,37 @@ func TestAccountDeactivate(t *testing.T) {
 	// account object as it used to make the request, and a wholly missing
 	// contacts field doesn't overwrite whatever eggsampler was holding in memory.
 }
+
+func TestAccountUpdate_UnspecifiedContacts(t *testing.T) {
+	t.Parallel()
+
+	c, err := acme.NewClient("http://boulder.service.consul:4001/directory")
+	if err != nil {
+		t.Fatalf("failed to connect to acme directory: %s", err)
+	}
+
+	acctKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatalf("failed to generate account key: %s", err)
+	}
+
+	acct, err := c.NewAccount(acctKey, false, true, "mailto:example@"+random_domain())
+	if err != nil {
+		t.Fatalf("failed to create initial account: %s", err)
+	}
+
+	// This request does not include the Contact field, meaning that the contacts
+	// should remain unchanged (i.e. not be removed).
+	acct, err = c.UpdateAccount(acct)
+	if err != nil {
+		t.Errorf("failed to no-op update account: %s", err)
+	}
+	if len(acct.Contact) != 1 {
+		t.Errorf("unexpected number of contacts: want 1, got %d", len(acct.Contact))
+	}
+
+	// TODO: Figure out a way to test that we don't update contacts even when the
+	// rest of the request is non-empty. We can't do this today because eggsampler
+	// makes it impossible to include the `status` field in an UpdateAccount
+	// request (it exposes DeactivateAccount for that instead).
+}
