654542b665978f3182883ce036183747dd0a7ecf
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2025-04-09 15:58:51 -0700

    Clean up mocks

--- mocks/ca.go
+++ mocks/ca.go
@@ -2,17 +2,13 @@ package mocks
 
 import (
 	"context"
-	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/types/known/timestamppb"
 
 	capb "github.com/letsencrypt/boulder/ca/proto"
-	corepb "github.com/letsencrypt/boulder/core/proto"
 )
 
 // MockCA is a mock of a CA that always returns the cert from PEM in response to
@@ -23,54 +19,15 @@ type MockCA struct {
 
 // IssueCertificate is a mock
 func (ca *MockCA) IssueCertificate(ctx context.Context, req *capb.IssueCertificateRequest, _ ...grpc.CallOption) (*capb.IssueCertificateResponse, error) {
-	precert, err := ca.issuePrecertificate(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-	cert, err := ca.issueCertificateForPrecertificate(ctx, &capb.IssueCertificateForPrecertificateRequest{
-		DER:             precert.DER,
-		SCTs:            nil,
-		RegistrationID:  req.RegistrationID,
-		OrderID:         req.OrderID,
-		CertProfileHash: precert.CertProfileHash,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return &capb.IssueCertificateResponse{DER: cert.Der}, nil
-}
-
-// issuePrecertificate is a mock
-func (ca *MockCA) issuePrecertificate(_ context.Context, req *capb.IssueCertificateRequest, _ ...grpc.CallOption) (*capb.IssuePrecertificateResponse, error) {
 	if ca.PEM == nil {
 		return nil, fmt.Errorf("MockCA's PEM field must be set before calling IssueCertificate")
 	}
 	block, _ := pem.Decode(ca.PEM)
-	cert, err := x509.ParseCertificate(block.Bytes)
+	sampleDER, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		return nil, err
 	}
-	profHash := sha256.Sum256([]byte(req.CertProfileName))
-	return &capb.IssuePrecertificateResponse{
-		DER:             cert.Raw,
-		CertProfileHash: profHash[:8],
-		CertProfileName: req.CertProfileName,
-	}, nil
-}
-
-// issueCertificateForPrecertificate is a mock
-func (ca *MockCA) issueCertificateForPrecertificate(_ context.Context, req *capb.IssueCertificateForPrecertificateRequest, _ ...grpc.CallOption) (*corepb.Certificate, error) { //nolint:unparam // `error` is always nil
-	now := time.Now()
-	expires := now.Add(1 * time.Hour)
-
-	return &corepb.Certificate{
-		Der:            req.DER,
-		RegistrationID: 1,
-		Serial:         "mock",
-		Digest:         "mock",
-		Issued:         timestamppb.New(now),
-		Expires:        timestamppb.New(expires),
-	}, nil
+	return &capb.IssueCertificateResponse{DER: sampleDER.Raw}, nil
 }
 
 type MockOCSPGenerator struct{}
