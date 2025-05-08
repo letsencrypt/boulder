f86f88d5630da41a67e7f334ae2db460e815ef06
Aaron Gable <aaron@letsencrypt.org>
2025-05-07 18:29:14 -0700

    Include supported algs in badSignatureAlgorithm problem doc (#8170)
    Add an "algorithms" field to all problem documents, but tag it so it
    won't be included in the serialized json unless populated. Populate it
    only when the problem type is "badSignatureAlgorithm", as specified in
    RFC 8555 Section 6.2.
    
    The resulting problem document looks like this:
    ```json
    {
        "type": "urn:ietf:params:acme:error:badSignatureAlgorithm",
        "detail": "Unable to validate JWS :: JWS signature header contains unsupported algorithm
     \"RS512\", expected one of [RS256 ES256 ES384 ES512]",
        "status": 400,
        "algorithms": [
            "RS256",
            "ES256",
            "ES384",
            "ES512"
        ]
    }
    ```
    
    Fixes https://github.com/letsencrypt/boulder/issues/8155

--- test/integration/errors_test.go
+++ test/integration/errors_test.go
@@ -3,11 +3,19 @@
 package integration
 
 import (
+	"bytes"
+	"crypto"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"strings"
 	"testing"
 
 	"github.com/eggsampler/acme/v3"
+	"github.com/go-jose/go-jose/v4"
 
 	"github.com/letsencrypt/boulder/test"
 )
@@ -182,4 +190,90 @@ func TestRejectedIdentifier(t *testing.T) {
 	test.AssertEquals(t, prob.Type, "urn:ietf:params:acme:error:rejectedIdentifier")
 	test.AssertContains(t, prob.Detail, "Domain name contains an invalid character")
 	test.AssertContains(t, prob.Detail, "and 4 more problems")
+}
+
+// TestBadSignatureAlgorithm tests that supplying an unacceptable value for the
+// "alg" field of the JWS Protected Header results in a problem document with
+// the set of acceptable "alg" values listed in a custom extension field named
+// "algorithms". Creating a request with an unacceptable "alg" field requires
+// us to do some shenanigans.
+func TestBadSignatureAlgorithm(t *testing.T) {
+	t.Parallel()
+
+	client, err := makeClient()
+	if err != nil {
+		t.Fatal("creating test client")
+	}
+
+	header, err := json.Marshal(&struct {
+		Alg   string `json:"alg"`
+		KID   string `json:"kid"`
+		Nonce string `json:"nonce"`
+		URL   string `json:"url"`
+	}{
+		Alg:   string(jose.RS512), // This is the important bit; RS512 is unacceptable.
+		KID:   client.Account.URL,
+		Nonce: "deadbeef", // This nonce would fail, but that check comes after the alg check.
+		URL:   client.Directory().NewAccount,
+	})
+	if err != nil {
+		t.Fatalf("creating JWS protected header: %s", err)
+	}
+	protected := base64.RawURLEncoding.EncodeToString(header)
+
+	payload := base64.RawURLEncoding.EncodeToString([]byte(`{"onlyReturnExisting": true}`))
+	hash := crypto.SHA512.New()
+	hash.Write([]byte(protected + "." + payload))
+	sig, err := client.Account.PrivateKey.Sign(rand.Reader, hash.Sum(nil), crypto.SHA512)
+	if err != nil {
+		t.Fatalf("creating fake signature: %s", err)
+	}
+
+	data, err := json.Marshal(&struct {
+		Protected string `json:"protected"`
+		Payload   string `json:"payload"`
+		Signature string `json:"signature"`
+	}{
+		Protected: protected,
+		Payload:   payload,
+		Signature: base64.RawURLEncoding.EncodeToString(sig),
+	})
+
+	req, err := http.NewRequest(http.MethodPost, client.Directory().NewAccount, bytes.NewReader(data))
+	if err != nil {
+		t.Fatalf("creating HTTP request: %s", err)
+	}
+	req.Header.Set("Content-Type", "application/jose+json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("making HTTP request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("reading HTTP response: %s", err)
+	}
+
+	var prob struct {
+		Type       string                    `json:"type"`
+		Detail     string                    `json:"detail"`
+		Status     int                       `json:"status"`
+		Algorithms []jose.SignatureAlgorithm `json:"algorithms"`
+	}
+	err = json.Unmarshal(body, &prob)
+	if err != nil {
+		t.Fatalf("parsing HTTP response: %s", err)
+	}
+
+	if prob.Type != "urn:ietf:params:acme:error:badSignatureAlgorithm" {
+		t.Errorf("problem document has wrong type: want badSignatureAlgorithm, got %s", prob.Type)
+	}
+	if prob.Status != http.StatusBadRequest {
+		t.Errorf("problem document has wrong status: want 400, got %d", prob.Status)
+	}
+	if len(prob.Algorithms) == 0 {
+		t.Error("problem document MUST contain acceptable algorithms, got none")
+	}
 }
