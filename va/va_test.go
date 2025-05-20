6d9516b6e66d31af12bf36ee1f3178e3067f64fe
James Renken <james@renken.is>
2025-05-20 16:17:21 -0700

    Oops, fix VA test

--- va/va_test.go
+++ va/va_test.go
@@ -30,6 +30,7 @@ import (
 	"github.com/letsencrypt/boulder/identifier"
 	blog "github.com/letsencrypt/boulder/log"
 	"github.com/letsencrypt/boulder/metrics"
+	"github.com/letsencrypt/boulder/policy"
 	"github.com/letsencrypt/boulder/probs"
 	"github.com/letsencrypt/boulder/test"
 	vapb "github.com/letsencrypt/boulder/va/proto"
@@ -101,17 +102,17 @@ func createValidationRequest(ident identifier.ACMEIdentifier, challengeType core
 
 // isNonLoopbackReservedIP is a mock reserved IP checker that permits loopback
 // networks.
-func isNonLoopbackReservedIP(ip net.IP) bool {
+func isNonLoopbackReservedIP(ip net.IP) (bool, string, error) {
 	loopbackV4 := netip.MustParsePrefix("127.0.0.0/8")
 	loopbackV6 := netip.MustParsePrefix("::1/128")
 	netIPAddr, ok := netip.AddrFromSlice(ip)
 	if !ok {
-		panic(fmt.Sprintf("error parsing IP (%s)", ip))
+		return false, "", fmt.Errorf("error parsing IP (%s)", ip)
 	}
 	if loopbackV4.Contains(netIPAddr) || loopbackV6.Contains(netIPAddr) {
-		return false
+		return false, "", nil
 	}
-	return bdns.IsReservedIP(ip)
+	return policy.IsReservedIP(ip)
 }
 
 // setup returns an in-memory VA and a mock logger. The default resolver client
