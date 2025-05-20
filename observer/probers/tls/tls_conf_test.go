7ea51e5f91f5c0d567eb7f11915b9d9372778f41
Phil Porada <pgporada@users.noreply.github.com>
2025-05-20 09:24:21 -0700

    boulder-observer: check certificate status via CRL too (#8186)
    Let's Encrypt [recently removed OCSP URLs from
    certificates](https://community.letsencrypt.org/t/removing-ocsp-urls-from-certificates/236699)
    which unfortunately caused the boulder-observer TLS prober to panic.
    This change short circuits the OCSP checking logic if no OCSP URL exists
    in the to-be-checked certificate.
    
    Fixes https://github.com/letsencrypt/boulder/issues/8185
    
    ---------
    
    Co-authored-by: Aaron Gable <aaron@letsencrypt.org>

--- observer/probers/tls/tls_conf_test.go
+++ observer/probers/tls/tls_conf_test.go
@@ -4,9 +4,10 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/letsencrypt/boulder/observer/probers"
 	"github.com/prometheus/client_golang/prometheus"
 	"gopkg.in/yaml.v3"
+
+	"github.com/letsencrypt/boulder/observer/probers"
 )
 
 func TestTLSConf_MakeProber(t *testing.T) {
@@ -33,10 +34,12 @@ func TestTLSConf_MakeProber(t *testing.T) {
 		// valid
 		{"valid hostname", fields{"example.com", goodRootCN, "valid"}, colls, false},
 		{"valid hostname with path", fields{"example.com/foo/bar", "ISRG Root X2", "Revoked"}, colls, false},
+		{"valid hostname with port", fields{"example.com:8080", goodRootCN, "expired"}, colls, false},
 
 		// invalid hostname
 		{"bad hostname", fields{":::::", goodRootCN, goodResponse}, colls, true},
 		{"included scheme", fields{"https://example.com", goodRootCN, goodResponse}, colls, true},
+		{"included scheme and port", fields{"https://example.com:443", goodRootCN, goodResponse}, colls, true},
 
 		// invalid response
 		{"empty response", fields{goodHostname, goodRootCN, ""}, colls, true},
