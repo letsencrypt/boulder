28ff0d0db20d9a60ba68ba85298d4a57b488d116
Aaron Gable <aaron@letsencrypt.org>
2025-06-18 14:27:41 -0700

    Make the script more idiomatic

--- tools/release.go
+++ tools/release.go
@@ -37,6 +37,7 @@ pushing it if the -push flag was supplied.
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -48,34 +49,57 @@ import (
 	"golang.org/x/mod/semver"
 )
 
-func runOrDie(cmd *exec.Cmd) string {
-	fmt.Println(cmd.String())
+type cmdError struct {
+	error
+	output string
+}
+
+func run(cmd *exec.Cmd) (string, error) {
+	fmt.Println("Running:", cmd.String())
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println(string(out))
-		fmt.Println(err)
-		os.Exit(1)
+		return string(out), cmdError{
+			error:  fmt.Errorf("running %q: %w", cmd.String(), err),
+			output: string(out),
+		}
 	}
-	return string(out)
+	return string(out), nil
 }
 
 func main() {
+	var err error
 	if len(os.Args) >= 2 && os.Args[1] == "hotfix" {
-		os.Args = append([]string{os.Args[0]}, os.Args[2:]...)
-		hotfix()
+		err = hotfix(os.Args[2:])
 	} else {
-		release()
+		err = release(os.Args[1:])
+	}
+	if err != nil {
+		var cmdErr cmdError
+		if errors.As(err, &cmdErr) {
+			for line := range strings.SplitSeq(strings.TrimSpace(cmdErr.output), "\n") {
+				fmt.Println("  ", line)
+			}
+		}
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 }
 
-func release() {
+func release(args []string) error {
+	fs := flag.NewFlagSet("release", flag.ContinueOnError)
 	var push bool
-	flag.BoolVar(&push, "f", false, "If set, push the resulting release tag to GitHub.")
-	flag.Parse()
+	fs.BoolVar(&push, "push", false, "If set, push the resulting release tag to GitHub.")
+	err := fs.Parse(args)
+	if err != nil {
+		return fmt.Errorf("invalid flags: %w", err)
+	}
 
 	// Fetch all of the latest commits and refs from origin, so that we can ensure
 	// we're tagging the correct commit and not recreating an existing tag.
-	runOrDie(exec.Command("git", "fetch", "origin"))
+	_, err = run(exec.Command("git", "fetch", "origin"))
+	if err != nil {
+		return err
+	}
 
 	// We use semver's vMajor.Minor.Patch format, where the Major version is
 	// always 0 (no backwards compatibility guarantees), the Minor version is
@@ -87,60 +111,84 @@ func release() {
 
 	// Produce the tag, using -s to PGP sign it. This will fail if a tag with
 	// that name already exists.
-	runOrDie(exec.Command("git", "tag", "-s", "-m", message, version, "origin/main"))
+	_, err = run(exec.Command("git", "tag", "-s", "-m", message, version, "origin/main"))
+	if err != nil {
+		return err
+	}
 
 	// Show the result of the tagging operation, including the tag message and
 	// signature, and the commit hash and message, but not the diff.
-	show := runOrDie(exec.Command("git", "show", "-s", version))
-	fmt.Println(show)
+	out, err := run(exec.Command("git", "show", "-s", version))
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
 
 	if push {
-		runOrDie(exec.Command("git", "push", "origin", version))
+		_, err = run(exec.Command("git", "push", "origin", version))
+		if err != nil {
+			return err
+		}
 	} else {
-		fmt.Printf("created tag %s, exiting\n", version)
+		fmt.Println("please inspect the tag above, then run:")
+		fmt.Printf("    git push origin %s\n", version)
 	}
+	return nil
 }
 
-func hotfix() {
+func hotfix(args []string) error {
+	fs := flag.NewFlagSet("hotfix", flag.ContinueOnError)
 	var cherryPicks string
-	flag.StringVar(&cherryPicks, "c", "", "Comma-separated list of commit hashes to cherry-pick on top of the prior release.")
+	fs.StringVar(&cherryPicks, "pick", "", "Comma-separated list of commit hashes to cherry-pick on top of the prior release.")
 	var onto string
-	flag.StringVar(&onto, "o", "", "Name of the existing release tag to base this hotfix on top of.")
+	fs.StringVar(&onto, "onto", "", "Name of the existing release tag to base this hotfix on top of.")
 	var push bool
-	flag.BoolVar(&push, "f", false, "If set, push the resulting release tag to GitHub.")
-	flag.Parse()
+	fs.BoolVar(&push, "push", false, "If set, push the resulting release tag to GitHub.")
+	err := fs.Parse(args)
+	if err != nil {
+		return fmt.Errorf("invalid flags: %w", err)
+	}
 
 	if !semver.IsValid(onto) {
-		fmt.Printf("Tag name %q is not a valid release tag\n", onto)
-		os.Exit(1)
+		return fmt.Errorf("tag name %q is not a valid release tag", onto)
 	}
 
 	if len(cherryPicks) == 0 {
-		fmt.Printf("You must specify one or more commits to cherry-pick\n")
-		os.Exit(1)
+		return errors.New("you must specify one or more commits to cherry-pick")
 	}
 
 	// Fetch all of the latest commits and refs from origin, so that we can ensure
 	// we're hotfixing an existing release and cherry-picking on to the correct
 	// branch.
-	runOrDie(exec.Command("git", "fetch", "origin"))
+	_, err = run(exec.Command("git", "fetch", "origin"))
+	if err != nil {
+		return err
+	}
 
 	// Confirm that the release tag we're cherry-picking onto actually exists and
 	// is a tag.
-	committish := fmt.Sprintf("%s^{tag}", onto)
-	runOrDie(exec.Command("git", "rev-parse", "--verify", committish))
+	_, err = run(exec.Command("git", "rev-parse", "--verify", fmt.Sprintf("%s^{tag}", onto)))
+	if err != nil {
+		return fmt.Errorf("tag %q given by -onto does not exist: %w", onto, err)
+	}
 
 	// Check out the tag that is our starting point. This will put us into a
 	// "detached HEAD" state, but that's okay, because we're going to explicitly
 	// specify the branch we're pushing to when we're done.
-	runOrDie(exec.Command("git", "checkout", onto))
+	_, err = run(exec.Command("git", "checkout", onto))
+	if err != nil {
+		return err
+	}
 
 	// Cherry-pick each of the commits specified by the -c flag. If any of these
 	// fails, including because the commit can't be found, just bail out. Use the
 	// -x flag so that each of these cherry-picked commits has a commit message
 	// which indicates the original commit that it came from.
-	for _, sha := range strings.Split(cherryPicks, ",") {
-		runOrDie(exec.Command("git", "cherry-pick", "-x", sha))
+	for committish := range strings.SplitSeq(cherryPicks, ",") {
+		_, err = run(exec.Command("git", "cherry-pick", "-x", committish))
+		if err != nil {
+			return err
+		}
 	}
 
 	// Compute the next tag name by splitting the current tag into its component
@@ -148,13 +196,11 @@ func hotfix() {
 	// Unfortunately, the semver package doesn't provide a .Patch() method.
 	parts := strings.SplitN(onto, ".", 3)
 	if len(parts) != 3 {
-		fmt.Printf("Failed to parse patch version from release tag %q\n", onto)
-		os.Exit(1)
+		return fmt.Errorf("failed to parse patch version from release tag %q", onto)
 	}
 	patch, err := strconv.Atoi(parts[2])
 	if err != nil {
-		fmt.Printf("Failed to parse patch version %q as an integer\n", parts[2])
-		os.Exit(1)
+		return fmt.Errorf("failed to parse patch version %q as an integer", parts[2])
 	}
 	parts[2] = strconv.Itoa(patch + 1)
 	version := strings.Join(parts, ".")
@@ -162,12 +208,18 @@ func hotfix() {
 
 	// Produce the tag, using -s to PGP sign it. This will fail if a tag with
 	// that name already exists.
-	runOrDie(exec.Command("git", "tag", "-s", "-m", message, version, "HEAD"))
+	_, err = run(exec.Command("git", "tag", "-s", "-m", message, version, "HEAD"))
+	if err != nil {
+		return err
+	}
 
 	// Show the result of the tagging operation, including the tag message and
 	// signature, and the commit hash and message, but not the diff.
-	show := runOrDie(exec.Command("git", "show", "-s", version))
-	fmt.Println(show)
+	out, err := run(exec.Command("git", "show", "-s", version))
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
 
 	// Compute the name of the release branch that will contain the cherry-picked
 	// commits. This branch may or may not exist already, and it doesn't matter:
@@ -176,8 +228,13 @@ func hotfix() {
 	refspec := fmt.Sprintf("HEAD:%s", branch)
 
 	if push {
-		runOrDie(exec.Command("git", "push", "origin", refspec, version))
+		_, err = run(exec.Command("git", "push", "origin", refspec, version))
+		if err != nil {
+			return cmdError{output: out, error: err}
+		}
 	} else {
-		fmt.Printf("created tag %s on branch %s, exiting\n", version, branch)
+		fmt.Println("please inspect the tag above, then run:")
+		fmt.Printf("    git push origin %s %s\n", refspec, version)
 	}
+	return nil
 }
