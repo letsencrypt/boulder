591c0545af37ea05043a49c747f861be4f525add
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2025-08-06 15:13:57 -0700

    try-release: add docker-build.sh
    Also update docker-build.sh to emit boulder.tar.gz

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-docker build \
+docker buildx build \
     --build-arg COMMIT_ID=$(git rev-parse --short=8 HEAD) \
     --build-arg GO_VERSION=1.24.5 \
     --tag boulder:$(git rev-parse --short=8 HEAD) \
     --tag boulder \
     .
+
+docker run boulder tar -C /opt/boulder -cpz > boulder.tar.gz .
