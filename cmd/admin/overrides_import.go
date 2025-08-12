26c564b26f8631368450a500546fc795d461a82a
Samantha <hello@entropy.cat>
2025-08-12 14:48:18 -0400

    Make parallelism an int

--- cmd/admin/overrides_import.go
+++ cmd/admin/overrides_import.go
@@ -14,14 +14,14 @@ import (
 
 type subcommandImportOverrides struct {
 	file        string
-	parallelism uint
+	parallelism int
 }
 
 func (*subcommandImportOverrides) Desc() string { return "Push overrides to SA" }
 
 func (c *subcommandImportOverrides) Flags(f *flag.FlagSet) {
 	f.StringVar(&c.file, "file", "", "path to YAML file containing rate limit overrides")
-	f.UintVar(&c.parallelism, "parallelism", 10, "the number of concurrent RPCs to send to the SA (default: 10)")
+	f.IntVar(&c.parallelism, "parallelism", 10, "the number of concurrent RPCs to send to the SA (default: 10)")
 }
 
 func (c *subcommandImportOverrides) Run(ctx context.Context, a *admin) error {
@@ -54,10 +54,10 @@ func (c *subcommandImportOverrides) Run(ctx context.Context, a *admin) error {
 		ov  *sapb.RateLimitOverride
 		err error
 	}
-	results := make(chan result, int(c.parallelism))
+	results := make(chan result, c.parallelism)
 
 	var wg sync.WaitGroup
-	for i := uint(0); i < c.parallelism; i++ {
+	for i := 0; i < c.parallelism; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
