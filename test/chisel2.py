9fcb37890719a6814567695294deacb0da560384
James Renken <james@renken.is>
2025-10-01 12:56:39 -0700

    test: Improve nonce service health checks
    For integration tests, add health checks for nonce services' gRPC endpoints. Missing checks may have caused a flake where WFEs started before nonce services were healthy.
    
    This (hopefully) obviates the extra nonce retries introduced in #8393.
    
    Partially reverts #8393
    Fixes #8385

--- test/chisel2.py
+++ test/chisel2.py
@@ -142,21 +142,13 @@ def auth_and_issue(domains, chall_type="dns-01", email=None, cert_output=None, c
     else:
         raise Exception("invalid challenge type %s" % chall_type)
 
-    # Retry up to twice upon badNonce errors
-    for n in range(2):
-        try:
-            order = client.poll_and_finalize(order)
-            if cert_output is not None:
-                with open(cert_output, "w") as f:
-                    f.write(order.fullchain_pem)
-        except messages.Error as e:
-            if e.typ == "urn:ietf:params:acme:error:badNonce":
-                time.sleep(0.01)
-                continue
-        else:
-            break
-        finally:
-            cleanup()
+    try:
+        order = client.poll_and_finalize(order)
+        if cert_output is not None:
+            with open(cert_output, "w") as f:
+                f.write(order.fullchain_pem)
+    finally:
+        cleanup()
 
     return order
 
@@ -242,15 +234,8 @@ if __name__ == "__main__":
     if len(domains) == 0:
         print(__doc__)
         sys.exit(0)
-    # Retry up to twice upon badNonce errors
-    for n in range(2):
-        try:
-            auth_and_issue(domains)
-        except messages.Error as e:
-            if e.typ == "urn:ietf:params:acme:error:badNonce":
-                time.sleep(0.01)
-                continue
-            print(e)
-            sys.exit(1)
-        else:
-            break
+    try:
+        auth_and_issue(domains)
+    except messages.Error as e:
+        print(e)
+        sys.exit(1)
