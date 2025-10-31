d63fbb9d1df172cc220c395d247f00ee39a10884
Samantha <hello@entropy.cat>
2025-10-31 12:14:50 -0400

    database: Add vitess + mysql 8.0 to our development environment

---
+++ test/vtcomboserver/tag_and_upload.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -feuxo pipefail
+
+cd $(dirname $0)
+
+DATESTAMP=$(date +%Y-%m-%d)
+DOCKER_REPO="letsencrypt/boulder-vtcomboserver"
+VITESS_TAG=v22.0.0
+
+echo "Please login to allow push to DockerHub"
+docker login
+
+# Usage: build_and_push_image $VITESS_TAG
+build_and_push_image() {
+  VITESS_TAG="$1"
+  TAG_NAME="${DOCKER_REPO}:vitess${VITESS_TAG}_${DATESTAMP}"
+  echo "Building boulder-vtcomboserver image ${TAG_NAME}"
+
+  # build, tag, and push the image.
+  docker buildx build \
+    --build-arg "VITESS_TAG=${VITESS_TAG}" \
+    --progress plain \
+    --push \
+    --tag "${TAG_NAME}" \
+    --platform "linux/amd64" \
+    .
+}
+
+build_and_push_image $VITESS_TAG
