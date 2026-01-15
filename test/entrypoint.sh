ca80de9ce241d7b2ec6e37d7f7754cc1180849d9
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2026-01-14 20:02:36 -0800

    Move USE_VITESS variable into entrypoint.sh
    This allows standing up a boulder that uses Vitess even when not running a
    specific test:
    
    docker compose run -e USE_VITESS=true boulder
    
    It also allows us to run migrations for mariadb or Vitess depending on which
    environment we'll be using, which may save a small amount of startup time.
    
    Specify USE_VITESS: false as an environment variable in docker-compose.yml to
    indicate an easy place to change it to `true` if someone prefers to use
    `docker compose up` (which does not accept `-e`).

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -10,6 +10,36 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -f /var/run/rsyslogd.pid
 rsyslogd
 
+DB_URL_FILES=(
+  badkeyrevoker_dburl
+  cert_checker_dburl
+  incidents_dburl
+  revoker_dburl
+  sa_dburl
+  sa_ro_dburl
+)
+
+configure_database_endpoints() {
+  dburl_target_dir="proxysql"
+  export DB_ADDR="boulder-proxysql:6033"
+
+  if [[ "${USE_VITESS}" == "true" ]]
+  then
+    dburl_target_dir="vitess"
+    export DB_ADDR="boulder-vitess:33577"
+  fi
+
+  # Configure DBURL symlinks
+  rm -f test/secrets/*_dburl || true
+  for file in ${DB_URL_FILES:+${DB_URL_FILES[@]+"${DB_URL_FILES[@]}"}}
+  do
+    ln -sf "dburls/${dburl_target_dir}/${file}" "test/secrets/${file}"
+  done
+}
+
+# Defaults to MariaDB/ProxySQL unless USE_VITESS is true.
+configure_database_endpoints
+
 # make sure we can reach mariadb and proxysql
 ./test/wait-for-it.sh boulder-mariadb 3306
 ./test/wait-for-it.sh boulder-proxysql 6033
@@ -21,21 +51,23 @@ rsyslogd
 ./test/wait-for-it.sh bpkimetal 8080
 
 # create the databases
-MYSQL_CONTAINER=1 \
-DB_HOST="boulder-mariadb" \
-DB_PORT=3306 \
-DB_CONFIG_FILE="${DIR}/../sa/db/dbconfig.mariadb.yml" \
-SKIP_CREATE=0 \
-SKIP_USERS=0 \
-"$DIR/create_db.sh"
-
-MYSQL_CONTAINER=1 \
-DB_HOST="boulder-vitess" \
-DB_PORT=33577 \
-DB_CONFIG_FILE="${DIR}/../sa/db/dbconfig.mysql8.yml" \
-SKIP_CREATE=1 \
-SKIP_USERS=1 \
-"$DIR/create_db.sh"
+if [[ "${USE_VITESS}" == "true" ]]; then
+  MYSQL_CONTAINER=1 \
+  DB_HOST="boulder-vitess" \
+  DB_PORT=33577 \
+  DB_CONFIG_FILE="${DIR}/../sa/db/dbconfig.mysql8.yml" \
+  SKIP_CREATE=1 \
+  SKIP_USERS=1 \
+  "$DIR/create_db.sh"
+else
+  MYSQL_CONTAINER=1 \
+  DB_HOST="boulder-mariadb" \
+  DB_PORT=3306 \
+  DB_CONFIG_FILE="${DIR}/../sa/db/dbconfig.mariadb.yml" \
+  SKIP_CREATE=0 \
+  SKIP_USERS=0 \
+  "$DIR/create_db.sh"
+fi
 
 if [[ $# -eq 0 ]]; then
     exec python3 ./start.py
