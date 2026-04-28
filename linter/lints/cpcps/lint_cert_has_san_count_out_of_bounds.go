af0bac4894f77ae4da635761a9bb714be9227770
Kiel C <kchris@letsencrypt.org>
2026-04-28 20:38:57 +0000

    Better CheckApplies. Updated comments and Description.
    Addresses reviewer feedback.

--- linter/lints/cpcps/lint_cert_has_san_count_out_of_bounds.go
+++ linter/lints/cpcps/lint_cert_has_san_count_out_of_bounds.go
@@ -14,7 +14,7 @@ func init() {
 	lint.RegisterCertificateLint(&lint.CertificateLint{
 		LintMetadata: lint.LintMetadata{
 			Name:          "e_cert_has_san_count_out_of_bounds",
-			Description:   "Let's Encrypt Subscriber Certificaes must have a count of subjectAlternateNames within specific bounds defined by our CPS",
+			Description:   "Let's Encrypt Subscriber Certificates subjectAlternativeName must be a sequence of 1 to 100 dNSNames or ipAddresses",
 			Citation:      "CPS: 7.1",
 			Source:        lints.LetsEncryptCPS,
 			EffectiveDate: lints.CPSV33Date, // TODO: probably earlier?
@@ -28,18 +28,14 @@ func CertNamesCountOutOfBounds() lint.CertificateLintInterface {
 }
 
 func (l *certSubjectAltNamesCountOutOfBounds) CheckApplies(c *x509.Certificate) bool {
-	return util.IsExtInCert(c, util.SubjectAlternateNameOID) && (len(c.DNSNames) > 0 || len(c.IPAddresses) > 0)
+	return util.IsSubscriberCert(c)
 }
 
 func (l *certSubjectAltNamesCountOutOfBounds) Execute(c *x509.Certificate) *lint.LintResult {
-	/*
-	 * CP/CPS 7.1: "A sequence of 1 to 100 dNSNames or ipAddresses (critical if no CN)"
-	 *
-	 * more likely to encounter certs with greater than 100 than with fewer than 1
-	 * so testing that failure first
-	 */
 	totalSANs := len(c.DNSNames) + len(c.IPAddresses)
 
+	// more likely to encounter certs with greater than 100 vs with fewer than 1
+	// so testing that failure first
 	if totalSANs > 100 || totalSANs < 1 {
 		return &lint.LintResult{Status: lint.Error}
 	}
