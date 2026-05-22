4e280351b20d3c4f51d3bb18c59fd41804c67a70
Samantha Frank <hello@entropy.cat>
2026-05-22 16:23:57 -0400

    sfe/salesforce: Remove donation call-to-action and Cases support (#8751)
    Also, obviate the need to complete the proto changes in TODO #8410.
    These changes, documented in IN-12087, have not yet been applied in
    Staging or Production.
    
    Fixes #8744

--- cmd/email-exporter/main.go
+++ cmd/email-exporter/main.go
@@ -6,13 +6,11 @@ import (
 	"os"
 
 	"github.com/jmhodges/clock"
-	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/letsencrypt/boulder/cmd"
 	bgrpc "github.com/letsencrypt/boulder/grpc"
 	"github.com/letsencrypt/boulder/salesforce"
 	emailpb "github.com/letsencrypt/boulder/salesforce/email/proto"
-	salesforcepb "github.com/letsencrypt/boulder/salesforce/proto"
 )
 
 // Config holds the configuration for the email-exporter service.
@@ -62,38 +60,6 @@ type Config struct {
 	}
 	Syslog        cmd.SyslogConfig
 	OpenTelemetry cmd.OpenTelemetryConfig
-}
-
-// legacyEmailExporterServer is an adapter that implements the email.Exporter
-// gRPC interface by delegating to an inner salesforce.Exporter server.
-//
-// TODO(#8410): Remove legacyEmailExporterServer once fully migrated to
-// salesforcepb.Exporter.
-type legacyEmailExporterServer struct {
-	emailpb.UnimplementedExporterServer
-	inner salesforcepb.ExporterServer
-}
-
-// SendContacts is an interface adapter that forwards the request to the same
-// method on the inner salesforce.Exporter server.
-func (s legacyEmailExporterServer) SendContacts(ctx context.Context, req *emailpb.SendContactsRequest) (*emptypb.Empty, error) {
-	return s.inner.SendContacts(ctx, &salesforcepb.SendContactsRequest{Emails: req.GetEmails()})
-}
-
-// SendCase is an interface adapter that forwards the request to the same method
-// on the inner salesforce.Exporter server.
-func (s legacyEmailExporterServer) SendCase(ctx context.Context, req *emailpb.SendCaseRequest) (*emptypb.Empty, error) {
-	return s.inner.SendCase(ctx, &salesforcepb.SendCaseRequest{
-		Origin:        req.GetOrigin(),
-		Subject:       req.GetSubject(),
-		Description:   req.GetDescription(),
-		ContactEmail:  req.GetContactEmail(),
-		Organization:  req.GetOrganization(),
-		AccountId:     req.GetAccountId(),
-		RateLimitName: req.GetRateLimitName(),
-		RateLimitTier: req.GetRateLimitTier(),
-		UseCase:       req.GetUseCase(),
-	})
 }
 
 func main() {
@@ -152,10 +118,7 @@ func main() {
 	go server.Start(daemonCtx)
 
 	start, err := bgrpc.NewServer(c.EmailExporter.GRPC, logger).Add(
-		&salesforcepb.Exporter_ServiceDesc, server).Add(
-		// TODO(#8410): Remove emailpb.Exporter once fully migrated to
-		// salesforcepb.Exporter.
-		&emailpb.Exporter_ServiceDesc, legacyEmailExporterServer{inner: server}).Build(
+		&emailpb.Exporter_ServiceDesc, server).Build(
 		tlsConfig, scope, clk)
 	cmd.FailOnError(err, "Configuring email-exporter gRPC server")
 
--- salesforce/pardot.go
+++ salesforce/pardot.go
@@ -25,16 +25,6 @@ const (
 	// https://developer.salesforce.com/docs/marketing/pardot/guide/prospect-v5.html#prospect-upsert-by-email
 	contactsPath = "/api/v5/objects/prospects/do/upsertLatestByEmail"
 
-	// casesPath is the path to create a new Case object in Salesforce. This
-	// path includes the API version (v64.0). Normally, Salesforce maintains
-	// backward compatibility across versions. Update only if Salesforce retires
-	// this API version (rare) or we want to make use of new Case fields
-	// (unlikely).
-	//
-	// To check the current version for our org, see "Identify your current API
-	// version": https://help.salesforce.com/s/articleView?id=000386929&type=1
-	casesPath = "/services/data/v64.0/sobjects/Case"
-
 	// maxAttempts is the maximum number of attempts to retry a request.
 	maxAttempts = 3
 
@@ -56,7 +46,6 @@ const (
 // Salesforce APIs. It exists to facilitate testing mocks.
 type SalesforceClient interface {
 	SendContact(email string) error
-	SendCase(payload Case) error
 }
 
 // oAuthToken holds the OAuth2 access token and its expiration.
@@ -67,14 +56,12 @@ type oAuthToken struct {
 	expiresAt   time.Time
 }
 
-// SalesforceClientImpl handles authentication and sending contacts to Pardot
-// and creating Cases in Salesforce.
+// SalesforceClientImpl handles authentication and sending contacts to Pardot.
 type SalesforceClientImpl struct {
 	businessUnit string
 	clientId     string
 	clientSecret string
 	pardotURL    string
-	casesURL     string
 	tokenURL     string
 	token        *oAuthToken
 	clk          clock.Clock
@@ -92,17 +79,12 @@ func NewSalesforceClientImpl(clk clock.Clock, businessUnit, clientId, clientSecr
 	if err != nil {
 		return nil, fmt.Errorf("failed to join token path: %w", err)
 	}
-	casesURL, err := url.JoinPath(salesforceBaseURL, casesPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to join cases path: %w", err)
-	}
 
 	return &SalesforceClientImpl{
 		businessUnit: businessUnit,
 		clientId:     clientId,
 		clientSecret: clientSecret,
 		pardotURL:    pardotURL,
-		casesURL:     casesURL,
 		tokenURL:     tokenURL,
 		token:        &oAuthToken{},
 		clk:          clk,
@@ -228,105 +210,6 @@ func (pc *SalesforceClientImpl) SendContact(email string) error {
 			continue
 		}
 		finalErr = fmt.Errorf("create contact request returned status %d: %s", resp.StatusCode, redactEmail(body, email))
-		continue
-	}
-
-	return finalErr
-}
-
-// Case represents the payload for populating a new Case object in Salesforce.
-// For more information, see:
-// https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_case.htm
-// https://help.salesforce.com/s/articleView?id=platform.custom_field_types.htm&type=5
-type Case struct {
-	// Origin is required in all requests, a safe default is "Web".
-	Origin string `json:"Origin"`
-
-	// Subject is an optional standard field. Max length: 255 characters.
-	Subject string `json:"Subject,omitempty"`
-
-	// Description is an optional standard field. Max length: 32,768 characters.
-	Description string `json:"Description,omitempty"`
-
-	// ContactEmail is an optional standard field indicating the email address
-	// of the requester. Max length: 80 characters.
-	ContactEmail string `json:"ContactEmail,omitempty"`
-
-	// Note: Fields below this point are optional custom fields.
-
-	// Organization indicates the name of the requesting organization. Max
-	// length: 255 characters.
-	Organization string `json:"Organization__c,omitempty"`
-
-	// AccountId indicates the requester's ACME Account ID. Max length: 255
-	// characters.
-	AccountId string `json:"Account_ID__c,omitempty"`
-
-	// RateLimitName indicates which rate limit the override request is for. Max
-	// length: 255 characters.
-	RateLimitName string `json:"Rate_Limit_Name__c,omitempty"`
-
-	// Tier indicates the requested tier of the rate limit override. Max length:
-	// 255 characters.
-	RateLimitTier string `json:"Rate_Limit_Tier__c,omitempty"`
-
-	// UseCase indicates the intended to use case supplied by the requester. Max
-	// length: 131,072 characters.
-	UseCase string `json:"Use_Case__c,omitempty"`
-}
-
-// SendCase submits a new Case object to Salesforce. For more information, see:
-// https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/dome_sobject_create.htm
-func (pc *SalesforceClientImpl) SendCase(payload Case) error {
-	var err error
-	for attempt := range maxAttempts {
-		time.Sleep(core.RetryBackoff(attempt, retryBackoffMin, retryBackoffMax, retryBackoffBase))
-		err = pc.updateToken()
-		if err == nil {
-			break
-		}
-	}
-	if err != nil {
-		return fmt.Errorf("failed to update token: %w", err)
-	}
-
-	body, err := json.Marshal(payload)
-	if err != nil {
-		return fmt.Errorf("failed to marshal case payload: %w", err)
-	}
-
-	var finalErr error
-	for attempt := range maxAttempts {
-		time.Sleep(core.RetryBackoff(attempt, retryBackoffMin, retryBackoffMax, retryBackoffBase))
-
-		req, err := http.NewRequest("POST", pc.casesURL, bytes.NewReader(body))
-		if err != nil {
-			finalErr = fmt.Errorf("failed to create new case request: %w", err)
-			continue
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Authorization", "Bearer "+pc.token.accessToken)
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			finalErr = fmt.Errorf("create case request failed: %w", err)
-			continue
-		}
-
-		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-			resp.Body.Close()
-			return nil
-		}
-
-		respBody, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-
-		if err != nil {
-			finalErr = fmt.Errorf("create case request returned status %d; while reading body: %w", resp.StatusCode, err)
-			continue
-		}
-
-		finalErr = fmt.Errorf("create case request returned status %d: %s", resp.StatusCode, redactEmail(respBody, payload.ContactEmail))
 		continue
 	}
 
