73f3154f097317485edc9e7dcfbf16c84c1325e9
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2026-06-01 09:59:54 -0700

    use database name / keyspace name to split logs

--- sa/db/00-create-databases.sql
+++ sa/db/00-create-databases.sql
@@ -2,3 +2,6 @@ CREATE DATABASE boulder_sa;
 CREATE DATABASE incidents_sa;
 CREATE DATABASE boulder_sa_next;
 CREATE DATABASE incidents_sa_next;
+
+-- ISRG(44947) dev-reserved(4) MTCA1(1) logs(0) 44
+CREATE DATABASE mtcmeta_44947_4_1_0_44;
