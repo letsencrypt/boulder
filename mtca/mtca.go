1d8758f16e5f8696cc2ed090d9329aef14bfe1d9
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2026-06-09 18:37:30 -0700

    wip

--- mtca/mtca.go
+++ mtca/mtca.go
@@ -7,7 +7,9 @@ import (
 	"encoding/asn1"
 	"fmt"
 	"sync"
+	"time"
 
+	corepb "github.com/letsencrypt/boulder/core/proto"
 	"github.com/letsencrypt/boulder/issuance"
 	mtcapb "github.com/letsencrypt/boulder/mtca/proto"
 )
@@ -29,6 +31,7 @@ func New(issuer *issuance.Issuer) *mtca {
 		// TODO: collect this from config
 		logNumber:        0,
 		latestEntryIndex: 0,
+		pool:             &pool{},
 	}
 }
 
@@ -42,7 +45,32 @@ type mtca struct {
 	// This is just a dummy for testing; in reality this will come from the DB.
 	latestEntryIndex int64
 
-	sequencing sync.Mutex
+	pool *pool
+}
+
+type entry struct {
+	pubkey      []byte
+	identifiers []*corepb.Identifier
+	ch          chan<- int64
+}
+
+type pool struct {
+	sync.Mutex
+	entries []entry
+}
+
+func (p *pool) take() []entry {
+	p.Lock()
+	defer p.Unlock()
+	ret := p.entries
+	p.entries = nil
+	return ret
+}
+
+func (p *pool) append(e entry) {
+	p.Lock()
+	defer p.Unlock()
+	p.entries = append(p.entries, e)
 }
 
 // mtcLogID returns the string-formatted relative OID for this log.
@@ -53,12 +81,34 @@ func (m *mtca) mtcLogID() string {
 }
 
 func (m *mtca) Issue(ctx context.Context, req *mtcapb.IssueRequest) (*mtcapb.IssueResponse, error) {
-	m.sequencing.Lock()
-	defer m.sequencing.Unlock()
-	m.latestEntryIndex++
+	ch := make(chan int64)
+	m.pool.append(entry{
+		pubkey:      req.Pubkey,
+		identifiers: req.Identifiers,
+		ch:          ch,
+	})
+
+	entryIndex := <-ch
 
 	return &mtcapb.IssueResponse{
 		MtcLogID:      m.mtcLogID(),
-		MtcEntryIndex: m.latestEntryIndex,
+		MtcEntryIndex: entryIndex,
 	}, nil
+}
+
+func (m *mtca) Loop(ctx context.Context) {
+	ticker := time.NewTicker(50 * time.Millisecond)
+	for {
+		<-ticker.C
+		m.sequence(ctx)
+	}
+}
+
+func (m *mtca) sequence(_ context.Context) {
+	entries := m.pool.take()
+	for _, e := range entries {
+		e.ch <- m.latestEntryIndex
+		fmt.Printf("issuing %d\n", m.latestEntryIndex)
+		m.latestEntryIndex++
+	}
 }
