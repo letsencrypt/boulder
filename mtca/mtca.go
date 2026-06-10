9851a60103b2c211f3ce7aab7bc291a059680493
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2026-06-09 23:43:39 -0700

    Actually parse out the MTCA ID

--- mtca/mtca.go
+++ mtca/mtca.go
@@ -2,6 +2,7 @@ package mtca
 
 import (
 	"context"
+	"encoding/asn1"
 	"fmt"
 	"sync"
 
@@ -12,9 +13,17 @@ import (
 var _ mtcapb.MTCAServer = &mtca{}
 
 func New(issuer *issuance.Issuer) *mtca {
+	var mtcaID string
+	testingTrustAnchorIDOID := asn1.ObjectIdentifier{1, 3, 6, 1, 4, 1, 44363, 47, 1}
+	for _, attribute := range issuer.Cert.Subject.Names {
+		if attribute.Type.Equal(testingTrustAnchorIDOID) {
+			mtcaID, _ = attribute.Value.(string)
+			break
+		}
+	}
 	return &mtca{
 		issuer: issuer,
-		mtcaID: issuer.Cert.Subject.String(),
+		mtcaID: mtcaID,
 		// TODO: collect this from config
 		logNumber:        0,
 		latestEntryIndex: 0,
@@ -34,6 +43,9 @@ type mtca struct {
 	sequencing sync.Mutex
 }
 
+// mtcLogID returns the string-formatted relative OID for this log.
+// The .0. arc relative to the MTCA ID contains log numbers.
+// https://ietf-plants-wg.github.io/merkle-tree-certs/draft-ietf-plants-merkle-tree-certs.html#ca-ids
 func (m *mtca) mtcLogID() string {
 	return fmt.Sprintf("%s.0.%d", m.mtcaID, m.logNumber)
 }
