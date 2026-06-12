d8ea1790610257b5525ed109e68c9ae91b5ad31d
Aaron Gable <aaron@letsencrypt.org>
2026-06-12 06:43:56 -1000

    crl-storer: use IfMatch to do a compare-and-swap when uploading (#8791)
    The "IfMatch" field of the PutObjectRequest causes the S3 server to
    return 412 Precondition Failed if the existing object at this path does
    not have the matching ETag. ETags are opaque unique values that change
    with each version of the file, so this acts as a compare-and-swap, to
    ensure that this upload is not racing against another simultaneous
    upload.
    
    High-level S3 docs:
    https://docs.aws.amazon.com/AmazonS3/latest/userguide/conditional-requests.html
    If-Match header standard: https://datatracker.ietf.org/doc/html/rfc7232

--- crl/storer/storer.go
+++ crl/storer/storer.go
@@ -160,6 +160,7 @@ func (cs *crlStorer) UploadCRL(stream grpc.ClientStreamingServer[cspb.UploadCRLR
 	// additional safety check against clock skew and potential races, if multiple
 	// crl-updaters are working on the same shard at the same time. We only run
 	// these checks if we found a CRL, so we don't block uploading brand new CRLs.
+	var prevEtag *string
 	filename := fmt.Sprintf("%d/%d.crl", issuer.NameID(), shardIdx)
 	prevObj, err := cs.s3Client.GetObject(stream.Context(), &s3.GetObjectInput{
 		Bucket: &cs.s3Bucket,
@@ -207,6 +208,10 @@ func (cs *crlStorer) UploadCRL(stream grpc.ClientStreamingServer[cspb.UploadCRLR
 		if !uriMatch {
 			return fmt.Errorf("IDP does not match previous: %v !∩ %v", idpURIs, prevURIs)
 		}
+
+		// This ensures that the CRL object hasn't been replaced since we downloaded
+		// it above. Prevents races against another storer.
+		prevEtag = prevObj.ETag
 	}
 
 	// Finally actually upload the new CRL.
@@ -225,6 +230,7 @@ func (cs *crlStorer) UploadCRL(stream grpc.ClientStreamingServer[cspb.UploadCRLR
 		Metadata:          map[string]string{"crlNumber": crlNumber.String()},
 		Expires:           &expires,
 		CacheControl:      &cacheControl,
+		IfMatch:           prevEtag,
 	})
 
 	latency := cs.clk.Now().Sub(start)
