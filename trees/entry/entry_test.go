d8ce9a3b1048188fe47a6483854c084e93f77901
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2026-07-16 14:16:28 -0700

    Add malformed test case

--- trees/entry/entry_test.go
+++ trees/entry/entry_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestTBSCertificateLogEntryFromX509(t *testing.T) {
+func TestFromX509(t *testing.T) {
 	// Bytes from an example generated test/certs/ipki/wfe.boulder/cert.pem
 	input, err := base64.StdEncoding.DecodeString(strings.ReplaceAll(`
 MIIB4TCCAWegAwIBAgIIJuDkMteShO8wCgYIKoZIzj0EAwMwIDEeMBwGA1UEAxMV
@@ -52,6 +52,55 @@ AQUFBwMCMAwGA1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUgPmX4p5B3cSQ2bkaNcye
 		t.Errorf("TBSCertificateLogEntryFromX509(): got %s, want %s",
 			base64.StdEncoding.EncodeToString(mtce.Value),
 			base64.StdEncoding.EncodeToString(expectedOutput))
+	}
+}
+
+func TestFromX509Malformed(t *testing.T) {
+	valid, err := base64.StdEncoding.DecodeString(strings.ReplaceAll(`
+MIIB4TCCAWegAwIBAgIIJuDkMteShO8wCgYIKoZIzj0EAwMwIDEeMBwGA1UEAxMV
+bWluaWNhIHJvb3QgY2EgNGU0YjFkMB4XDTI2MDYyOTA1NDUyNloXDTI4MDcyOTA1
+NDUyNlowFjEUMBIGA1UEAxMLd2ZlLmJvdWxkZXIwdjAQBgcqhkjOPQIBBgUrgQQA
+IgNiAATLkY1wBrGl9+jhR4+HSycRv5kvVV8LUO3xY1styu8+q9kaSi03wrdH7LUf
+rJkRE6S60XzVXkeqL9N//jOXFsaM9JbsbeHFRoAx+mBEV68Vu69dblxtXIAKNlMM
+5dav5XOjeDB2MA4GA1UdDwEB/wQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAQYI
+KwYBBQUHAwIwDAYDVR0TAQH/BAIwADAfBgNVHSMEGDAWgBSA+ZfinkHdxJDZuRo1
+zJ7mHOmaCDAWBgNVHREEDzANggt3ZmUuYm91bGRlcjAKBggqhkjOPQQDAwNoADBl
+AjEApE8cwaAQ6hnGtUM/TWAb54E5/29ZVy5E/UY8mEzoE021pl3tq1fEof5qz5n/
+KrL4AjAuEpVOjRrRWWMnRJxd05Pfxq7gZmxgwppjnE9JZ9P6WRP7ZWqZcc9p8YLM
+YhKuXQo=`, "\n", ""))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	shortValid := valid[:len(valid)-2]
+	longValid := append(valid, 0)
+
+	wrongVersion := bytes.Clone(valid)
+	copy(wrongVersion[10:13], []byte{2, 1, 1})
+
+	type testCase struct {
+		name, hex string
+	}
+	testCases := []testCase{
+		{"empty", ""},
+		{"tag only", "30"},
+		{"nothing inside", "300100"},
+		{"too short", hex.EncodeToString(shortValid)},
+		{"too long", hex.EncodeToString(longValid)},
+		{"wrong version", hex.EncodeToString(wrongVersion)},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			val, err := hex.DecodeString(tc.hex)
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, err = FromX509(val, crypto.SHA256)
+			if err == nil {
+				t.Errorf("FromX509(): got nil err, want error")
+			}
+		})
 	}
 }
 
