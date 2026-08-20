431d850f0047467673708a70bdd5e85ab98cee43
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2026-08-20 16:44:44 -0700

    test: remove ai-test-srv
    This functionality was moved from wfe1 to wfe2 in #5840, then extracted into a
    standalone server in #7411. However, no current tests fetch the issuer URL.
    PR #5840 notes that, at the time, it was OCSP tests that fetched it, so it makes
    sense that we no longer need it now that OCSP is gone.

--- test/certs/webpki.go
+++ test/certs/webpki.go
@@ -102,11 +102,6 @@ func main() {
 
 		for _, inst := range []string{"a", "b", "c"} {
 			name := fmt.Sprintf("int %s %s", alg, inst)
-			// Note: The file names produced by this script (as a combination of this
-			// line, and the rest of the file name as specified in the various yaml
-			// template files) are meaningful and are consumed by aia-test-srv. If
-			// you change the structure of these file names, you will need to change
-			// aia-test-srv as well to recognize and consume the resulting files.
 			fileName := strings.ReplaceAll(name, " ", "-")
 
 			// Create SoftHSM slot
