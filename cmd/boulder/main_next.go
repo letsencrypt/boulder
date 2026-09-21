d8d1974b3f6225c9f52a2bc6a09f5c819a883746
Jacob Hoffman-Andrews <jsha+github@letsencrypt.org>
2026-09-21 16:32:32 -0700

    remove go1.27 build tags and gotip (#9027)

--- cmd/boulder/main_next.go
+++
@@ -1,8 +0,0 @@
-//go:build go1.27
-
-package main
-
-import (
-	_ "github.com/letsencrypt/boulder/cmd/boulder-mtca"
-	_ "github.com/letsencrypt/boulder/cmd/boulder-mtpublisher"
-)
--- linter/linter.go
+++ linter/linter.go
@@ -3,7 +3,10 @@ package linter
 import (
 	"bytes"
 	"crypto"
+	"crypto/ecdsa"
+	"crypto/mldsa"
 	"crypto/rand"
+	"crypto/rsa"
 	"crypto/x509"
 	"errors"
 	"fmt"
@@ -182,6 +185,32 @@ func (l *Linter) CheckCRL(tbs *x509.RevocationList, reg lint.Registry) error {
 	}
 	lintRes := zlint.LintRevocationListEx(crl, reg)
 	return ProcessResultSet(lintRes)
+}
+
+// makeSigner makes a signer with a throwaway key that matches `realSigner`'s type.
+func makeSigner(realSigner crypto.Signer) (crypto.Signer, error) {
+	var lintSigner crypto.Signer
+	var err error
+	switch k := realSigner.Public().(type) {
+	case *rsa.PublicKey:
+		lintSigner, err = rsa.GenerateKey(rand.Reader, k.Size()*8)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create RSA lint signer: %w", err)
+		}
+	case *ecdsa.PublicKey:
+		lintSigner, err = ecdsa.GenerateKey(k.Curve, rand.Reader)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create ECDSA lint signer: %w", err)
+		}
+	case *mldsa.PublicKey:
+		lintSigner, err = mldsa.GenerateKey(k.Parameters())
+		if err != nil {
+			return nil, fmt.Errorf("failed to create ML-DSA lint signer: %w", err)
+		}
+	default:
+		return nil, fmt.Errorf("unsupported lint signer type: %T", k)
+	}
+	return lintSigner, nil
 }
 
 func makeIssuer(realIssuer *x509.Certificate, lintSigner crypto.Signer) (*x509.Certificate, error) {
